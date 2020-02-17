3e2326c54481b5b720a4713701aa77f6e9d11729
Marianne Myhre <marianne.myhre@nav.no>
2020-02-17 13:14:10 +0100

    Nojira: Bumper kalkulus

--- domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/MÅ_LIGGE_HOS_FPSAK/mappers/til_kalkulus/MapBehandlingRef.java
+++ domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/MÅ_LIGGE_HOS_FPSAK/mappers/til_kalkulus/MapBehandlingRef.java
@@ -2,9 +2,7 @@ package no.nav.foreldrepenger.domene.MÅ_LIGGE_HOS_FPSAK.mappers.til_kalkulus;
 
 import no.nav.folketrygdloven.kalkulator.modell.behandling.BehandlingReferanse;
 import no.nav.folketrygdloven.kalkulator.modell.behandling.BehandlingStatus;
-import no.nav.folketrygdloven.kalkulator.modell.behandling.BehandlingType;
 import no.nav.folketrygdloven.kalkulator.modell.behandling.FagsakYtelseType;
-import no.nav.folketrygdloven.kalkulator.modell.behandling.RelasjonsRolleType;
 import no.nav.folketrygdloven.kalkulator.modell.behandling.Skjæringstidspunkt;
 import no.nav.folketrygdloven.kalkulator.modell.typer.AktørId;
 
@@ -13,10 +11,7 @@ public class MapBehandlingRef {
 
     public static BehandlingReferanse mapRef(no.nav.foreldrepenger.behandling.BehandlingReferanse behandlingReferanse) {
         return BehandlingReferanse.fra(FagsakYtelseType.fraKode(behandlingReferanse.getFagsakYtelseType().getKode()),
-            BehandlingType.fraKode(behandlingReferanse.getBehandlingType().getKode()),
-            RelasjonsRolleType.fraKode(behandlingReferanse.getRelasjonsRolleType().getKode()),
             new AktørId(behandlingReferanse.getAktørId().getId()),
-            behandlingReferanse.getFagsakId(),
             behandlingReferanse.getBehandlingId(),
             behandlingReferanse.getBehandlingUuid(),
             behandlingReferanse.getOriginalBehandlingId(),
