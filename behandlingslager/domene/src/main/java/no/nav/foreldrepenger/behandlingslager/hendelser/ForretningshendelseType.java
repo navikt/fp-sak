e3ac609b49dfe06686485f78fbcc2997670306ef
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2020-02-19 08:48:58 +0100

    TFP-1201 etterarbeid fjerne ytelsehendelse og bruk (#54)

--- behandlingslager/domene/src/main/java/no/nav/foreldrepenger/behandlingslager/hendelser/ForretningshendelseType.java
+++ behandlingslager/domene/src/main/java/no/nav/foreldrepenger/behandlingslager/hendelser/ForretningshendelseType.java
@@ -17,21 +17,17 @@ import no.nav.foreldrepenger.behandlingslager.kodeverk.Kodeverdi;
 @JsonFormat(shape = Shape.OBJECT)
 @JsonAutoDetect(getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE, fieldVisibility = Visibility.ANY)
 public enum ForretningshendelseType implements Kodeverdi {
-    
+
     INGEN_HENDELSE("INGEN_HENDELSE", "Ingen hendelse"),
     FØDSEL("FØDSEL", "Fødsel"),
     DØD("DØD", "Død"),
     DØDFØDSEL("DØDFØDSEL", "Dødfødsel"),
-    YTELSE_INNVILGET("YTELSE_INNVILGET", "Ytelse innvilget"),
-    YTELSE_ENDRET("YTELSE_ENDRET", "Ytelse endret"),
-    YTELSE_OPPHØRT("YTELSE_OPPHØRT", "Ytelse opphørt"),
-    YTELSE_ANNULERT("YTELSE_ANNULERT", "Ytelse anullert"),
 
     UDEFINERT("-", "Ikke definert"),
     ;
 
     private static final Map<String, ForretningshendelseType> KODER = new LinkedHashMap<>();
-    
+
     public static final String KODEVERK = "FORRETNINGSHENDELSE_TYPE";
 
     @JsonIgnore
@@ -44,13 +40,6 @@ public enum ForretningshendelseType implements Kodeverdi {
         this.navn = navn;
     }
 
-    public static boolean erYtelseHendelseType(ForretningshendelseType forretningshendelseType) {
-        return YTELSE_INNVILGET.equals(forretningshendelseType)
-            || YTELSE_ENDRET.equals(forretningshendelseType)
-            || YTELSE_OPPHØRT.equals(forretningshendelseType)
-            || YTELSE_ANNULERT.equals(forretningshendelseType);
-    }
-    
     @JsonCreator
     public static ForretningshendelseType fraKode(@JsonProperty("kode") String kode) {
         if (kode == null) {
@@ -66,7 +55,7 @@ public enum ForretningshendelseType implements Kodeverdi {
     public static Map<String, ForretningshendelseType> kodeMap() {
         return Collections.unmodifiableMap(KODER);
     }
-    
+
     @Override
     public String getNavn() {
         return navn;
@@ -75,7 +64,7 @@ public enum ForretningshendelseType implements Kodeverdi {
     public static void main(String[] args) {
         System.out.println(KODER.keySet());
     }
-    
+
     @JsonProperty
     @Override
     public String getKodeverk() {
@@ -87,12 +76,12 @@ public enum ForretningshendelseType implements Kodeverdi {
     public String getKode() {
         return kode;
     }
-    
+
     @Override
     public String getOffisiellKode() {
         return getKode();
     }
-    
+
     static {
         for (var v : values()) {
             if (KODER.putIfAbsent(v.kode, v) != null) {
@@ -100,6 +89,6 @@ public enum ForretningshendelseType implements Kodeverdi {
             }
         }
     }
-    
+
 
 }
