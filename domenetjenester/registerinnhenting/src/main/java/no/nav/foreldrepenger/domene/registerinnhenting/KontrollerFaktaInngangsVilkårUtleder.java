e4af82a4610686d7c8bfe3de3c63f4097fcb4076
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2020-03-17 09:31:53 +0100

    TFP-2933 håndter apdiff når ikke tilbakehopp

--- domenetjenester/registerinnhenting/src/main/java/no/nav/foreldrepenger/domene/registerinnhenting/KontrollerFaktaInngangsVilkårUtleder.java
+++ domenetjenester/registerinnhenting/src/main/java/no/nav/foreldrepenger/domene/registerinnhenting/KontrollerFaktaInngangsVilkårUtleder.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import no.nav.foreldrepenger.behandling.BehandlingReferanse;
 import no.nav.foreldrepenger.behandlingskontroll.AksjonspunktResultat;
+import no.nav.foreldrepenger.behandlingslager.behandling.BehandlingStegType;
 import no.nav.foreldrepenger.behandlingslager.behandling.aksjonspunkt.AksjonspunktDefinisjon;
 import no.nav.foreldrepenger.behandlingslager.hendelser.StartpunktType;
 
@@ -12,6 +13,8 @@ public interface KontrollerFaktaInngangsVilkårUtleder {
     List<AksjonspunktResultat> utledAksjonspunkter(BehandlingReferanse ref);
 
     List<AksjonspunktResultat> utledAksjonspunkterTilHøyreForStartpunkt(BehandlingReferanse ref, StartpunktType startpunktType);
+
+    List<AksjonspunktResultat> utledAksjonspunkterFomSteg(BehandlingReferanse ref, BehandlingStegType steg);
 
     boolean skalOverstyringLøsesTilHøyreForStartpunkt(BehandlingReferanse ref, StartpunktType startpunktType, AksjonspunktDefinisjon aksjonspunktDefinisjon);
 
