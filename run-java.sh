cb92a40b439bf61db96077cf350dc2f453a71378
V134773 <eirik.vimme@nav.no>
2020-04-24 15:38:36 +0200

    TFP-3069 Fjerner parameter user.timezone som ikke fungerer som det skal. JVM ser ikke ut til å forstå verdien 'Europe/Oslo'

--- run-java.sh
+++ run-java.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -eu
 
-export JAVA_OPTS="${JAVA_OPTS:-} -Xmx2048m -Xms256m -Djava.security.egd=file:/dev/./urandom -Duser.timezone='Europe/Oslo'"
+export JAVA_OPTS="${JAVA_OPTS:-} -Xmx2048m -Xms256m -Djava.security.egd=file:/dev/./urandom "
 export STARTUP_CLASS=${STARTUP_CLASS:-"no.nav.foreldrepenger.web.server.jetty.JettyServer"}
 
 exec java -cp "app.jar:lib/*"${EXTRA_CLASS_PATH:-""} ${DEFAULT_JAVA_OPTS:-} ${JAVA_OPTS} \
