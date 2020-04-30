3efd727214e3df2cf0cd10177a412305172062eb
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2020-04-30 12:47:09 +0200

    TFP-1106 bruk kontrakt mot fpfordel (#441)

--- web/src/main/java/no/nav/foreldrepenger/web/app/soap/sak/v1/FpfordelRestKlient.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/soap/sak/v1/FpfordelRestKlient.java
@@ -8,6 +8,7 @@ import javax.inject.Inject;
 import org.apache.http.client.utils.URIBuilder;
 
 import no.nav.foreldrepenger.domene.typer.JournalpostId;
+import no.nav.foreldrepenger.kontrakter.fordel.JournalpostVurderingDto;
 import no.nav.vedtak.felles.integrasjon.rest.OidcRestClient;
 import no.nav.vedtak.konfig.KonfigVerdi;
 
@@ -30,11 +31,11 @@ public class FpfordelRestKlient {
         this.endpointUtledYtelseType = URI.create(endpoint.toString() + PATH_UTLED);
     }
 
-    public String utledYtelestypeFor(JournalpostId journalpost) {
+    public JournalpostVurderingDto utledYtelestypeFor(JournalpostId journalpost) {
         try {
             var request = new URIBuilder(endpointUtledYtelseType)
                 .addParameter("journalpostId", journalpost.getVerdi());
-            return oidcRestClient.get(request.build(), String.class);
+            return oidcRestClient.get(request.build(), JournalpostVurderingDto.class);
         } catch (Exception e) {
             throw new IllegalArgumentException("Noe galt skjedde", e);
         }
