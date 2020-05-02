697975a2b7ed28f896bc3120a5936a7e383fac6d
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2020-05-02 22:58:18 +0200

    TFP-1106 bruk kun fordelrest og fjern inngaaende

--- web/src/main/java/no/nav/foreldrepenger/web/app/soap/sak/v1/OpprettSakServiceFeil.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/soap/sak/v1/OpprettSakServiceFeil.java
@@ -9,12 +9,9 @@ import no.nav.vedtak.feil.deklarasjon.FunksjonellFeil;
 interface OpprettSakServiceFeil extends DeklarerteFeil {
     OpprettSakServiceFeil FACTORY = FeilFactory.create(OpprettSakServiceFeil.class);
 
-    @FunksjonellFeil(feilkode = "FP-785354", feilmelding = "Kan ikke opprette sak basert på: %s", løsningsforslag = "Journalføre dokument på annen sak", logLevel = LogLevel.WARN)
-    Feil ikkeStøttetDokumentType(String dokumentType);
+    @FunksjonellFeil(feilkode = "FP-785354", feilmelding = "Kan ikke opprette sak basert på oppgitt dokument", løsningsforslag = "Journalføre dokument på annen sak", logLevel = LogLevel.WARN)
+    Feil ikkeStøttetDokumentType();
 
-    @FunksjonellFeil(feilkode = "FP-785355", feilmelding = "Kan ikke opprette sak basert på vedlegg", løsningsforslag = "Journalføre dokument på annen sak", logLevel = LogLevel.WARN)
-    Feil ikkeStøttetKunVedlegg();
-
-    @FunksjonellFeil(feilkode = "FP-785356", feilmelding = "Dokument og valgt ytelsetype i uoverenstemmelse: %s", løsningsforslag = "Velg ytelsetype som samstemmer med dokument", logLevel = LogLevel.WARN)
-    Feil inkonsistensTemaVsDokument(String dokumentType);
+    @FunksjonellFeil(feilkode = "FP-785356", feilmelding = "Dokument og valgt ytelsetype i uoverenstemmelse", løsningsforslag = "Velg ytelsetype som samstemmer med dokument", logLevel = LogLevel.WARN)
+    Feil inkonsistensTemaVsDokument();
 }
