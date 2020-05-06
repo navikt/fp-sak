770ce9c1a8c69d55c65fb9fbac14da1676b2d60b
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2020-05-06 09:08:31 +0200

    TFP-3236 feilretting avstemming enkeltsak (#459)

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/forvaltning/ForvaltningUttrekkRestTjeneste.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/forvaltning/ForvaltningUttrekkRestTjeneste.java
@@ -6,6 +6,7 @@ import static no.nav.vedtak.sikkerhet.abac.BeskyttetRessursResourceAttributt.FAG
 
 import java.sql.Timestamp;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,12 +16,14 @@ import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -33,6 +36,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import no.nav.foreldrepenger.behandlingslager.behandling.aksjonspunkt.AksjonspunktDefinisjon;
 import no.nav.foreldrepenger.behandlingslager.behandling.aksjonspunkt.AksjonspunktStatus;
 import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepositoryProvider;
+import no.nav.foreldrepenger.behandlingslager.behandling.vedtak.BehandlingOverlappInfotrygd;
 import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakRepository;
 import no.nav.foreldrepenger.behandlingsprosess.dagligejobber.infobrev.InformasjonssakRepository;
 import no.nav.foreldrepenger.domene.vedtak.infotrygd.overlapp.IdentifiserOverlappendeInfotrygdYtelseTjeneste;
@@ -165,7 +169,7 @@ public class ForvaltningUttrekkRestTjeneste {
     @Operation(description = "Lagrer task for å finne overlapp. Resultat i app-logg", tags = "FORVALTNING-uttrekk")
     @BeskyttetRessurs(action = READ, ressurs = DRIFT)
     @SuppressWarnings("findsecbugs:JAXRS_ENDPOINT")
-    public Response avstemSakForOverlapp(@Parameter(description = "Periode") @BeanParam @Valid AvstemmingEnkeltSakDto dto) {
+    public Response avstemSakForOverlapp(@Parameter(description = "Saksnummer") @BeanParam @Valid AvstemmingEnkeltSakDto dto) {
         ProsessTaskData prosessTaskData = new ProsessTaskData(VurderOpphørAvYtelserTask.TASKTYPE);
         prosessTaskData.setProperty(VurderOpphørAvYtelserTask.HIJACK_KEY_KEY, dto.getKey());
         prosessTaskData.setProperty(VurderOpphørAvYtelserTask.HIJACK_SAKSNUMMER_KEY, dto.getSaksnummer());
@@ -177,25 +181,20 @@ public class ForvaltningUttrekkRestTjeneste {
         return Response.ok().build();
     }
 
-    @POST
-    @Path("/avstemSakOverlappInteraktiv")
-    @Consumes(MediaType.APPLICATION_JSON)
+    @GET
+    @Path("/avstemSakOverlappTrexInteraktiv")
     @Produces(MediaType.APPLICATION_JSON)
     @Operation(description = "Prøver å finne overlapp og returnere resultat", tags = "FORVALTNING-uttrekk")
-    @BeskyttetRessurs(action = READ, ressurs = DRIFT)
+    @BeskyttetRessurs(action = READ, ressurs = FAGSAK)
     @SuppressWarnings("findsecbugs:JAXRS_ENDPOINT")
-    public Response avstemSakForOverlappInteraktiv(@Parameter(description = "Periode") @BeanParam @Valid AvstemmingEnkeltSakDto dto) {
-        if (VurderOpphørAvYtelserTask.HIJACK_OTH_KEY.equals(dto.getKey())) {
-            var vedtak = informasjonssakRepository.finnSakerOpprettetInnenIntervallMedKunUtbetalte(null, null, dto.getSaksnummer());
-            if (!vedtak.isEmpty()) {
-                return Response.ok(spbsLog.vurderEventueltOverlapp(vedtak.get(0).getBehandlingId(), vedtak.get(0).getTidligsteDato())).build();
-            }
-        } else if (VurderOpphørAvYtelserTask.HIJACK_FOR_KEY.equals(dto.getKey())) {
-            var vedtak = informasjonssakRepository.finnSakerOpprettetInnenIntervallMedSisteVedtak(null, null, dto.getSaksnummer());
-            if (!vedtak.isEmpty()) {
-                return Response.ok(forLog.vurderEventueltOverlapp(vedtak.get(0).getBehandlingId(), vedtak.get(0).getAnnenPartAktørId(), vedtak.get(0).getTidligsteDato())).build();
-            }
-        }
-        return Response.status(Response.Status.BAD_REQUEST).build();
+    public Response hentAlleBehandlinger(@NotNull @QueryParam("saksnummer") @Parameter(description = "Eksisterende saksnummer") @Valid SaksnummerDto s) {
+        List<BehandlingOverlappInfotrygd> resultat = new ArrayList<>();
+        var vedtakAndre = informasjonssakRepository.finnSakerOpprettetInnenIntervallMedKunUtbetalte(null, null, s.getVerdi());
+        if (!vedtakAndre.isEmpty())
+            resultat.addAll(spbsLog.vurderEventueltOverlapp(vedtakAndre.get(0).getBehandlingId(), vedtakAndre.get(0).getTidligsteDato()));
+        var vedtakFor = informasjonssakRepository.finnSakerOpprettetInnenIntervallMedSisteVedtak(null, null, s.getVerdi());
+        if (!vedtakFor.isEmpty())
+            resultat.addAll(forLog.vurderEventueltOverlapp(vedtakFor.get(0).getBehandlingId(), vedtakFor.get(0).getAnnenPartAktørId(), vedtakFor.get(0).getTidligsteDato()));
+        return Response.ok(resultat).build();
     }
 }
