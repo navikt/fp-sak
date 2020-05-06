0221a84629c4cae900861da498af5f2882c28e42
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2020-05-06 15:22:01 +0200

    TFP-3236 endret mønster for opphenting av resultat (#467)

--- behandlingslager/domene/src/main/java/no/nav/foreldrepenger/behandlingslager/behandling/vedtak/BehandlingOverlappInfotrygdRepository.java
+++ behandlingslager/domene/src/main/java/no/nav/foreldrepenger/behandlingslager/behandling/vedtak/BehandlingOverlappInfotrygdRepository.java
@@ -1,8 +1,11 @@
 package no.nav.foreldrepenger.behandlingslager.behandling.vedtak;
 
+import java.util.List;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import no.nav.vedtak.felles.jpa.VLPersistenceUnit;
 
@@ -18,6 +21,14 @@ public class BehandlingOverlappInfotrygdRepository {
     @Inject
     public BehandlingOverlappInfotrygdRepository(@VLPersistenceUnit EntityManager entityManager) {
         this.entityManager = entityManager;
+    }
+
+    public List<BehandlingOverlappInfotrygd> hentForBehandling(Long behandlingId) {
+        TypedQuery<BehandlingOverlappInfotrygd> query = entityManager
+            .createQuery("from BehandlingOverlappInfotrygd where behandlingId=:behandlingId",
+                BehandlingOverlappInfotrygd.class);
+        query.setParameter("behandlingId", behandlingId); // NOSONAR
+        return query.getResultList();
     }
 
     public Long lagre(BehandlingOverlappInfotrygd behandlingOverlappInfotrygd) {
