3c9341d72bf424ce106cb56b536b67485ad3a79a
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-05-15 12:51:45 +0200

    Feature/noop sakobehandling utenfor prod (#510)
    * FIX: gjør Sakogbehandling til nooop utenfor prod
    
    * FIX: fjerner sakogbehandling funny business fra getmqclients
    
    * FIX: reverter endringer
    
    allerede håndtert av @jolarsen

--- vtp/getmqclients-GA.sh
+++ vtp/getmqclients-GA.sh
@@ -8,17 +8,9 @@ then
   echo "Autentisering mot github vil feile og wget vil returnere 400: Bad Response"
 else
   echo "Token OK."
-  echo "Henter siste test-jars for sakogbehandling-klienten, okonomistotte-klienten og felles-integrasjon-jms-klienten"
+  echo "Henter siste test-jars for okonomistotte-klienten og felles-integrasjon-jms-klienten"
 fi
 
-
-
-wget --no-check-certificate --quiet --method GET --timeout=0 --header "Authorization: Bearer $token" \
-   'https://maven.pkg.github.com/navikt/fp-felles/no/nav/foreldrepenger/felles/integrasjon/sakogbehandling-klient/maven-metadata.xml' \
-   -O maven-metadata-sb.xml -nv -t 2
-sbversion=$(grep "<latest>" maven-metadata-sb.xml | sed -e 's/.*<latest>\(.*\)<\/latest>.*/\1/' | cut -c1,2,3,6-)
-echo "Siste versjon sak og behandling er $sbversion"
-rm maven-metadata-sb.xml
 
 wget --no-check-certificate --quiet --method GET --timeout=0 --header "Authorization: Bearer $token" \
    'https://maven.pkg.github.com/navikt/fp-felles/no/nav/foreldrepenger/felles/integrasjon/okonomistotte-jms/maven-metadata.xml' \
@@ -33,12 +25,6 @@ wget --no-check-certificate --quiet --method GET --timeout=0 --header "Authoriza
 jmsversion=$(grep "<latest>" maven-metadata-jms.xml | sed -e 's/.*<latest>\(.*\)<\/latest>.*/\1/' | cut -c1,2,3,6-)
 echo "Siste versjon felles-integrasjon-jms er $jmsversion"
 rm maven-metadata-jms.xml
-
-
-
-echo "Henter sak og behandling..."
-wget --user=x-token --password=$token --content-on-error "https://maven.pkg.github.com/navikt/fp-felles/no/nav/foreldrepenger/felles/integrasjon/sakogbehandling-klient/$sbversion/sakogbehandling-klient-$sbversion-tests.jar" \
-    -O sakogbehandling-klient.jar -nv -t 2
 
 
 echo "Henterokonomi..."
