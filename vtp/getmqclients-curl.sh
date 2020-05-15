80ce8b6e525124b885be99a8708b6010dfa39cf8
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-05-15 12:19:13 +0200

    FIX: fjerner sakogbehandling funny business fra getmqclients

--- vtp/getmqclients-curl.sh
+++ vtp/getmqclients-curl.sh
@@ -8,12 +8,7 @@ else
   echo "Token som brukes for autentisering mot github er IKKE NULL"
 fi
 
-echo "Henter siste test-jars for sakogbehandling-klient og okonomistotte-klient"
-
-curl -u $username:$token https://maven.pkg.github.com/navikt/fp-felles/no/nav/foreldrepenger/felles/integrasjon/sakogbehandling-klient/maven-metadata.xml -o maven-metadata-sb.xml -s
-sbversion=$(grep "<latest>" maven-metadata-sb.xml | sed -e 's/.*<latest>\(.*\)<\/latest>.*/\1/' | cut -c1,2,3,6-)
-echo "Siste versjon sak og behandling er $sbversion"
-rm maven-metadata-sb.xml
+echo "Henter siste test-jars for okonomistotte-klient"
 
 curl -u $username:$token https://maven.pkg.github.com/navikt/fp-felles/no/nav/foreldrepenger/felles/integrasjon/okonomistotte-jms/maven-metadata.xml -o maven-metadata-o.xml -s
 oversion=$(grep "<latest>" maven-metadata-o.xml | sed -e 's/.*<latest>\(.*\)<\/latest>.*/\1/' | cut -c1,2,3,6-)
@@ -24,10 +19,6 @@ curl -u $username:$token https://maven.pkg.github.com/navikt/fp-felles/no/nav/fo
 jmsversion=$(grep "<latest>" maven-metadata-jms.xml | sed -e 's/.*<latest>\(.*\)<\/latest>.*/\1/' | cut -c1,2,3,6-)
 echo "Siste versjon felles-integrasjon-jms er $jmsversion"
 rm maven-metadata-jms.xml
-
-echo "Henter sak og behandling..."
-curl --location --request GET "https://maven.pkg.github.com/navikt/fp-felles/no/nav/foreldrepenger/felles/integrasjon/sakogbehandling-klient/$sbversion/sakogbehandling-klient-$sbversion-tests.jar" \
---header "Authorization: Bearer $token" > sakogbehandling-klient.jar
 
 echo "Henter okonomi..."
 curl --location --request GET "https://maven.pkg.github.com/navikt/fp-felles/no/nav/foreldrepenger/felles/integrasjon/okonomistotte-jms/$oversion/okonomistotte-jms-$oversion-tests.jar" \
