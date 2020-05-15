233b172f385095cccad149a417b8de775c1157a6
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-05-15 10:45:23 +0200

    FIX: flytter inn konfig for sakogbehandling. Denne brukes bare av fpsak ,og kan følgelig fjernes fra felles nå

--- web/src/main/java/no/nav/foreldrepenger/web/app/healthchecks/checks/SakOgBehandlingQueueHealthCheck.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/healthchecks/checks/SakOgBehandlingQueueHealthCheck.java
@@ -3,7 +3,7 @@ package no.nav.foreldrepenger.web.app.healthchecks.checks;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
-import no.nav.vedtak.felles.integrasjon.sakogbehandling.SakOgBehandlingClient;
+import no.nav.foreldrepenger.produksjonsstyring.sakogbehandling.mq.SakOgBehandlingClient;
 
 @ApplicationScoped
 public class SakOgBehandlingQueueHealthCheck extends QueueHealthCheck {
