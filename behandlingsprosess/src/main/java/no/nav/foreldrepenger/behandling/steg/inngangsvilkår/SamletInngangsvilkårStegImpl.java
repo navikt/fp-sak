331059b193433510fcf37ca11b04baf9d5595d06
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2020-05-20 09:41:24 +0200

    TFP-2248 vurdersamlet hopper til avslag hvis noen IV er ikke oppfylt (#532)
    * TFP-2248 vurdersamlet hopper til avslag hvis noen IV er ikke oppfylt
    
    * NOJIRA fjerne precondition i beregning

--- behandlingsprosess/src/main/java/no/nav/foreldrepenger/behandling/steg/inngangsvilkår/SamletInngangsvilkårStegImpl.java
+++ behandlingsprosess/src/main/java/no/nav/foreldrepenger/behandling/steg/inngangsvilkår/SamletInngangsvilkårStegImpl.java
@@ -3,6 +3,8 @@ package no.nav.foreldrepenger.behandling.steg.inngangsvilkår;
 import static java.util.Collections.emptyList;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -12,7 +14,11 @@ import no.nav.foreldrepenger.behandlingskontroll.BehandlingTypeRef;
 import no.nav.foreldrepenger.behandlingskontroll.FagsakYtelseTypeRef;
 import no.nav.foreldrepenger.behandlingslager.behandling.BehandlingStegType;
 import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepositoryProvider;
+import no.nav.foreldrepenger.behandlingslager.behandling.vilkår.Vilkår;
 import no.nav.foreldrepenger.behandlingslager.behandling.vilkår.VilkårType;
+import no.nav.foreldrepenger.behandlingslager.behandling.vilkår.VilkårUtfallType;
+import no.nav.foreldrepenger.inngangsvilkaar.InngangsvilkårTjeneste;
+import no.nav.foreldrepenger.inngangsvilkaar.RegelResultat;
 
 
 // Steget sikrer at vilkårne blir vurdert samlet som inngangsvilkår
@@ -22,15 +28,30 @@ import no.nav.foreldrepenger.behandlingslager.behandling.vilkår.VilkårType;
 @ApplicationScoped
 public class SamletInngangsvilkårStegImpl extends InngangsvilkårStegImpl {
 
-    private static List<VilkårType> STØTTEDE_VILKÅR = emptyList();
+    private static final List<VilkårType> STØTTEDE_VILKÅR = emptyList();
+
+    private final Set<VilkårType> alleInngangsVilkår;
 
     @Inject
-    public SamletInngangsvilkårStegImpl(BehandlingRepositoryProvider repositoryProvider, InngangsvilkårFellesTjeneste inngangsvilkårFellesTjeneste) {
+    public SamletInngangsvilkårStegImpl(BehandlingRepositoryProvider repositoryProvider,
+                                        InngangsvilkårFellesTjeneste inngangsvilkårFellesTjeneste,
+                                        InngangsvilkårTjeneste inngangsvilkårTjeneste) {
         super(repositoryProvider, inngangsvilkårFellesTjeneste, BehandlingStegType.VURDER_SAMLET);
+        alleInngangsVilkår = VilkårType.kodeMap().values().stream()
+            .filter(inngangsvilkårTjeneste::erInngangsvilkår)
+            .collect(Collectors.toSet());
     }
 
     @Override
     public List<VilkårType> vilkårHåndtertAvSteg() {
         return STØTTEDE_VILKÅR;
+    }
+
+    @Override
+    protected boolean erNoenVilkårIkkeOppfylt(RegelResultat regelResultat) {
+        return regelResultat.getVilkårResultat().getVilkårene().stream()
+            .filter(v -> alleInngangsVilkår.contains(v.getVilkårType()))
+            .map(Vilkår::getGjeldendeVilkårUtfall)
+            .anyMatch(VilkårUtfallType.IKKE_OPPFYLT::equals);
     }
 }
