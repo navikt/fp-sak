a47a16365ab120c33b1551c9af0b2be288e3f3e1
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2020-05-24 17:40:52 +0200

    TFP-3391 forenkling av mottak bli kvitt SporingsloggHelper (#556)

--- domenetjenester/mottak/src/main/java/no/nav/foreldrepenger/mottak/hendelser/ForretningshendelseOversetter.java
+++
@@ -1,8 +0,0 @@
-package no.nav.foreldrepenger.mottak.hendelser;
-
-import no.nav.foreldrepenger.behandlingslager.hendelser.Forretningshendelse;
-import no.nav.foreldrepenger.mottak.hendelser.kontrakt.ForretningshendelseDto;
-
-public interface ForretningshendelseOversetter<T extends Forretningshendelse> {
-    T oversett(ForretningshendelseDto forretningshendelse);
-}
