0548ea14ff83f779f093ea23120ab9d557694a0f
Marianne Myhre <marianne.myhre@nav.no>
2020-05-26 12:50:52 +0200

    TFP-3374: Sjekker forskjell i sammenligningsgrunnlagPrStatus

--- domenetjenester/beregningsgrunnlag/src/test/java/no/nav/foreldrepenger/domene/MÅ_LIGGE_HOS_FPSAK/BeregningsgrunnlagDiffSjekkerTest.java
+++ domenetjenester/beregningsgrunnlag/src/test/java/no/nav/foreldrepenger/domene/MÅ_LIGGE_HOS_FPSAK/BeregningsgrunnlagDiffSjekkerTest.java
@@ -19,6 +19,8 @@ import no.nav.foreldrepenger.domene.SKAL_FLYTTES_TIL_KALKULUS.Beregningsgrunnlag
 import no.nav.foreldrepenger.domene.SKAL_FLYTTES_TIL_KALKULUS.Inntektskategori;
 import no.nav.foreldrepenger.domene.SKAL_FLYTTES_TIL_KALKULUS.PeriodeÅrsak;
 import no.nav.foreldrepenger.domene.SKAL_FLYTTES_TIL_KALKULUS.Sammenligningsgrunnlag;
+import no.nav.foreldrepenger.domene.SKAL_FLYTTES_TIL_KALKULUS.SammenligningsgrunnlagPrStatus;
+import no.nav.foreldrepenger.domene.SKAL_FLYTTES_TIL_KALKULUS.SammenligningsgrunnlagType;
 
 public class BeregningsgrunnlagDiffSjekkerTest {
 
@@ -358,6 +360,125 @@ public class BeregningsgrunnlagDiffSjekkerTest {
 
         // Assert
         assertThat(harDiff).isTrue();
+    }
+
+    @Test
+    public void skalIkkeGiForskjellNårSammenligningsgrunnlagPrStatusListeErLik(){
+        SammenligningsgrunnlagPrStatus.Builder sammenligningsgrunnlagPrStatusAt = new SammenligningsgrunnlagPrStatus.Builder();
+        sammenligningsgrunnlagPrStatusAt.medRapportertPrÅr(BigDecimal.valueOf(100_000));
+        sammenligningsgrunnlagPrStatusAt.medAvvikPromille(BigDecimal.ZERO);
+        sammenligningsgrunnlagPrStatusAt.medSammenligningsperiode(LocalDate.now().minusYears(1), LocalDate.now());
+        sammenligningsgrunnlagPrStatusAt.medSammenligningsgrunnlagType(no.nav.foreldrepenger.domene.SKAL_FLYTTES_TIL_KALKULUS.SammenligningsgrunnlagType.SAMMENLIGNING_AT);
+
+        SammenligningsgrunnlagPrStatus.Builder sammenligningsgrunnlagPrStatusFl = new SammenligningsgrunnlagPrStatus.Builder();
+        sammenligningsgrunnlagPrStatusFl.medRapportertPrÅr(BigDecimal.valueOf(200_000));
+        sammenligningsgrunnlagPrStatusFl.medAvvikPromille(BigDecimal.valueOf(250));
+        sammenligningsgrunnlagPrStatusFl.medSammenligningsperiode(LocalDate.now().minusYears(1), LocalDate.now());
+        sammenligningsgrunnlagPrStatusFl.medSammenligningsgrunnlagType(no.nav.foreldrepenger.domene.SKAL_FLYTTES_TIL_KALKULUS.SammenligningsgrunnlagType.SAMMENLIGNING_FL);
+
+        BeregningsgrunnlagEntitet beregningsgrunnlagEntitet = BeregningsgrunnlagEntitet.builder()
+                .leggTilSammenligningsgrunnlag(sammenligningsgrunnlagPrStatusAt)
+                .leggTilSammenligningsgrunnlag(sammenligningsgrunnlagPrStatusFl)
+                .medSkjæringstidspunkt(LocalDate.now())
+                .build();
+        boolean resultat = BeregningsgrunnlagDiffSjekker.harSignifikantDiffIBeregningsgrunnlag(beregningsgrunnlagEntitet, beregningsgrunnlagEntitet);
+        assertThat(resultat).isFalse();
+    }
+
+    @Test
+    public void skalGiForskjellNårSammenligningsgrunnlagPrStatusListeIkkeInneholderSammeTyper(){
+        SammenligningsgrunnlagPrStatus.Builder sammenligningsgrunnlagPrStatusAt = new SammenligningsgrunnlagPrStatus.Builder();
+        sammenligningsgrunnlagPrStatusAt.medRapportertPrÅr(BigDecimal.valueOf(100_000));
+        sammenligningsgrunnlagPrStatusAt.medAvvikPromille(BigDecimal.ZERO);
+        sammenligningsgrunnlagPrStatusAt.medSammenligningsperiode(LocalDate.now().minusYears(1), LocalDate.now());
+        sammenligningsgrunnlagPrStatusAt.medSammenligningsgrunnlagType(no.nav.foreldrepenger.domene.SKAL_FLYTTES_TIL_KALKULUS.SammenligningsgrunnlagType.SAMMENLIGNING_AT);
+
+        SammenligningsgrunnlagPrStatus.Builder sammenligningsgrunnlagPrStatusFl = new SammenligningsgrunnlagPrStatus.Builder();
+        sammenligningsgrunnlagPrStatusFl.medRapportertPrÅr(BigDecimal.valueOf(200_000));
+        sammenligningsgrunnlagPrStatusFl.medAvvikPromille(BigDecimal.valueOf(250));
+        sammenligningsgrunnlagPrStatusFl.medSammenligningsperiode(LocalDate.now().minusYears(1), LocalDate.now());
+        sammenligningsgrunnlagPrStatusFl.medSammenligningsgrunnlagType(no.nav.foreldrepenger.domene.SKAL_FLYTTES_TIL_KALKULUS.SammenligningsgrunnlagType.SAMMENLIGNING_FL);
+
+        BeregningsgrunnlagEntitet aktivt = BeregningsgrunnlagEntitet.builder()
+                .leggTilSammenligningsgrunnlag(sammenligningsgrunnlagPrStatusAt)
+                .medSkjæringstidspunkt(LocalDate.now())
+                .build();
+        BeregningsgrunnlagEntitet forrige = BeregningsgrunnlagEntitet.builder()
+                .leggTilSammenligningsgrunnlag(sammenligningsgrunnlagPrStatusFl)
+                .medSkjæringstidspunkt(LocalDate.now())
+                .build();
+        boolean resultat = BeregningsgrunnlagDiffSjekker.harSignifikantDiffIBeregningsgrunnlag(aktivt, forrige);
+        assertThat(resultat).isTrue();
+    }
+
+    @Test
+    public void skalGiForskjellNårSammenligningsgrunnlagPrStatusListeIkkeHarLikeVerdierForSammeType(){
+        BigDecimal avvikPromilleAtAktivt = BigDecimal.ZERO;
+        BigDecimal avvikPromilleAtForrige = BigDecimal.valueOf(10);
+        SammenligningsgrunnlagPrStatus.Builder sammenligningsgrunnlagPrStatusAtAktivt = new SammenligningsgrunnlagPrStatus.Builder();
+        sammenligningsgrunnlagPrStatusAtAktivt.medRapportertPrÅr(BigDecimal.valueOf(100_000));
+        sammenligningsgrunnlagPrStatusAtAktivt.medAvvikPromille(avvikPromilleAtAktivt);
+        sammenligningsgrunnlagPrStatusAtAktivt.medSammenligningsperiode(LocalDate.now().minusYears(1), LocalDate.now());
+        sammenligningsgrunnlagPrStatusAtAktivt.medSammenligningsgrunnlagType(no.nav.foreldrepenger.domene.SKAL_FLYTTES_TIL_KALKULUS.SammenligningsgrunnlagType.SAMMENLIGNING_AT);
+
+        SammenligningsgrunnlagPrStatus.Builder sammenligningsgrunnlagPrStatusFlAktivt = new SammenligningsgrunnlagPrStatus.Builder();
+        sammenligningsgrunnlagPrStatusFlAktivt.medRapportertPrÅr(BigDecimal.valueOf(200_000));
+        sammenligningsgrunnlagPrStatusFlAktivt.medAvvikPromille(BigDecimal.valueOf(250));
+        sammenligningsgrunnlagPrStatusFlAktivt.medSammenligningsperiode(LocalDate.now().minusYears(1), LocalDate.now());
+        sammenligningsgrunnlagPrStatusFlAktivt.medSammenligningsgrunnlagType(no.nav.foreldrepenger.domene.SKAL_FLYTTES_TIL_KALKULUS.SammenligningsgrunnlagType.SAMMENLIGNING_FL);
+
+        SammenligningsgrunnlagPrStatus.Builder sammenligningsgrunnlagPrStatusFlForrige = new SammenligningsgrunnlagPrStatus.Builder();
+        sammenligningsgrunnlagPrStatusFlForrige.medRapportertPrÅr(BigDecimal.valueOf(200_000));
+        sammenligningsgrunnlagPrStatusFlForrige.medAvvikPromille(BigDecimal.valueOf(250));
+        sammenligningsgrunnlagPrStatusFlForrige.medSammenligningsperiode(LocalDate.now().minusYears(1), LocalDate.now());
+        sammenligningsgrunnlagPrStatusFlForrige.medSammenligningsgrunnlagType(no.nav.foreldrepenger.domene.SKAL_FLYTTES_TIL_KALKULUS.SammenligningsgrunnlagType.SAMMENLIGNING_FL);
+
+        SammenligningsgrunnlagPrStatus.Builder sammenligningsgrunnlagPrStatusAtForrige = new SammenligningsgrunnlagPrStatus.Builder();
+        sammenligningsgrunnlagPrStatusAtForrige.medRapportertPrÅr(BigDecimal.valueOf(100_000));
+        sammenligningsgrunnlagPrStatusAtForrige.medAvvikPromille(avvikPromilleAtForrige);
+        sammenligningsgrunnlagPrStatusAtForrige.medSammenligningsperiode(LocalDate.now().minusYears(1), LocalDate.now());
+        sammenligningsgrunnlagPrStatusAtForrige.medSammenligningsgrunnlagType(no.nav.foreldrepenger.domene.SKAL_FLYTTES_TIL_KALKULUS.SammenligningsgrunnlagType.SAMMENLIGNING_AT);
+
+        BeregningsgrunnlagEntitet aktivt = BeregningsgrunnlagEntitet.builder()
+                .leggTilSammenligningsgrunnlag(sammenligningsgrunnlagPrStatusAtAktivt)
+                .leggTilSammenligningsgrunnlag(sammenligningsgrunnlagPrStatusFlAktivt)
+                .medSkjæringstidspunkt(LocalDate.now())
+                .build();
+        BeregningsgrunnlagEntitet forrige = BeregningsgrunnlagEntitet.builder()
+                .leggTilSammenligningsgrunnlag(sammenligningsgrunnlagPrStatusAtForrige)
+                .leggTilSammenligningsgrunnlag(sammenligningsgrunnlagPrStatusFlForrige)
+                .medSkjæringstidspunkt(LocalDate.now())
+                .build();
+        boolean resultat = BeregningsgrunnlagDiffSjekker.harSignifikantDiffIBeregningsgrunnlag(aktivt, forrige);
+        assertThat(resultat).isTrue();
+    }
+
+    @Test
+    public void skalIkkeGiForskjellNårSammenligningsgrunnlagPrStatusListeIkkeErSattForBeggeBeregningsgrunnlag(){
+        BeregningsgrunnlagEntitet beregningsgrunnlagEntitet = BeregningsgrunnlagEntitet.builder()
+                .medSkjæringstidspunkt(LocalDate.now())
+                .build();
+        boolean resultat = BeregningsgrunnlagDiffSjekker.harSignifikantDiffIBeregningsgrunnlag(beregningsgrunnlagEntitet, beregningsgrunnlagEntitet);
+        assertThat(resultat).isFalse();
+    }
+
+    @Test
+    public void skalReturnereTrueNårSammenligningsgrunnlagPrStatusListeIkkeErSattForDetEneBeregningsgrunnlaget(){
+        SammenligningsgrunnlagPrStatus.Builder sammenligningsgrunnlagPrStatusAt = new SammenligningsgrunnlagPrStatus.Builder();
+        sammenligningsgrunnlagPrStatusAt.medRapportertPrÅr(BigDecimal.valueOf(100_000));
+        sammenligningsgrunnlagPrStatusAt.medAvvikPromille(BigDecimal.ZERO);
+        sammenligningsgrunnlagPrStatusAt.medSammenligningsperiode(LocalDate.now().minusYears(1), LocalDate.now());
+        sammenligningsgrunnlagPrStatusAt.medSammenligningsgrunnlagType(SammenligningsgrunnlagType.SAMMENLIGNING_AT);
+
+        BeregningsgrunnlagEntitet aktivt = BeregningsgrunnlagEntitet.builder()
+                .leggTilSammenligningsgrunnlag(sammenligningsgrunnlagPrStatusAt)
+                .medSkjæringstidspunkt(LocalDate.now())
+                .build();
+        BeregningsgrunnlagEntitet forrige = BeregningsgrunnlagEntitet.builder()
+                .medSkjæringstidspunkt(LocalDate.now())
+                .build();
+        boolean resultat = BeregningsgrunnlagDiffSjekker.harSignifikantDiffIBeregningsgrunnlag(aktivt, forrige);
+        assertThat(resultat).isTrue();
     }
 
 }
