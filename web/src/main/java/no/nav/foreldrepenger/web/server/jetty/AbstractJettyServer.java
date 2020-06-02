838d42506273baae1c01e61fc4bdf32ed3ef2013
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-06-02 07:29:17 +0200

    Update AbstractJettyServer.java

--- web/src/main/java/no/nav/foreldrepenger/web/server/jetty/AbstractJettyServer.java
+++ web/src/main/java/no/nav/foreldrepenger/web/server/jetty/AbstractJettyServer.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.security.auth.message.config.AuthConfigFactory;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
