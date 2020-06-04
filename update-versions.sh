4b8ee58fb6b936c9fd60609a482dd02db969dc60
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2020-06-04 09:36:44 +0200

    Fjerner docker-compose og testendepunkt som ikke brukes (#614)

--- update-versions.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo ABAKUS_IMAGE=docker.pkg.github.com/navikt/fp-abakus/fpabakus > .env
-echo VTP_IMAGE=docker.pkg.github.com/navikt/vtp/vtp >> .env
-echo ORACLE_IMAGE=docker.pkg.github.com/navikt/fpsak-autotest/oracle-flattened >> .env
-echo POSTGRES_IMAGE=postgres:12 >> .env
-
-
-echo ".env fil opprettet - Klart for docker-compose up abakus [oracle]"
