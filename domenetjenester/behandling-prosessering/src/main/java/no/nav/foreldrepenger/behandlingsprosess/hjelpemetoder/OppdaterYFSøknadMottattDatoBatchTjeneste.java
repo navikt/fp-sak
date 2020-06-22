70912a95be2d1ae1f415afe6d9e349e0866a3a92
Anders Palfi <anderspalfi@gmail.com>
2020-06-22 09:16:26 +0200

    TFP-2394 ytelse på task oppretting mottatt dato (#709)

--- domenetjenester/behandling-prosessering/src/main/java/no/nav/foreldrepenger/behandlingsprosess/hjelpemetoder/OppdaterYFSøknadMottattDatoBatchTjeneste.java
+++ domenetjenester/behandling-prosessering/src/main/java/no/nav/foreldrepenger/behandlingsprosess/hjelpemetoder/OppdaterYFSøknadMottattDatoBatchTjeneste.java
@@ -1,23 +1,22 @@
 package no.nav.foreldrepenger.behandlingsprosess.hjelpemetoder;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
 
 import no.nav.foreldrepenger.batch.BatchArguments;
 import no.nav.foreldrepenger.batch.BatchStatus;
 import no.nav.foreldrepenger.batch.BatchTjeneste;
-import no.nav.foreldrepenger.behandlingslager.behandling.Behandling;
-import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepository;
-import no.nav.foreldrepenger.behandlingslager.fagsak.Fagsak;
+import no.nav.foreldrepenger.behandlingslager.behandling.BehandlingType;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskGruppe;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskRepository;
@@ -36,15 +35,12 @@ public class OppdaterYFSøknadMottattDatoBatchTjeneste implements BatchTjeneste 
 
     private OppdaterMottattDatoRepositoryRepository repository;
     private ProsessTaskRepository prosessTaskRepository;
-    private BehandlingRepository behandlingRepository;
 
     @Inject
     public OppdaterYFSøknadMottattDatoBatchTjeneste(OppdaterMottattDatoRepositoryRepository repository,
-                                                    ProsessTaskRepository prosessTaskRepository,
-                                                    BehandlingRepository behandlingRepository) {
+                                                    ProsessTaskRepository prosessTaskRepository) {
         this.repository = repository;
         this.prosessTaskRepository = prosessTaskRepository;
-        this.behandlingRepository = behandlingRepository;
     }
 
     OppdaterYFSøknadMottattDatoBatchTjeneste() {
@@ -61,7 +57,7 @@ public class OppdaterYFSøknadMottattDatoBatchTjeneste implements BatchTjeneste 
         var fagsaker = repository.hentFagsakerSomManglerSøknadMottattDato(args.getAntall());
         var kjøres = LocalDateTime.now();
         for (var fagsak : fagsaker) {
-            opprettTaskGruppe(fagsak, callId, kjøres);
+            opprettTaskGruppe(callId, kjøres, fagsak);
             kjøres = kjøres.plus(500, ChronoUnit.MILLIS);
         }
 
@@ -73,18 +69,16 @@ public class OppdaterYFSøknadMottattDatoBatchTjeneste implements BatchTjeneste 
         return new YfMottattDatoBatchArguments(jobArguments);
     }
 
-    private void opprettTaskGruppe(Fagsak fagsak, String callId, LocalDateTime kjøres) {
+    private void opprettTaskGruppe(String callId, LocalDateTime kjøres, FagsakIdMedBruker fagsak) {
 
-        var behandlinger = behandlingRepository.hentAbsoluttAlleBehandlingerForFagsak(fagsak.getId()).stream()
-            .filter(b -> b.erYtelseBehandling())
-            .collect(Collectors.toSet());
+        var behandlinger = repository.hentBehandlinger(fagsak.getFagsakId());
 
         var prosessTaskGruppe = new ProsessTaskGruppe();
-        for (Behandling behandling : behandlinger) {
+        for (var behandling : behandlinger) {
             ProsessTaskData prosessTaskData = new ProsessTaskData(OppdaterYFSøknadMottattDatoTask.TASKTYPE);
-            prosessTaskData.setBehandling(fagsak.getId(), behandling.getId(), behandling.getAktørId().getId());
+            prosessTaskData.setBehandling(fagsak.getFagsakId(), behandling, fagsak.getAktorId());
 
-            String nyCallId = callId + behandling.getId();
+            String nyCallId = callId + behandling;
             prosessTaskData.setCallId(nyCallId);
             prosessTaskData.setAntallFeiledeForsøk(1);
             prosessTaskData.setPrioritet(999);
@@ -120,19 +114,67 @@ public class OppdaterYFSøknadMottattDatoBatchTjeneste implements BatchTjeneste 
             //CDI
         }
 
-        public List<Fagsak> hentFagsakerSomManglerSøknadMottattDato(long antall) {
+        public List<FagsakIdMedBruker> hentFagsakerSomManglerSøknadMottattDato(long antall) {
             var sql = "select * from " +
-                "(select distinct f.* from fagsak f " +
+                "(select distinct f.id, bruker.AKTOER_ID from fagsak f " +
                 "join behandling b on f.id = b.fagsak_id " +
+                "join BRUKER bruker on f.BRUKER_ID = bruker.ID " +
                 "join GR_YTELSES_FORDELING gryf on gryf.behandling_id = b.id and gryf.aktiv = 'J'" +
                 "join YF_FORDELING yf on yf.id in (gryf.SO_FORDELING_ID, gryf.JUSTERT_FORDELING_ID, gryf.OVERSTYRT_FORDELING_ID) " +
                 "join YF_FORDELING_PERIODE yfp on yfp.FORDELING_ID = yf.ID " +
                 "where b.id = gryf.behandling_id and yfp.mottatt_dato_temp is null) " +
                 "where ROWNUM <= :antall";
 
-            Query query = entityManager.createNativeQuery(sql, Fagsak.class);
+            var query = entityManager.createNativeQuery(sql);
             query.setParameter("antall", antall);
-            return ((List<Fagsak>) query.getResultList());
+            var resultList = (List<Object[]>)query.getResultList();
+            return resultList.stream().map(o -> new FagsakIdMedBruker(((BigDecimal)o[0]).longValue(), (String)o[1])).collect(Collectors.toList());
+        }
+
+        public List<Long> hentBehandlinger(Long fagsakId) {
+            var query = entityManager.createNativeQuery(
+                "SELECT beh.id from BEHANDLING beh join fagsak f on f.id = beh.FAGSAK_ID " +
+                    " WHERE f.id=:fagsakId AND beh.BEHANDLING_TYPE in (:typer)");
+            query.setParameter("fagsakId", fagsakId);
+            var behandlingstyper = BehandlingType.getYtelseBehandlingTyper().stream()
+                .map(behandlingType -> behandlingType.getKode())
+                .collect(Collectors.toList());
+            query.setParameter("typer", behandlingstyper);
+            var resultList = (List<BigDecimal>)query.getResultList();
+            return resultList.stream().map(o -> o.longValue()).collect(Collectors.toList());
+        }
+    }
+
+    private static class FagsakIdMedBruker {
+
+        private final Long fagsakId;
+        private final String aktorId;
+
+        public FagsakIdMedBruker(Long fagsakId, String aktorId) {
+            this.fagsakId = fagsakId;
+            this.aktorId = aktorId;
+        }
+
+        public Long getFagsakId() {
+            return fagsakId;
+        }
+
+        public String getAktorId() {
+            return aktorId;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            FagsakIdMedBruker that = (FagsakIdMedBruker) o;
+            return Objects.equals(fagsakId, that.fagsakId) &&
+                Objects.equals(aktorId, that.aktorId);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(fagsakId, aktorId);
         }
     }
 
