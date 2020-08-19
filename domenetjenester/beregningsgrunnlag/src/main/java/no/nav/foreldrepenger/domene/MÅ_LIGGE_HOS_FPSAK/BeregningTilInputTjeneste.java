fbee292096268b7095ef765a235889ac71859222
Espen Velsvik <espenjv@users.noreply.github.com>
2020-08-19 15:08:46 +0200

    TFP-3599 Mapper uregulert grunnbeløp / G-verdi før regulering til kalkulus (#898)

--- domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/MÅ_LIGGE_HOS_FPSAK/BeregningTilInputTjeneste.java
+++ domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/MÅ_LIGGE_HOS_FPSAK/BeregningTilInputTjeneste.java
@@ -1,6 +1,8 @@
 package no.nav.foreldrepenger.domene.MÅ_LIGGE_HOS_FPSAK;
 
 import java.time.LocalDate;
+import java.time.MonthDay;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
@@ -12,21 +14,27 @@ import no.nav.folketrygdloven.beregningsgrunnlag.Grunnbeløp;
 import no.nav.folketrygdloven.kalkulator.input.BeregningsgrunnlagInput;
 import no.nav.folketrygdloven.kalkulator.modell.behandling.BehandlingReferanse;
 import no.nav.folketrygdloven.kalkulator.modell.behandling.Skjæringstidspunkt;
+import no.nav.foreldrepenger.behandlingslager.BaseEntitet;
+import no.nav.foreldrepenger.behandlingslager.behandling.Behandling;
+import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.foreldrepenger.domene.MÅ_LIGGE_HOS_FPSAK.mappers.til_kalkulus.BehandlingslagerTilKalkulusMapper;
 import no.nav.foreldrepenger.domene.SKAL_FLYTTES_TIL_KALKULUS.BeregningAktivitetAggregatEntitet;
 import no.nav.foreldrepenger.domene.SKAL_FLYTTES_TIL_KALKULUS.BeregningsgrunnlagEntitet;
 import no.nav.foreldrepenger.domene.SKAL_FLYTTES_TIL_KALKULUS.BeregningsgrunnlagGrunnlagEntitet;
 import no.nav.foreldrepenger.domene.SKAL_FLYTTES_TIL_KALKULUS.BeregningsgrunnlagRepository;
 import no.nav.foreldrepenger.domene.SKAL_FLYTTES_TIL_KALKULUS.BeregningsgrunnlagTilstand;
+import no.nav.foreldrepenger.domene.typer.Beløp;
 
 @ApplicationScoped
 public class BeregningTilInputTjeneste {
 
     private static final String UTVIKLER_FEIL_SKAL_HA_BEREGNINGSGRUNNLAG_HER = "Utvikler-feil: skal ha beregningsgrunnlag her";
     private static final Supplier<IllegalStateException> INGEN_BG_EXCEPTION_SUPPLIER = () -> new IllegalStateException(UTVIKLER_FEIL_SKAL_HA_BEREGNINGSGRUNNLAG_HER);
+    private static final MonthDay ENDRING_AV_GRUNNBELØP = MonthDay.of(5, 1);
 
     private BeregningsgrunnlagRepository beregningsgrunnlagRepository;
     private KalkulusKonfigInjecter kalkulusKonfigInjecter;
+    private BehandlingRepository behandlingRepository;
 
 
     public BeregningTilInputTjeneste() {
@@ -34,9 +42,12 @@ public class BeregningTilInputTjeneste {
     }
 
     @Inject
-    public BeregningTilInputTjeneste(BeregningsgrunnlagRepository beregningsgrunnlagRepository, KalkulusKonfigInjecter kalkulusKonfigInjecter) {
+    public BeregningTilInputTjeneste(BeregningsgrunnlagRepository beregningsgrunnlagRepository,
+                                     KalkulusKonfigInjecter kalkulusKonfigInjecter,
+                                     BehandlingRepository behandlingRepository) {
         this.beregningsgrunnlagRepository = beregningsgrunnlagRepository;
         this.kalkulusKonfigInjecter = kalkulusKonfigInjecter;
+        this.behandlingRepository = behandlingRepository;
     }
 
     public BeregningsgrunnlagInput lagInputMedVerdierFraBeregning(BeregningsgrunnlagInput input) {
@@ -58,9 +69,14 @@ public class BeregningTilInputTjeneste {
         }
         kalkulusKonfigInjecter.leggTilKonfigverdier(input);
         kalkulusKonfigInjecter.leggTilFeatureToggles(input);
+        Optional<BeregningsgrunnlagGrunnlagEntitet> førsteFastsatteGrunnlagEtterEndringAvG = finnFørsteFastsatteGrunnlagEtterEndringAvGrunnbeløp(behandlingId);
+        newInput = førsteFastsatteGrunnlagEtterEndringAvG.flatMap(BeregningsgrunnlagGrunnlagEntitet::getBeregningsgrunnlag)
+            .map(BeregningsgrunnlagEntitet::getGrunnbeløp)
+            .map(Beløp::getVerdi)
+            .map(newInput::medUregulertGrunnbeløp)
+            .orElse(newInput);
         return lagBeregningsgrunnlagHistorikk(newInput);
     }
-
 
     private BeregningsgrunnlagInput lagBeregningsgrunnlagHistorikk(BeregningsgrunnlagInput input) {
         BeregningsgrunnlagTilstand[] tilstander = BeregningsgrunnlagTilstand.values();
@@ -95,4 +111,18 @@ public class BeregningTilInputTjeneste {
             .medSkjæringstidspunktBeregning(skjæringstidspunktBeregning).build();
         return ref.medSkjæringstidspunkt(skjæringstidspunkt);
     }
+
+    private Optional<BeregningsgrunnlagGrunnlagEntitet> finnFørsteFastsatteGrunnlagEtterEndringAvGrunnbeløp(Long behandlingId) {
+        Long fagsakId = behandlingRepository.hentBehandling(behandlingId).getFagsak().getId();
+        List<Behandling> behandlinger = behandlingRepository.hentAbsoluttAlleBehandlingerForFagsak(fagsakId);
+        return behandlinger.stream()
+            .filter(b -> b.getStatus().erFerdigbehandletStatus())
+            .map(kobling -> beregningsgrunnlagRepository.hentSisteBeregningsgrunnlagGrunnlagEntitet(kobling.getId(), BeregningsgrunnlagTilstand.FASTSATT))
+            .filter(Optional::isPresent)
+            .map(Optional::get)
+            .filter(gr -> MonthDay.from(gr.getBeregningsgrunnlag().orElseThrow(() -> new IllegalStateException("Skal ha beregningsgrunnlag"))
+                .getSkjæringstidspunkt()).isAfter(ENDRING_AV_GRUNNBELØP))
+            .min(Comparator.comparing(BaseEntitet::getOpprettetTidspunkt));
+    }
+
 }
