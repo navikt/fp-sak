bc019f78bc3085d8504437da60c2d414e81f1acd
Anders Palfi <anderspalfi@gmail.com>
2020-09-04 12:09:48 +0200

    TFP-2394 mottatt dato task, ikke ta med merget og henlagt (#1009)

--- domenetjenester/behandling-prosessering/src/main/java/no/nav/foreldrepenger/behandlingsprosess/hjelpemetoder/OppdaterYFSøknadMottattDatoBatchTjeneste.java
+++ domenetjenester/behandling-prosessering/src/main/java/no/nav/foreldrepenger/behandlingsprosess/hjelpemetoder/OppdaterYFSøknadMottattDatoBatchTjeneste.java
@@ -118,6 +118,7 @@ public class OppdaterYFSøknadMottattDatoBatchTjeneste implements BatchTjeneste 
             var sql = "select * from " +
                 "(select distinct f.id, bruker.AKTOER_ID from fagsak f " +
                 "join behandling b on f.id = b.fagsak_id " +
+                "join BEHANDLING_RESULTAT br on b.id = br.behandling_id and br.BEHANDLING_RESULTAT_TYPE <> 'MERGET_OG_HENLAGT' " +
                 "join BRUKER bruker on f.BRUKER_ID = bruker.ID " +
                 "join GR_YTELSES_FORDELING gryf on gryf.behandling_id = b.id and gryf.aktiv = 'J'" +
                 "join YF_FORDELING yf on yf.id in (gryf.SO_FORDELING_ID, gryf.JUSTERT_FORDELING_ID, gryf.OVERSTYRT_FORDELING_ID) " +
