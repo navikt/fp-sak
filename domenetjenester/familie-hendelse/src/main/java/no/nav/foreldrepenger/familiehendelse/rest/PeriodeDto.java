952c69bb223049990b19830d86f4fb0e302f2db9
EirikVimmeNav <56635717+EirikVimmeNav@users.noreply.github.com>
2020-09-15 13:57:29 +0200

    Nojira oppdatering av avslutningsdato 2 (#1083)
    * TFP-2202 Hinder bruk av UttakRepository
    
    * TFP-2552 Ignorere perioder uten utbetalingsgrad for å finne siste uttaksdato
    
    * NOJIRA Opprettet tjeneste for å revurdere avslutningsdato for fagsaker
    
    * NOJIRA Opprettet tjeneste for å revurdere avslutningsdato for fagsaker
    
    * NOJIRA Opprettet tjeneste for å revurdere avslutningsdato for fagsaker
    
    * NOJIRA Fjerner ubrukte imports
    
    * NOJIRA Legger til tjeneste for å hente ut prosesstasks
    
    * NOJIRA Legger til tjeneste for å hente ut prosesstasks
    
    * NOJIRA Legger til tjeneste for å hente ut prosesstasks
    
    * NOJIRA Legger til tjeneste for å opprette prosesstasks for ny avslutningsdato
    
    * NOJIRA Modifiserer revurderAvslutningForFagsaker slik at der kun bli opprettet task, og ikke taskgruppe
    
    * NOJIRA Modifiserer revurderAvslutningForFagsaker slik at der kun bli opprettet task, og ikke taskgruppe

--- domenetjenester/familie-hendelse/src/main/java/no/nav/foreldrepenger/familiehendelse/rest/PeriodeDto.java
+++ domenetjenester/familie-hendelse/src/main/java/no/nav/foreldrepenger/familiehendelse/rest/PeriodeDto.java
@@ -1,10 +1,23 @@
 package no.nav.foreldrepenger.familiehendelse.rest;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import no.nav.foreldrepenger.sikkerhet.abac.AppAbacAttributtType;
+import no.nav.vedtak.sikkerhet.abac.AbacDataAttributter;
+import no.nav.vedtak.sikkerhet.abac.AbacDto;
+
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
-public class PeriodeDto {
+@JsonAutoDetect(getterVisibility= JsonAutoDetect.Visibility.NONE, setterVisibility= JsonAutoDetect.Visibility.NONE, fieldVisibility= JsonAutoDetect.Visibility.ANY)
+public class PeriodeDto implements AbacDto {
 
+    @JsonProperty("periodeFom")
+    @NotNull
     private LocalDate periodeFom;
+
+    @JsonProperty("periodeTom")
+    @NotNull
     private LocalDate periodeTom;
 
     public LocalDate getPeriodeFom() {
@@ -21,5 +34,10 @@ public class PeriodeDto {
 
     public void setPeriodeTom(LocalDate periodeTom) {
         this.periodeTom = periodeTom;
+    }
+
+    @Override
+    public AbacDataAttributter abacAttributter() {
+        return AbacDataAttributter.opprett();
     }
 }
