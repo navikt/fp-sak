3b5b6f64f231e4de1067aac94e162695440586ed
Anders Palfi <anderspalfi@gmail.com>
2020-09-17 12:42:20 +0200

    TFP-3727 fikser valideringsfeil i AP (#1108)

---
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/forvaltning/dto/PeriodeDto.java
@@ -0,0 +1,44 @@
+package no.nav.foreldrepenger.web.app.tjenester.forvaltning.dto;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import no.nav.vedtak.sikkerhet.abac.AbacDataAttributter;
+import no.nav.vedtak.sikkerhet.abac.AbacDto;
+
+@JsonAutoDetect(getterVisibility= JsonAutoDetect.Visibility.NONE, setterVisibility= JsonAutoDetect.Visibility.NONE, fieldVisibility= JsonAutoDetect.Visibility.ANY)
+public class PeriodeDto implements AbacDto {
+
+    @JsonProperty("periodeFom")
+    @NotNull
+    private LocalDate periodeFom;
+
+    @JsonProperty("periodeTom")
+    @NotNull
+    private LocalDate periodeTom;
+
+    public LocalDate getPeriodeFom() {
+        return periodeFom;
+    }
+
+    public void setPeriodeFom(LocalDate periodeFom) {
+        this.periodeFom = periodeFom;
+    }
+
+    public LocalDate getPeriodeTom() {
+        return periodeTom;
+    }
+
+    public void setPeriodeTom(LocalDate periodeTom) {
+        this.periodeTom = periodeTom;
+    }
+
+    @Override
+    public AbacDataAttributter abacAttributter() {
+        return AbacDataAttributter.opprett();
+    }
+}
