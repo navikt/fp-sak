c2840dba0f02440414d33183e6109c2de572ada0
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-09-24 14:14:51 +0200

    sakte sakte bli kvitt dette monsteret (#1173)

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/vedtak/aksjonspunkt/ForeslåVedtakAksjonspunktOppdaterer.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/vedtak/aksjonspunkt/ForeslåVedtakAksjonspunktOppdaterer.java
@@ -12,9 +12,10 @@ import no.nav.foreldrepenger.behandling.aksjonspunkt.OppdateringResultat;
 import no.nav.foreldrepenger.behandlingslager.behandling.Behandling;
 import no.nav.foreldrepenger.behandlingslager.behandling.BehandlingType;
 import no.nav.foreldrepenger.behandlingslager.behandling.Behandlingsresultat;
+import no.nav.foreldrepenger.behandlingslager.behandling.BehandlingsresultatRepository;
 import no.nav.foreldrepenger.behandlingslager.behandling.dokument.BehandlingDokumentEntitet;
 import no.nav.foreldrepenger.behandlingslager.behandling.dokument.BehandlingDokumentRepository;
-import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepositoryProvider;
+import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.foreldrepenger.domene.vedtak.VedtakTjeneste;
 import no.nav.foreldrepenger.domene.vedtak.impl.KlageAnkeVedtakTjeneste;
 import no.nav.foreldrepenger.historikk.HistorikkTjenesteAdapter;
@@ -22,7 +23,8 @@ import no.nav.vedtak.sikkerhet.context.SubjectHandler;
 
 @ApplicationScoped
 @DtoTilServiceAdapter(dto = ForeslaVedtakAksjonspunktDto.class, adapter = AksjonspunktOppdaterer.class)
-public class ForeslåVedtakAksjonspunktOppdaterer extends AbstractVedtaksbrevOverstyringshåndterer implements AksjonspunktOppdaterer<ForeslaVedtakAksjonspunktDto> {
+public class ForeslåVedtakAksjonspunktOppdaterer extends AbstractVedtaksbrevOverstyringshåndterer
+        implements AksjonspunktOppdaterer<ForeslaVedtakAksjonspunktDto> {
 
     private BehandlingDokumentRepository behandlingDokumentRepository;
     private OpprettToTrinnsgrunnlag opprettToTrinnsgrunnlag;
@@ -33,13 +35,14 @@ public class ForeslåVedtakAksjonspunktOppdaterer extends AbstractVedtaksbrevOve
     }
 
     @Inject
-    public ForeslåVedtakAksjonspunktOppdaterer(BehandlingRepositoryProvider repositoryProvider,
-                                               HistorikkTjenesteAdapter historikkApplikasjonTjeneste,
-                                               OpprettToTrinnsgrunnlag opprettToTrinnsgrunnlag,
-                                               VedtakTjeneste vedtakTjeneste,
-                                               BehandlingDokumentRepository behandlingDokumentRepository,
-                                               KlageAnkeVedtakTjeneste klageAnkeVedtakTjeneste) {
-        super(repositoryProvider, historikkApplikasjonTjeneste, vedtakTjeneste, behandlingDokumentRepository);
+    public ForeslåVedtakAksjonspunktOppdaterer(BehandlingRepository behandlingRepository,
+            BehandlingsresultatRepository behandlingsresultatRepository,
+            HistorikkTjenesteAdapter historikkApplikasjonTjeneste,
+            OpprettToTrinnsgrunnlag opprettToTrinnsgrunnlag,
+            VedtakTjeneste vedtakTjeneste,
+            BehandlingDokumentRepository behandlingDokumentRepository,
+            KlageAnkeVedtakTjeneste klageAnkeVedtakTjeneste) {
+        super(behandlingRepository, behandlingsresultatRepository, historikkApplikasjonTjeneste, vedtakTjeneste, behandlingDokumentRepository);
         this.behandlingDokumentRepository = behandlingDokumentRepository;
         this.opprettToTrinnsgrunnlag = opprettToTrinnsgrunnlag;
         this.klageAnkeVedtakTjeneste = klageAnkeVedtakTjeneste;
@@ -49,7 +52,8 @@ public class ForeslåVedtakAksjonspunktOppdaterer extends AbstractVedtaksbrevOve
     public OppdateringResultat oppdater(ForeslaVedtakAksjonspunktDto dto, AksjonspunktOppdaterParameter param) {
         String begrunnelse = dto.getBegrunnelse();
         Behandling behandling = param.getBehandling();
-        // Unntak for Klageinstans ettersom Frontend sender samme Dto uansett hvilket knapp man velger (5018 -> Ferdigstill -> entrinn, men kommer hit)
+        // Unntak for Klageinstans ettersom Frontend sender samme Dto uansett hvilket
+        // knapp man velger (5018 -> Ferdigstill -> entrinn, men kommer hit)
         if (KlageAnkeVedtakTjeneste.behandlingErKlageEllerAnke(behandling) && klageAnkeVedtakTjeneste.erGodkjentHosMedunderskriver(behandling)) {
             return standardHåndteringUtenTotrinn(dto, param);
         }
@@ -72,7 +76,7 @@ public class ForeslåVedtakAksjonspunktOppdaterer extends AbstractVedtaksbrevOve
 
         behandlingsresultatRepository.hentHvisEksisterer(behandling.getId()).ifPresent(behandlingsresultat -> {
             if (BehandlingType.KLAGE.equals(behandling.getType()) || behandlingsresultat.isBehandlingsresultatAvslåttOrOpphørt()
-                || begrunnelse != null || skalNullstilleFritekstfelt(behandling, behandlingsresultat, behandlingDokument)) {
+                    || begrunnelse != null || skalNullstilleFritekstfelt(behandling, behandlingsresultat, behandlingDokument)) {
 
                 BehandlingDokumentEntitet.Builder behandlingDokumentBuilder = getBehandlingDokumentBuilder(behandlingDokument);
                 behandlingDokumentBuilder.medBehandling(behandling.getId());
@@ -83,9 +87,10 @@ public class ForeslåVedtakAksjonspunktOppdaterer extends AbstractVedtaksbrevOve
         behandling.setAnsvarligSaksbehandler(getCurrentUserId());
     }
 
-    private boolean skalNullstilleFritekstfelt(Behandling behandling, Behandlingsresultat behandlingsresultat, Optional<BehandlingDokumentEntitet> behandlingDokument) {
+    private boolean skalNullstilleFritekstfelt(Behandling behandling, Behandlingsresultat behandlingsresultat,
+            Optional<BehandlingDokumentEntitet> behandlingDokument) {
         return !BehandlingType.KLAGE.equals(behandling.getType()) && !behandlingsresultat.isBehandlingsresultatAvslåttOrOpphørt()
-            && behandlingDokument.isPresent() && behandlingDokument.get().getVedtakFritekst() != null;
+                && behandlingDokument.isPresent() && behandlingDokument.get().getVedtakFritekst() != null;
     }
 
     protected String getCurrentUserId() {
