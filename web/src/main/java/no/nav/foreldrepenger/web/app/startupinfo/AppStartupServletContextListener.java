528dba101153e41ea426c784394dcf25befcd943
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2020-09-28 14:17:17 +0200

    NOJIRA fiks mottaksvelger (#1201)

--- web/src/main/java/no/nav/foreldrepenger/web/app/startupinfo/AppStartupServletContextListener.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/startupinfo/AppStartupServletContextListener.java
@@ -11,35 +11,12 @@ import no.nav.foreldrepenger.web.app.healthchecks.HealthCheckRestService;
 
 public class AppStartupServletContextListener implements ServletContextListener {
 
-    private static final Logger logger = LoggerFactory.getLogger(AppStartupServletContextListener.class);
-
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
-
-        // start denne async, logg til slutt når ferdig
-        Thread thread = new Thread(this::startupLogging, getClass().getSimpleName() + "-thread");
-        thread.setDaemon(true);
-        thread.start();
-
         CDI.current().select(HealthCheckRestService.class).get().setIsContextStartupReady(Boolean.TRUE);
     }
 
-    private void startupLogging() {
-        // Henter dependent instance og destroyer etterpå.
-        AppStartupInfoLogger appStartupInfoLogger = null;
-        try {
-            Thread.sleep(5L * 1000L); // La verden gå litt videre får vi dumper ut
-            appStartupInfoLogger = CDI.current().select(AppStartupInfoLogger.class).get();
-            appStartupInfoLogger.logAppStartupInfo();
-        } catch (Exception e) {
-            OppstartFeil.FACTORY.uventetExceptionVedOppstart(e).log(logger);
-            // men ikke re-throw - vi ønsker ikke at oppstart skal feile pga. feil i logging
-        } finally {
-            if (appStartupInfoLogger != null) {
-                CDI.current().destroy(appStartupInfoLogger);
-            }
-        }
-    }
+
 
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
