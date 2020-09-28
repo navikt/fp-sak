13d38d6d6c426f5881285994be48ec65589562ca
Petter Ekern <43809623+pekern@users.noreply.github.com>
2020-09-28 13:48:17 +0200

    TFP-2709: Fjerner toggle som skjuler nytt beregningssteg (#1193)

--- behandlingsprosess/src/main/java/no/nav/foreldrepenger/behandling/steg/beregningsgrunnlag/VurderRefusjonBeregningsgrunnlagSteg.java
+++ behandlingsprosess/src/main/java/no/nav/foreldrepenger/behandling/steg/beregningsgrunnlag/VurderRefusjonBeregningsgrunnlagSteg.java
@@ -17,19 +17,15 @@ import no.nav.foreldrepenger.behandlingslager.behandling.BehandlingStegType;
 import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakYtelseType;
 import no.nav.foreldrepenger.domene.MÅ_LIGGE_HOS_FPSAK.BeregningsgrunnlagKopierOgLagreTjeneste;
-import no.nav.vedtak.util.env.Environment;
 
 @BehandlingStegRef(kode = "VURDER_REF_BERGRUNN")
 @BehandlingTypeRef("BT-004")
 @FagsakYtelseTypeRef("*")
 @ApplicationScoped
 public class VurderRefusjonBeregningsgrunnlagSteg implements BeregningsgrunnlagSteg {
-    private static final Environment ENV = Environment.current();
-
     private BeregningsgrunnlagKopierOgLagreTjeneste beregningsgrunnlagKopierOgLagreTjeneste;
     private BehandlingRepository behandlingRepository;
     private BeregningsgrunnlagInputProvider beregningsgrunnlagInputProvider;
-    private boolean erProdMiljø;
 
     protected VurderRefusjonBeregningsgrunnlagSteg() {
         // CDI Proxy
@@ -42,20 +38,15 @@ public class VurderRefusjonBeregningsgrunnlagSteg implements BeregningsgrunnlagS
         this.beregningsgrunnlagInputProvider = Objects.requireNonNull(inputTjenesteProvider, "inputTjenesteProvider");
         this.beregningsgrunnlagKopierOgLagreTjeneste = beregningsgrunnlagKopierOgLagreTjeneste;
         this.behandlingRepository = behandlingRepository;
-        this.erProdMiljø = erProd();
     }
 
     @Override
     public BehandleStegResultat utførSteg(BehandlingskontrollKontekst kontekst) {
-        if (erProdMiljø) {
-            return BehandleStegResultat.utførtUtenAksjonspunkter();
-        } else {
-            Long behandlingId = kontekst.getBehandlingId();
-            Behandling behandling = behandlingRepository.hentBehandling(kontekst.getBehandlingId());
-            var input = getInputTjeneste(behandling.getFagsakYtelseType()).lagInput(behandlingId);
-            var aksjonspunkter = beregningsgrunnlagKopierOgLagreTjeneste.vurderRefusjonBeregningsgrunnlag(input);
-            return BehandleStegResultat.utførtMedAksjonspunktResultater(aksjonspunkter.stream().map(BeregningResultatMapper::map).collect(Collectors.toList()));
-        }
+        Long behandlingId = kontekst.getBehandlingId();
+        Behandling behandling = behandlingRepository.hentBehandling(kontekst.getBehandlingId());
+        var input = getInputTjeneste(behandling.getFagsakYtelseType()).lagInput(behandlingId);
+        var aksjonspunkter = beregningsgrunnlagKopierOgLagreTjeneste.vurderRefusjonBeregningsgrunnlag(input);
+        return BehandleStegResultat.utførtMedAksjonspunktResultater(aksjonspunkter.stream().map(BeregningResultatMapper::map).collect(Collectors.toList()));
     }
 
     private BeregningsgrunnlagInputFelles getInputTjeneste(FagsakYtelseType ytelseType) {
@@ -67,10 +58,6 @@ public class VurderRefusjonBeregningsgrunnlagSteg implements BeregningsgrunnlagS
         if (tilSteg.equals(BehandlingStegType.VURDER_REF_BERGRUNN)) {
             beregningsgrunnlagKopierOgLagreTjeneste.getRyddBeregningsgrunnlag(kontekst).ryddVurderRefusjonBeregningsgrunnlagVedTilbakeføring();
         }
-    }
-
-    private boolean erProd() {
-        return ENV.isProd();
     }
 
 }
