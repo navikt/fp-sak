403798befa2b148db715c2cc855efd54cbae8fc8
Anders Palfi <anderspalfi@gmail.com>
2020-10-02 15:56:55 +0200

    Skriver om flere tester for å fungere mot db som ikke er clean

--- behandlingslager/domene/src/test/java/no/nav/foreldrepenger/behandlingslager/behandling/repository/BehandlingKandidaterRepositoryTest.java
+++ behandlingslager/domene/src/test/java/no/nav/foreldrepenger/behandlingslager/behandling/repository/BehandlingKandidaterRepositoryTest.java
@@ -1,49 +1,48 @@
 package no.nav.foreldrepenger.behandlingslager.behandling.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.time.LocalDateTime;
 
-import javax.inject.Inject;
-
-import org.assertj.core.api.Assertions;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import no.nav.foreldrepenger.behandlingslager.behandling.BasicBehandlingBuilder;
 import no.nav.foreldrepenger.behandlingslager.behandling.aksjonspunkt.AksjonspunktDefinisjon;
 import no.nav.foreldrepenger.behandlingslager.behandling.aksjonspunkt.AksjonspunktTestSupport;
 import no.nav.foreldrepenger.behandlingslager.behandling.aksjonspunkt.Venteårsak;
 import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakYtelseType;
-import no.nav.foreldrepenger.dbstoette.UnittestRepositoryRule;
-import no.nav.vedtak.felles.testutilities.cdi.CdiRunner;
+import no.nav.foreldrepenger.dbstoette.FPsakEntityManagerAwareExtension;
+import no.nav.vedtak.felles.testutilities.db.EntityManagerAwareTest;
 
-@RunWith(CdiRunner.class)
-public class BehandlingKandidaterRepositoryTest {
+@ExtendWith(FPsakEntityManagerAwareExtension.class)
+public class BehandlingKandidaterRepositoryTest extends EntityManagerAwareTest {
 
-    @Rule
-    public final UnittestRepositoryRule repoRule = new UnittestRepositoryRule();
 
-    @Inject
-    private BehandlingKandidaterRepository sutRepo;
-
-    @Inject
+    private BehandlingKandidaterRepository behandlingKandidaterRepository;
     private BehandlingRepository behandlingRepository;
 
-    private BasicBehandlingBuilder behandlingBuilder = new BasicBehandlingBuilder(repoRule.getEntityManager());
+    @BeforeEach
+    public void setUp() {
+        var entityManager = getEntityManager();
+        behandlingKandidaterRepository = new BehandlingKandidaterRepository(entityManager);
+        behandlingRepository = new BehandlingRepository(entityManager);
+    }
 
     @Test
-    public void skal_finne_en_kandidat_for_automatisk_gjenopptagelse() throws Exception {
+    public void skal_finne_en_kandidat_for_automatisk_gjenopptagelse() {
         // Arrange
-        var behandling = behandlingBuilder.opprettOgLagreFørstegangssøknad(FagsakYtelseType.FORELDREPENGER);
+        var behandling = new BasicBehandlingBuilder(getEntityManager()).opprettOgLagreFørstegangssøknad(FagsakYtelseType.FORELDREPENGER);
         var aksjonspunkt = AksjonspunktTestSupport.leggTilAksjonspunkt(behandling, AksjonspunktDefinisjon.AUTO_MANUELT_SATT_PÅ_VENT);
         AksjonspunktTestSupport.setFrist(aksjonspunkt, LocalDateTime.now().minusMinutes(1), Venteårsak.FOR_TIDLIG_SOKNAD);
         behandlingRepository.lagre(behandling);
 
         // Act
-        var behandlinger = sutRepo.finnBehandlingerForAutomatiskGjenopptagelse();
+        var behandlinger = behandlingKandidaterRepository.finnBehandlingerForAutomatiskGjenopptagelse();
 
         // Assert
-        Assertions.assertThat(behandlinger).containsOnly(behandling);
+        assertThat(behandlinger).contains(behandling);
 
     }
 }
