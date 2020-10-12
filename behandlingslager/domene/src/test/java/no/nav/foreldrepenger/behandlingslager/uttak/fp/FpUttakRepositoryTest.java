89cd8f5cb77152cd966f97ab7d6bf60cf80011d8
Anders Palfi <anderspalfi@gmail.com>
2020-10-12 13:26:29 +0200

    Junit5 uttak repo tests

--- behandlingslager/domene/src/test/java/no/nav/foreldrepenger/behandlingslager/uttak/fp/FpUttakRepositoryTest.java
+++ behandlingslager/domene/src/test/java/no/nav/foreldrepenger/behandlingslager/uttak/fp/FpUttakRepositoryTest.java
@@ -9,9 +9,9 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import no.nav.foreldrepenger.behandlingslager.behandling.BasicBehandlingBuilder;
 import no.nav.foreldrepenger.behandlingslager.behandling.Behandlingsresultat;
@@ -21,24 +21,20 @@ import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakYtelseType;
 import no.nav.foreldrepenger.behandlingslager.uttak.PeriodeResultatType;
 import no.nav.foreldrepenger.behandlingslager.uttak.UttakArbeidType;
 import no.nav.foreldrepenger.behandlingslager.virksomhet.Arbeidsgiver;
-import no.nav.foreldrepenger.dbstoette.UnittestRepositoryRule;
+import no.nav.foreldrepenger.dbstoette.FPsakEntityManagerAwareExtension;
 import no.nav.foreldrepenger.domene.typer.InternArbeidsforholdRef;
-import no.nav.vedtak.felles.testutilities.db.RepositoryRule;
+import no.nav.vedtak.felles.testutilities.db.EntityManagerAwareTest;
 
-public class FpUttakRepositoryTest {
+@ExtendWith(FPsakEntityManagerAwareExtension.class)
+public class FpUttakRepositoryTest extends EntityManagerAwareTest {
 
     private static final String ORGNR = KUNSTIG_ORG;
-    @Rule
-    public final RepositoryRule repoRule = new UnittestRepositoryRule();
 
-    private final FpUttakRepository fpUttakRepository = new FpUttakRepository(repoRule.getEntityManager());
-    private Behandlingsresultat behandlingsresultat;
+    private FpUttakRepository fpUttakRepository;
 
-    @Before
+    @BeforeEach
     public void setUp() {
-        var behandling = new BasicBehandlingBuilder(repoRule.getEntityManager()).opprettOgLagreFørstegangssøknad(FagsakYtelseType.FORELDREPENGER);
-        behandlingsresultat = Behandlingsresultat.opprettFor(behandling);
-        new BehandlingsresultatRepository(repoRule.getEntityManager()).lagre(behandling.getId(), behandlingsresultat);
+        fpUttakRepository = new FpUttakRepository(getEntityManager());
     }
 
     @Test
@@ -51,12 +47,13 @@ public class FpUttakRepositoryTest {
         UttakResultatPerioderEntitet perioder = opprettUttakResultatPeriode(resultatType, fom, tom, stønadskontoType);
 
         //Act
-        fpUttakRepository.lagreOpprinneligUttakResultatPerioder(behandlingsresultat.getBehandlingId(), perioder);
+        Long behandlingId = lagBehandling();
+        fpUttakRepository.lagreOpprinneligUttakResultatPerioder(behandlingId, perioder);
 
         //Assert
-        Optional<UttakResultatEntitet> hentetUttakResultatOpt = fpUttakRepository.hentUttakResultatHvisEksisterer(behandlingsresultat.getBehandlingId());
+        Optional<UttakResultatEntitet> hentetUttakResultatOpt = fpUttakRepository.hentUttakResultatHvisEksisterer(behandlingId);
         assertThat(hentetUttakResultatOpt).isPresent();
-        UttakResultatEntitet hentetUttakResultat = hentetUttakResultatOpt.get();//NOSONAR
+        UttakResultatEntitet hentetUttakResultat = hentetUttakResultatOpt.get();
 
         List<UttakResultatPeriodeEntitet> resultat = hentetUttakResultat.getOpprinneligPerioder().getPerioder();
         assertThat(resultat).hasSize(1);
@@ -78,15 +75,16 @@ public class FpUttakRepositoryTest {
         UttakResultatPerioderEntitet uttakResultat3 = opprettUttakResultatPeriode(PeriodeResultatType.INNVILGET, LocalDate.now(), LocalDate.now().plusMonths(3), StønadskontoType.FORELDREPENGER);
 
         //Act
-        fpUttakRepository.lagreOpprinneligUttakResultatPerioder(behandlingsresultat.getBehandlingId(), uttakResultat1);
-        fpUttakRepository.lagreOverstyrtUttakResultatPerioder(behandlingsresultat.getBehandlingId(), overstyrt1);
-        assertOpprinneligHarResultatType(PeriodeResultatType.IKKE_FASTSATT);
-        assertThat(fpUttakRepository.hentUttakResultatHvisEksisterer(behandlingsresultat.getBehandlingId()).get().getOverstyrtPerioder()).isNotNull(); //NOSONAR
-        fpUttakRepository.lagreOpprinneligUttakResultatPerioder(behandlingsresultat.getBehandlingId(), uttakResultat2);
-        assertOpprinneligHarResultatType(PeriodeResultatType.AVSLÅTT);
-        assertThat(fpUttakRepository.hentUttakResultatHvisEksisterer(behandlingsresultat.getBehandlingId()).get().getOverstyrtPerioder()).isNull(); //NOSONAR
-        fpUttakRepository.lagreOpprinneligUttakResultatPerioder(behandlingsresultat.getBehandlingId(), uttakResultat3);
-        assertOpprinneligHarResultatType(PeriodeResultatType.INNVILGET);
+        Long behandlingId = lagBehandling();
+        fpUttakRepository.lagreOpprinneligUttakResultatPerioder(behandlingId, uttakResultat1);
+        fpUttakRepository.lagreOverstyrtUttakResultatPerioder(behandlingId, overstyrt1);
+        assertOpprinneligHarResultatType(PeriodeResultatType.IKKE_FASTSATT, behandlingId);
+        assertThat(fpUttakRepository.hentUttakResultatHvisEksisterer(behandlingId).get().getOverstyrtPerioder()).isNotNull();
+        fpUttakRepository.lagreOpprinneligUttakResultatPerioder(behandlingId, uttakResultat2);
+        assertOpprinneligHarResultatType(PeriodeResultatType.AVSLÅTT, behandlingId);
+        assertThat(fpUttakRepository.hentUttakResultatHvisEksisterer(behandlingId).get().getOverstyrtPerioder()).isNull();
+        fpUttakRepository.lagreOpprinneligUttakResultatPerioder(behandlingId, uttakResultat3);
+        assertOpprinneligHarResultatType(PeriodeResultatType.INNVILGET, behandlingId);
     }
 
     @Test
@@ -94,7 +92,8 @@ public class FpUttakRepositoryTest {
         //Arrange
         UttakResultatPerioderEntitet opprinnelig = opprettUttakResultatPeriode(PeriodeResultatType.INNVILGET,
             LocalDate.now(), LocalDate.now().plusMonths(3), StønadskontoType.FORELDREPENGER);
-        fpUttakRepository.lagreOpprinneligUttakResultatPerioder(behandlingsresultat.getBehandlingId(), opprinnelig);
+        Long behandlingId = lagBehandling();
+        fpUttakRepository.lagreOpprinneligUttakResultatPerioder(behandlingId, opprinnelig);
 
         LocalDate overstyrtFom = LocalDate.now().plusDays(1);
         LocalDate overstyrtTom = LocalDate.now().plusMonths(4);
@@ -107,12 +106,12 @@ public class FpUttakRepositoryTest {
             overstyrtKonto);
 
         //Act
-        fpUttakRepository.lagreOverstyrtUttakResultatPerioder(behandlingsresultat.getBehandlingId(), overstyrt);
+        fpUttakRepository.lagreOverstyrtUttakResultatPerioder(behandlingId, overstyrt);
 
         //Assert
-        Optional<UttakResultatEntitet> hentetUttakResultatOpt = fpUttakRepository.hentUttakResultatHvisEksisterer(behandlingsresultat.getBehandlingId());
+        Optional<UttakResultatEntitet> hentetUttakResultatOpt = fpUttakRepository.hentUttakResultatHvisEksisterer(behandlingId);
         assertThat(hentetUttakResultatOpt).isPresent();
-        UttakResultatEntitet hentetUttakResultat = hentetUttakResultatOpt.get();//NOSONAR
+        UttakResultatEntitet hentetUttakResultat = hentetUttakResultatOpt.get();
 
         assertThat(hentetUttakResultat.getOpprinneligPerioder().getPerioder()).hasSize(1);
         List<UttakResultatPeriodeEntitet> resultat = hentetUttakResultat.getOverstyrtPerioder().getPerioder();
@@ -129,15 +128,16 @@ public class FpUttakRepositoryTest {
         UttakResultatPerioderEntitet opprinnelig = opprettUttakResultatPeriode(PeriodeResultatType.IKKE_FASTSATT, LocalDate.now(), LocalDate.now().plusMonths(3), StønadskontoType.FORELDREPENGER);
         UttakResultatPerioderEntitet overstyrt1 = opprettUttakResultatPeriode(PeriodeResultatType.AVSLÅTT, LocalDate.now(), LocalDate.now().plusMonths(3), StønadskontoType.FORELDREPENGER);
         UttakResultatPerioderEntitet overstyrt2 = opprettUttakResultatPeriode(PeriodeResultatType.INNVILGET, LocalDate.now(), LocalDate.now().plusMonths(3), StønadskontoType.FORELDREPENGER);
-        fpUttakRepository.lagreOpprinneligUttakResultatPerioder(behandlingsresultat.getBehandlingId(), opprinnelig);
+        Long behandlingId = lagBehandling();
+        fpUttakRepository.lagreOpprinneligUttakResultatPerioder(behandlingId, opprinnelig);
 
         //Act
-        fpUttakRepository.lagreOverstyrtUttakResultatPerioder(behandlingsresultat.getBehandlingId(), overstyrt1);
-        assertOverstyrtHarResultatType(PeriodeResultatType.AVSLÅTT);
-        assertOpprinneligHarResultatType(PeriodeResultatType.IKKE_FASTSATT);
-        fpUttakRepository.lagreOverstyrtUttakResultatPerioder(behandlingsresultat.getBehandlingId(), overstyrt2);
-        assertOverstyrtHarResultatType(PeriodeResultatType.INNVILGET);
-        assertOpprinneligHarResultatType(PeriodeResultatType.IKKE_FASTSATT);
+        fpUttakRepository.lagreOverstyrtUttakResultatPerioder(behandlingId, overstyrt1);
+        assertOverstyrtHarResultatType(PeriodeResultatType.AVSLÅTT, behandlingId);
+        assertOpprinneligHarResultatType(PeriodeResultatType.IKKE_FASTSATT, behandlingId);
+        fpUttakRepository.lagreOverstyrtUttakResultatPerioder(behandlingId, overstyrt2);
+        assertOverstyrtHarResultatType(PeriodeResultatType.INNVILGET, behandlingId);
+        assertOpprinneligHarResultatType(PeriodeResultatType.IKKE_FASTSATT, behandlingId);
     }
 
     @Test
@@ -146,26 +146,27 @@ public class FpUttakRepositoryTest {
         UttakResultatPerioderEntitet opprinnelig = opprettUttakResultatPeriode(PeriodeResultatType.INNVILGET,
             LocalDate.now(), LocalDate.now().plusMonths(3), StønadskontoType.FORELDREPENGER,
             new BigDecimal("10.55"), new Utbetalingsgrad(20.57));
-        fpUttakRepository.lagreOpprinneligUttakResultatPerioder(behandlingsresultat.getBehandlingId(), opprinnelig);
+        Long behandlingId = lagBehandling();
+        fpUttakRepository.lagreOpprinneligUttakResultatPerioder(behandlingId, opprinnelig);
 
         //Assert
-        Optional<UttakResultatEntitet> hentetUttakResultatOpt = fpUttakRepository.hentUttakResultatHvisEksisterer(behandlingsresultat.getBehandlingId());
+        Optional<UttakResultatEntitet> hentetUttakResultatOpt = fpUttakRepository.hentUttakResultatHvisEksisterer(behandlingId);
 
         UttakResultatPeriodeAktivitetEntitet aktivitet = hentetUttakResultatOpt.orElseThrow().getGjeldendePerioder().getPerioder().get(0).getAktiviteter().get(0);
         assertThat(aktivitet.getUtbetalingsgrad().decimalValue()).isEqualTo(new BigDecimal("20.57"));
         assertThat(aktivitet.getArbeidsprosent()).isEqualTo(new BigDecimal("10.55"));
     }
 
-    private void assertOverstyrtHarResultatType(PeriodeResultatType type) {
-        Optional<UttakResultatEntitet> uttakResultatEntitet = fpUttakRepository.hentUttakResultatHvisEksisterer(behandlingsresultat.getBehandlingId());
+    private void assertOverstyrtHarResultatType(PeriodeResultatType type, Long behandlingId) {
+        Optional<UttakResultatEntitet> uttakResultatEntitet = fpUttakRepository.hentUttakResultatHvisEksisterer(behandlingId);
         assertThat(uttakResultatEntitet).isPresent();
-        assertHarResultatType(type, uttakResultatEntitet.get().getOverstyrtPerioder()); //NOSONAR
+        assertHarResultatType(type, uttakResultatEntitet.get().getOverstyrtPerioder());
     }
 
-    private void assertOpprinneligHarResultatType(PeriodeResultatType type) {
-        Optional<UttakResultatEntitet> uttakResultatEntitet = fpUttakRepository.hentUttakResultatHvisEksisterer(behandlingsresultat.getBehandlingId());
+    private void assertOpprinneligHarResultatType(PeriodeResultatType type, Long behandlingId) {
+        Optional<UttakResultatEntitet> uttakResultatEntitet = fpUttakRepository.hentUttakResultatHvisEksisterer(behandlingId);
         assertThat(uttakResultatEntitet).isPresent();
-        assertHarResultatType(type, uttakResultatEntitet.get().getOpprinneligPerioder()); //NOSONAR
+        assertHarResultatType(type, uttakResultatEntitet.get().getOpprinneligPerioder());
     }
 
     private void assertHarResultatType(PeriodeResultatType type, UttakResultatPerioderEntitet perioderEntitet) {
@@ -231,5 +232,14 @@ public class FpUttakRepositoryTest {
         perioder.leggTilPeriode(uttakResultatPeriode);
 
         return perioder;
+    }
+
+    private Long lagBehandling() {
+        var entityManager = getEntityManager();
+        var behandling = new BasicBehandlingBuilder(entityManager)
+            .opprettOgLagreFørstegangssøknad(FagsakYtelseType.FORELDREPENGER);
+        var behandlingsresultat = Behandlingsresultat.opprettFor(behandling);
+        new BehandlingsresultatRepository(entityManager).lagre(behandling.getId(), behandlingsresultat);
+        return behandling.getId();
     }
 }
