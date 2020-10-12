526cac34b89c1d7a1affcb926d0863864f0a2017
Anders Palfi <anderspalfi@gmail.com>
2020-10-12 14:31:14 +0200

    Siste junit4 bort fra uttak

--- domenetjenester/uttak/src/test/java/no/nav/foreldrepenger/domene/uttak/kontroller/fakta/KontrollerFaktaUttakTjenesteTest.java
+++ domenetjenester/uttak/src/test/java/no/nav/foreldrepenger/domene/uttak/kontroller/fakta/KontrollerFaktaUttakTjenesteTest.java
@@ -7,21 +7,25 @@ import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 
-import javax.inject.Inject;
+import javax.enterprise.inject.spi.CDI;
 
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import no.nav.foreldrepenger.behandling.BehandlingReferanse;
+import no.nav.foreldrepenger.behandlingskontroll.FagsakYtelseTypeRef;
 import no.nav.foreldrepenger.behandlingslager.behandling.Behandling;
+import no.nav.foreldrepenger.behandlingslager.behandling.personopplysning.PersonopplysningRepository;
 import no.nav.foreldrepenger.behandlingslager.behandling.ytelsefordeling.OppgittRettighetEntitet;
+import no.nav.foreldrepenger.behandlingslager.behandling.ytelsefordeling.YtelsesFordelingRepository;
 import no.nav.foreldrepenger.behandlingslager.behandling.ytelsefordeling.periode.OppgittFordelingEntitet;
 import no.nav.foreldrepenger.behandlingslager.behandling.ytelsefordeling.periode.OppgittPeriodeBuilder;
 import no.nav.foreldrepenger.behandlingslager.behandling.ytelsefordeling.periode.OppgittPeriodeEntitet;
 import no.nav.foreldrepenger.behandlingslager.behandling.ytelsefordeling.periode.UttakPeriodeType;
+import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakYtelseType;
 import no.nav.foreldrepenger.behandlingslager.geografisk.Landkoder;
-import no.nav.foreldrepenger.dbstoette.UnittestRepositoryRule;
+import no.nav.foreldrepenger.dbstoette.FPsakEntityManagerAwareExtension;
 import no.nav.foreldrepenger.domene.typer.AktørId;
 import no.nav.foreldrepenger.domene.uttak.UttakRepositoryProvider;
 import no.nav.foreldrepenger.domene.uttak.input.Barn;
@@ -31,32 +35,25 @@ import no.nav.foreldrepenger.domene.uttak.input.ForeldrepengerGrunnlag;
 import no.nav.foreldrepenger.domene.uttak.input.UttakInput;
 import no.nav.foreldrepenger.domene.uttak.input.YtelsespesifiktGrunnlag;
 import no.nav.foreldrepenger.domene.uttak.testutilities.behandling.ScenarioFarSøkerForeldrepenger;
-import no.nav.vedtak.felles.testutilities.cdi.CdiRunner;
+import no.nav.foreldrepenger.domene.ytelsefordeling.YtelseFordelingTjeneste;
+import no.nav.vedtak.felles.testutilities.db.EntityManagerAwareTest;
 
-@RunWith(CdiRunner.class) //TODO skrive bort fra cdi
-public class KontrollerFaktaUttakTjenesteTest {
+@ExtendWith(FPsakEntityManagerAwareExtension.class)
+public class KontrollerFaktaUttakTjenesteTest extends EntityManagerAwareTest {
 
     private final AktørId aktørId = AktørId.dummy();
-    private final LocalDate idag = LocalDate.now();
 
-    @Rule
-    public UnittestRepositoryRule repositoryRule = new UnittestRepositoryRule();
-    private UttakRepositoryProvider repositoryProvider = new UttakRepositoryProvider(repositoryRule.getEntityManager());
+    private UttakRepositoryProvider repositoryProvider;
 
-    @Inject
     private KontrollerFaktaUttakTjeneste tjeneste;
 
-    private Behandling opprettBehandlingForFarSomSøker() {
-        var scenario = ScenarioFarSøkerForeldrepenger.forFødselMedGittAktørId(aktørId);
-        OppgittRettighetEntitet rettighet = new OppgittRettighetEntitet(true, false, false);
-        scenario.medOppgittRettighet(rettighet);
-
-        OppgittPeriodeEntitet periode = OppgittPeriodeBuilder.ny().medPeriodeType(UttakPeriodeType.FEDREKVOTE)
-            .medPeriode(idag.plusWeeks(6), idag.plusWeeks(10))
-            .build();
-
-        scenario.medFordeling(new OppgittFordelingEntitet(Collections.singletonList(periode), true));
-        return scenario.lagre(repositoryProvider);
+    @BeforeEach
+    void setUp() {
+        var entityManager = getEntityManager();
+        repositoryProvider = new UttakRepositoryProvider(entityManager);
+        var ytelseFordelingTjeneste = new YtelseFordelingTjeneste(new YtelsesFordelingRepository(entityManager));
+        var personopplysningTjeneste = new PersonopplysningRepository(entityManager);
+        tjeneste = tjeneste(ytelseFordelingTjeneste, personopplysningTjeneste);
     }
 
     @Test
@@ -64,10 +61,10 @@ public class KontrollerFaktaUttakTjenesteTest {
         //Arrange
         Behandling behandling = opprettBehandlingForFarSomSøker();
         //Act
-        var familieHendelse = FamilieHendelse.forFødsel(null, idag, List.of(new Barn()), 1);
+        var familieHendelse = FamilieHendelse.forFødsel(null, LocalDate.now(), List.of(new Barn()), 1);
         YtelsespesifiktGrunnlag fpGrunnlag = new ForeldrepengerGrunnlag().medFamilieHendelser(new FamilieHendelser()
             .medBekreftetHendelse(familieHendelse));
-        var input = new UttakInput(BehandlingReferanse.fra(behandling, idag), null, fpGrunnlag);
+        var input = new UttakInput(BehandlingReferanse.fra(behandling, LocalDate.now()), null, fpGrunnlag);
         var aksjonspunktResultater = tjeneste.utledAksjonspunkter(input);
 
         //Assert
@@ -138,5 +135,24 @@ public class KontrollerFaktaUttakTjenesteTest {
 
         var perioderAnnenforelderHarRett = repositoryProvider.getYtelsesFordelingRepository().hentAggregat(behandling.getId()).getPerioderAnnenforelderHarRett();
         assertThat(perioderAnnenforelderHarRett).isEmpty();
+    }
+
+    private KontrollerFaktaUttakTjeneste tjeneste(YtelseFordelingTjeneste ytelseFordelingTjeneste, PersonopplysningRepository personopplysningTjeneste) {
+        var annotationLiteral = new FagsakYtelseTypeRef.FagsakYtelseTypeRefLiteral(FagsakYtelseType.FORELDREPENGER);
+        var utledere = CDI.current().select(FaktaUttakAksjonspunktUtleder.class, annotationLiteral);
+        return new KontrollerFaktaUttakTjeneste(utledere, ytelseFordelingTjeneste, personopplysningTjeneste);
+    }
+
+    private Behandling opprettBehandlingForFarSomSøker() {
+        var scenario = ScenarioFarSøkerForeldrepenger.forFødselMedGittAktørId(aktørId);
+        OppgittRettighetEntitet rettighet = new OppgittRettighetEntitet(true, false, false);
+        scenario.medOppgittRettighet(rettighet);
+
+        OppgittPeriodeEntitet periode = OppgittPeriodeBuilder.ny().medPeriodeType(UttakPeriodeType.FEDREKVOTE)
+            .medPeriode(LocalDate.now().plusWeeks(6), LocalDate.now().plusWeeks(10))
+            .build();
+
+        scenario.medFordeling(new OppgittFordelingEntitet(Collections.singletonList(periode), true));
+        return scenario.lagre(repositoryProvider);
     }
 }
