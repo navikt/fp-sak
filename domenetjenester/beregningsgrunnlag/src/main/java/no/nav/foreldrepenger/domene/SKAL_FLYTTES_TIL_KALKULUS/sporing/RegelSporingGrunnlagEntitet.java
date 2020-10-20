c9c4860412b0df2057f91a244ac47f1fc5e0182f
Espen Velsvik <espenjv@users.noreply.github.com>
2020-10-20 11:44:00 +0200

    NOJIRA Nye tabeller for lagring av regelsporing (#1365)

---
+++ domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/SKAL_FLYTTES_TIL_KALKULUS/sporing/RegelSporingGrunnlagEntitet.java
@@ -0,0 +1,117 @@
+package no.nav.foreldrepenger.domene.SKAL_FLYTTES_TIL_KALKULUS.sporing;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+import no.nav.folketrygdloven.kalkulus.felles.jpa.BaseEntitet;
+import no.nav.folketrygdloven.kalkulus.felles.kodeverk.domene.BeregningsgrunnlagRegelType;
+
+@Entity(name = "RegelSporingGrunnlagEntitet")
+@Table(name = "REGEL_SPORING_GRUNNLAG")
+public class RegelSporingGrunnlagEntitet extends BaseEntitet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_REGEL_SPORING_GRUNNLAG")
+    private Long id;
+
+    @Version
+    @Column(name = "versjon", nullable = false)
+    private long versjon;
+
+    @Column(name = "behandling_id", nullable = false, updatable = false)
+    private Long behandlingId;
+
+    @Lob
+    @Column(name = "regel_evaluering")
+    private String regelEvaluering;
+
+    @Lob
+    @Column(name = "regel_input")
+    private String regelInput;
+
+    @Convert(converter= BeregningsgrunnlagRegelType.KodeverdiConverter.class)
+    @Column(name="regel_type", nullable = false)
+    private BeregningsgrunnlagRegelType regelType;
+
+    @Column(name = "aktiv", nullable = false)
+    private boolean aktiv = true;
+
+    public RegelSporingGrunnlagEntitet() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getBehandlingId() {
+        return behandlingId;
+    }
+
+    public String getRegelEvaluering() {
+        return this.regelEvaluering;
+    }
+
+    public String getRegelInput() {
+        return regelInput;
+    }
+
+    public BeregningsgrunnlagRegelType getRegelType() {
+        return regelType;
+    }
+
+    public boolean erAktiv() {
+        return aktiv;
+    }
+
+    public void setAktiv(boolean aktiv) {
+        this.aktiv = aktiv;
+    }
+
+    public static Builder ny() {
+        return new Builder();
+    }
+
+    public static class Builder {
+
+        private RegelSporingGrunnlagEntitet kladd;
+
+        Builder() {
+            kladd = new RegelSporingGrunnlagEntitet();
+        }
+
+        public Builder medRegelInput(String regelInput) {
+            Objects.requireNonNull(regelInput, "regelInput");
+            kladd.regelInput = regelInput;
+            return this;
+        }
+
+        public Builder medRegelEvaluering(String regelEvaluering) {
+            Objects.requireNonNull(regelEvaluering, "regelInput");
+            kladd.regelEvaluering = regelEvaluering;
+            return this;
+        }
+
+        public RegelSporingGrunnlagEntitet build(Long behandlingId, BeregningsgrunnlagRegelType regelType) {
+            Objects.requireNonNull(behandlingId, "behandlingId");
+            Objects.requireNonNull(regelType, "regelType");
+            Objects.requireNonNull(kladd.regelEvaluering, "regelEvaluering");
+            Objects.requireNonNull(kladd.regelInput, "regelInput");
+            kladd.behandlingId = behandlingId;
+            kladd.regelType = regelType;
+            kladd.aktiv = true;
+            return kladd;
+        }
+
+    }
+
+
+}
