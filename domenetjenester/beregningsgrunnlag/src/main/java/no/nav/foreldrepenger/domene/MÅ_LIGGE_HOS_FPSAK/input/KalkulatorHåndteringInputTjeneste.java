793c205ebce9e1ed32bcc22757abda2fff0c942d
Espen Velsvik <espenjv@users.noreply.github.com>
2020-10-21 18:04:37 +0200

    TFP-3826 Spesialhåndtering av overstyring i mapping til kalkulus (#1407)

--- domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/MÅ_LIGGE_HOS_FPSAK/input/KalkulatorHåndteringInputTjeneste.java
+++ domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/MÅ_LIGGE_HOS_FPSAK/input/KalkulatorHåndteringInputTjeneste.java
@@ -8,11 +8,12 @@ import javax.inject.Inject;
 
 import no.nav.folketrygdloven.kalkulator.input.BeregningsgrunnlagInput;
 import no.nav.folketrygdloven.kalkulator.input.HåndterBeregningsgrunnlagInput;
-import no.nav.foreldrepenger.behandlingslager.behandling.Behandling;
+import no.nav.folketrygdloven.kalkulus.kodeverk.HåndteringKode;
 import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.foreldrepenger.domene.MÅ_LIGGE_HOS_FPSAK.mappers.til_kalkulus.BehandlingslagerTilKalkulusMapper;
 import no.nav.foreldrepenger.domene.SKAL_FLYTTES_TIL_KALKULUS.BeregningsgrunnlagGrunnlagEntitet;
 import no.nav.foreldrepenger.domene.SKAL_FLYTTES_TIL_KALKULUS.BeregningsgrunnlagRepository;
+import no.nav.foreldrepenger.domene.SKAL_FLYTTES_TIL_KALKULUS.BeregningsgrunnlagTilstand;
 
 @ApplicationScoped
 public class KalkulatorHåndteringInputTjeneste {
@@ -50,8 +51,25 @@ public class KalkulatorHåndteringInputTjeneste {
                                                                                 String håndteringKode,
                                                                                 Optional<BeregningsgrunnlagGrunnlagEntitet> grunnlagFraHåndteringTilstand) {
         BeregningsgrunnlagInput inputMedBG = beregningTilInputTjeneste.lagInputMedVerdierFraBeregning(input);
+
+        // Til vi får flyttet tilbakerulling før overstyring: https://github.com/navikt/fp-sak/pull/1406
+        inputMedBG = spesialhåndterOverstyring(input, håndteringKode, inputMedBG);
+
         return new HåndterBeregningsgrunnlagInput(inputMedBG, no.nav.folketrygdloven.kalkulus.felles.kodeverk.domene.BeregningsgrunnlagTilstand.fraKode(MapHåndteringskodeTilTilstand.map(håndteringKode).getKode()))
             .medForrigeGrunnlagFraHåndtering(grunnlagFraHåndteringTilstand.map(BehandlingslagerTilKalkulusMapper::mapGrunnlag).orElse(null));
+    }
+
+    private BeregningsgrunnlagInput spesialhåndterOverstyring(BeregningsgrunnlagInput input, String håndteringKode, BeregningsgrunnlagInput inputMedBG) {
+        if (håndteringKode.equals(HåndteringKode.OVERSTYRING_AV_BEREGNINGSGRUNNLAG_KODE.getKode())) {
+            Long behandlingId = input.getKoblingReferanse().getKoblingId();
+            var oppdatertMedAndeler = beregningsgrunnlagRepository.hentSisteBeregningsgrunnlagGrunnlagEntitetForBehandlinger(
+                behandlingId,
+                behandlingRepository.hentBehandling(behandlingId).getOriginalBehandlingId(),
+                BeregningsgrunnlagTilstand.OPPDATERT_MED_ANDELER)
+                .map(BehandlingslagerTilKalkulusMapper::mapGrunnlag);
+            inputMedBG = oppdatertMedAndeler.map(inputMedBG::medBeregningsgrunnlagGrunnlag).orElse(inputMedBG);
+        }
+        return inputMedBG;
     }
 
 }
