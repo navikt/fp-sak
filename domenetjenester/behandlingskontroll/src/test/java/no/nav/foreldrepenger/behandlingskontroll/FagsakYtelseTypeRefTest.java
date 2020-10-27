12e99f5d2fed2211ca331b39e4a3cd4a40913b3c
Anders Palfi <anderspalfi@gmail.com>
2020-10-27 13:31:49 +0100

    junit5

--- domenetjenester/behandlingskontroll/src/test/java/no/nav/foreldrepenger/behandlingskontroll/FagsakYtelseTypeRefTest.java
+++ domenetjenester/behandlingskontroll/src/test/java/no/nav/foreldrepenger/behandlingskontroll/FagsakYtelseTypeRefTest.java
@@ -1,39 +1,40 @@
 package no.nav.foreldrepenger.behandlingskontroll;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import javax.enterprise.context.ApplicationScoped;
 
-import org.assertj.core.api.Assertions;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakYtelseType;
-import no.nav.vedtak.felles.testutilities.cdi.CdiRunner;
+import no.nav.foreldrepenger.dbstoette.FPsakEntityManagerAwareExtension;
+import no.nav.vedtak.felles.testutilities.db.EntityManagerAwareTest;
 
-/** Demonstrerer lookup med repeatble annotations. */
-@RunWith(CdiRunner.class)
-public class FagsakYtelseTypeRefTest {
+/** Demonstrerer lookup med repeatble annotations.
+ * Extender @{@link EntityManagerAwareTest} fpr at cdi settes opp
+ * Sikkert en bedre måte å gjøre dette på...
+ * */
+@ExtendWith(FPsakEntityManagerAwareExtension.class)
+public class FagsakYtelseTypeRefTest extends EntityManagerAwareTest {
 
-    @Rule
-    public final ExpectedException expected = ExpectedException.none();
-    
     @Test
-    public void skal_få_duplikat_instans_av_cdi_bean() throws Exception {
-        expected.expect(IllegalStateException.class);
-        expected.expectMessage("Har flere matchende instanser");
-        FagsakYtelseTypeRef.Lookup.find(Bokstav.class, FagsakYtelseType.FORELDREPENGER);
+    public void skal_få_duplikat_instans_av_cdi_bean() {
+        assertThatThrownBy(() -> FagsakYtelseTypeRef.Lookup.find(Bokstav.class, FagsakYtelseType.FORELDREPENGER))
+            .isInstanceOf(IllegalStateException.class)
+            .hasMessageContaining("Har flere matchende instanser");
     }
-    
+
     @Test
-    public void skal_få_unik_instans_av_cdi_bean() throws Exception {
+    public void skal_få_unik_instans_av_cdi_bean() {
         var instans = FagsakYtelseTypeRef.Lookup.find(Bokstav.class, FagsakYtelseType.SVANGERSKAPSPENGER);
-        Assertions.assertThat(instans).isNotNull();
+        assertThat(instans).isNotNull();
     }
-    
+
     public interface Bokstav {
     }
-    
+
     @ApplicationScoped
     @FagsakYtelseTypeRef("FP")
     @FagsakYtelseTypeRef("SVP")
