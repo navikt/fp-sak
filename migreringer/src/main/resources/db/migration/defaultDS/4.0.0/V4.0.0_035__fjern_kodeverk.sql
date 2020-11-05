352df97048461cf536207c282553471f520b2f3c
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2020-11-05 18:52:53 +0100

    NOJIRA siste spiker

---
+++ migreringer/src/main/resources/db/migration/defaultDS/4.0.0/V4.0.0_035__fjern_kodeverk.sql
@@ -0,0 +1,8 @@
+drop table KODELISTE_RELASJON purge;
+drop table KODELISTE_NAVN_I18N purge;
+drop table KODELISTE purge;
+drop table KODEVERK purge;
+
+drop sequence SEQ_KODELISTE;
+drop sequence SEQ_KODELISTE_NAVN_I18N;
+drop sequence SEQ_KODELISTE_RELASJON;
