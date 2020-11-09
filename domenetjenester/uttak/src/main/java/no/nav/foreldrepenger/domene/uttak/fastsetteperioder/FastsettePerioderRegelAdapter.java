2b2c464b780952be157ae957c39b6991bc03b434
Anders Palfi <anderspalfi@gmail.com>
2020-11-09 14:46:10 +0100

    Trycatch uttak-regler, logger grunnlag ved evt feil

--- domenetjenester/uttak/src/main/java/no/nav/foreldrepenger/domene/uttak/fastsetteperioder/FastsettePerioderRegelAdapter.java
+++ domenetjenester/uttak/src/main/java/no/nav/foreldrepenger/domene/uttak/fastsetteperioder/FastsettePerioderRegelAdapter.java
@@ -1,18 +1,32 @@
 package no.nav.foreldrepenger.domene.uttak.fastsetteperioder;
 
 
+import java.util.List;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import no.nav.foreldrepenger.behandlingslager.uttak.fp.UttakResultatPerioderEntitet;
 import no.nav.foreldrepenger.domene.uttak.input.UttakInput;
+import no.nav.foreldrepenger.regler.jackson.JacksonJsonConfig;
+import no.nav.foreldrepenger.regler.uttak.fastsetteperiode.FastsettePeriodeResultat;
 import no.nav.foreldrepenger.regler.uttak.fastsetteperiode.FastsettePerioderRegelOrkestrering;
+import no.nav.foreldrepenger.regler.uttak.fastsetteperiode.grunnlag.RegelGrunnlag;
 import no.nav.foreldrepenger.regler.uttak.konfig.FeatureToggles;
 
 @ApplicationScoped
 public class FastsettePerioderRegelAdapter {
 
-    private static final FastsettePerioderRegelOrkestrering regel = new FastsettePerioderRegelOrkestrering();
+    private static final Logger LOG = LoggerFactory.getLogger(FastsettePerioderRegelAdapter.class);
+    private static final FastsettePerioderRegelOrkestrering REGEL = new FastsettePerioderRegelOrkestrering();
+
+    private final JacksonJsonConfig jackson = new JacksonJsonConfig();
+
     private FastsettePerioderRegelGrunnlagBygger regelGrunnlagBygger;
     private FastsettePerioderRegelResultatKonverterer regelResultatKonverterer;
 
@@ -29,8 +43,22 @@ public class FastsettePerioderRegelAdapter {
 
     UttakResultatPerioderEntitet fastsettePerioder(UttakInput input) {
         var grunnlag = regelGrunnlagBygger.byggGrunnlag(input);
-        var resultat = regel.fastsettePerioder(grunnlag, new FeatureTogglesImpl());
+        List<FastsettePeriodeResultat> resultat;
+        try {
+            resultat = REGEL.fastsettePerioder(grunnlag, new FeatureTogglesImpl());
+        } catch (Exception e) {
+            log(grunnlag);
+            throw new RuntimeException("Automatisk fastsetting av uttak feilet", e);
+        }
         return regelResultatKonverterer.konverter(input, resultat);
+    }
+
+    private void log(RegelGrunnlag grunnlag) {
+        try {
+            LOG.info("Fastsette uttaksperioder feilet med grunnlag {}", jackson.toJson(grunnlag));
+        } catch (JsonProcessingException jsonProcessingException) {
+            LOG.warn("Feil ved logging av regelgrunnlaget", jsonProcessingException);
+        }
     }
 
     private static class FeatureTogglesImpl implements FeatureToggles {
