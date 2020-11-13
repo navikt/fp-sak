aecb2c8c5e54d8a9558e14a4fbbd4d730116fd1b
Anders Palfi <anderspalfi@gmail.com>
2020-11-13 12:20:51 +0100

    Endringer i jettyserver oppsett og flyway

--- web/src/main/java/no/nav/foreldrepenger/web/server/jetty/AbstractJettyServer.java
+++ web/src/main/java/no/nav/foreldrepenger/web/server/jetty/AbstractJettyServer.java
@@ -5,9 +5,9 @@ import java.io.IOException;
 import java.security.Security;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.security.auth.message.config.AuthConfigFactory;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -20,7 +20,6 @@ import org.eclipse.jetty.security.ConstraintSecurityHandler;
 import org.eclipse.jetty.security.DefaultIdentityService;
 import org.eclipse.jetty.security.SecurityHandler;
 import org.eclipse.jetty.security.jaspi.JaspiAuthenticatorFactory;
-import org.eclipse.jetty.server.AbstractNetworkConnector;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.HttpConfiguration;
 import org.eclipse.jetty.server.HttpConnectionFactory;
@@ -30,13 +29,16 @@ import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.util.resource.ResourceCollection;
 import org.eclipse.jetty.webapp.Configuration;
+import org.eclipse.jetty.webapp.MetaData;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.eclipse.jetty.webapp.WebInfConfiguration;
 import org.eclipse.jetty.webapp.WebXmlConfiguration;
 import org.slf4j.MDC;
 
 abstract class AbstractJettyServer {
+    private static final String SERVER_HOST = "0.0.0.0";
 
     /**
      * Legges først slik at alltid resetter context før prosesserer nye requests.
@@ -44,21 +46,11 @@ abstract class AbstractJettyServer {
      */
     static final class ResetLogContextHandler extends AbstractHandler {
         @Override
-        public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
-                throws IOException, ServletException {
+        public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) {
             MDC.clear();
         }
     }
 
-    /**
-     * @see AbstractNetworkConnector#getHost()
-     * @see org.eclipse.jetty.server.ServerConnector#openAcceptChannel()
-     */
-    // TODO (u139158): Trenger vi egentlig å sette denne? Spec ser ut til å si at
-    // det er eq med null, settes den default til null eller binder
-    // den mot et interface?
-
-    protected static final String SERVER_HOST = "0.0.0.0";
     /**
      * nedstrippet sett med Jetty configurations for raskere startup.
      */
@@ -69,72 +61,69 @@ abstract class AbstractJettyServer {
             new EnvConfiguration(),
             new PlusConfiguration(),
     };
-    private AppKonfigurasjon appKonfigurasjon;
+    private final JettyWebKonfigurasjon webKonfigurasjon;
 
-    public AbstractJettyServer(AppKonfigurasjon appKonfigurasjon) {
-        this.appKonfigurasjon = appKonfigurasjon;
+    public AbstractJettyServer(JettyWebKonfigurasjon webKonfigurasjon) {
+        this.webKonfigurasjon = webKonfigurasjon;
     }
 
-    protected void bootStrap() throws Exception { // NOSONAR
+    protected void bootStrap() throws Exception {
         konfigurer();
         migrerDatabaser();
-        start(appKonfigurasjon);
+        start(webKonfigurasjon);
     }
 
-    protected void konfigurer() throws Exception { // NOSONAR
+    protected void konfigurer() throws Exception {
         konfigurerMiljø();
         konfigurerSikkerhet();
         konfigurerJndi();
+        konfigurerJms();
     }
 
-    protected abstract void konfigurerMiljø() throws Exception; // NOSONAR
+    private void konfigurerJms() throws Exception {
+        JmsKonfig.settOppJndiConnectionfactory("jms/ConnectionFactory", "mqGateway02", "fpsak.channel");
+        JmsKonfig.settOppJndiMessageQueue("jms/QueueFpsakOkonomiOppdragSend", "fpsak.okonomi.oppdrag.send");
+        JmsKonfig.settOppJndiMessageQueue("jms/QueueFpsakOkonomiOppdragMotta", "fpsak.okonomi.oppdrag.mottak");
+        JmsKonfig.settOppJndiMessageQueue("jms/QueueFpsakGrensesnittavstemmingSend", "ray.avstem.data", true);
+    }
+
+    protected abstract void konfigurerMiljø() throws Exception;
 
     protected void konfigurerSikkerhet() {
         Security.setProperty(AuthConfigFactory.DEFAULT_FACTORY_SECURITY_PROPERTY, AuthConfigFactoryImpl.class.getCanonicalName());
 
-        File jaspiConf = new File(System.getProperty("conf", "./conf") + "/jaspi-conf.xml"); // NOSONAR
+        var jaspiConf = new File(System.getProperty("conf", "./conf") + "/jaspi-conf.xml");
         if (!jaspiConf.exists()) {
             throw new IllegalStateException("Missing required file: " + jaspiConf.getAbsolutePath());
         }
         System.setProperty("org.apache.geronimo.jaspic.configurationFile", jaspiConf.getAbsolutePath());
-
-        konfigurerSwaggerHash();
     }
 
-    /**
-     * @see SecurityFilter#getSwaggerHash()
-     */
-    protected void konfigurerSwaggerHash() {
-        // System.setProperty(SecurityFilter.SWAGGER_HASH_KEY,
-        // appKonfigurasjon.getSwaggerHash());
-    }
+    protected abstract void konfigurerJndi() throws Exception;
 
-    protected abstract void konfigurerJndi() throws Exception; // NOSONAR
+    protected abstract void migrerDatabaser();
 
-    protected abstract void migrerDatabaser() throws IOException;
-
-    protected void start(AppKonfigurasjon appKonfigurasjon) throws Exception {
-        Server server = new Server(appKonfigurasjon.getServerPort());
-        server.setConnectors(createConnectors(appKonfigurasjon, server).toArray(new Connector[] {}));
-        var handlers = new HandlerList(new ResetLogContextHandler(), createContext(appKonfigurasjon));
+    protected void start(JettyWebKonfigurasjon jettyWebKonfigurasjon) throws Exception {
+        Server server = new Server(jettyWebKonfigurasjon.getServerPort());
+        server.setConnectors(createConnectors(jettyWebKonfigurasjon, server).toArray(new Connector[] {}));
+        var handlers = new HandlerList(new ResetLogContextHandler(), createContext(jettyWebKonfigurasjon));
         server.setHandler(handlers);
         server.start();
         server.join();
     }
 
-    @SuppressWarnings("resource")
-    protected List<Connector> createConnectors(AppKonfigurasjon appKonfigurasjon, Server server) {
+    protected List<Connector> createConnectors(JettyWebKonfigurasjon jettyWebKonfigurasjon, Server server) {
         List<Connector> connectors = new ArrayList<>();
-        ServerConnector httpConnector = new ServerConnector(server, new HttpConnectionFactory(createHttpConfiguration()));
-        httpConnector.setPort(appKonfigurasjon.getServerPort());
+        var httpConnector = new ServerConnector(server, new HttpConnectionFactory(createHttpConfiguration()));
+        httpConnector.setPort(jettyWebKonfigurasjon.getServerPort());
         httpConnector.setHost(SERVER_HOST);
         connectors.add(httpConnector);
 
         return connectors;
     }
 
-    protected WebAppContext createContext(AppKonfigurasjon appKonfigurasjon) throws IOException {
-        WebAppContext webAppContext = new WebAppContext();
+    protected WebAppContext createContext(JettyWebKonfigurasjon webKonfigurasjon) throws IOException {
+        var webAppContext = new WebAppContext();
         webAppContext.setParentLoaderPriority(true);
 
         // må hoppe litt bukk for å hente web.xml fra classpath i stedet for fra
@@ -145,13 +134,33 @@ abstract class AbstractJettyServer {
         }
         webAppContext.setDescriptor(descriptor);
         webAppContext.setBaseResource(createResourceCollection());
-        webAppContext.setContextPath(appKonfigurasjon.getContextPath());
+        webAppContext.setContextPath(webKonfigurasjon.getContextPath());
         webAppContext.setConfigurations(CONFIGURATIONS);
         webAppContext.setAttribute("org.eclipse.jetty.server.webapp.WebInfIncludeJarPattern", "^.*resteasy-.*.jar$|^.*felles-.*.jar$");
         webAppContext.setSecurityHandler(createSecurityHandler());
-        return webAppContext;
 
+        webAppContext.setParentLoaderPriority(true);
+        updateMetaData(webAppContext.getMetaData());
+        return webAppContext;
     }
+
+    private ResourceCollection createResourceCollection() {
+        return new ResourceCollection(
+            Resource.newClassPathResource("META-INF/resources/webjars/"),
+            Resource.newClassPathResource("/web"));
+    }
+
+    private void updateMetaData(MetaData metaData) {
+        // Find path to class-files while starting jetty from development environment.
+        var resources = getWebInfClasses().stream()
+            .map(c -> Resource.newResource(c.getProtectionDomain().getCodeSource().getLocation()))
+            .distinct()
+            .collect(Collectors.toList());
+
+        metaData.setWebInfClassesDirs(resources);
+    }
+
+    protected abstract List<Class<?>> getWebInfClasses();
 
     protected HttpConfiguration createHttpConfiguration() {
         // Create HTTP Config
@@ -161,10 +170,7 @@ abstract class AbstractJettyServer {
         httpConfig.addCustomizer(new org.eclipse.jetty.server.ForwardedRequestCustomizer());
 
         return httpConfig;
-
     }
-
-    protected abstract Resource createResourceCollection() throws IOException;
 
     private static SecurityHandler createSecurityHandler() {
         ConstraintSecurityHandler securityHandler = new ConstraintSecurityHandler();
