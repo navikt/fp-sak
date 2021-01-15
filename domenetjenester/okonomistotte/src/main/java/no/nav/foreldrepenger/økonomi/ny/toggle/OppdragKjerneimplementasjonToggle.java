c66805ee5bdff259e585da9fa0e6099fe9e435b7
Michal J. Sladek <mrsladek@users.noreply.github.com>
2021-01-15 18:58:50 +0100

    Feature/toggle off for ny okomodul (#1901)
    * FIX: Toggler av nye moduler til å fikse autotest.

--- domenetjenester/okonomistotte/src/main/java/no/nav/foreldrepenger/økonomi/ny/toggle/OppdragKjerneimplementasjonToggle.java
+++ domenetjenester/okonomistotte/src/main/java/no/nav/foreldrepenger/økonomi/ny/toggle/OppdragKjerneimplementasjonToggle.java
@@ -15,25 +15,13 @@ import no.nav.vedtak.util.env.Environment;
 public class OppdragKjerneimplementasjonToggle {
 
     //TODO legg inn saksnumre i denne lista for å lansere ny impl for utvalgte saker
-    public static final Set<Saksnummer> LANSERT_I_PROD = Set.of(
-        new Saksnummer("147260073")
-    );
-
-    private BehandlingRepository behandlingRepository;
+    public static final Set<Saksnummer> LANSERT_I_PROD = Set.of();
 
     OppdragKjerneimplementasjonToggle() {
         //cdi proxy
     }
 
-    @Inject
-    public OppdragKjerneimplementasjonToggle(BehandlingRepository behandlingRepository) {
-        this.behandlingRepository = behandlingRepository;
-    }
-
     public boolean brukNyImpl(Long behandlingId) {
-        Behandling behandling = behandlingRepository.hentBehandling(behandlingId);
-        Saksnummer saksnummer = behandling.getFagsak().getSaksnummer();
-        return (!Environment.current().isProd() || LANSERT_I_PROD.contains(saksnummer))
-            && !behandling.getFagsak().getYtelseType().equals(FagsakYtelseType.ENGANGSTØNAD);
+        return false;
     }
 }
