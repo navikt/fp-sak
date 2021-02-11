55298d058c96a43432bb6117066f59b462df82f0
Petter Ekern <43809623+pekern@users.noreply.github.com>
2021-02-11 11:06:41 +0100

    TFP-4115: Fjerner tjeneste som ikke lenger trengs for brev (#2104)

--- behandlingsprosess/src/main/java/no/nav/foreldrepenger/behandling/steg/beregningsgrunnlag/BeregningsgrunnlagGUIInputFelles.java
+++ behandlingsprosess/src/main/java/no/nav/foreldrepenger/behandling/steg/beregningsgrunnlag/BeregningsgrunnlagGUIInputFelles.java
@@ -1,14 +1,5 @@
 package no.nav.foreldrepenger.behandling.steg.beregningsgrunnlag;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-import javax.inject.Inject;
-
 import no.nav.folketrygdloven.kalkulator.input.BeregningsgrunnlagGUIInput;
 import no.nav.folketrygdloven.kalkulator.input.YtelsespesifiktGrunnlag;
 import no.nav.folketrygdloven.kalkulator.modell.iay.InntektArbeidYtelseGrunnlagDto;
@@ -19,22 +10,22 @@ import no.nav.foreldrepenger.behandling.BehandlingReferanse;
 import no.nav.foreldrepenger.behandling.revurdering.ytelse.fp.AndelGraderingTjeneste;
 import no.nav.foreldrepenger.behandlingslager.behandling.Behandling;
 import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepository;
-import no.nav.foreldrepenger.behandlingslager.virksomhet.Arbeidsgiver;
 import no.nav.foreldrepenger.domene.MÅ_LIGGE_HOS_FPSAK.mappers.til_kalkulus.IAYMapperTilKalkulus;
 import no.nav.foreldrepenger.domene.MÅ_LIGGE_HOS_FPSAK.mappers.til_kalkulus.MapBehandlingRef;
 import no.nav.foreldrepenger.domene.arbeidsforhold.InntektArbeidYtelseTjeneste;
 import no.nav.foreldrepenger.domene.arbeidsforhold.InntektsmeldingTjeneste;
-import no.nav.foreldrepenger.domene.arbeidsgiver.ArbeidsgiverOpplysninger;
-import no.nav.foreldrepenger.domene.arbeidsgiver.ArbeidsgiverTjeneste;
-import no.nav.foreldrepenger.domene.iay.modell.AktørArbeid;
 import no.nav.foreldrepenger.domene.iay.modell.InntektArbeidYtelseGrunnlag;
 import no.nav.foreldrepenger.domene.iay.modell.Inntektsmelding;
 import no.nav.foreldrepenger.domene.iay.modell.RefusjonskravDato;
-import no.nav.foreldrepenger.domene.iay.modell.Yrkesaktivitet;
 import no.nav.foreldrepenger.skjæringstidspunkt.SkjæringstidspunktTjeneste;
 import no.nav.vedtak.util.env.Environment;
 
-import static no.nav.foreldrepenger.domene.MÅ_LIGGE_HOS_FPSAK.mappers.til_kalkulus.IAYMapperTilKalkulus.mapArbeidsforholdOpplysninger;
+import javax.inject.Inject;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public abstract class BeregningsgrunnlagGUIInputFelles {
 
@@ -43,21 +34,18 @@ public abstract class BeregningsgrunnlagGUIInputFelles {
     private SkjæringstidspunktTjeneste skjæringstidspunktTjeneste;
     private AndelGraderingTjeneste andelGraderingTjeneste;
     private InntektsmeldingTjeneste inntektsmeldingTjeneste;
-    private ArbeidsgiverTjeneste arbeidsgiverTjeneste;
 
     @Inject
     public BeregningsgrunnlagGUIInputFelles(BehandlingRepository behandlingRepository,
-            InntektArbeidYtelseTjeneste iayTjeneste,
-            SkjæringstidspunktTjeneste skjæringstidspunktTjeneste,
-            AndelGraderingTjeneste andelGraderingTjeneste,
-            InntektsmeldingTjeneste inntektsmeldingTjeneste,
-            ArbeidsgiverTjeneste arbeidsgiverTjeneste) {
+                                            InntektArbeidYtelseTjeneste iayTjeneste,
+                                            SkjæringstidspunktTjeneste skjæringstidspunktTjeneste,
+                                            AndelGraderingTjeneste andelGraderingTjeneste,
+                                            InntektsmeldingTjeneste inntektsmeldingTjeneste) {
         this.behandlingRepository = Objects.requireNonNull(behandlingRepository, "behandlingRepository");
         this.iayTjeneste = Objects.requireNonNull(iayTjeneste, "iayTjeneste");
         this.skjæringstidspunktTjeneste = Objects.requireNonNull(skjæringstidspunktTjeneste, "skjæringstidspunktTjeneste");
         this.andelGraderingTjeneste = Objects.requireNonNull(andelGraderingTjeneste, "andelGrderingTjeneste");
         this.inntektsmeldingTjeneste = inntektsmeldingTjeneste;
-        this.arbeidsgiverTjeneste = arbeidsgiverTjeneste;
     }
 
     protected BeregningsgrunnlagGUIInputFelles() {
@@ -106,20 +94,9 @@ public abstract class BeregningsgrunnlagGUIInputFelles {
             iayGrunnlagDto = iayGrunnlagDtoUtenIMDiff;
         }
 
-        Map<Arbeidsgiver, ArbeidsgiverOpplysninger> arbeidsgiverOpplysninger = iayGrunnlag.getAktørArbeidFraRegister(ref.getAktørId())
-            .map(AktørArbeid::hentAlleYrkesaktiviteter)
-            .orElse(Collections.emptyList())
-            .stream()
-            .map(Yrkesaktivitet::getArbeidsgiver)
-            .distinct()
-            .collect(Collectors.toMap(a -> a, arbeidsgiverTjeneste::hent));
-        InntektArbeidYtelseGrunnlagDto iayGrunnlagMedArbeidsforholdOpplysninger = InntektArbeidYtelseGrunnlagDtoBuilder.oppdatere(iayGrunnlagDto)
-            .medArbeidsgiverOpplysninger(mapArbeidsforholdOpplysninger(arbeidsgiverOpplysninger, iayGrunnlag.getArbeidsforholdOverstyringer()))
-            .build();
-
         BeregningsgrunnlagGUIInput input = new BeregningsgrunnlagGUIInput(
             MapBehandlingRef.mapRef(ref),
-            iayGrunnlagMedArbeidsforholdOpplysninger,
+            iayGrunnlagDto,
             aktivitetGradering,
             IAYMapperTilKalkulus.mapRefusjonskravDatoer(refusjonskravDatoer),
             ytelseGrunnlag);
