83d0c84f50f681cdc29881ddf65074e174ce9a17
Anders Palfi <anderspalfi@gmail.com>
2021-03-24 11:34:02 +0100

    Logger aksjonspunktkode ved constraintviolation

--- web/src/main/java/no/nav/foreldrepenger/web/app/exceptions/ConstraintViolationMapper.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/exceptions/ConstraintViolationMapper.java
@@ -20,6 +20,8 @@ import org.slf4j.LoggerFactory;
 
 import no.nav.foreldrepenger.behandling.aksjonspunkt.AksjonspunktKode;
 import no.nav.foreldrepenger.validering.FeltFeilDto;
+import no.nav.foreldrepenger.web.app.tjenester.behandling.aksjonspunkt.BekreftedeAksjonspunkterDto;
+import no.nav.foreldrepenger.web.app.tjenester.behandling.aksjonspunkt.OverstyrteAksjonspunkterDto;
 
 public class ConstraintViolationMapper implements ExceptionMapper<ConstraintViolationException> {
 
@@ -33,19 +35,21 @@ public class ConstraintViolationMapper implements ExceptionMapper<ConstraintViol
             return håndterFeilKonfigurering((ResteasyViolationException) exception);
         }
         log(exception);
-        return lagResponse(constraintViolations);
+        return lagResponse(exception);
     }
 
     private void log(ConstraintViolationException exception) {
-        LOG.warn("Det oppstod en valideringsfeil: {}", toString(exception));
+        var aksjonspunktKoder = finnAksjonspunktKoder(exception);
+        //De fleste innkommende dto er klyttet til et aksjonspunkt
+        LOG.warn("Det oppstod en valideringsfeil: Aksjonspunkt {} {}", aksjonspunktKoder, constraints(exception));
     }
 
-    private Response lagResponse(Set<ConstraintViolation<?>> constraintViolations) {
+    private static Response lagResponse(ConstraintViolationException exception) {
         Collection<FeltFeilDto> feilene = new ArrayList<>();
-        for (var constraintViolation : constraintViolations) {
-            var kode = getKode(constraintViolation.getLeafBean());
+        var koder = finnAksjonspunktKoder(exception);
+        for (var constraintViolation : exception.getConstraintViolations()) {
             var feltNavn = getFeltNavn(constraintViolation.getPropertyPath());
-            feilene.add(new FeltFeilDto(feltNavn, constraintViolation.getMessage(), kode));
+            feilene.add(new FeltFeilDto(feltNavn, constraintViolation.getMessage(), koder.toString()));
         }
         var feltNavn = feilene.stream().map(FeltFeilDto::getNavn).collect(Collectors.toList());
         var feilmelding = String.format(
@@ -57,7 +61,7 @@ public class ConstraintViolationMapper implements ExceptionMapper<ConstraintViol
             .build();
     }
 
-    private Response håndterFeilKonfigurering(ResteasyViolationException exception) {
+    private static Response håndterFeilKonfigurering(ResteasyViolationException exception) {
         var message = exception.getException().getMessage();
         LOG.error(message);
         return Response.status(Response.Status.BAD_REQUEST)
@@ -66,15 +70,41 @@ public class ConstraintViolationMapper implements ExceptionMapper<ConstraintViol
             .build();
     }
 
-    private List<String> toString(ConstraintViolationException exception) {
+    private static Set<String> constraints(ConstraintViolationException exception) {
         return exception.getConstraintViolations()
             .stream()
             .map(cv -> cv.getRootBeanClass().getSimpleName() + "." + cv.getLeafBean().getClass().getSimpleName()
                 + "." + fieldName(cv) + " - " + cv.getMessage())
-            .collect(Collectors.toList());
+            .collect(Collectors.toSet());
     }
 
-    private String fieldName(ConstraintViolation<?> cv) {
+    private static List<String> finnAksjonspunktKoder(ConstraintViolationException exception) {
+        var førsteConstraint = exception.getConstraintViolations().iterator().next();
+        var executableParameters = førsteConstraint.getExecutableParameters();
+        if (executableParameters.length > 0) {
+            var executableParameter = executableParameters[0];
+            if (executableParameter instanceof BekreftedeAksjonspunkterDto) {
+                //Flere aksjonspunkt kan bekreftes i samme kall
+                return ((BekreftedeAksjonspunkterDto) executableParameter).getBekreftedeAksjonspunktDtoer()
+                    .stream()
+                    .map(dto -> getKode(dto))
+                    .collect(Collectors.toList());
+            }
+            if (executableParameter instanceof OverstyrteAksjonspunkterDto) {
+                return ((OverstyrteAksjonspunkterDto) executableParameter).getOverstyrteAksjonspunktDtoer()
+                    .stream()
+                    .map(dto -> getKode(dto))
+                    .collect(Collectors.toList());
+            }
+        }
+        var aksjonspunktKode = getKode(førsteConstraint.getLeafBean());
+        if (aksjonspunktKode != null) {
+            return List.of(aksjonspunktKode);
+        }
+        return List.of();
+    }
+
+    private static String fieldName(ConstraintViolation<?> cv) {
         String field = null;
         for (Path.Node node : cv.getPropertyPath()) {
             field = node.getName();
@@ -82,11 +112,11 @@ public class ConstraintViolationMapper implements ExceptionMapper<ConstraintViol
         return field;
     }
 
-    private String getKode(Object leafBean) {
+    private static String getKode(Object leafBean) {
         return leafBean instanceof AksjonspunktKode ? ((AksjonspunktKode) leafBean).getKode() : null;
     }
 
-    private String getFeltNavn(Path propertyPath) {
+    private static String getFeltNavn(Path propertyPath) {
         return propertyPath instanceof PathImpl ? ((PathImpl) propertyPath).getLeafNode().toString() : null;
     }
 
