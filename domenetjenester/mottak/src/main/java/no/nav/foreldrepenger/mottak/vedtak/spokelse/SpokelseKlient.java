a398f955e777ddca58b0e5f41ba9286721314aa8
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2021-03-25 10:51:09 +0100

    Nytt aad oppsett og bruk i SPokelse

--- domenetjenester/mottak/src/main/java/no/nav/foreldrepenger/mottak/vedtak/spokelse/SpokelseKlient.java
+++ domenetjenester/mottak/src/main/java/no/nav/foreldrepenger/mottak/vedtak/spokelse/SpokelseKlient.java
@@ -33,9 +33,9 @@ public class SpokelseKlient {
     public SpokelseKlient(
         @KonfigVerdi(value = "SPOKELSE_GRUNNLAG_URL", defaultVerdi = SPOKELSE_GRUNNLAG_DEFAULT_URL) URI uri,
         @KonfigVerdi(value = "SPOKELSE_GRUNNLAG_SCOPES", defaultVerdi = "spokelse/.default") String scopesCsv,
-        @KonfigVerdi(value = "AZURE_CLIENT_ID", defaultVerdi = "fp-sak") String clientId,
-        @KonfigVerdi(value = "AZURE_CLIENT_SECRET", defaultVerdi = "fp-sak") String clientSecret,
-        @KonfigVerdi(value = "AZURE_V2_TOKEN_ENDPOINT", defaultVerdi = AZURE_V2_TOKEN_ENDPOINT_PROD) URI tokenEndpoint,
+        @KonfigVerdi(value = "AZURE_APP_CLIENT_ID", defaultVerdi = "fp-sak") String clientId,
+        @KonfigVerdi(value = "AZURE_APP_CLIENT_SECRET", defaultVerdi = "fp-sak") String clientSecret,
+        @KonfigVerdi(value = "AZURE_OPENID_CONFIG_TOKEN_ENDPOINT", defaultVerdi = AZURE_V2_TOKEN_ENDPOINT_PROD) URI tokenEndpoint,
         @KonfigVerdi(value = "AZURE_HTTP_PROXY", required = false) URI httpProxy) {
         this.restClient = OAuth2RestClient.builder()
             .clientId(clientId)
