f605eefa43e9f8c99dc1cb98c0fb828dbb9e19c4
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-04-26 10:03:12 +0200

    rydd opp mdc slik det skal gjøres

---
+++ web/src/main/java/no/nav/foreldrepenger/web/server/jetty/SubjectHandlerCleanupRequestListener.java
@@ -0,0 +1,32 @@
+package no.nav.foreldrepenger.web.server.jetty;
+
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.annotation.WebListener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import no.nav.vedtak.sikkerhet.context.SubjectHandler;
+import no.nav.vedtak.sikkerhet.context.ThreadLocalSubjectHandler;
+
+@WebListener
+public class SubjectHandlerCleanupRequestListener implements ServletRequestListener {
+
+    private final Logger LOG = LoggerFactory.getLogger(SubjectHandlerCleanupRequestListener.class);
+
+    @Override
+    public void requestDestroyed(ServletRequestEvent sre) {
+        try {
+            var subjectHandler = SubjectHandler.getSubjectHandler();
+            var subject = subjectHandler.getSubject();
+            if (subject != null) {
+                LOG.info("Servlet request destroyed, fjerner dangling subject");
+                ThreadLocalSubjectHandler.class.cast(subjectHandler).setSubject(null);
+                LOG.info("Subject fjernet fra ThreadLocal OK");
+            }
+        } catch (Exception e) {
+            LOG.trace("Kunne ikke fjerne subject", e);
+        }
+    }
+}
