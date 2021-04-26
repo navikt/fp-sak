0fbf0a9225a1db987fd08a8dddff75e7066f3742
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2021-04-26 08:46:09 +0200

    TFP-4311 mer presis startpunktutledning for PO og MEDL (#2519)

--- domenetjenester/medlem/src/main/java/no/nav/foreldrepenger/domene/medlem/identifiserer/MedlemEndringIdentifiserer.java
+++ domenetjenester/medlem/src/main/java/no/nav/foreldrepenger/domene/medlem/identifiserer/MedlemEndringIdentifiserer.java
@@ -1,35 +1,21 @@
 package no.nav.foreldrepenger.domene.medlem.identifiserer;
 
-import java.time.LocalDate;
-import java.time.Period;
-
-import javax.enterprise.context.Dependent;
-import javax.inject.Inject;
+import java.util.stream.Collectors;
 
 import no.nav.foreldrepenger.behandlingslager.behandling.RegisterdataDiffsjekker;
 import no.nav.foreldrepenger.behandlingslager.behandling.medlemskap.MedlemskapAggregat;
-import no.nav.foreldrepenger.behandlingslager.behandling.medlemskap.MedlemskapPerioderEntitet;
-import no.nav.foreldrepenger.behandlingslager.diff.Node;
+import no.nav.foreldrepenger.domene.tid.DatoIntervallEntitet;
 
-@Dependent
 public class MedlemEndringIdentifiserer {
 
-    private static Period PERIODE_FØR_STP = Period.ofMonths(12);
-
-    @Inject
-    MedlemEndringIdentifiserer() {
-    }
-
-    public boolean erEndretFørSkjæringstidspunkt(MedlemskapAggregat grunnlag1, MedlemskapAggregat grunnlag2, LocalDate skjæringstidspunkt) {
+    public static boolean erEndretForPeriode(MedlemskapAggregat grunnlag1, MedlemskapAggregat grunnlag2, DatoIntervallEntitet periode) {
         var differ = new RegisterdataDiffsjekker(true);
-        final var nodeEndringer = differ.finnForskjellerPå(grunnlag1.getRegistrertMedlemskapPerioder(), grunnlag2.getRegistrertMedlemskapPerioder());
-
-        return nodeEndringer.keySet().stream()
-            .map(Node::getObject)
-            .filter(it -> it instanceof MedlemskapPerioderEntitet)
-            .anyMatch(adr -> {
-                var medlPer = ((MedlemskapPerioderEntitet) adr).getPeriode();
-                return medlPer != null && medlPer.getFomDato().isBefore(skjæringstidspunkt) && medlPer.getTomDato().isAfter(skjæringstidspunkt.minus(PERIODE_FØR_STP));
-            });
+        var medlemPerioder1 = grunnlag1.getRegistrertMedlemskapPerioder().stream()
+            .filter(p -> p.getPeriode().overlapper(periode))
+            .collect(Collectors.toSet());
+        var medlemPerioder2 = grunnlag2.getRegistrertMedlemskapPerioder().stream()
+            .filter(p -> p.getPeriode().overlapper(periode))
+            .collect(Collectors.toSet());
+        return differ.erForskjellPå(medlemPerioder1, medlemPerioder2);
     }
 }
--- domenetjenester/registerinnhenting/src/test/java/no/nav/foreldrepenger/domene/registerinnhenting/impl/startpunkt/StartpunktUtlederPersonopplysningTest.java
+++ domenetjenester/registerinnhenting/src/test/java/no/nav/foreldrepenger/domene/registerinnhenting/impl/startpunkt/StartpunktUtlederPersonopplysningTest.java
@@ -3,6 +3,7 @@ package no.nav.foreldrepenger.domene.registerinnhenting.impl.startpunkt;
 import static no.nav.foreldrepenger.behandlingslager.hendelser.StartpunktType.BEREGNING;
 import static no.nav.foreldrepenger.behandlingslager.hendelser.StartpunktType.SØKERS_RELASJON_TIL_BARNET;
 import static no.nav.foreldrepenger.behandlingslager.hendelser.StartpunktType.UDEFINERT;
+import static no.nav.foreldrepenger.behandlingslager.hendelser.StartpunktType.UTTAKSVILKÅR;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -24,6 +25,7 @@ import no.nav.foreldrepenger.behandlingslager.behandling.personopplysning.Relasj
 import no.nav.foreldrepenger.behandlingslager.behandling.personopplysning.SivilstandType;
 import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepositoryProvider;
 import no.nav.foreldrepenger.behandlingslager.geografisk.Region;
+import no.nav.foreldrepenger.behandlingslager.testutilities.behandling.ScenarioFarSøkerForeldrepenger;
 import no.nav.foreldrepenger.behandlingslager.testutilities.behandling.ScenarioMorSøkerForeldrepenger;
 import no.nav.foreldrepenger.behandlingslager.testutilities.behandling.personopplysning.PersonRelasjon;
 import no.nav.foreldrepenger.behandlingslager.testutilities.behandling.personopplysning.Personopplysning;
@@ -105,7 +107,7 @@ public class StartpunktUtlederPersonopplysningTest extends EntityManagerAwareTes
     }
 
     @Test
-    public void skal_returnere_startpunkt_srb_ved_ekteskap() {
+    public void skal_returnere_startpunkt_srb_ved_ekteskap_med_annen_enn_annenpart() {
         // Arrange
         var origSkjæringsdato = LocalDate.now();
         var nyBekreftetfødselsdato = origSkjæringsdato.minusDays(1); // fødselsdato før skjæringstidspunkt
@@ -139,6 +141,46 @@ public class StartpunktUtlederPersonopplysningTest extends EntityManagerAwareTes
         assertThat(utleder.utledStartpunkt(BehandlingReferanse.fra(revurdering, skjæringstidspunkt),
             repositoryProvider.getPersonopplysningRepository().hentPersonopplysninger(behandlingId).getId(),
             repositoryProvider.getPersonopplysningRepository().hentPersonopplysninger(revurderingId).getId())).isEqualTo(SØKERS_RELASJON_TIL_BARNET);
+    }
+
+    @Test
+    public void skal_returnere_startpunkt_uttak_ved_endring_har_samme_bosted() {
+        // Arrange
+        var fødselsdato = LocalDate.now().minusDays(10);
+
+        var førstegangScenario = ScenarioFarSøkerForeldrepenger.forFødsel()
+            .medBehandlingType(BehandlingType.FØRSTEGANGSSØKNAD);
+        var søkerAktørId = førstegangScenario.getDefaultBrukerAktørId();
+        førstegangScenario.medBekreftetHendelse().medFødselsDato(fødselsdato);
+        var builderForRegisteropplysninger = førstegangScenario.opprettBuilderForRegisteropplysninger()
+            .leggTilPersonopplysninger(Personopplysning.builder().aktørId(søkerAktørId).navn("Ola N").brukerKjønn(NavBrukerKjønn.MANN).fødselsdato(LocalDate.now().minusYears(28)).sivilstand(SivilstandType.UGIFT).region(Region.NORDEN))
+            .leggTilPersonopplysninger(Personopplysning.builder().aktørId(BARN_AKTØR_ID).navn("Barn Abbas").brukerKjønn(NavBrukerKjønn.KVINNE).fødselsdato(fødselsdato).sivilstand(SivilstandType.UGIFT).region(Region.NORDEN))
+            .leggTilRelasjon(PersonRelasjon.builder().fraAktørId(søkerAktørId).tilAktørId(BARN_AKTØR_ID).harSammeBosted(true).relasjonsrolle(RelasjonsRolleType.BARN))
+            .leggTilRelasjon(PersonRelasjon.builder().fraAktørId(BARN_AKTØR_ID).tilAktørId(søkerAktørId).harSammeBosted(true).relasjonsrolle(RelasjonsRolleType.FARA));
+        førstegangScenario.medRegisterOpplysninger(builderForRegisteropplysninger.build());
+        var originalBehandling = førstegangScenario.lagre(repositoryProvider);
+        var behandlingId = originalBehandling.getId();
+
+        var skjæringstidspunkt = Skjæringstidspunkt.builder().medUtledetSkjæringstidspunkt(fødselsdato).build();
+
+        var revurderingScenario = ScenarioFarSøkerForeldrepenger.forFødsel()
+            .medBehandlingType(BehandlingType.REVURDERING);
+        revurderingScenario.medOriginalBehandling(originalBehandling, BehandlingÅrsakType.RE_ANNET);
+        revurderingScenario.medBekreftetHendelse().medFødselsDato(fødselsdato);
+        var revurderingPOBuilder = revurderingScenario.opprettBuilderForRegisteropplysninger()
+            .leggTilPersonopplysninger(Personopplysning.builder().aktørId(søkerAktørId).navn("Ola N").brukerKjønn(NavBrukerKjønn.MANN).fødselsdato(LocalDate.now().minusYears(28)).sivilstand(SivilstandType.UGIFT).region(Region.NORDEN))
+            .leggTilPersonopplysninger(Personopplysning.builder().aktørId(BARN_AKTØR_ID).navn("Barn Abbas").brukerKjønn(NavBrukerKjønn.KVINNE).fødselsdato(fødselsdato).sivilstand(SivilstandType.UGIFT).region(Region.NORDEN))
+            .leggTilRelasjon(PersonRelasjon.builder().fraAktørId(søkerAktørId).tilAktørId(BARN_AKTØR_ID).harSammeBosted(false).relasjonsrolle(RelasjonsRolleType.BARN))
+            .leggTilRelasjon(PersonRelasjon.builder().fraAktørId(BARN_AKTØR_ID).tilAktørId(søkerAktørId).harSammeBosted(false).relasjonsrolle(RelasjonsRolleType.FARA));
+        revurderingScenario.medRegisterOpplysninger(revurderingPOBuilder.build());
+        var revurdering = revurderingScenario.lagre(repositoryProvider);
+        var revurderingId = revurdering.getId();
+
+        // Act/Assert
+        var utleder = new StartpunktUtlederPersonopplysning(repositoryProvider.getPersonopplysningRepository(), barnBorteEndringIdentifiserer);
+        assertThat(utleder.utledStartpunkt(BehandlingReferanse.fra(revurdering, skjæringstidspunkt),
+            repositoryProvider.getPersonopplysningRepository().hentPersonopplysninger(behandlingId).getId(),
+            repositoryProvider.getPersonopplysningRepository().hentPersonopplysninger(revurderingId).getId())).isEqualTo(UTTAKSVILKÅR);
     }
 
 }
