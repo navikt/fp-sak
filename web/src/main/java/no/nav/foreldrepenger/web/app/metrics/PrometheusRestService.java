4e6e5d0c6a0cf5cdea3dd4b733ab4f08dfaa73a4
Jan Olav Eide <80393418+jan-olaveide@users.noreply.github.com>
2021-06-01 15:14:04 +0200

    instrumentering (#2703)
    * nstrument
    
    * ikke finn opp hjulet på nytt
    
    Co-authored-by: Jan-Olav Eide <jan-olav.eide@nav.no>

--- web/src/main/java/no/nav/foreldrepenger/web/app/metrics/PrometheusRestService.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/metrics/PrometheusRestService.java
@@ -1,6 +1,5 @@
 package no.nav.foreldrepenger.web.app.metrics;
 
-
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
@@ -12,22 +11,18 @@ import javax.ws.rs.core.Response;
 
 import io.prometheus.client.CollectorRegistry;
 import io.swagger.v3.oas.annotations.Operation;
+import static no.nav.vedtak.log.metrics.REGISTRY;
 
 @Path("/metrics")
 @ApplicationScoped
+
 public class PrometheusRestService {
 
     @GET
     @Operation(hidden = true)
     @Path("/prometheus")
-    public Response prometheus() {
-
-        final Writer writer = new StringWriter();
-        try {
-            TextFormater.write004(writer, CollectorRegistry.defaultRegistry.metricFamilySamples());
-            return Response.ok().encoding("UTF-8").entity(writer.toString()).header("content-type", TextFormater.CONTENT_TYPE_004).build();
-        } catch (IOException e) {
-            throw new IllegalStateException(e);
-        }
+    public String prometheus() {
+       return REGISTRY.scrape();
+      
     }
 }
