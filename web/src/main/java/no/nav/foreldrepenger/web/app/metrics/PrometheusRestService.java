984d87b907bea66b13a490a2345b39a3d062669b
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-06-03 13:33:15 +0200

    fix

--- web/src/main/java/no/nav/foreldrepenger/web/app/metrics/PrometheusRestService.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/metrics/PrometheusRestService.java
@@ -1,12 +1,12 @@
 package no.nav.foreldrepenger.web.app.metrics;
 
-import static no.nav.vedtak.log.metrics.MetricsUtil.REGISTRY;
 import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
-import javax.ws.rs.Produces;
+import static no.nav.vedtak.log.metrics.MetricsUtil.REGISTRY;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 import io.swagger.v3.oas.annotations.Operation;
 
@@ -20,6 +20,6 @@ public class PrometheusRestService {
     @Operation(hidden = true)
     @Path("/prometheus")
     public String prometheus() {
-       return REGISTRY.scrape();
+        return REGISTRY.scrape();
     }
 }
