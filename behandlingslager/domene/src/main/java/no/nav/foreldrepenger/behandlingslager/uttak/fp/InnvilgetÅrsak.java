1ce925d15c3b62e26ea3118213df85281c171a16
Anders Palfi <anderspalfi@gmail.com>
2021-06-09 12:28:07 +0200

    Lagt inn filter på resultatårsaker i uttak for å skille på årsaker som tilhører lovendringer (#2761)

--- behandlingslager/domene/src/main/java/no/nav/foreldrepenger/behandlingslager/uttak/fp/InnvilgetÅrsak.java
+++ behandlingslager/domene/src/main/java/no/nav/foreldrepenger/behandlingslager/uttak/fp/InnvilgetÅrsak.java
@@ -37,18 +37,18 @@ import no.nav.vedtak.konfig.Tid;
 @JsonAutoDetect(getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE, fieldVisibility = Visibility.ANY)
 public enum InnvilgetÅrsak implements PeriodeResultatÅrsak {
 
-    UTTAK_OPPFYLT("2001", "§14-6: Uttak er oppfylt", "{\"fagsakYtelseType\": {\"FP\": {\"lovreferanse\": \"14-6\"}}}", Set.of(UTTAK), LocalDate.of(2001, 1, 1)),
+    UTTAK_OPPFYLT("2001", "§14-6: Uttak er oppfylt", "{\"fagsakYtelseType\": {\"FP\": {\"lovreferanse\": \"14-6\"}}}", Set.of(UTTAK), null, null, LocalDate.of(2001, 1, 1), null),
     FELLESPERIODE_ELLER_FORELDREPENGER("2002", "§14-9: Innvilget fellesperiode/foreldrepenger", "{\"fagsakYtelseType\": {\"FP\": {\"lovreferanse\": \"14-9\"}}}", Set.of(UTTAK), Set.of(FELLESPERIODE, FORELDREPENGER)),
     KVOTE_ELLER_OVERFØRT_KVOTE("2003", "§14-12: Innvilget uttak av kvote", "{\"fagsakYtelseType\": {\"FP\": {\"lovreferanse\": \"14-12\"}}}", Set.of(UTTAK), Set.of(MØDREKVOTE, FEDREKVOTE)),
     FORELDREPENGER_KUN_FAR_HAR_RETT("2004", "§14-14, jf. §14-13 : Innvilget foreldrepenger, kun far har rett", "{\"fagsakYtelseType\": {\"FP\": {\"lovreferanse\": \"14-14,14-13\"}}}", Set.of(UTTAK), Set.of(FORELDREPENGER)),
     FORELDREPENGER_ALENEOMSORG("2005", "§14-15: Innvilget foreldrepenger ved aleneomsorg", "{\"fagsakYtelseType\": {\"FP\": {\"lovreferanse\": \"14-15\"}}}", Set.of(UTTAK), Set.of(FORELDREPENGER)),
     INNVILGET_FORELDREPENGER_FØR_FØDSEL("2006", "§14-10: Innvilget foreldrepenger før fødsel", "{\"fagsakYtelseType\": {\"FP\": {\"lovreferanse\": \"14-10\"}}}", Set.of(UTTAK), Set.of(FORELDREPENGER_FØR_FØDSEL)),
     FORELDREPENGER_KUN_MOR_HAR_RETT("2007", "§14-10: Innvilget foreldrepenger, kun mor har rett", "{\"fagsakYtelseType\": {\"FP\": {\"lovreferanse\": \"14-10\"}}}", Set.of(UTTAK), Set.of(FORELDREPENGER)),
-    UTSETTELSE_GYLDIG_PGA_FERIE("2010", "§14-11 første ledd bokstav a: Gyldig utsettelse pga. ferie", "{\"fagsakYtelseType\": {\"FP\": {\"lovreferanse\": \"14-11\"}}}", Set.of(UTSETTELSE)),
-    UTSETTELSE_GYLDIG_PGA_100_PROSENT_ARBEID("2011", "§14-11 første ledd bokstav b: Gyldig utsettelse pga. 100% arbeid", "{\"fagsakYtelseType\": {\"FP\": {\"lovreferanse\": \"14-11\"}}}", Set.of(UTSETTELSE)),
-    UTSETTELSE_GYLDIG_PGA_INNLEGGELSE("2012", "§14-11 første ledd bokstav c: Gyldig utsettelse pga. innleggelse", "{\"fagsakYtelseType\": {\"FP\": {\"lovreferanse\": \"14-11\"}}}", Set.of(UTSETTELSE)),
-    UTSETTELSE_GYLDIG_PGA_BARN_INNLAGT("2013", "§14-11 første ledd bokstav d: Gyldig utsettelse pga. barn innlagt", "{\"fagsakYtelseType\": {\"FP\": {\"lovreferanse\": \"14-11\"}}}", Set.of(UTSETTELSE)),
-    UTSETTELSE_GYLDIG_PGA_SYKDOM("2014", "§14-11 første ledd bokstav c: Gyldig utsettelse pga. sykdom", "{\"fagsakYtelseType\": {\"FP\": {\"lovreferanse\": \"14-11\"}}}", Set.of(UTSETTELSE)),
+    UTSETTELSE_GYLDIG_PGA_FERIE("2010", "§14-11 første ledd bokstav a: Gyldig utsettelse pga. ferie", "{\"fagsakYtelseType\": {\"FP\": {\"lovreferanse\": \"14-11\"}}}", Set.of(UTSETTELSE), null, null, null, Set.of(LovEndring.KREVER_SAMMENHENGENDE_UTTAK)),
+    UTSETTELSE_GYLDIG_PGA_100_PROSENT_ARBEID("2011", "§14-11 første ledd bokstav b: Gyldig utsettelse pga. 100% arbeid", "{\"fagsakYtelseType\": {\"FP\": {\"lovreferanse\": \"14-11\"}}}", Set.of(UTSETTELSE), null, null, null, Set.of(LovEndring.KREVER_SAMMENHENGENDE_UTTAK)),
+    UTSETTELSE_GYLDIG_PGA_INNLEGGELSE("2012", "§14-11 første ledd bokstav c: Gyldig utsettelse pga. innleggelse", "{\"fagsakYtelseType\": {\"FP\": {\"lovreferanse\": \"14-11\"}}}", Set.of(UTSETTELSE), null, null, null, Set.of(LovEndring.KREVER_SAMMENHENGENDE_UTTAK)),
+    UTSETTELSE_GYLDIG_PGA_BARN_INNLAGT("2013", "§14-11 første ledd bokstav d: Gyldig utsettelse pga. barn innlagt", "{\"fagsakYtelseType\": {\"FP\": {\"lovreferanse\": \"14-11\"}}}", Set.of(UTSETTELSE), null, null, null, Set.of(LovEndring.KREVER_SAMMENHENGENDE_UTTAK)),
+    UTSETTELSE_GYLDIG_PGA_SYKDOM("2014", "§14-11 første ledd bokstav c: Gyldig utsettelse pga. sykdom", "{\"fagsakYtelseType\": {\"FP\": {\"lovreferanse\": \"14-11\"}}}", Set.of(UTSETTELSE), null, null, null, Set.of(LovEndring.KREVER_SAMMENHENGENDE_UTTAK)),
     UTSETTELSE_GYLDIG_PGA_FERIE_KUN_FAR_HAR_RETT("2015", "§14-11 første ledd bokstav a, jf. §14-14, jf. §14-13: Utsettelse pga. ferie, kun far har rett", "{\"fagsakYtelseType\": {\"FP\": {\"lovreferanse\": \"14-11,14-14,14-13\"}}}", Set.of(UTSETTELSE)),
     UTSETTELSE_GYLDIG_PGA_ARBEID_KUN_FAR_HAR_RETT("2016", "§14-11 første ledd bokstav b, jf. §14-14, jf. §14-13: Utsettelse pga. 100% arbeid, kun far har rett", "{\"fagsakYtelseType\": {\"FP\": {\"lovreferanse\": \"14-11,14-14,14-13\"}}}", Set.of(UTSETTELSE)),
     UTSETTELSE_GYLDIG_PGA_SYKDOM_KUN_FAR_HAR_RETT("2017", "§14-11 første ledd bokstav c, jf. §14-14, jf. §14-13: Utsettelse pga. sykdom, skade, kun far har rett", "{\"fagsakYtelseType\": {\"FP\": {\"lovreferanse\": \"14-11,14-14,14-13\"}}}", Set.of(UTSETTELSE)),
@@ -99,34 +99,34 @@ public enum InnvilgetÅrsak implements PeriodeResultatÅrsak {
 
     private Set<StønadskontoType> valgbarForKonto;
 
+    private Set<LovEndring> gyldigForLovendringer;
+
     InnvilgetÅrsak(String kode,
-                     String navn,
-                     String lovHjemmel,
-                     Set<UttakType> uttakTyper,
-                     Set<StønadskontoType> valgbarForKonto,
-                     LocalDate gyldigFom,
-                     LocalDate gyldigTom) {
+                   String navn,
+                   String lovHjemmel,
+                   Set<UttakType> uttakTyper,
+                   Set<StønadskontoType> valgbarForKonto,
+                   LocalDate gyldigFom,
+                   LocalDate gyldigTom,
+                   Set<LovEndring> gyldigForLovendringer) {
         this.kode = kode;
         this.navn = navn;
         this.lovHjemmel = lovHjemmel;
         this.gyldigFom = gyldigFom == null ? LocalDate.of(2000, 1, 1) : gyldigFom;
         this.gyldigTom = gyldigTom == null ? Tid.TIDENES_ENDE : gyldigTom;
         this.uttakTyper = uttakTyper == null ? Set.of(UTTAK) : uttakTyper;
-        this.valgbarForKonto = valgbarForKonto == null ?
+        this.valgbarForKonto = valgbarForKonto == null || valgbarForKonto.isEmpty() ?
             Set.of(FELLESPERIODE, MØDREKVOTE, FEDREKVOTE, FORELDREPENGER, FORELDREPENGER_FØR_FØDSEL)
             : valgbarForKonto;
+        this.gyldigForLovendringer = gyldigForLovendringer == null ? Set.of(LovEndring.values()) : gyldigForLovendringer;
     }
 
     InnvilgetÅrsak(String kode, String navn, String lovHjemmel, Set<UttakType> uttakTyper, Set<StønadskontoType> valgbarForKonto) {
-        this(kode, navn, lovHjemmel, uttakTyper, valgbarForKonto, null, null);
+        this(kode, navn, lovHjemmel, uttakTyper, valgbarForKonto, null, null, null);
     }
 
     InnvilgetÅrsak(String kode, String navn, String lovHjemmel, Set<UttakType> uttakTyper) {
-        this(kode, navn, lovHjemmel, uttakTyper, null, null, null);
-    }
-
-    InnvilgetÅrsak(String kode, String navn, String lovHjemmel, Set<UttakType> uttakTyper, LocalDate gyldigTom) {
-        this(kode, navn, lovHjemmel, uttakTyper, null, null, gyldigTom);
+        this(kode, navn, lovHjemmel, uttakTyper, Set.of());
     }
 
     @Override
@@ -147,6 +147,11 @@ public enum InnvilgetÅrsak implements PeriodeResultatÅrsak {
     @Override
     public Set<StønadskontoType> getValgbarForKonto() {
         return valgbarForKonto;
+    }
+
+    @Override
+    public Set<LovEndring> getGyldigForLovendringer() {
+        return gyldigForLovendringer;
     }
 
     @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
