c7a5937e9e615d5a77b3437b2cd629bd03356ffb
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2021-08-18 10:31:39 +0200

    Startpunkt beregning ved dødfødsel og Dekningsgrad80

--- domenetjenester/registerinnhenting/src/main/java/no/nav/foreldrepenger/domene/registerinnhenting/impl/startpunkt/StartpunktUtlederFamilieHendelse.java
+++ domenetjenester/registerinnhenting/src/main/java/no/nav/foreldrepenger/domene/registerinnhenting/impl/startpunkt/StartpunktUtlederFamilieHendelse.java
@@ -4,9 +4,12 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import no.nav.foreldrepenger.behandling.BehandlingReferanse;
+import no.nav.foreldrepenger.behandling.FagsakRelasjonTjeneste;
 import no.nav.foreldrepenger.behandlingslager.behandling.GrunnlagRef;
 import no.nav.foreldrepenger.behandlingslager.behandling.familiehendelse.FamilieHendelseEntitet;
 import no.nav.foreldrepenger.behandlingslager.behandling.familiehendelse.FamilieHendelseGrunnlagEntitet;
+import no.nav.foreldrepenger.behandlingslager.fagsak.Dekningsgrad;
+import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakRelasjon;
 import no.nav.foreldrepenger.behandlingslager.hendelser.StartpunktType;
 import no.nav.foreldrepenger.domene.registerinnhenting.StartpunktUtleder;
 import no.nav.foreldrepenger.familiehendelse.FamilieHendelseTjeneste;
@@ -18,15 +21,19 @@ class StartpunktUtlederFamilieHendelse implements StartpunktUtleder {
 
     private SkjæringstidspunktTjeneste skjæringstidspunktTjeneste;
     private FamilieHendelseTjeneste familieHendelseTjeneste;
+    private FagsakRelasjonTjeneste fagsakRelasjonTjeneste;
 
     StartpunktUtlederFamilieHendelse() {
         // For CDI
     }
 
     @Inject
-    StartpunktUtlederFamilieHendelse(SkjæringstidspunktTjeneste skjæringstidspunktTjeneste, FamilieHendelseTjeneste familieHendelseTjeneste) {
+    StartpunktUtlederFamilieHendelse(SkjæringstidspunktTjeneste skjæringstidspunktTjeneste,
+                                     FamilieHendelseTjeneste familieHendelseTjeneste,
+                                     FagsakRelasjonTjeneste fagsakRelasjonTjeneste) {
         this.skjæringstidspunktTjeneste = skjæringstidspunktTjeneste;
         this.familieHendelseTjeneste = familieHendelseTjeneste;
+        this.fagsakRelasjonTjeneste = fagsakRelasjonTjeneste;
     }
 
     @Override
@@ -42,6 +49,10 @@ class StartpunktUtlederFamilieHendelse implements StartpunktUtleder {
         if (erAntallBekreftedeBarnEndret(grunnlag1, grunnlag2)) {
             FellesStartpunktUtlederLogger.loggEndringSomFørteTilStartpunkt(this.getClass().getSimpleName(), StartpunktType.SØKERS_RELASJON_TIL_BARNET, "antall barn", grunnlag1, grunnlag2);
             return StartpunktType.SØKERS_RELASJON_TIL_BARNET;
+        }
+        if (erTilkommetRegisterDødfødselMedDekningsgrad80(ref, grunnlag1, grunnlag2)) {
+            FellesStartpunktUtlederLogger.loggEndringSomFørteTilStartpunkt(this.getClass().getSimpleName(), StartpunktType.BEREGNING, "antall barn", grunnlag1, grunnlag2);
+            return StartpunktType.BEREGNING;
         }
         FellesStartpunktUtlederLogger.loggEndringSomFørteTilStartpunkt(this.getClass().getSimpleName(), StartpunktType.UTTAKSVILKÅR, "familiehendelse", grunnlag1, grunnlag2);
         return StartpunktType.UTTAKSVILKÅR;
@@ -82,6 +93,19 @@ class StartpunktUtlederFamilieHendelse implements StartpunktUtleder {
         var antallBarn2 = grunnlag2.getGjeldendeVersjon().getAntallBarn();
 
         return !antallBarn1.equals(antallBarn2);
+    }
+
+    private boolean erTilkommetRegisterDødfødselMedDekningsgrad80(BehandlingReferanse referanse, Long id1, Long id2) {
+        var grunnlag1 = familieHendelseTjeneste.hentGrunnlagPåId(id1);
+        var grunnlag2 = familieHendelseTjeneste.hentGrunnlagPåId(id2);
+        var dødfødsel1 = grunnlag1.getGjeldendeBekreftetVersjon().filter(FamilieHendelseEntitet::getInnholderDøfødtBarn).isPresent();
+        var dødfødsel2 = grunnlag2.getGjeldendeBekreftetVersjon().filter(FamilieHendelseEntitet::getInnholderDøfødtBarn).isPresent();
+
+        return dødfødsel1 != dødfødsel2 && fagsakRelasjonTjeneste.finnRelasjonForHvisEksisterer(referanse.getFagsakId())
+            .map(FagsakRelasjon::getGjeldendeDekningsgrad)
+            .filter(Dekningsgrad._80::equals)
+            .isPresent();
+
     }
 
 }
