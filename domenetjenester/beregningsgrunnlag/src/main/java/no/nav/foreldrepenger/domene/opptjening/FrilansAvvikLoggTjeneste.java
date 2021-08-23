5482431344c32f22016141390966494d0abccd4b
Petter Ekern <petter.ekern@nav.no>
2021-08-23 14:27:11 +0200

    FIX: Legger på ny logging av frilanscaser, fjerner gammel logg

--- domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/opptjening/FrilansAvvikLoggTjeneste.java
+++ domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/opptjening/FrilansAvvikLoggTjeneste.java
@@ -15,9 +15,7 @@ import no.nav.foreldrepenger.domene.iay.modell.YrkesaktivitetFilter;
 import no.nav.foreldrepenger.domene.iay.modell.kodeverk.InntektsKilde;
 import no.nav.foreldrepenger.domene.modell.BeregningsgrunnlagEntitet;
 import no.nav.foreldrepenger.domene.modell.BeregningsgrunnlagRepository;
-import no.nav.foreldrepenger.domene.tid.DatoIntervallEntitet;
 import no.nav.foreldrepenger.domene.typer.AktørId;
-import no.nav.foreldrepenger.domene.typer.Saksnummer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,7 +23,6 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import java.time.LocalDate;
 import java.util.Collection;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -57,101 +54,40 @@ public class FrilansAvvikLoggTjeneste {
             return;
         }
         LocalDate stpBG = stpBGOpt.get();
-        InntektArbeidYtelseGrunnlag iayGrunnlag = inntektArbeidYtelseTjeneste.hentGrunnlag(ref.getBehandlingId());
 
+        InntektArbeidYtelseGrunnlag iayGrunnlag = inntektArbeidYtelseTjeneste.hentGrunnlag(ref.getBehandlingId());
         Optional<OppgittFrilans> relevantOppgittFrilans = finnOppgittFrilansFraSøknad(iayGrunnlag);
 
-        List<Yrkesaktivitet> registerFrilansPåSTP = finnFrilansIRegisterSomKrysserSTP(stpBG, iayGrunnlag, ref.getAktørId());
-        List<Yrkesaktivitet> registerFrilansSiste12MndIkkePåSTP = finnFrilansIRegisterSomErAktivtSiste12MndMenIkkePåStp(stpBG, iayGrunnlag, ref.getAktørId());
-        if (!registerFrilansPåSTP.isEmpty() || !registerFrilansSiste12MndIkkePåSTP.isEmpty()) {
-            loggFrilansLister(ref.getSaksnummer(), registerFrilansPåSTP, registerFrilansSiste12MndIkkePåSTP);
-        }
-        List<Inntektspost> inntektsposterFrilansSiste12Mnd = inntektsposterFrilansSiste12Mnd(stpBG, iayGrunnlag, ref.getAktørId());
-        if (relevantOppgittFrilans.isEmpty() && registerFrilansPåSTP.isEmpty()) {
-            if (!inntektsposterFrilansSiste12Mnd.isEmpty()) {
-                // Har ikke frilans i hverken søknad eller register, men har hatt frilansinntekt siste 12 mnd.
-                loggInntektUtenAktivFrilans(ref.getSaksnummer(), inntektsposterFrilansSiste12Mnd);
-                return;
-            }
-            return;
-        }
-        if (inntektsposterFrilansSiste12Mnd.isEmpty()) {
-            // Har frilans i enten søknad eller register men er uten frilansinntekt siste 12 mnd
-            LOG.info("FP-654893: Saksnr {} har frilans i enten søknad / register men er uten frilansinntekt siste 12 mnd", ref.getSaksnummer().getVerdi());
-            return;
-        }
-        if (relevantOppgittFrilans.isEmpty() || registerFrilansPåSTP.isEmpty()) {
-            loggAvvik(ref.getSaksnummer(), relevantOppgittFrilans, registerFrilansPåSTP);
-        }
-    }
-
-    private void loggFrilansLister(Saksnummer saksnummer,
-                                   List<Yrkesaktivitet> registerFrilansPåSTP,
-                                   List<Yrkesaktivitet> registerFrilansSiste12MndIkkePåSTP) {
-        LOG.info("FP-654891: Saksnr {}. Antall frilansaktiviteter siste 12 mnd ikke aktivt på stp: {}.  Antall frilansaktiviteter på stp: {}",
-            saksnummer.getVerdi(), registerFrilansSiste12MndIkkePåSTP.size(), registerFrilansPåSTP.size());
-    }
-
-    private List<Yrkesaktivitet> finnFrilansIRegisterSomErAktivtSiste12MndMenIkkePåStp(LocalDate stpBG, InntektArbeidYtelseGrunnlag grunnlag, AktørId aktørId) {
-        YrkesaktivitetFilter filter = new YrkesaktivitetFilter(grunnlag.getArbeidsforholdInformasjon(),
-            grunnlag.getAktørArbeidFraRegister(aktørId)).før(stpBG);
-        return filter.getFrilansOppdrag().stream()
-            .filter(ya -> erAnsattFørDato(ya.getAlleAktivitetsAvtaler(), stpBG))
-            .filter(ya -> !erAnsattPåDato(ya.getAlleAktivitetsAvtaler(), stpBG))
+        List<Yrkesaktivitet> frilansPåSTP = finnFrilansIRegisterSomKrysserSTP(stpBG, iayGrunnlag, ref.getAktørId());
+        List<Arbeidsgiver> arbeidsgivereMedInntektSiste3Mnd = finnArbeidsgivereMedInntekterSiste3Mnd(stpBG, iayGrunnlag, ref.getAktørId());
+        List<Yrkesaktivitet> frilansaktiviteterPåSTPMedInntektSiste3Mnd = frilansPåSTP.stream()
+            .filter(ya -> arbeidsgivereMedInntektSiste3Mnd.contains(ya.getArbeidsgiver()))
             .collect(Collectors.toList());
 
+        if (relevantOppgittFrilans.isEmpty()) {
+            frilansaktiviteterPåSTPMedInntektSiste3Mnd.forEach(fl -> {
+                LOG.info("FP-654895: Saksnr {}. Ikke oppgitt frilans i søknad, men arbeidsgiver {} har gitt utbetaling som frilans siste 3 mnd",
+                    ref.getSaksnummer().getVerdi(), fl.getArbeidsgiver().toString());
+            });
+        } else if (frilansaktiviteterPåSTPMedInntektSiste3Mnd.isEmpty()) {
+            LOG.info("FP-654896: Saksnr {}. Oppgitt frilans i søknad, men ingen utbetalinger som frilans siste 3 mnd",
+                ref.getSaksnummer().getVerdi());
+        }
     }
 
-    private boolean erAnsattFørDato(Collection<AktivitetsAvtale> alleAktivitetsAvtaler, LocalDate stpBG) {
-        return alleAktivitetsAvtaler.stream()
-            .filter(AktivitetsAvtale::erAnsettelsesPeriode)
-            .anyMatch(aa -> aa.getPeriode().getFomDato().isBefore(stpBG));
-    }
-
-    private void loggInntektUtenAktivFrilans(Saksnummer saksnummer,
-                                             List<Inntektspost> inntektsposterFrilansSiste12Mnd) {
-        Inntektspost sisteInntektspost = inntektsposterFrilansSiste12Mnd.stream()
-            .max(Comparator.comparing(ip -> ip.getPeriode().getFomDato()))
-            .orElseThrow();
-        Arbeidsgiver ag = sisteInntektspost.getInntekt().getArbeidsgiver();
-        LOG.info("FP-654892: Saksnr {} har hatt frilansinntekt siste 12 mnd men har ikke aktivt frilansforhold på stp. " +
-            "Siste inntektspost var hos {} i perioden {}", saksnummer.getVerdi(), ag, sisteInntektspost.getPeriode());
-    }
-
-    private List<Inntektspost> inntektsposterFrilansSiste12Mnd(LocalDate stpBG,
-                                                               InntektArbeidYtelseGrunnlag grunnlag,
-                                                               AktørId aktørId) {
-        YrkesaktivitetFilter filter = new YrkesaktivitetFilter(grunnlag.getArbeidsforholdInformasjon(),
-            grunnlag.getAktørArbeidFraRegister(aktørId)).før(stpBG);
-        List<Arbeidsgiver> alleFrilansarbeidsgivere = filter.getFrilansOppdrag().stream()
-            .map(Yrkesaktivitet::getArbeidsgiver)
-            .collect(Collectors.toList());
-
+    private List<Arbeidsgiver> finnArbeidsgivereMedInntekterSiste3Mnd(LocalDate stpBG,
+                                                                      InntektArbeidYtelseGrunnlag grunnlag,
+                                                                      AktørId aktørId) {
         InntektFilter inntektfilter = new InntektFilter(grunnlag.getAktørInntektFraRegister(aktørId));
         return inntektfilter.getAlleInntekter(InntektsKilde.INNTEKT_BEREGNING).stream()
-            .filter(ip -> alleFrilansarbeidsgivere.contains(ip.getArbeidsgiver()))
-            .map(Inntekt::getAlleInntektsposter)
-            .filter(alleInntektsposter -> finnesInntektEtterDato(alleInntektsposter, stpBG.minusMonths(12).withDayOfMonth(1)))
-            .flatMap(Collection::stream)
+            .filter(innt -> innt.getArbeidsgiver() != null)
+            .filter(innt -> finnesInntektEtterDato(innt.getAlleInntektsposter(), stpBG.minusMonths(3).withDayOfMonth(1)))
+            .map(Inntekt::getArbeidsgiver)
             .collect(Collectors.toList());
     }
 
     private boolean finnesInntektEtterDato(Collection<Inntektspost> alleInntektsposter, LocalDate dato) {
-        return alleInntektsposter.stream().anyMatch(ip -> ip.getPeriode().getFomDato().isAfter(dato));
-    }
-
-    private void loggAvvik(Saksnummer saksnummer,
-                           Optional<OppgittFrilans> relevantOppgittFrilans,
-                           List<Yrkesaktivitet> relevantRegisterFrilans) {
-        List<DatoIntervallEntitet> registerPerioder = relevantRegisterFrilans.stream()
-            .map(Yrkesaktivitet::getAlleAktivitetsAvtaler)
-            .flatMap(Collection::stream)
-            .filter(AktivitetsAvtale::erAnsettelsesPeriode)
-            .map(AktivitetsAvtale::getPeriode)
-            .collect(Collectors.toList());
-        LOG.info("FP-654894: Missmatch mellom frilans fra søknad og frilans fra register på saksnr {}." +
-            " Frilans er oppgitt i søknaden: {}. Frilans på skjæringstidspunkt i register: {}",
-            saksnummer.getVerdi(), relevantOppgittFrilans.isPresent(), registerPerioder);
+        return alleInntektsposter.stream().anyMatch(ip -> !ip.getPeriode().getFomDato().isBefore(dato));
     }
 
     private Optional<OppgittFrilans> finnOppgittFrilansFraSøknad(InntektArbeidYtelseGrunnlag iayGrunnlag) {
@@ -173,6 +109,4 @@ public class FrilansAvvikLoggTjeneste {
             .filter(AktivitetsAvtale::erAnsettelsesPeriode)
             .anyMatch(aa -> aa.getPeriode().inkluderer(stpBG));
     }
-
-
 }
