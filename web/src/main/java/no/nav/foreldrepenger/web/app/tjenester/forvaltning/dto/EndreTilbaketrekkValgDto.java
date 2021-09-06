e098c2ea3e1ae44f3cf5f57d34b85f813f7f9019
Petter Ekern <petter.ekern@nav.no>
2021-09-06 17:03:17 +0200

    FIX: Fikser swaggertjeneste for å endre tilbaketrekksvalg til å fungerer med behandlingUUID

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/forvaltning/dto/EndreTilbaketrekkValgDto.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/forvaltning/dto/EndreTilbaketrekkValgDto.java
@@ -1,43 +1,13 @@
 package no.nav.foreldrepenger.web.app.tjenester.forvaltning.dto;
 
-import no.nav.vedtak.sikkerhet.abac.AbacDataAttributter;
-import no.nav.vedtak.sikkerhet.abac.AbacDto;
-
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.QueryParam;
 
-public class EndreTilbaketrekkValgDto implements AbacDto {
-
-    @QueryParam("behandlingId")
-    @Min(0)
-    @Max(Long.MAX_VALUE)
-    @NotNull
-    private Long behandlingId;
+public class EndreTilbaketrekkValgDto extends ForvaltningBehandlingIdDto {
 
     @NotNull
     @QueryParam("skalHindreTilbaketrekk")
     private Boolean skalHindreTilbaketrekk;
-
-
-    public EndreTilbaketrekkValgDto(@Min(0) @Max(Long.MAX_VALUE) @NotNull Long behandlingId,
-                                    @NotNull Boolean skalHindreTilbaketrekk) {
-        this.behandlingId = behandlingId;
-        this.skalHindreTilbaketrekk = skalHindreTilbaketrekk;
-    }
-
-    public EndreTilbaketrekkValgDto() {
-    }
-
-    @Override
-    public AbacDataAttributter abacAttributter() {
-        return AbacDataAttributter.opprett();
-    }
-
-    public Long getBehandlingId() {
-        return behandlingId;
-    }
 
     public Boolean getSkalHindreTilbaketrekk() {
         return skalHindreTilbaketrekk;
