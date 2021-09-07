c029632b12255d6e0cbc057520a1a5e47edef1ff
Anders Palfi <anderspalfi@gmail.com>
2021-09-07 12:51:12 +0200

    Manuell behandling der søknad om fp og pp overlapper

--- domenetjenester/uttak/src/main/java/no/nav/foreldrepenger/domene/uttak/fastsetteperioder/grunnlagbyggere/YtelserGrunnlagBygger.java
+++ domenetjenester/uttak/src/main/java/no/nav/foreldrepenger/domene/uttak/fastsetteperioder/grunnlagbyggere/YtelserGrunnlagBygger.java
@@ -1,40 +1,85 @@
 package no.nav.foreldrepenger.domene.uttak.fastsetteperioder.grunnlagbyggere;
 
+import static no.nav.foreldrepenger.behandlingslager.kodeverk.Fagsystem.K9SAK;
+import static no.nav.foreldrepenger.domene.tid.DatoIntervallEntitet.fraOgMedTilOgMed;
+
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import no.nav.foreldrepenger.behandlingslager.ytelse.RelatertYtelseType;
+import no.nav.foreldrepenger.domene.iay.modell.AktørYtelse;
+import no.nav.foreldrepenger.domene.iay.modell.YtelseAnvist;
+import no.nav.foreldrepenger.domene.tid.VirkedagUtil;
+import no.nav.foreldrepenger.domene.typer.Stillingsprosent;
 import no.nav.foreldrepenger.domene.uttak.input.ForeldrepengerGrunnlag;
 import no.nav.foreldrepenger.domene.uttak.input.UttakInput;
-import no.nav.foreldrepenger.regler.uttak.fastsetteperiode.grunnlag.PeriodeMedBarnInnlagt;
-import no.nav.foreldrepenger.regler.uttak.fastsetteperiode.grunnlag.ytelser.PleiepengerMedInnleggelse;
+import no.nav.foreldrepenger.regler.uttak.fastsetteperiode.grunnlag.ytelser.Pleiepenger;
+import no.nav.foreldrepenger.regler.uttak.fastsetteperiode.grunnlag.ytelser.PleiepengerPeriode;
 import no.nav.foreldrepenger.regler.uttak.fastsetteperiode.grunnlag.ytelser.Ytelser;
+import no.nav.fpsak.tidsserie.LocalDateSegment;
+import no.nav.fpsak.tidsserie.LocalDateTimeline;
+import no.nav.fpsak.tidsserie.StandardCombinators;
 
 @ApplicationScoped
 public class YtelserGrunnlagBygger {
 
     public Ytelser byggGrunnlag(UttakInput uttakInput) {
-        ForeldrepengerGrunnlag fpGrunnlag = uttakInput.getYtelsespesifiktGrunnlag();
-        var pleiepengerMedInnleggelse = getPleiepengerMedInnleggelse(fpGrunnlag);
+        var pleiepengerMedInnleggelse = getPleiepengerMedInnleggelse(uttakInput);
         return new Ytelser(pleiepengerMedInnleggelse.orElse(null));
     }
 
-    private Optional<PleiepengerMedInnleggelse> getPleiepengerMedInnleggelse(ForeldrepengerGrunnlag fpGrunnlag) {
-        var pleiepengerGrunnlag = fpGrunnlag.getPleiepengerGrunnlag();
-        if (pleiepengerGrunnlag.isPresent()) {
-            var perioderMedInnleggelse = pleiepengerGrunnlag.get().getPerioderMedInnleggelse();
-            if (perioderMedInnleggelse.isPresent()) {
-                var perioder = perioderMedInnleggelse.get()
-                    .getInnleggelser()
-                    .stream()
-                    .map(p -> {
-                        var periode = p.getPeriode();
-                        return new PeriodeMedBarnInnlagt(periode.getFomDato(), periode.getTomDato());
-                    })
-                    .toList();
-                return Optional.of(new PleiepengerMedInnleggelse(perioder));
-            }
+    private Optional<Pleiepenger> getPleiepengerMedInnleggelse(UttakInput uttakInput) {
+        var iayGrunnlag = uttakInput.getIayGrunnlag();
+        var aktørYtelseFraRegister = iayGrunnlag.getAktørYtelseFraRegister(
+            uttakInput.getBehandlingReferanse().getAktørId());
+        if (aktørYtelseFraRegister.isEmpty()) {
+            return Optional.empty();
         }
-        return Optional.empty();
+
+        ForeldrepengerGrunnlag fpGrunnlag = uttakInput.getYtelsespesifiktGrunnlag();
+
+        var perioder = pleiepengerAnvistePerioderMedUtbetaling(aktørYtelseFraRegister.get())
+            .map(ya -> new PleiepengerPeriode(ya.getAnvistFOM(), ya.getAnvistTOM(), erInnlagt(ya, fpGrunnlag)))
+            .toList();
+        var slåttSammen = slåSammenLike(perioder);
+        return Optional.of(new Pleiepenger(slåttSammen));
+    }
+
+    private List<PleiepengerPeriode> slåSammenLike(List<PleiepengerPeriode> perioder) {
+        var segments = perioder.stream()
+            .map(p -> new LocalDateSegment<>(p.getFom(), VirkedagUtil.tomSøndag(p.getTom()), p.isBarnInnlagt()))
+            .collect(Collectors.toList());
+        var timeline = new LocalDateTimeline<>(segments);
+
+        return timeline.compress((b1, b2) -> b1 == b2, StandardCombinators::leftOnly)
+            .stream()
+            .map(s -> new PleiepengerPeriode(s.getFom(), VirkedagUtil.tomVirkedag(s.getTom()), s.getValue()))
+            .toList();
+    }
+
+    private Stream<YtelseAnvist> pleiepengerAnvistePerioderMedUtbetaling(AktørYtelse aktørYtelseFraRegister) {
+        return aktørYtelseFraRegister.getAlleYtelser().stream()
+            .filter(y -> K9SAK.equals(y.getKilde()))
+            .filter(y -> y.getRelatertYtelseType().equals(RelatertYtelseType.PLEIEPENGER_SYKT_BARN))
+            .flatMap(ytelse -> ytelse.getYtelseAnvist().stream()
+                .filter(ya -> !ya.getUtbetalingsgradProsent().orElse(Stillingsprosent.ZERO).erNulltall()));
+    }
+
+    private boolean erInnlagt(YtelseAnvist ya, ForeldrepengerGrunnlag fpGrunnlag) {
+        if (fpGrunnlag.getPleiepengerGrunnlag().isEmpty()) {
+            return false;
+        }
+        var perioderMedInnleggelse = fpGrunnlag.getPleiepengerGrunnlag().get()
+            .getPerioderMedInnleggelse();
+        if (perioderMedInnleggelse.isEmpty()) {
+            return false;
+        }
+        return perioderMedInnleggelse.get().getInnleggelser().stream()
+            .anyMatch(p -> fraOgMedTilOgMed(ya.getAnvistFOM(), ya.getAnvistTOM()).erOmsluttetAv(p.getPeriode())
+        );
     }
 }
