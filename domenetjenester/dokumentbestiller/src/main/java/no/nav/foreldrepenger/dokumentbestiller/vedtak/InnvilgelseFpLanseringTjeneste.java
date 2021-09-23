2a448f804774c86f2f8d1c250098e4281d842cf7
AnjaAalerud <52454508+AnjaAalerud@users.noreply.github.com>
2021-09-23 12:57:21 +0200

    TFP-4552 Lansere innvilgelse foreldrepengerbrev med gradering og samtidig uttak (#3163)

--- domenetjenester/dokumentbestiller/src/main/java/no/nav/foreldrepenger/dokumentbestiller/vedtak/InnvilgelseFpLanseringTjeneste.java
+++ domenetjenester/dokumentbestiller/src/main/java/no/nav/foreldrepenger/dokumentbestiller/vedtak/InnvilgelseFpLanseringTjeneste.java
@@ -6,33 +6,25 @@ import no.nav.foreldrepenger.behandlingslager.behandling.familiehendelse.Familie
 import no.nav.foreldrepenger.behandlingslager.behandling.søknad.SøknadRepository;
 import no.nav.foreldrepenger.behandlingslager.geografisk.Språkkode;
 import no.nav.foreldrepenger.dokumentbestiller.DokumentMalType;
-import no.nav.foreldrepenger.domene.uttak.ForeldrepengerUttak;
-import no.nav.foreldrepenger.domene.uttak.ForeldrepengerUttakTjeneste;
 import no.nav.foreldrepenger.konfig.Environment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 
 @ApplicationScoped
 public class InnvilgelseFpLanseringTjeneste {
     private static final Logger LOGGER = LoggerFactory.getLogger(InnvilgelseFpLanseringTjeneste.class);
     private static final Environment ENV = Environment.current();
-    private static final List<String> GRADERINGS_ÅRSAKER = List.of("4025", "4080", "4093", "4094");
 
-    private ForeldrepengerUttakTjeneste fpUttakRepository;
     private FamilieHendelseRepository familieHendelseRepository;
     private SøknadRepository søknadRepository;
 
     @Inject
-    public InnvilgelseFpLanseringTjeneste(ForeldrepengerUttakTjeneste fpUttakRepository,
-                                          FamilieHendelseRepository familieHendelseRepository,
+    public InnvilgelseFpLanseringTjeneste(FamilieHendelseRepository familieHendelseRepository,
                                           SøknadRepository søknadRepository) {
-        this.fpUttakRepository = fpUttakRepository;
         this.familieHendelseRepository = familieHendelseRepository;
         this.søknadRepository = søknadRepository;
     }
@@ -46,11 +38,7 @@ public class InnvilgelseFpLanseringTjeneste {
     }
 
     public DokumentMalType velgFpInnvilgelsesmalProd(Behandling behandling) {
-        boolean lansertDokGen = behandling.erYtelseBehandling()
-            && !harPerioderMedGraderingEllerSamtidigUttak(behandling)
-            && !harPerioderMedGradertOgAvslått(behandling)
-            && !harDødtBarn(behandling)
-            && harSøknadMedSpråkkodeNB(behandling);
+        boolean lansertDokGen = !harDødtBarn(behandling) && harSøknadMedSpråkkodeNB(behandling);
 
         loggSaksnummerForNesteLansering(behandling);
 
@@ -71,25 +59,8 @@ public class InnvilgelseFpLanseringTjeneste {
     }
 
     private boolean vilBrukeDokgenVedNesteLansering(Behandling behandling) {
-        return (harPerioderMedGraderingEllerSamtidigUttak(behandling)
-            || harPerioderMedGradertOgAvslått(behandling))
-            && harSøknadMedSpråkkodeNB(behandling);
-    }
-
-    private boolean harPerioderMedGraderingEllerSamtidigUttak(Behandling behandling) {
-        return fpUttakRepository.hentUttakHvisEksisterer(behandling.getId())
-            .map(ForeldrepengerUttak::getGjeldendePerioder)
-            .orElse(Collections.emptyList())
-            .stream()
-            .anyMatch(p -> p.isGraderingInnvilget() || p.isSøktGradering() || p.isSamtidigUttak());
-    }
-
-    private boolean harPerioderMedGradertOgAvslått(Behandling behandling) {
-        return fpUttakRepository.hentUttakHvisEksisterer(behandling.getId())
-            .map(ForeldrepengerUttak::getGjeldendePerioder)
-            .orElse(Collections.emptyList())
-            .stream()
-            .anyMatch(p -> !p.isInnvilget() && GRADERINGS_ÅRSAKER.contains(p.getResultatÅrsak().getKode()));
+        return harDødtBarn(behandling)
+            || !harSøknadMedSpråkkodeNB(behandling);
     }
 
     private boolean harDødtBarn(Behandling behandling) {
