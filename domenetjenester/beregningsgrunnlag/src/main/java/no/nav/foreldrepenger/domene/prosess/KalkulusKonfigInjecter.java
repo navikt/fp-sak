10c9d058dcb97aa5e7b621cffc618bd860aaadd2
Petter Ekern <petter.ekern@nav.no>
2021-10-21 11:32:23 +0200

    TFP-4651: Legger til toggle

--- domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/prosess/KalkulusKonfigInjecter.java
+++ domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/prosess/KalkulusKonfigInjecter.java
@@ -22,6 +22,8 @@ public class KalkulusKonfigInjecter {
     static {
         TOGGLES.put("fpsak.splitteSammenligningATFL", false);
         TOGGLES.put("automatisk-besteberegning", true);
+        // Kan fjernes når vi har fått gui for å vise lønnsendring
+        TOGGLES.put("AUTOMATISK_BERGNE_LØNNENDRING", false);
     }
 
     public KalkulusKonfigInjecter() {
