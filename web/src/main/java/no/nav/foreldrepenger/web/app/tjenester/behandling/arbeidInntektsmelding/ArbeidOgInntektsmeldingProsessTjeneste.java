a1f85aa91e60850a7eb3751410d21016ae2d2329
Petter Ekern <petter.ekern@nav.no>
2022-02-04 15:57:54 +0100

    TFP-4857: Mulighet for å overstyre arbeidsforhold i 5085

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/arbeidInntektsmelding/ArbeidOgInntektsmeldingProsessTjeneste.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/arbeidInntektsmelding/ArbeidOgInntektsmeldingProsessTjeneste.java
@@ -1,9 +1,12 @@
 package no.nav.foreldrepenger.web.app.tjenester.behandling.arbeidInntektsmelding;
 
+import java.util.List;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import no.nav.foreldrepenger.behandlingskontroll.BehandlingskontrollTjeneste;
+import no.nav.foreldrepenger.behandlingslager.behandling.Behandling;
 import no.nav.foreldrepenger.behandlingslager.behandling.BehandlingStatus;
 import no.nav.foreldrepenger.behandlingslager.behandling.BehandlingStegType;
 import no.nav.foreldrepenger.behandlingslager.behandling.aksjonspunkt.AksjonspunktDefinisjon;
@@ -12,6 +15,7 @@ import no.nav.foreldrepenger.behandlingsprosess.prosessering.BehandlingProsesser
 import no.nav.foreldrepenger.web.app.tjenester.behandling.aksjonspunkt.BehandlingsprosessTjeneste;
 import no.nav.foreldrepenger.web.app.tjenester.behandling.aksjonspunkt.BehandlingsutredningTjeneste;
 import no.nav.foreldrepenger.web.app.tjenester.behandling.dto.BehandlingIdVersjonDto;
+import no.nav.vedtak.exception.TekniskException;
 
 @ApplicationScoped
 public class ArbeidOgInntektsmeldingProsessTjeneste {
@@ -40,12 +44,38 @@ public class ArbeidOgInntektsmeldingProsessTjeneste {
     }
 
     public void rullTilbakeTilKontrollerArbeidOgInntektsmelding(BehandlingIdVersjonDto dto) {
+        var behandling = behandlingRepository.hentBehandling(dto.getBehandlingUuid());
 
         // Diverse kontroller for å sjekke at vi har lov til å flytte behandlingen bakover i prosessen.
+        validerAtOperasjonErLovlig(behandling, false);
+
+        behandlingsutredningTjeneste.kanEndreBehandling(behandling, dto.getBehandlingVersjon());
+        behandlingProsesseringTjeneste.reposisjonerBehandlingTilbakeTil(behandling, BehandlingStegType.KONTROLLER_FAKTA_ARBEIDSFORHOLD_INNTEKTSMELDING );
+        behandlingsprosessTjeneste.asynkKjørProsess(behandling);
+    }
+
+    public void tillTilbakeOgOpprettAksjonspunkt(BehandlingIdVersjonDto dto) {
         var behandling = behandlingRepository.hentBehandling(dto.getBehandlingUuid());
-        if (!behandling.harAksjonspunktMedType(AksjonspunktDefinisjon.VURDER_ARBEIDSFORHOLD_INNTEKTSMELDING)) {
+
+        // Diverse kontroller for å sjekke at vi har lov til å flytte behandlingen bakover i prosessen.
+        validerAtOperasjonErLovlig(behandling, true);
+
+        var kontekst = behandlingskontrollTjeneste.initBehandlingskontroll(behandling);
+        behandlingskontrollTjeneste.lagreAksjonspunkterFunnet(kontekst, List.of(AksjonspunktDefinisjon.VURDER_ARBEIDSFORHOLD_INNTEKTSMELDING));
+        behandlingProsesseringTjeneste.reposisjonerBehandlingTilbakeTil(behandling,
+                BehandlingStegType.KONTROLLER_FAKTA_ARBEIDSFORHOLD_INNTEKTSMELDING);
+        behandlingProsesseringTjeneste.opprettTasksForFortsettBehandling(behandling);
+    }
+
+    private void validerAtOperasjonErLovlig(Behandling behandling, boolean aksjonspunktSkalOpprettes) {
+        if (!aksjonspunktSkalOpprettes && !behandling.harAksjonspunktMedType(AksjonspunktDefinisjon.VURDER_ARBEIDSFORHOLD_INNTEKTSMELDING)) {
             throw new IllegalStateException("FEIL: Prøver å tilbakestille en behandling som aldri har hatt aksjonspunkt" +
-                " for arbeidsforhold / inntektsmelding. Ugyldig aksjon.");
+                    " for arbeidsforhold / inntektsmelding. Ugyldig aksjon.");
+        }
+        if (aksjonspunktSkalOpprettes && behandling.harÅpentAksjonspunktMedType(AksjonspunktDefinisjon.VURDER_ARBEIDSFORHOLD_INNTEKTSMELDING)) {
+            var msg = String.format("Behandling %s har allerede aksjonspunkt 5085 for arbeid og inntektsmelding",
+                    behandling.getId());
+            throw new TekniskException("FP-658124", msg);
         }
         if (behandling.erAvsluttet()) {
             throw new IllegalStateException("FEIL: Prøver å tilbakestille en behandling som er avsluttet. Ugyldig aksjon.");
@@ -64,9 +94,6 @@ public class ArbeidOgInntektsmeldingProsessTjeneste {
         if (behandling.getStatus().equals(BehandlingStatus.FATTER_VEDTAK) || behandling.getStatus().equals(BehandlingStatus.IVERKSETTER_VEDTAK)) {
             throw new IllegalStateException("FEIL: Prøver å tilbakestille en behandling som er i ferd med å fatte vedtak. Ugyldig aksjon.");
         }
-        behandlingsutredningTjeneste.kanEndreBehandling(behandling, dto.getBehandlingVersjon());
-
-        behandlingProsesseringTjeneste.reposisjonerBehandlingTilbakeTil(behandling, BehandlingStegType.KONTROLLER_FAKTA_ARBEIDSFORHOLD_INNTEKTSMELDING );
-        behandlingsprosessTjeneste.asynkKjørProsess(behandling);
     }
+
 }
--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/arbeidInntektsmelding/ArbeidOgInntektsmeldingRestTjeneste.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/arbeidInntektsmelding/ArbeidOgInntektsmeldingRestTjeneste.java
@@ -7,16 +7,20 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import no.nav.foreldrepenger.abac.FPSakBeskyttetRessursAttributt;
 import no.nav.foreldrepenger.behandling.BehandlingReferanse;
+import no.nav.foreldrepenger.behandlingslager.behandling.aksjonspunkt.AksjonspunktDefinisjon;
+import no.nav.foreldrepenger.behandlingslager.behandling.arbeidsforhold.ArbeidsforholdKomplettVurderingType;
 import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.foreldrepenger.domene.arbeidInntektsmelding.ArbeidsforholdInntektsmeldingMangelTjeneste;
 import no.nav.foreldrepenger.domene.arbeidInntektsmelding.ManglendeOpplysningerVurderingDto;
 import no.nav.foreldrepenger.domene.arbeidInntektsmelding.ManueltArbeidsforholdDto;
 import no.nav.foreldrepenger.domene.arbeidsforhold.dto.arbeidInntektsmelding.ArbeidOgInntektsmeldingDto;
 import no.nav.foreldrepenger.skjæringstidspunkt.SkjæringstidspunktTjeneste;
+import no.nav.foreldrepenger.tilganger.TilgangerTjeneste;
 import no.nav.foreldrepenger.web.app.tjenester.behandling.dto.BehandlingAbacSuppliers;
 import no.nav.foreldrepenger.web.app.tjenester.behandling.dto.BehandlingIdVersjonDto;
 import no.nav.foreldrepenger.web.app.tjenester.behandling.dto.UuidDto;
 import no.nav.foreldrepenger.web.server.abac.AppAbacAttributtType;
+import no.nav.vedtak.exception.TekniskException;
 import no.nav.vedtak.sikkerhet.abac.AbacDataAttributter;
 import no.nav.vedtak.sikkerhet.abac.BeskyttetRessurs;
 import no.nav.vedtak.sikkerhet.abac.TilpassetAbacAttributt;
@@ -34,6 +38,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.transform.TransformerConfigurationException;
 
 import java.util.UUID;
 import java.util.function.Function;
@@ -61,6 +66,7 @@ public class ArbeidOgInntektsmeldingRestTjeneste {
     private ArbeidOgInntektsmeldingDtoTjeneste arbeidOgInntektsmeldingDtoTjeneste;
     private ArbeidsforholdInntektsmeldingMangelTjeneste arbeidsforholdInntektsmeldingMangelTjeneste;
     private ArbeidOgInntektsmeldingProsessTjeneste arbeidOgInntektsmeldingProsessTjeneste;
+    private TilgangerTjeneste tilgangerTjeneste;
 
     ArbeidOgInntektsmeldingRestTjeneste() {
         // CDI
@@ -71,12 +77,14 @@ public class ArbeidOgInntektsmeldingRestTjeneste {
                                                SkjæringstidspunktTjeneste skjæringstidspunktTjeneste,
                                                ArbeidOgInntektsmeldingDtoTjeneste arbeidOgInntektsmeldingDtoTjeneste,
                                                ArbeidsforholdInntektsmeldingMangelTjeneste arbeidsforholdInntektsmeldingMangelTjeneste,
-                                               ArbeidOgInntektsmeldingProsessTjeneste arbeidOgInntektsmeldingProsessTjeneste) {
+                                               ArbeidOgInntektsmeldingProsessTjeneste arbeidOgInntektsmeldingProsessTjeneste,
+                                               TilgangerTjeneste tilgangerTjeneste) {
         this.behandlingRepository = behandlingRepository;
         this.skjæringstidspunktTjeneste = skjæringstidspunktTjeneste;
         this.arbeidOgInntektsmeldingDtoTjeneste = arbeidOgInntektsmeldingDtoTjeneste;
         this.arbeidsforholdInntektsmeldingMangelTjeneste = arbeidsforholdInntektsmeldingMangelTjeneste;
         this.arbeidOgInntektsmeldingProsessTjeneste = arbeidOgInntektsmeldingProsessTjeneste;
+        this.tilgangerTjeneste = tilgangerTjeneste;
     }
 
     @GET
@@ -113,6 +121,11 @@ public class ArbeidOgInntektsmeldingRestTjeneste {
     public Response lagreManuelleArbeidsforhold(@TilpassetAbacAttributt(supplierClass = ManueltArbeidsforholdDtoAbacDataSupplier.class)
                                                  @NotNull @Parameter(description = "Registrering av arbeidsforhold.") @Valid ManueltArbeidsforholdDto manueltArbeidsforholdDto) {
         BehandlingReferanse ref = lagReferanse(manueltArbeidsforholdDto.getBehandlingUuid());
+        if (endringGjelderHelmanueltArbeidsforhold(manueltArbeidsforholdDto) && !erOverstyringLovlig()) {
+            var msg = String.format(
+                    "Feil: Prøve å gjøre endringer på et helmanuelt arbeidsforhold uten å være overstyrer på behandling %s", ref.getBehandlingId());
+            throw new TekniskException("FP-657812", msg);
+        }
         arbeidsforholdInntektsmeldingMangelTjeneste.lagreManuelleArbeidsforhold(ref, manueltArbeidsforholdDto);
         return Response.ok().build();
     }
@@ -124,7 +137,12 @@ public class ArbeidOgInntektsmeldingRestTjeneste {
     @BeskyttetRessurs(action = UPDATE, resource = FPSakBeskyttetRessursAttributt.FAGSAK)
     public Response åpneForEndring(@TilpassetAbacAttributt(supplierClass = BehandlingAbacSuppliers.BehandlingIdAbacDataSupplier.class)
                                                 @NotNull @Parameter(description = "BehandlingUID og versjon på behadlingen.") @Valid BehandlingIdVersjonDto behandlingIdVersjonDto) {
-        arbeidOgInntektsmeldingProsessTjeneste.rullTilbakeTilKontrollerArbeidOgInntektsmelding(behandlingIdVersjonDto);
+        var behandling = behandlingRepository.hentBehandling(behandlingIdVersjonDto.getBehandlingUuid());
+        if (behandling.harAksjonspunktMedType(AksjonspunktDefinisjon.VURDER_ARBEIDSFORHOLD_INNTEKTSMELDING)) {
+            arbeidOgInntektsmeldingProsessTjeneste.rullTilbakeTilKontrollerArbeidOgInntektsmelding(behandlingIdVersjonDto);
+        } else if (erOverstyringLovlig()) {
+            arbeidOgInntektsmeldingProsessTjeneste.tillTilbakeOgOpprettAksjonspunkt(behandlingIdVersjonDto);
+        }
         return Response.ok().build();
     }
 
@@ -132,6 +150,16 @@ public class ArbeidOgInntektsmeldingRestTjeneste {
         var behandling = behandlingRepository.hentBehandling(uuid);
         var stp = skjæringstidspunktTjeneste.getSkjæringstidspunkter(behandling.getId());
         return BehandlingReferanse.fra(behandling, stp);
+    }
+
+    private boolean erOverstyringLovlig() {
+        var innloggetBruker = tilgangerTjeneste.innloggetBruker();
+        return innloggetBruker.getKanOverstyre();
+    }
+
+    private boolean endringGjelderHelmanueltArbeidsforhold(ManueltArbeidsforholdDto manueltArbeidsforholdDto) {
+        return manueltArbeidsforholdDto.getVurdering().equals(ArbeidsforholdKomplettVurderingType.MANUELT_OPPRETTET_AV_SAKSBEHANDLER)
+                || manueltArbeidsforholdDto.getVurdering().equals(ArbeidsforholdKomplettVurderingType.FJERN_FRA_BEHANDLINGEN);
     }
 
     public static class ManglendeInntektsmeldingVurderingAbacDataSupplier implements Function<Object, AbacDataAttributter> {
