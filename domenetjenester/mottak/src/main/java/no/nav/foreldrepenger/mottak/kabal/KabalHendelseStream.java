51f9088ebb9fbefeb77e978bea2c38d675d4af52
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2022-03-07 15:52:42 +0100

    TFP-4902 konsumere kabal-hendelser

---
+++ domenetjenester/mottak/src/main/java/no/nav/foreldrepenger/mottak/kabal/KabalHendelseStream.java
@@ -0,0 +1,93 @@
+package no.nav.foreldrepenger.mottak.kabal;
+
+import java.time.Duration;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import no.nav.foreldrepenger.domene.liveness.KafkaIntegration;
+import no.nav.vedtak.apptjeneste.AppServiceHandler;
+import no.nav.vedtak.log.metrics.LivenessAware;
+import no.nav.vedtak.log.metrics.ReadinessAware;
+
+/*
+ * Dokumentasjon https://github.com/navikt/kabal-api/tree/main/docs/integrasjon
+ */
+@ApplicationScoped
+public class KabalHendelseStream implements LivenessAware, ReadinessAware, AppServiceHandler, KafkaIntegration {
+
+    private static final Logger LOG = LoggerFactory.getLogger(KabalHendelseStream.class);
+
+    private KafkaStreams stream;
+    private String topicName;
+
+    KabalHendelseStream() {
+    }
+
+    @Inject
+    public KabalHendelseStream(KabalHendelseHåndterer kabalHendelseHåndterer,
+                               KabalHendelseProperties streamKafkaProperties) {
+        this.topicName = streamKafkaProperties.getTopicName();
+
+        final Consumed<String, String> consumed = Consumed.with(Topology.AutoOffsetReset.EARLIEST);
+
+        final StreamsBuilder builder = new StreamsBuilder();
+        builder.stream(topicName, consumed)
+            .foreach(kabalHendelseHåndterer::handleMessage);
+
+        this.stream = new KafkaStreams(builder.build(), streamKafkaProperties.getProperties());
+    }
+
+
+    private void addShutdownHooks() {
+        stream.setStateListener((newState, oldState) -> {
+            LOG.info("{} :: From state={} to state={}", getTopicName(), oldState, newState);
+
+            if (newState == KafkaStreams.State.ERROR) {
+                // if the stream has died there is no reason to keep spinning
+                LOG.warn("{} :: No reason to keep living, closing stream", getTopicName());
+                stop();
+            }
+        });
+        stream.setUncaughtExceptionHandler((t, e) -> {
+            LOG.error("{} :: Caught exception in stream, exiting", getTopicName(), e);
+            stop();
+        });
+    }
+
+    @Override
+    public void start() {
+        addShutdownHooks();
+
+        stream.start();
+        LOG.info("Starter konsumering av topic={}, tilstand={}", getTopicName(), stream.state());
+    }
+
+    private String getTopicName() {
+        return topicName;
+    }
+
+    @Override
+    public boolean isAlive() {
+        return true; //(stream != null) && stream.state().isRunningOrRebalancing();
+    }
+
+    @Override
+    public boolean isReady() {
+        return isAlive();
+    }
+
+    @Override
+    public void stop() {
+        LOG.info("Starter shutdown av topic={}, tilstand={} med 15 sekunder timeout", getTopicName(), stream.state());
+        stream.close(Duration.ofSeconds(15));
+        LOG.info("Shutdown av topic={}, tilstand={} med 15 sekunder timeout", getTopicName(), stream.state());
+    }
+}
---
+++ domenetjenester/mottak/src/test/java/no/nav/foreldrepenger/mottak/kabal/KabelHendelseTest.java
@@ -0,0 +1,43 @@
+package no.nav.foreldrepenger.mottak.kabal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import no.nav.foreldrepenger.behandlingslager.kodeverk.Fagsystem;
+import no.nav.foreldrepenger.domene.json.StandardJsonConfig;
+
+public class KabelHendelseTest  {
+
+    @Test
+    public void kabalJsonParse() throws IOException {
+        var payload = """
+                {
+                    "eventId": "b79cae6f-4afc-4f51-9e0b-623bb53f1805",
+                    "kildeReferanse": "23da11e6-8130-4edc-acee-2eb2a5fd4d97",
+                    "kilde": "FS36",
+                    "type": "KLAGEBEHANDLING_AVSLUTTET",
+                    "detaljer": {
+                       "klagebehandlingAvsluttet": {
+                          "utfall": "MEDHOLD",
+                          "journalpostReferanser": [
+                             "510857598"
+                          ]
+                       }
+                    },
+                    "kabalReferanse": "af4204bb-7f2c-4354-8af9-b279a5492104"
+                }
+            """;
+
+        var hendelse = StandardJsonConfig.fromJson(payload, KabalHendelse.class);
+
+        assertThat(hendelse.kilde()).isEqualTo(Fagsystem.FPSAK.getOffisiellKode());
+        assertThat(hendelse.type()).isEqualTo(KabalHendelse.BehandlingEventType.KLAGEBEHANDLING_AVSLUTTET);
+        assertThat(hendelse.detaljer().klagebehandlingAvsluttet().utfall()).isEqualTo(KabalUtfall.MEDHOLD);
+        assertThat(hendelse.detaljer().klagebehandlingAvsluttet().journalpostReferanser().get(0)).isEqualTo("510857598");
+
+    }
+
+}
