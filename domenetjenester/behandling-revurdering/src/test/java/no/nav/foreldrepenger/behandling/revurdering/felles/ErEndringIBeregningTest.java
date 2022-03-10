62876328238196c41f9a66452a436d067dbb7ff5
Petter Ekern <petter.ekern@nav.no>
2022-03-10 15:25:25 +0100

    Skriver om ErEndringIBeregningTest til å ikke trenge database

--- domenetjenester/behandling-revurdering/src/test/java/no/nav/foreldrepenger/behandling/revurdering/felles/ErEndringIBeregningTest.java
+++ domenetjenester/behandling-revurdering/src/test/java/no/nav/foreldrepenger/behandling/revurdering/felles/ErEndringIBeregningTest.java
@@ -1,112 +1,50 @@
 package no.nav.foreldrepenger.behandling.revurdering.felles;
 
+import static no.nav.foreldrepenger.behandling.revurdering.ytelse.fp.RevurderingBehandlingsresultatutlederTest.ARBEIDSFORHOLDLISTE;
+import static no.nav.foreldrepenger.behandling.revurdering.ytelse.fp.RevurderingBehandlingsresultatutlederTest.ORGNR;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Optional;
 
-import javax.inject.Inject;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-
-import no.nav.foreldrepenger.behandling.revurdering.BeregningRevurderingTestUtil;
-import no.nav.foreldrepenger.behandling.revurdering.RevurderingEndring;
-import no.nav.foreldrepenger.behandling.revurdering.RevurderingTjeneste;
-import no.nav.foreldrepenger.behandling.revurdering.RevurderingTjenesteFelles;
-import no.nav.foreldrepenger.behandling.revurdering.ytelse.fp.RevurderingTjenesteImpl;
-import no.nav.foreldrepenger.behandlingskontroll.FagsakYtelseTypeRef;
-import no.nav.foreldrepenger.behandlingskontroll.impl.BehandlingskontrollTjenesteImpl;
-import no.nav.foreldrepenger.behandlingskontroll.spi.BehandlingskontrollServiceProvider;
-import no.nav.foreldrepenger.behandlingslager.aktør.OrganisasjonsEnhet;
-import no.nav.foreldrepenger.behandlingslager.behandling.Behandling;
-import no.nav.foreldrepenger.behandlingslager.behandling.BehandlingStegType;
-import no.nav.foreldrepenger.behandlingslager.behandling.BehandlingÅrsakType;
-import no.nav.foreldrepenger.behandlingslager.behandling.aksjonspunkt.AksjonspunktDefinisjon;
-import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingGrunnlagRepositoryProvider;
-import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepositoryProvider;
-import no.nav.foreldrepenger.behandlingslager.behandling.vedtak.VedtakResultatType;
-import no.nav.foreldrepenger.behandlingslager.behandling.verge.VergeRepository;
-import no.nav.foreldrepenger.behandlingslager.testutilities.behandling.ScenarioMorSøkerForeldrepenger;
-import no.nav.foreldrepenger.dbstoette.CdiDbAwareTest;
-import no.nav.foreldrepenger.domene.arbeidsforhold.InntektArbeidYtelseTjeneste;
+import no.nav.foreldrepenger.behandlingslager.virksomhet.Arbeidsgiver;
+import no.nav.foreldrepenger.domene.modell.AktivitetStatus;
+import no.nav.foreldrepenger.domene.modell.BGAndelArbeidsforhold;
+import no.nav.foreldrepenger.domene.modell.BeregningsgrunnlagAktivitetStatus;
 import no.nav.foreldrepenger.domene.modell.BeregningsgrunnlagEntitet;
-import no.nav.foreldrepenger.domene.modell.BeregningsgrunnlagRepository;
-import no.nav.foreldrepenger.domene.modell.BeregningsgrunnlagTilstand;
-import no.nav.foreldrepenger.domene.tid.ÅpenDatoIntervallEntitet;
-import no.nav.foreldrepenger.domene.uttak.uttaksgrunnlag.fp.EndringsdatoRevurderingUtlederImpl;
+import no.nav.foreldrepenger.domene.modell.BeregningsgrunnlagPeriode;
+import no.nav.foreldrepenger.domene.modell.BeregningsgrunnlagPrStatusOgAndel;
+import no.nav.vedtak.konfig.Tid;
 
-@CdiDbAwareTest
 public class ErEndringIBeregningTest {
     private static final LocalDate SKJÆRINGSTIDSPUNKT_BEREGNING = LocalDate.now();
 
-    @Inject
-    private BehandlingskontrollServiceProvider serviceProvider;
-
-    @Inject
-    private InntektArbeidYtelseTjeneste iayTjeneste;
-    @Inject
-    private BeregningRevurderingTestUtil revurderingTestUtil;
-    @Inject
-    @FagsakYtelseTypeRef("FP")
-    private RevurderingEndring revurderingEndring;
-
-    @Inject
-    private VergeRepository vergeRepository;
-
-    @Inject
-    private BeregningsgrunnlagRepository beregningsgrunnlagRepository;
-    @Inject
-    private BehandlingGrunnlagRepositoryProvider grunnlagRepositoryProvider;
-
-    @Inject
-    private BehandlingRepositoryProvider repositoryProvider;
-    @Mock
-    private EndringsdatoRevurderingUtlederImpl endringsdatoRevurderingUtlederImpl;
-    private Behandling behandlingSomSkalRevurderes;
-    private Behandling revurdering;
-
-    @BeforeEach
-    public void setUp() {
-        var scenario = ScenarioMorSøkerForeldrepenger.forFødsel();
-        scenario.leggTilAksjonspunkt(AksjonspunktDefinisjon.AVKLAR_TERMINBEKREFTELSE,
-                BehandlingStegType.KONTROLLER_FAKTA);
-        scenario.medBehandlingVedtak()
-                .medVedtakstidspunkt(LocalDateTime.now())
-                .medVedtakResultatType(VedtakResultatType.INNVILGET);
-
-        behandlingSomSkalRevurderes = scenario.lagre(repositoryProvider);
-        repositoryProvider.getOpptjeningRepository()
-                .lagreOpptjeningsperiode(behandlingSomSkalRevurderes, LocalDate.now().minusYears(1), LocalDate.now(),
-                        false);
-        revurderingTestUtil.avsluttBehandling(behandlingSomSkalRevurderes);
-
-        var behandlingskontrollTjeneste = new BehandlingskontrollTjenesteImpl(serviceProvider);
-        var revurderingTjenesteFelles = new RevurderingTjenesteFelles(repositoryProvider);
-        RevurderingTjeneste revurderingTjeneste = new RevurderingTjenesteImpl(repositoryProvider,
-                grunnlagRepositoryProvider, behandlingskontrollTjeneste,
-                iayTjeneste, revurderingEndring, revurderingTjenesteFelles, vergeRepository);
-        revurdering = revurderingTjeneste
-                .opprettAutomatiskRevurdering(behandlingSomSkalRevurderes.getFagsak(),
-                        BehandlingÅrsakType.RE_HENDELSE_FØDSEL, new OrganisasjonsEnhet("1234", "Test"));
-    }
-
     @Test
-    public void skal_gi_ingen_endring_i_beregningsgrunnlag_ved_lik_dagsats_på_periodenoivå() {
+    public void skal_gi_ingen_endring_i_beregningsgrunnlag_ved_like_grunnlag() {
         // Arrange
-        var bgPeriode = Collections.singletonList(
-                ÅpenDatoIntervallEntitet.fraOgMedTilOgMed(SKJÆRINGSTIDSPUNKT_BEREGNING, null));
-        var originalGrunnlag = byggBeregningsgrunnlagForBehandling(behandlingSomSkalRevurderes,
-                false, true, bgPeriode);
-        var revurderingGrunnlag = byggBeregningsgrunnlagForBehandling(revurdering, false, true,
-                bgPeriode);
+        // Originalgrunnlag
+        var bgOrg = byggBeregningsgrunnlag(SKJÆRINGSTIDSPUNKT_BEREGNING, AktivitetStatus.ARBEIDSTAKER);
+        var p1Org = byggBGPeriode(bgOrg, SKJÆRINGSTIDSPUNKT_BEREGNING, etterSTP(50));
+        var p2Org = byggBGPeriode(bgOrg, etterSTP(51), Tid.TIDENES_ENDE);
+        byggAndel(p1Org, AktivitetStatus.ARBEIDSTAKER, 1500, 0);
+        byggAndel(p2Org, AktivitetStatus.ARBEIDSTAKER, 1500, 0);
+        BeregningsgrunnlagPeriode.oppdater(p1Org).build(bgOrg);
+        BeregningsgrunnlagPeriode.oppdater(p2Org).build(bgOrg);
+
+        // Revurderingsgrunnlag
+        var bgRev = byggBeregningsgrunnlag(SKJÆRINGSTIDSPUNKT_BEREGNING, AktivitetStatus.ARBEIDSTAKER);
+        var p1Rev = BeregningsgrunnlagPeriode.oppdater(byggBGPeriode(bgRev, SKJÆRINGSTIDSPUNKT_BEREGNING, etterSTP(50))).build(bgRev);
+        var p2Rev = BeregningsgrunnlagPeriode.oppdater(byggBGPeriode(bgRev, etterSTP(51), Tid.TIDENES_ENDE)).build(bgRev);
+        byggAndel(p1Rev, AktivitetStatus.ARBEIDSTAKER, 1500, 0);
+        byggAndel(p2Rev, AktivitetStatus.ARBEIDSTAKER, 1500, 0);
+        BeregningsgrunnlagPeriode.oppdater(p1Rev).build(bgRev);
+        BeregningsgrunnlagPeriode.oppdater(p2Rev).build(bgRev);
 
         // Act
-        var endring = ErEndringIBeregning.vurder(Optional.of(revurderingGrunnlag), Optional.of(originalGrunnlag));
+        var endring = ErEndringIBeregning.vurder(Optional.of(bgRev), Optional.of(bgOrg));
 
         // Assert
         assertThat(endring).isFalse();
@@ -115,13 +53,17 @@ public class ErEndringIBeregningTest {
     @Test
     public void skal_gi_endring_når_vi_mangler_beregningsgrunnlag_på_en_av_behandlingene() {
         // Arrange
-        var bgPeriode = Collections.singletonList(
-                ÅpenDatoIntervallEntitet.fraOgMedTilOgMed(SKJÆRINGSTIDSPUNKT_BEREGNING, null));
-        var revurderingGrunnlag = byggBeregningsgrunnlagForBehandling(revurdering, false, true,
-                bgPeriode);
+        // Revurderingsgrunnlag
+        var bgRev = byggBeregningsgrunnlag(SKJÆRINGSTIDSPUNKT_BEREGNING, AktivitetStatus.ARBEIDSTAKER);
+        var p1Rev = BeregningsgrunnlagPeriode.oppdater(byggBGPeriode(bgRev, SKJÆRINGSTIDSPUNKT_BEREGNING, etterSTP(50))).build(bgRev);
+        var p2Rev = BeregningsgrunnlagPeriode.oppdater(byggBGPeriode(bgRev, etterSTP(51), Tid.TIDENES_ENDE)).build(bgRev);
+        byggAndel(p1Rev, AktivitetStatus.ARBEIDSTAKER, 1500, 0);
+        byggAndel(p2Rev, AktivitetStatus.ARBEIDSTAKER, 1500, 0);
+        BeregningsgrunnlagPeriode.oppdater(p1Rev).build(bgRev);
+        BeregningsgrunnlagPeriode.oppdater(p2Rev).build(bgRev);
 
         // Act
-        var endring = ErEndringIBeregning.vurder(Optional.of(revurderingGrunnlag), Optional.empty());
+        var endring = ErEndringIBeregning.vurder(Optional.of(bgRev), Optional.empty());
 
         // Assert
         assertThat(endring).isTrue();
@@ -139,21 +81,26 @@ public class ErEndringIBeregningTest {
     @Test
     public void skal_gi_ingen_endring_når_vi_har_like_mange_perioder_med_med_forskjellige_fom_og_tom() {
         // Arrange
-        var bgPerioderNyttGrunnlag = List.of(
-                ÅpenDatoIntervallEntitet.fraOgMedTilOgMed(SKJÆRINGSTIDSPUNKT_BEREGNING,
-                        SKJÆRINGSTIDSPUNKT_BEREGNING.plusDays(35)),
-                ÅpenDatoIntervallEntitet.fraOgMedTilOgMed(SKJÆRINGSTIDSPUNKT_BEREGNING.plusDays(36), null));
-        var bgPerioderOriginaltGrunnlag = List.of(
-                ÅpenDatoIntervallEntitet.fraOgMedTilOgMed(SKJÆRINGSTIDSPUNKT_BEREGNING,
-                        SKJÆRINGSTIDSPUNKT_BEREGNING.plusDays(40)),
-                ÅpenDatoIntervallEntitet.fraOgMedTilOgMed(SKJÆRINGSTIDSPUNKT_BEREGNING.plusDays(41), null));
-        var originalGrunnlag = byggBeregningsgrunnlagForBehandling(behandlingSomSkalRevurderes,
-                false, true, bgPerioderOriginaltGrunnlag);
-        var revurderingGrunnlag = byggBeregningsgrunnlagForBehandling(revurdering, false, true,
-                bgPerioderNyttGrunnlag);
+        // Originalgrunnlag
+        var bgOrg = byggBeregningsgrunnlag(SKJÆRINGSTIDSPUNKT_BEREGNING, AktivitetStatus.ARBEIDSTAKER);
+        var p1Org = byggBGPeriode(bgOrg, SKJÆRINGSTIDSPUNKT_BEREGNING, etterSTP(50));
+        var p2Org = byggBGPeriode(bgOrg, etterSTP(51), Tid.TIDENES_ENDE);
+        byggAndel(p1Org, AktivitetStatus.ARBEIDSTAKER, 1500, 500);
+        byggAndel(p2Org, AktivitetStatus.ARBEIDSTAKER, 1500, 500);
+        BeregningsgrunnlagPeriode.oppdater(p1Org).build(bgOrg);
+        BeregningsgrunnlagPeriode.oppdater(p2Org).build(bgOrg);
+
+        // Revurderingsgrunnlag
+        var bgRev = byggBeregningsgrunnlag(SKJÆRINGSTIDSPUNKT_BEREGNING, AktivitetStatus.ARBEIDSTAKER);
+        var p1Rev = BeregningsgrunnlagPeriode.oppdater(byggBGPeriode(bgRev, SKJÆRINGSTIDSPUNKT_BEREGNING, etterSTP(30))).build(bgRev);
+        var p2Rev = BeregningsgrunnlagPeriode.oppdater(byggBGPeriode(bgRev, etterSTP(31), Tid.TIDENES_ENDE)).build(bgRev);
+        byggAndel(p1Rev, AktivitetStatus.ARBEIDSTAKER, 1500, 500);
+        byggAndel(p2Rev, AktivitetStatus.ARBEIDSTAKER, 1500, 500);
+        BeregningsgrunnlagPeriode.oppdater(p1Rev).build(bgRev);
+        BeregningsgrunnlagPeriode.oppdater(p2Rev).build(bgRev);
 
         // Act
-        var endring = ErEndringIBeregning.vurder(Optional.of(revurderingGrunnlag), Optional.of(originalGrunnlag));
+        var endring = ErEndringIBeregning.vurder(Optional.of(bgRev), Optional.of(bgOrg));
 
         // Assert
         assertThat(endring).isFalse();
@@ -162,60 +109,95 @@ public class ErEndringIBeregningTest {
     @Test
     public void skal_gi_ingen_endring_når_vi_har_like_mange_perioder_med_forskjellig_startdato() {
         // Arrange
-        var bgPerioderNyttGrunnlag = List.of(
-                ÅpenDatoIntervallEntitet.fraOgMedTilOgMed(SKJÆRINGSTIDSPUNKT_BEREGNING.minusDays(1),
-                        SKJÆRINGSTIDSPUNKT_BEREGNING.plusDays(35)),
-                ÅpenDatoIntervallEntitet.fraOgMedTilOgMed(SKJÆRINGSTIDSPUNKT_BEREGNING.plusDays(36), null));
-        var bgPerioderOriginaltGrunnlag = List.of(
-                ÅpenDatoIntervallEntitet.fraOgMedTilOgMed(SKJÆRINGSTIDSPUNKT_BEREGNING,
-                        SKJÆRINGSTIDSPUNKT_BEREGNING.plusDays(40)),
-                ÅpenDatoIntervallEntitet.fraOgMedTilOgMed(SKJÆRINGSTIDSPUNKT_BEREGNING.plusDays(41), null));
-        var originalGrunnlag = byggBeregningsgrunnlagForBehandling(behandlingSomSkalRevurderes,
-                false, true, bgPerioderOriginaltGrunnlag);
-        var revurderingGrunnlag = byggBeregningsgrunnlagForBehandling(revurdering, false, true,
-                bgPerioderNyttGrunnlag);
+        var bgOrg = byggBeregningsgrunnlag(SKJÆRINGSTIDSPUNKT_BEREGNING, AktivitetStatus.ARBEIDSTAKER);
+        var p1Org = byggBGPeriode(bgOrg, SKJÆRINGSTIDSPUNKT_BEREGNING, etterSTP(50));
+        var p2Org = byggBGPeriode(bgOrg, etterSTP(51), Tid.TIDENES_ENDE);
+        byggAndel(p1Org, AktivitetStatus.ARBEIDSTAKER, 1500, 500);
+        byggAndel(p2Org, AktivitetStatus.ARBEIDSTAKER, 1500, 500);
+        BeregningsgrunnlagPeriode.oppdater(p1Org).build(bgOrg);
+        BeregningsgrunnlagPeriode.oppdater(p2Org).build(bgOrg);
+
+        // Revurderingsgrunnlag
+        var bgRev = byggBeregningsgrunnlag(SKJÆRINGSTIDSPUNKT_BEREGNING.minusDays(10), AktivitetStatus.ARBEIDSTAKER);
+        var p1Rev = BeregningsgrunnlagPeriode.oppdater(byggBGPeriode(bgRev, SKJÆRINGSTIDSPUNKT_BEREGNING.minusDays(10), etterSTP(30))).build(bgRev);
+        var p2Rev = BeregningsgrunnlagPeriode.oppdater(byggBGPeriode(bgRev, etterSTP(31), Tid.TIDENES_ENDE)).build(bgRev);
+        byggAndel(p1Rev, AktivitetStatus.ARBEIDSTAKER, 1500, 500);
+        byggAndel(p2Rev, AktivitetStatus.ARBEIDSTAKER, 1500, 500);
+        BeregningsgrunnlagPeriode.oppdater(p1Rev).build(bgRev);
+        BeregningsgrunnlagPeriode.oppdater(p2Rev).build(bgRev);
 
         // Act
-        var endring = ErEndringIBeregning.vurder(Optional.of(revurderingGrunnlag), Optional.of(originalGrunnlag));
+        var endring = ErEndringIBeregning.vurder(Optional.of(bgRev), Optional.of(bgOrg));
 
         // Assert
         assertThat(endring).isFalse();
     }
 
     @Test
-    public void skal_gi_endring_i_beregningsgrunnlag_ved_ulik_dagsats_på_periodenoivå() {
+    public void skal_gi_endring_i_beregningsgrunnlag_ved_ulik_dagsats_på_periodenivå() {
         // Arrange
-        var bgPeriode = Collections.singletonList(
-                ÅpenDatoIntervallEntitet.fraOgMedTilOgMed(SKJÆRINGSTIDSPUNKT_BEREGNING, null));
-        var originalGrunnlag = byggBeregningsgrunnlagForBehandling(behandlingSomSkalRevurderes,
-                false, true, bgPeriode);
-        var revurderingGrunnlag = byggBeregningsgrunnlagForBehandling(revurdering, true, true,
-                bgPeriode);
+        // Originalgrunnlag
+        var bgOrg = byggBeregningsgrunnlag(SKJÆRINGSTIDSPUNKT_BEREGNING, AktivitetStatus.ARBEIDSTAKER);
+        var p1Org = byggBGPeriode(bgOrg, SKJÆRINGSTIDSPUNKT_BEREGNING, etterSTP(50));
+        var p2Org = byggBGPeriode(bgOrg, etterSTP(51), Tid.TIDENES_ENDE);
+        byggAndel(p1Org, AktivitetStatus.ARBEIDSTAKER, 1500, 300);
+        byggAndel(p2Org, AktivitetStatus.ARBEIDSTAKER, 1500, 300);
+        BeregningsgrunnlagPeriode.oppdater(p1Org).build(bgOrg);
+        BeregningsgrunnlagPeriode.oppdater(p2Org).build(bgOrg);
+
+        // Revurderingsgrunnlag
+        var bgRev = byggBeregningsgrunnlag(SKJÆRINGSTIDSPUNKT_BEREGNING, AktivitetStatus.ARBEIDSTAKER);
+        var p1Rev = BeregningsgrunnlagPeriode.oppdater(byggBGPeriode(bgRev, SKJÆRINGSTIDSPUNKT_BEREGNING, etterSTP(50))).build(bgRev);
+        var p2Rev = BeregningsgrunnlagPeriode.oppdater(byggBGPeriode(bgRev, etterSTP(51), Tid.TIDENES_ENDE)).build(bgRev);
+        byggAndel(p1Rev, AktivitetStatus.ARBEIDSTAKER, 1500, 299);
+        byggAndel(p2Rev, AktivitetStatus.ARBEIDSTAKER, 1500, 299);
+        BeregningsgrunnlagPeriode.oppdater(p1Rev).build(bgRev);
+        BeregningsgrunnlagPeriode.oppdater(p2Rev).build(bgRev);
 
         // Act
-        var endring = ErEndringIBeregning.vurder(Optional.of(revurderingGrunnlag), Optional.of(originalGrunnlag));
+        var endring = ErEndringIBeregning.vurder(Optional.of(bgRev), Optional.of(bgOrg));
 
         // Assert
         assertThat(endring).isTrue();
     }
 
-    private BeregningsgrunnlagEntitet byggBeregningsgrunnlagForBehandling(Behandling behandling,
-            boolean medOppjustertDagsat,
-            boolean skalDeleAndelMellomArbeidsgiverOgBruker,
-            List<ÅpenDatoIntervallEntitet> perioder) {
-        return byggBeregningsgrunnlagForBehandling(behandling, medOppjustertDagsat,
-                skalDeleAndelMellomArbeidsgiverOgBruker, perioder, new LagEnAndelTjeneste());
+    private LocalDate etterSTP(int dagerEtter) {
+        return SKJÆRINGSTIDSPUNKT_BEREGNING.plusDays(dagerEtter);
     }
 
-    private BeregningsgrunnlagEntitet byggBeregningsgrunnlagForBehandling(Behandling behandling,
-            boolean medOppjustertDagsat,
-            boolean skalDeleAndelMellomArbeidsgiverOgBruker,
-            List<ÅpenDatoIntervallEntitet> perioder,
-            LagAndelTjeneste lagAndelTjeneste) {
-        var beregningsgrunnlag = LagBeregningsgrunnlagTjeneste.lagBeregningsgrunnlag(
-                SKJÆRINGSTIDSPUNKT_BEREGNING, medOppjustertDagsat, skalDeleAndelMellomArbeidsgiverOgBruker, perioder,
-                lagAndelTjeneste);
-        beregningsgrunnlagRepository.lagre(behandling.getId(), beregningsgrunnlag, BeregningsgrunnlagTilstand.FASTSATT);
+    private void byggAndel(BeregningsgrunnlagPeriode periode, AktivitetStatus aktivitetStatus, int dagsatsBruker, int dagsatsAG) {
+        var andelBuilder = BeregningsgrunnlagPrStatusOgAndel.builder()
+            .medAktivitetStatus(aktivitetStatus)
+            .medBeregnetPrÅr(BigDecimal.valueOf(240000))
+            .medRedusertBrukersAndelPrÅr(BigDecimal.valueOf(dagsatsBruker * 260L));
+        if (aktivitetStatus.erArbeidstaker()) {
+            var bga = BGAndelArbeidsforhold
+                .builder()
+                .medArbeidsgiver(Arbeidsgiver.virksomhet(ORGNR))
+                .medArbeidsforholdRef(ARBEIDSFORHOLDLISTE.get(0))
+                .medArbeidsperiodeFom(LocalDate.now().minusYears(1))
+                .medArbeidsperiodeTom(LocalDate.now().plusYears(2));
+            andelBuilder
+                .medBGAndelArbeidsforhold(bga)
+                .medRedusertRefusjonPrÅr(BigDecimal.valueOf(dagsatsAG * 260L));
+        }
+        andelBuilder.build(periode);
+    }
+
+    public static BeregningsgrunnlagEntitet byggBeregningsgrunnlag(LocalDate skjæringstidspunktBeregning, AktivitetStatus... statuser) {
+        var beregningsgrunnlag = BeregningsgrunnlagEntitet.ny()
+            .medSkjæringstidspunkt(skjæringstidspunktBeregning)
+            .medGrunnbeløp(BigDecimal.valueOf(91425L))
+            .build();
+        Arrays.asList(statuser).forEach(status -> BeregningsgrunnlagAktivitetStatus.builder()
+            .medAktivitetStatus(status)
+            .build(beregningsgrunnlag));
         return beregningsgrunnlag;
+    }
+
+    private static BeregningsgrunnlagPeriode byggBGPeriode(BeregningsgrunnlagEntitet bg, LocalDate fom, LocalDate tom) {
+        return BeregningsgrunnlagPeriode.ny()
+            .medBeregningsgrunnlagPeriode(fom, tom)
+            .build(bg);
     }
 }
