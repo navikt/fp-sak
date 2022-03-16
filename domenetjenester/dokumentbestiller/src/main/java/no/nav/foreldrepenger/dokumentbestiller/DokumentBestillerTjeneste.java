370787ac121c8223ec92a5a616a893ae6ebc0be3
Michal J. Sladek <mrsladek@users.noreply.github.com>
2022-03-16 18:58:04 +0100

    TFP-4708: Fjerner brev bestilling gjennom kafka. (#3893)

--- domenetjenester/dokumentbestiller/src/main/java/no/nav/foreldrepenger/dokumentbestiller/DokumentBestillerTjeneste.java
+++ domenetjenester/dokumentbestiller/src/main/java/no/nav/foreldrepenger/dokumentbestiller/DokumentBestillerTjeneste.java
@@ -1,5 +1,10 @@
 package no.nav.foreldrepenger.dokumentbestiller;
 
+import static no.nav.foreldrepenger.dokumentbestiller.vedtak.VedtaksbrevUtleder.velgDokumentMalForVedtak;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
 import no.nav.foreldrepenger.behandlingslager.behandling.anke.AnkeRepository;
 import no.nav.foreldrepenger.behandlingslager.behandling.historikk.HistorikkAktør;
 import no.nav.foreldrepenger.behandlingslager.behandling.klage.KlageRepository;
@@ -8,13 +13,6 @@ import no.nav.foreldrepenger.behandlingslager.behandling.vedtak.BehandlingVedtak
 import no.nav.foreldrepenger.behandlingslager.behandling.vedtak.Vedtaksbrev;
 import no.nav.foreldrepenger.dokumentbestiller.dto.BestillBrevDto;
 import no.nav.foreldrepenger.dokumentbestiller.formidling.DokumentBestiller;
-import no.nav.foreldrepenger.dokumentbestiller.kafka.DokumentKafkaBestiller;
-import no.nav.foreldrepenger.konfig.Environment;
-
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-
-import static no.nav.foreldrepenger.dokumentbestiller.vedtak.VedtaksbrevUtleder.velgDokumentMalForVedtak;
 
 @ApplicationScoped
 public class DokumentBestillerTjeneste {
@@ -24,7 +22,6 @@ public class DokumentBestillerTjeneste {
     private AnkeRepository ankeRepository;
 
     private BrevHistorikkinnslag brevHistorikkinnslag;
-    private DokumentKafkaBestiller dokumentKafkaBestiller;
     private DokumentBestiller dokumentBestiller;
 
     public DokumentBestillerTjeneste() {
@@ -36,13 +33,11 @@ public class DokumentBestillerTjeneste {
                                      KlageRepository klageRepository,
                                      AnkeRepository ankeRepository,
                                      BrevHistorikkinnslag brevHistorikkinnslag,
-                                     DokumentKafkaBestiller dokumentKafkaBestiller,
                                      DokumentBestiller dokumentBestiller) {
         this.behandlingRepository = behandlingRepository;
         this.klageRepository = klageRepository;
         this.ankeRepository = ankeRepository;
         this.brevHistorikkinnslag = brevHistorikkinnslag;
-        this.dokumentKafkaBestiller = dokumentKafkaBestiller;
         this.dokumentBestiller = dokumentBestiller;
     }
 
@@ -54,8 +49,7 @@ public class DokumentBestillerTjeneste {
         }
 
         var behandling = behandlingRepository.hentBehandling(behandlingsresultat.getBehandlingId());
-        var dokumentMal = velgDokumentMalForVedtak(behandling, behandlingsresultat, behandlingVedtak,
-            klageRepository, ankeRepository);
+        var dokumentMal = velgDokumentMalForVedtak(behandling, behandlingsresultat, behandlingVedtak, klageRepository, ankeRepository);
 
         dokumentBestiller.bestillBrev(behandling, dokumentMal, null, null, HistorikkAktør.VEDTAKSLØSNINGEN);
     }
@@ -66,10 +60,9 @@ public class DokumentBestillerTjeneste {
 
     public void bestillDokument(BestillBrevDto bestillBrevDto, HistorikkAktør aktør, boolean manueltBrev) {
         if (manueltBrev) {
-            var behandling = bestillBrevDto.getBehandlingUuid() == null ? behandlingRepository.hentBehandling(bestillBrevDto.getBehandlingId())
-                : behandlingRepository.hentBehandling(bestillBrevDto.getBehandlingUuid());
-            brevHistorikkinnslag.opprettHistorikkinnslagForManueltBestiltBrev(aktør, behandling,
-                bestillBrevDto.getBrevmalkode());
+            var behandling = bestillBrevDto.getBehandlingUuid() == null ? behandlingRepository.hentBehandling(
+                bestillBrevDto.getBehandlingId()) : behandlingRepository.hentBehandling(bestillBrevDto.getBehandlingUuid());
+            brevHistorikkinnslag.opprettHistorikkinnslagForManueltBestiltBrev(aktør, behandling, bestillBrevDto.getBrevmalkode());
         }
 
         dokumentBestiller.bestillBrev(bestillBrevDto, aktør);
