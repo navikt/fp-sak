44cd9dfa31214dc4dcf94a786898c33bf3d6210b
Michal J. Sladek <mrsladek@users.noreply.github.com>
2022-04-08 12:01:08 +0200

    Fix: Bruker text block i testen til å unngå å hardkode nl\cr manuelt. (#4050)
    * Fix: Bruker text block i testen til å ungå å hardkode nl\cr manuelt.
    * Disabler test på Windows maskiner

--- domenetjenester/dokumentbestiller/src/test/java/no/nav/foreldrepenger/dokumentbestiller/dto/BestillBrevDtoTest.java
+++ domenetjenester/dokumentbestiller/src/test/java/no/nav/foreldrepenger/dokumentbestiller/dto/BestillBrevDtoTest.java
@@ -1,10 +1,12 @@
 package no.nav.foreldrepenger.dokumentbestiller.dto;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.condition.OS.WINDOWS;
 
 import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
 
 import no.nav.foreldrepenger.behandlingslager.behandling.RevurderingVarslingÅrsak;
 import no.nav.foreldrepenger.dokumentbestiller.DokumentMalType;
@@ -14,27 +16,38 @@ class BestillBrevDtoTest {
 
     @Test
     void deserialize() {
-        var dokumentMalType = DokumentMalType.FRITEKSTBREV;
+        var dokumentMal = DokumentMalType.FRITEKSTBREV;
         var årsak = RevurderingVarslingÅrsak.BARN_IKKE_REGISTRERT_FOLKEREGISTER;
 
-        String json = "{\"brevmalkode\":\"" + dokumentMalType.getKode() + "\",\"navn\":\"sdf\",\"arsakskode\":\"" + årsak.getKode() + "\"}";
+        String json = """
+        {
+            "brevmalkode":"%s",
+            "navn":"sdf",
+            "arsakskode":"%s"
+        }""".formatted(dokumentMal.getKode(), årsak.getKode());
 
         BestillBrevDto bean = StandardJsonConfig.fromJson(json, BestillBrevDto.class);
-        assertEquals(dokumentMalType, bean.getBrevmalkode());
+        assertEquals(dokumentMal, bean.getBrevmalkode());
         assertEquals(årsak, bean.getÅrsakskode());
     }
 
     @Test
+    @DisabledOnOs(WINDOWS)
     void serialize() {
         var behandlingId = 12L;
         var uuid = "5ffbf59b-76d5-4c78-bd27-6c84e0d445a3";
-        var dokumentMalType = DokumentMalType.FRITEKSTBREV;
+        var dokumentMal = DokumentMalType.FRITEKSTBREV;
         var arsak = RevurderingVarslingÅrsak.BARN_IKKE_REGISTRERT_FOLKEREGISTER;
 
-        String expected = "{\n" + "  \"behandlingId\" : " + behandlingId + ",\n" + "  \"behandlingUuid\" : \"" + uuid +"\",\n"
-            + "  \"brevmalkode\" : \"" + dokumentMalType.getKode() + "\",\n" + "  \"arsakskode\" : \"" + arsak.getKode() + "\"\n" + "}";
+        String expected = """
+                             {
+                               "behandlingId" : %s,
+                               "behandlingUuid" : "%s",
+                               "brevmalkode" : "%s",
+                               "arsakskode" : "%s"
+                             }""".formatted(behandlingId, uuid, dokumentMal.getKode(), arsak.getKode());
 
-        var brev = new BestillBrevDto(behandlingId, UUID.fromString(uuid), dokumentMalType, null, arsak);
+        var brev = new BestillBrevDto(behandlingId, UUID.fromString(uuid), dokumentMal, null, arsak);
         var serialized = StandardJsonConfig.toJson(brev);
         assertEquals(expected, serialized);
     }
