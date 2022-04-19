fbf804fc8e376791451cecbf13f96646e32c2444
Michal J. Sladek <michal.sladek@nav.no>
2022-04-19 13:11:20 +0200

    Fikser test på windows platform

--- domenetjenester/dokumentbestiller/src/test/java/no/nav/foreldrepenger/dokumentbestiller/dto/BestillBrevDtoTest.java
+++ domenetjenester/dokumentbestiller/src/test/java/no/nav/foreldrepenger/dokumentbestiller/dto/BestillBrevDtoTest.java
@@ -1,6 +1,7 @@
 package no.nav.foreldrepenger.dokumentbestiller.dto;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.condition.OS.WINDOWS;
 
 import java.util.UUID;
@@ -32,24 +33,25 @@ class BestillBrevDtoTest {
     }
 
     @Test
-    @DisabledOnOs(WINDOWS)
     void serialize() {
         var behandlingId = 12L;
         var uuid = "5ffbf59b-76d5-4c78-bd27-6c84e0d445a3";
         var dokumentMal = DokumentMalType.FRITEKSTBREV;
         var arsak = RevurderingVarslingÅrsak.BARN_IKKE_REGISTRERT_FOLKEREGISTER;
 
-        String expected = """
-                             {
-                               "behandlingId" : %s,
-                               "behandlingUuid" : "%s",
-                               "brevmalkode" : "%s",
-                               "arsakskode" : "%s"
-                             }""".formatted(behandlingId, uuid, dokumentMal.getKode(), arsak.getKode());
-
         var brev = new BestillBrevDto(behandlingId, UUID.fromString(uuid), dokumentMal, null, arsak);
-        var serialized = StandardJsonConfig.toJson(brev);
-        assertEquals(expected, serialized);
+        var etterRoundtrip = StandardJsonConfig.fromJson(StandardJsonConfig.toJson(brev), BestillBrevDto.class);
+
+        assertEquals(brev.getBehandlingId(), etterRoundtrip.getBehandlingId());
+        assertEquals(behandlingId, etterRoundtrip.getBehandlingId());
+        assertEquals(brev.getBrevmalkode(), etterRoundtrip.getBrevmalkode());
+        assertEquals(dokumentMal, brev.getBrevmalkode());
+        assertEquals(brev.getÅrsakskode(), etterRoundtrip.getÅrsakskode());
+        assertEquals(arsak, etterRoundtrip.getÅrsakskode());
+        assertEquals(brev.getFritekst(), etterRoundtrip.getFritekst());
+        assertNull(etterRoundtrip.getFritekst());
+        assertEquals(brev.getBehandlingUuid(), etterRoundtrip.getBehandlingUuid());
+        assertEquals(uuid, etterRoundtrip.getBehandlingUuid().toString());
     }
 
 }
