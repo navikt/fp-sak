b2498bc48a9e2e65900aa382a4658234fe953551
Michal J. Sladek <michal.sladek@nav.no>
2022-04-19 15:30:04 +0200

    Samordne testene.

--- domenetjenester/dokumentbestiller/src/test/java/no/nav/foreldrepenger/dokumentbestiller/dto/BestillBrevDtoTest.java
+++ domenetjenester/dokumentbestiller/src/test/java/no/nav/foreldrepenger/dokumentbestiller/dto/BestillBrevDtoTest.java
@@ -14,30 +14,14 @@ import no.nav.foreldrepenger.domene.json.StandardJsonConfig;
 class BestillBrevDtoTest {
 
     @Test
-    void deserialize() {
-        var dokumentMal = DokumentMalType.FRITEKSTBREV;
-        var årsak = RevurderingVarslingÅrsak.BARN_IKKE_REGISTRERT_FOLKEREGISTER;
-
-        String json = """
-            {
-                "brevmalkode":"%s",
-                "navn":"sdf",
-                "arsakskode":"%s"
-            }""".formatted(dokumentMal.getKode(), årsak.getKode());
-
-        BestillBrevDto bean = StandardJsonConfig.fromJson(json, BestillBrevDto.class);
-        assertEquals(dokumentMal, bean.getBrevmalkode());
-        assertEquals(årsak, bean.getÅrsakskode());
-    }
-
-    @Test
-    void serialize() {
+    void serdes() {
         var behandlingId = 12L;
         var uuid = "5ffbf59b-76d5-4c78-bd27-6c84e0d445a3";
         var dokumentMal = DokumentMalType.FRITEKSTBREV;
         var arsak = RevurderingVarslingÅrsak.BARN_IKKE_REGISTRERT_FOLKEREGISTER;
 
         var brev = new BestillBrevDto(behandlingId, UUID.fromString(uuid), dokumentMal, null, arsak);
+
         var etterRoundtrip = StandardJsonConfig.fromJson(StandardJsonConfig.toJson(brev), BestillBrevDto.class);
 
         assertEquals(brev.getBehandlingId(), etterRoundtrip.getBehandlingId());
