d0e3e1b353b450dd572d76c5f3bab8ab99b8500b
Michal J. Sladek <mrsladek@users.noreply.github.com>
2022-04-20 21:01:56 +0200

    Rydder opp unleash (#4091)
    * Rydder opp unleash

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/saksbehandler/dto/InitLinksDto.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/saksbehandler/dto/InitLinksDto.java
@@ -7,7 +7,6 @@ import no.nav.foreldrepenger.web.app.rest.ResourceLink;
 
 public class InitLinksDto {
     private List<ResourceLink> links = new ArrayList<>();
-    private List<ResourceLink> toggleLinks = new ArrayList<>();
     private List<ResourceLink> sakLinks = new ArrayList<>();
 
     public InitLinksDto() {
@@ -15,21 +14,14 @@ public class InitLinksDto {
     }
 
     public InitLinksDto(List<ResourceLink> links,
-                        List<ResourceLink> linksToggle,
                         List<ResourceLink> linksSak) {
         this.links = links;
-        this.toggleLinks = linksToggle;
         this.sakLinks = linksSak;
     }
 
     public List<ResourceLink> getLinks() {
         return links;
     }
-
-    public List<ResourceLink> getToggleLinks() {
-        return toggleLinks;
-    }
-
     public List<ResourceLink> getSakLinks() {
         return sakLinks;
     }
