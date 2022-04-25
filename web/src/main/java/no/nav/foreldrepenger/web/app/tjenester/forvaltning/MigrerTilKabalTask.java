89538127ed243f9688a8a7bbf472f8395ec7d423
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2022-04-25 19:43:19 +0200

    Riktig init av migreringstask

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/forvaltning/MigrerTilKabalTask.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/forvaltning/MigrerTilKabalTask.java
@@ -9,6 +9,7 @@ import no.nav.foreldrepenger.behandlingskontroll.BehandlingskontrollTjeneste;
 import no.nav.foreldrepenger.behandlingslager.behandling.BehandlingStegType;
 import no.nav.foreldrepenger.behandlingslager.behandling.aksjonspunkt.AksjonspunktDefinisjon;
 import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepository;
+import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepositoryProvider;
 import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakProsesstaskRekkefølge;
 import no.nav.foreldrepenger.behandlingslager.task.BehandlingProsessTask;
 import no.nav.foreldrepenger.web.app.tjenester.behandling.aksjonspunkt.BehandlingsprosessTjeneste;
@@ -29,10 +30,11 @@ public class MigrerTilKabalTask extends BehandlingProsessTask {
     }
 
     @Inject
-    public MigrerTilKabalTask(BehandlingRepository behandlingRepository,
+    public MigrerTilKabalTask(BehandlingRepositoryProvider repositoryProvider,
                               BehandlingskontrollTjeneste behandlingskontrollTjeneste,
                               BehandlingsprosessTjeneste prosesseringTjeneste) {
-        this.behandlingRepository = behandlingRepository;
+        super(repositoryProvider.getBehandlingLåsRepository());
+        this.behandlingRepository = repositoryProvider.getBehandlingRepository();
         this.behandlingskontrollTjeneste = behandlingskontrollTjeneste;
         this.prosesseringTjeneste = prosesseringTjeneste;
     }
