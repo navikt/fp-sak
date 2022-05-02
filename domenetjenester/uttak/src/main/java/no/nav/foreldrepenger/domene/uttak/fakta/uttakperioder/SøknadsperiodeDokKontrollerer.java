a5c3c2a665db5e9decca45bea53b0636d46b9fa1
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2022-05-02 19:17:50 +0200

    Lommerusk

--- domenetjenester/uttak/src/main/java/no/nav/foreldrepenger/domene/uttak/fakta/uttakperioder/SøknadsperiodeDokKontrollerer.java
+++ domenetjenester/uttak/src/main/java/no/nav/foreldrepenger/domene/uttak/fakta/uttakperioder/SøknadsperiodeDokKontrollerer.java
@@ -11,7 +11,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 
