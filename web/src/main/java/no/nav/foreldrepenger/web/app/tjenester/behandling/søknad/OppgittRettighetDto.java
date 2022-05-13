1a039a80cb211e9bf41978152bc87352564b07a1
Anders Palfi <anderspalfi@gmail.com>
2022-05-13 12:35:57 +0200

    TFP-4962 rydder bort ubrukt omsorg felt fra oppgittrettighetdto

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/søknad/OppgittRettighetDto.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/søknad/OppgittRettighetDto.java
@@ -2,13 +2,12 @@ package no.nav.foreldrepenger.web.app.tjenester.behandling.søknad;
 
 import no.nav.foreldrepenger.behandlingslager.behandling.ytelsefordeling.OppgittRettighetEntitet;
 
-public record OppgittRettighetDto(boolean omsorgForBarnet, boolean aleneomsorgForBarnet) {
+public record OppgittRettighetDto(boolean aleneomsorgForBarnet) {
 
     public static OppgittRettighetDto mapFra(OppgittRettighetEntitet oppgittRettighet) {
         if (oppgittRettighet == null) {
             return null;
         }
-        //TODO TFP-4962 palfi, rydd frontend
-        return new OppgittRettighetDto(true, oppgittRettighet.getHarAleneomsorgForBarnet());
+        return new OppgittRettighetDto(oppgittRettighet.getHarAleneomsorgForBarnet());
     }
 }
