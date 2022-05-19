9ed9b457bc1befb03b7a010866044914c452ffab
Michal J. Sladek <michal.sladek@nav.no>
2022-05-19 18:02:00 +0200

    Forenkler Jetty oppsett.

---
+++ web/src/main/java/no/nav/foreldrepenger/web/server/jetty/DatasourceUtil.java
@@ -0,0 +1,33 @@
+package no.nav.foreldrepenger.web.server.jetty;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import io.micrometer.core.instrument.Metrics;
+import no.nav.foreldrepenger.konfig.Environment;
+
+public class DatasourceUtil {
+    private static final Environment ENV = Environment.current();
+
+    public static DataSource createDatasource(String schemaName, int maxPoolSize) {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(ENV.getRequiredProperty(schemaName + ".url"));
+        config.setUsername(ENV.getRequiredProperty(schemaName + ".username"));
+        config.setPassword(ENV.getRequiredProperty(schemaName + ".password"));
+        config.setConnectionTimeout(1000);
+        config.setMinimumIdle(5);
+        config.setMaximumPoolSize(maxPoolSize);
+        config.setConnectionTestQuery("select 1 from dual");
+        config.setDriverClassName("oracle.jdbc.OracleDriver");
+        config.setMetricRegistry(Metrics.globalRegistry);
+
+        Properties dsProperties = new Properties();
+        config.setDataSourceProperties(dsProperties);
+
+        return new HikariDataSource(config);
+    }
+}
--- web/src/test/java/no/nav/foreldrepenger/web/server/jetty/JettyDevServer.java
+++ web/src/test/java/no/nav/foreldrepenger/web/server/jetty/JettyDevServer.java
@@ -1,100 +1,24 @@
 package no.nav.foreldrepenger.web.server.jetty;
 
-import static no.nav.foreldrepenger.dbstoette.Databaseskjemainitialisering.migrer;
-import static no.nav.foreldrepenger.dbstoette.Databaseskjemainitialisering.settJdniOppslag;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-
-import org.eclipse.jetty.server.Connector;
-import org.eclipse.jetty.server.SecureRequestCustomizer;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.webapp.WebAppContext;
-
 import no.nav.foreldrepenger.konfig.Environment;
-import no.nav.foreldrepenger.web.app.ApplicationConfig;
-import no.nav.foreldrepenger.web.app.FrontendApiConfig;
-import no.nav.vedtak.isso.IssoApplication;
 
-public class JettyDevServer extends AbstractJettyServer {
+public class JettyDevServer extends JettyServer {
 
     private static final Environment ENV = Environment.current();
-    private static final String TRUSTSTORE_PASSW_PROP = "javax.net.ssl.trustStorePassword";
-    private static final String TRUSTSTORE_PATH_PROP = "javax.net.ssl.trustStore";
-
-    public JettyDevServer(JettyWebKonfigurasjon webKonfigurasjon) {
-        super(webKonfigurasjon);
-    }
 
     public static void main(String[] args) throws Exception {
-        new JettyDevServer(new JettyWebKonfigurasjon(8080)).bootStrap();
+        jettyServer(args).bootStrap();
     }
 
-    @Override
-    protected void konfigurer() throws Exception {
-        System.setProperty("conf", "src/main/resources/jetty/");
-        super.konfigurer();
-    }
-
-    @Override
-    protected void konfigurerJndi() {
-        settJdniOppslag();
-    }
-
-    @Override
-    protected void konfigurerMiljø() {
-
-    }
-
-    @Override
-    protected void migrerDatabaser() {
-        migrer();
-    }
-
-    @Override
-    protected void konfigurerSikkerhet() {
-        initCryptoStoreConfig();
-        super.konfigurerSikkerhet();
-    }
-
-    @Override
-    protected List<Connector> createConnectors(JettyWebKonfigurasjon appKonfigurasjon, Server server) {
-        var connectors = super.createConnectors(appKonfigurasjon, server);
-        var https = createHttpConfiguration();
-        https.addCustomizer(new SecureRequestCustomizer());
-
-        return connectors;
-    }
-
-    @Override
-    protected WebAppContext createContext(JettyWebKonfigurasjon appKonfigurasjon) throws IOException {
-        var ctx = super.createContext(appKonfigurasjon);
-        ctx.setInitParameter("org.eclipse.jetty.servlet.Default.useFileMappedBuffer", "false");
-        return ctx;
-    }
-
-    @Override
-    protected List<Class<?>> getWebInfClasses() {
-        return List.of(ApplicationConfig.class, FrontendApiConfig.class, IssoApplication.class);
-    }
-
-    private static void initCryptoStoreConfig() {
-        var defaultLocation = ENV.getProperty("user.home", ".") + "/.modig/truststore.jks";
-
-        var storePath = ENV.getProperty(JettyDevServer.TRUSTSTORE_PATH_PROP, defaultLocation);
-        var storeFile = new File(storePath);
-        if (!storeFile.exists()) {
-            throw new IllegalStateException("Finner ikke " + "truststore" + " i " + storePath
-                    + "\n\tKonfigurer enten som System property '" + JettyDevServer.TRUSTSTORE_PATH_PROP + "' eller environment variabel '"
-                    + JettyDevServer.TRUSTSTORE_PATH_PROP.toUpperCase().replace('.', '_') + "'");
+    protected static JettyDevServer jettyServer(String[] args) {
+        if (args.length > 0) {
+            return new JettyDevServer(Integer.parseUnsignedInt(args[0]));
         }
-        var password = ENV.getProperty(JettyDevServer.TRUSTSTORE_PASSW_PROP, "changeit");
-        if (password == null) {
-            throw new IllegalStateException("Passord for å aksessere store " + "truststore" + " i " + storePath + " er null");
-        }
-
-        System.setProperty(JettyDevServer.TRUSTSTORE_PATH_PROP, storeFile.getAbsolutePath());
-        System.setProperty(JettyDevServer.TRUSTSTORE_PASSW_PROP, password);
+        return new JettyDevServer(ENV.getProperty("server.port", Integer.class, 8080));
     }
+
+    private JettyDevServer(int serverPort) {
+        super(serverPort);
+    }
+
 }
