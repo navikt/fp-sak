129e4d466bd2446dfa78b25e230b8bc6ed9b90ef
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2022-06-03 11:24:39 +0200

    Tydeligere logikk

--- behandlingsprosess/src/main/java/no/nav/foreldrepenger/behandling/steg/søknadsfrist/fp/SøknadsfristRegelAdapter.java
+++ behandlingsprosess/src/main/java/no/nav/foreldrepenger/behandling/steg/søknadsfrist/fp/SøknadsfristRegelAdapter.java
@@ -1,10 +1,12 @@
 package no.nav.foreldrepenger.behandling.steg.søknadsfrist.fp;
 
+import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.List;
 
 import no.nav.foreldrepenger.behandlingslager.behandling.søknad.SøknadEntitet;
 import no.nav.foreldrepenger.behandlingslager.behandling.ytelsefordeling.periode.OppgittPeriodeEntitet;
+import no.nav.foreldrepenger.regler.SøknadsfristUtil;
 import no.nav.foreldrepenger.regler.soknadsfrist.SøknadsfristRegelOrkestrering;
 import no.nav.foreldrepenger.regler.soknadsfrist.SøknadsfristResultat;
 import no.nav.foreldrepenger.regler.soknadsfrist.grunnlag.SøknadsfristGrunnlag;
@@ -26,7 +28,7 @@ final class SøknadsfristRegelAdapter {
 
         var førsteUttaksdato = oppgittePerioder.stream()
             .filter(p -> !p.isUtsettelse())
-            .filter(p -> p.getTidligstMottattDato().filter(tmd -> tmd.isBefore(søknadMottattDato)).isEmpty()) // TMD er fra vedtatt original
+            .filter(p -> periodeSkalVurderes(p, søknadMottattDato))
             .min(Comparator.comparing(OppgittPeriodeEntitet::getFom))
             .map(o -> o.getFom());
 
@@ -34,5 +36,18 @@ final class SøknadsfristRegelAdapter {
             .søknadMottattDato(søknadMottattDato)
             .førsteUttaksdato(førsteUttaksdato.orElse(null))
             .build();
+    }
+
+    private static boolean periodeSkalVurderes(OppgittPeriodeEntitet periode, LocalDate søknadMottattDato) {
+        // Mangler tidligst mottatt dato, eller tidligst mottatt er >= søknadMottatt/periodeMottatt
+        if (periode.getTidligstMottattDato().isEmpty() || periode.getMottattDato() == null ||
+            periode.getTidligstMottattDato().filter(tmd -> tmd.isBefore(søknadMottattDato)).isEmpty() ||
+            periode.getTidligstMottattDato().filter(tmd -> tmd.isBefore(periode.getMottattDato())).isEmpty()) {
+            return true;
+        }
+        // Det skal finnes en tidligst mottatt dato og den skal være før mottatt dato. Perioden har vært behandlet i tidligere behandling
+        var tidligstedato = periode.getTidligstMottattDato().map(SøknadsfristUtil::finnFørsteLoveligeUttaksdag).orElseThrow();
+        // Sjekk perioder med for tidlig fom - de kan ha blitt underkjent i tidligere behandling (frist eller uttaksregler).
+        return periode.getFom().isBefore(tidligstedato);
     }
 }
