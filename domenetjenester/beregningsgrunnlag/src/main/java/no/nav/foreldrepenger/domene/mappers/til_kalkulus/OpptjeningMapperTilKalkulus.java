f5a0c5b2decff5db48bb3b9f4f1987519ce2f280
Petter Ekern <43809623+pekern@users.noreply.github.com>
2022-08-11 11:29:50 +0200

    FIX: Filtrerer ikke ut opptjeningsaktiviteter om vi har fått IM for annet arbeidsforhold i samme bedrift som starter på eller etter stp (#4497)

--- domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/mappers/til_kalkulus/OpptjeningMapperTilKalkulus.java
+++ domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/mappers/til_kalkulus/OpptjeningMapperTilKalkulus.java
@@ -1,5 +1,7 @@
 package no.nav.foreldrepenger.domene.mappers.til_kalkulus;
 
+import java.time.LocalDate;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,6 +14,7 @@ import no.nav.folketrygdloven.kalkulator.modell.opptjening.OpptjeningAktiviteter
 import no.nav.folketrygdloven.kalkulator.tid.Intervall;
 import no.nav.foreldrepenger.behandling.BehandlingReferanse;
 import no.nav.foreldrepenger.behandlingslager.virksomhet.Arbeidsgiver;
+import no.nav.foreldrepenger.domene.iay.modell.AktivitetsAvtale;
 import no.nav.foreldrepenger.domene.iay.modell.InntektArbeidYtelseGrunnlag;
 import no.nav.foreldrepenger.domene.iay.modell.Inntektsmelding;
 import no.nav.foreldrepenger.domene.iay.modell.InntektsmeldingAggregat;
@@ -31,7 +34,7 @@ public class OpptjeningMapperTilKalkulus {
             iayGrunnlag.getAktørArbeidFraRegister(ref.aktørId()));
         var opptjeningInput = new OpptjeningAktiviteterDto(opptjeningAktiviteter.getOpptjeningPerioder()
             .stream()
-            .filter(opp -> finnesInntektsmeldingForEllerKanBeregnesUten(opp, inntektsmeldinger, yrkesfilter))
+            .filter(opp -> finnesInntektsmeldingForEllerKanBeregnesUten(opp, inntektsmeldinger, yrkesfilter, ref.getUtledetSkjæringstidspunkt()))
             .map(opptjeningPeriode -> OpptjeningAktiviteterDto.nyPeriode(
                 OpptjeningAktivitetType.fraKode(opptjeningPeriode.opptjeningAktivitetType().getKode()), mapPeriode(opptjeningPeriode),
                 opptjeningPeriode.arbeidsgiverOrgNummer(), opptjeningPeriode.arbeidsgiverAktørId(),
@@ -48,7 +51,8 @@ public class OpptjeningMapperTilKalkulus {
 
     private static boolean finnesInntektsmeldingForEllerKanBeregnesUten(OpptjeningAktiviteter.OpptjeningPeriode opp,
                                                                         List<Inntektsmelding> inntektsmeldinger,
-                                                                        YrkesaktivitetFilter yrkesfilter) {
+                                                                        YrkesaktivitetFilter yrkesfilter,
+                                                                        LocalDate utledetSkjæringstidspunkt) {
         if (opp.arbeidsgiverAktørId() == null && opp.arbeidsgiverOrgNummer() == null) {
             // Ikke et arbeidsforhold, trenger ikke ta stilling til IM
             return true;
@@ -56,7 +60,7 @@ public class OpptjeningMapperTilKalkulus {
         var inntektsmeldingerForArbeidsforholdHosAG = inntektsmeldinger.stream()
             .filter(im -> im.getArbeidsgiver().equals(getArbeidsgiver(opp)))
             // Trenger ikke se på inntektsmeldinger med arbeidsforholdId som ikke er knyttet til et reelt arbeidsforhold
-            .filter(im -> harArbeidsforholdIdSomEksisterer(im, yrkesfilter))
+            .filter(im -> harArbeidsforholdIdSomEksisterer(im, yrkesfilter, utledetSkjæringstidspunkt))
             .collect(Collectors.toList());
         if (inntektsmeldingerForArbeidsforholdHosAG.isEmpty()) {
             return true;
@@ -68,9 +72,17 @@ public class OpptjeningMapperTilKalkulus {
             .anyMatch(im -> im.getArbeidsforholdRef().gjelderFor(opp.arbeidsforholdId()));
     }
 
-    private static boolean harArbeidsforholdIdSomEksisterer(Inntektsmelding inntektsmelding, YrkesaktivitetFilter yrkesfilter) {
+    private static boolean harArbeidsforholdIdSomEksisterer(Inntektsmelding inntektsmelding,
+                                                            YrkesaktivitetFilter yrkesfilter,
+                                                            LocalDate utledetSkjæringstidspunkt) {
         return yrkesfilter.getYrkesaktiviteter().stream()
+            .filter(ya -> starterFørStp(ya.getAlleAktivitetsAvtaler(), utledetSkjæringstidspunkt))
             .anyMatch(ya -> ya.gjelderFor(inntektsmelding.getArbeidsgiver(), inntektsmelding.getArbeidsforholdRef()));
+    }
+
+    private static boolean starterFørStp(Collection<AktivitetsAvtale> alleAktivitetsAvtaler, LocalDate utledetSkjæringstidspunkt) {
+        return alleAktivitetsAvtaler.stream().anyMatch(aa -> aa.erAnsettelsesPeriode() &&
+            aa.getPeriode().getFomDato().isBefore(utledetSkjæringstidspunkt));
     }
 
     private static Arbeidsgiver getArbeidsgiver(OpptjeningAktiviteter.OpptjeningPeriode opp) {
