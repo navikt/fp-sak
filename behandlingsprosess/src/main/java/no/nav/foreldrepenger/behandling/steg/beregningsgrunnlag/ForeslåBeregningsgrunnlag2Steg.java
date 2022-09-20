2aad531799cb30007ffa1071c34e16f1a626b941
Petter Ekern <petter.ekern@nav.no>
2022-09-20 12:59:46 +0200

    TFP-5172: Ryddejobb i nytt steg

--- behandlingsprosess/src/main/java/no/nav/foreldrepenger/behandling/steg/beregningsgrunnlag/ForeslåBeregningsgrunnlag2Steg.java
+++ behandlingsprosess/src/main/java/no/nav/foreldrepenger/behandling/steg/beregningsgrunnlag/ForeslåBeregningsgrunnlag2Steg.java
@@ -2,12 +2,16 @@ package no.nav.foreldrepenger.behandling.steg.beregningsgrunnlag;
 
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
 import no.nav.foreldrepenger.behandlingskontroll.BehandleStegResultat;
+import no.nav.foreldrepenger.behandlingskontroll.BehandlingStegModell;
 import no.nav.foreldrepenger.behandlingskontroll.BehandlingStegRef;
 import no.nav.foreldrepenger.behandlingskontroll.BehandlingTypeRef;
 import no.nav.foreldrepenger.behandlingskontroll.BehandlingskontrollKontekst;
 import no.nav.foreldrepenger.behandlingskontroll.FagsakYtelseTypeRef;
 import no.nav.foreldrepenger.behandlingslager.behandling.BehandlingStegType;
+import no.nav.foreldrepenger.domene.prosess.BeregningsgrunnlagKopierOgLagreTjeneste;
 
 @FagsakYtelseTypeRef
 @BehandlingStegRef(BehandlingStegType.FORESLÅ_BEREGNINGSGRUNNLAG_2)
@@ -15,12 +19,29 @@ import no.nav.foreldrepenger.behandlingslager.behandling.BehandlingStegType;
 @ApplicationScoped
 public class ForeslåBeregningsgrunnlag2Steg implements BeregningsgrunnlagSteg {
 
+    private BeregningsgrunnlagKopierOgLagreTjeneste beregningsgrunnlagKopierOgLagreTjeneste;
+
     protected ForeslåBeregningsgrunnlag2Steg() {
+        // CDI
+    }
+
+    @Inject
+    public ForeslåBeregningsgrunnlag2Steg(BeregningsgrunnlagKopierOgLagreTjeneste beregningsgrunnlagKopierOgLagreTjeneste) {
         // for CDI proxy
+        this.beregningsgrunnlagKopierOgLagreTjeneste = beregningsgrunnlagKopierOgLagreTjeneste;
     }
 
     @Override
     public BehandleStegResultat utførSteg(BehandlingskontrollKontekst kontekst) {
         return BehandleStegResultat.utførtUtenAksjonspunkter();
     }
+
+    @Override
+    public void vedHoppOverBakover(BehandlingskontrollKontekst kontekst, BehandlingStegModell modell, BehandlingStegType tilSteg,
+                                   BehandlingStegType fraSteg) {
+        if (tilSteg.equals(BehandlingStegType.FORESLÅ_BEREGNINGSGRUNNLAG_2)) {
+            beregningsgrunnlagKopierOgLagreTjeneste.getRyddBeregningsgrunnlag(kontekst).ryddForeslåBeregningsgrunnlagVedTilbakeføring();
+        }
+    }
+
 }
