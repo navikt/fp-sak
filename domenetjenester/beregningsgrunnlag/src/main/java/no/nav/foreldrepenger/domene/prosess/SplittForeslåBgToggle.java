6fc71f3a292276860b44a96377eeffcb1a98dda6
AnjaAalerud <52454508+AnjaAalerud@users.noreply.github.com>
2022-10-03 09:57:32 +0200

    TFP-5172 Skrur på splitt av foreslå beregning i 2 steg som en del av å implementere ny beregningsregel for 8.41 (#4703)

--- domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/prosess/SplittForeslåBgToggle.java
+++ domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/prosess/SplittForeslåBgToggle.java
@@ -1,10 +1,8 @@
 package no.nav.foreldrepenger.domene.prosess;
 
-import no.nav.foreldrepenger.konfig.Environment;
-
 public class SplittForeslåBgToggle {
 
     public static boolean erTogglePå() {
-        return !Environment.current().isProd();
+        return true;
     }
 }
