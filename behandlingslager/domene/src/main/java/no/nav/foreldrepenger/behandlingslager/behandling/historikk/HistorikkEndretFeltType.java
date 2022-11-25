f36e2c5871be92fe002fb89ce7cb22209c97dc18
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2022-11-25 09:07:46 +0100

    TFP-4873 Enkelt fakta endret historikkinnslag for dokvurdering (#5051)

--- behandlingslager/domene/src/main/java/no/nav/foreldrepenger/behandlingslager/behandling/historikk/HistorikkEndretFeltType.java
+++ behandlingslager/domene/src/main/java/no/nav/foreldrepenger/behandlingslager/behandling/historikk/HistorikkEndretFeltType.java
@@ -138,7 +138,9 @@ public enum HistorikkEndretFeltType implements Kodeverdi {
     ORGANISASJONSNUMMER("ORGANISASJONSNUMMER", "Organisasjonsnummer"),
     ARBEIDSFORHOLD_BEKREFTET_TOM_DATO("ARBEIDSFORHOLD_BEKREFTET_TOM_DATO", "Til og med dato fastsatt av saksbehandler"),
     ANKE_AVVIST_ÅRSAK("ANKE_AVVIST_ÅRSAK", "Årsak til avvist anke"),
-    AKTIVITETSKRAV_AVKLARING("AKTIVITETSKRAV_AVKLARING", "Avklaring om mor er i aktivitet");
+    AKTIVITETSKRAV_AVKLARING("AKTIVITETSKRAV_AVKLARING", "Avklaring om mor er i aktivitet"),
+    UTTAKPERIODE_DOK_AVKLARING("UTTAKPERIODE_DOK_AVKLARING", "Avklart dokumentasjon for periode"),
+    ;
 
     private static final Map<String, HistorikkEndretFeltType> KODER = new LinkedHashMap<>();
 
---
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/uttak/dokumentasjon/VurderUttakDokumentasjonHistorikkinnslagTjeneste.java
@@ -0,0 +1,72 @@
+package no.nav.foreldrepenger.web.app.tjenester.behandling.uttak.dokumentasjon;
+
+import static no.nav.foreldrepenger.historikk.HistorikkInnslagTekstBuilder.formatString;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import no.nav.foreldrepenger.behandlingslager.behandling.historikk.HistorikkEndretFeltType;
+import no.nav.foreldrepenger.behandlingslager.behandling.skjermlenke.SkjermlenkeType;
+import no.nav.foreldrepenger.behandlingslager.behandling.ytelsefordeling.periode.DokumentasjonVurdering;
+import no.nav.foreldrepenger.behandlingslager.behandling.ytelsefordeling.periode.OppgittPeriodeEntitet;
+import no.nav.foreldrepenger.behandlingslager.kodeverk.Kodeverdi;
+import no.nav.foreldrepenger.historikk.HistorikkInnslagTekstBuilder;
+import no.nav.foreldrepenger.historikk.HistorikkTjenesteAdapter;
+
+@ApplicationScoped
+public class VurderUttakDokumentasjonHistorikkinnslagTjeneste {
+
+    private HistorikkTjenesteAdapter historikkTjenesteAdapter;
+
+    @Inject
+    public VurderUttakDokumentasjonHistorikkinnslagTjeneste(HistorikkTjenesteAdapter historikkTjenesteAdapter) {
+        this.historikkTjenesteAdapter = historikkTjenesteAdapter;
+    }
+
+    VurderUttakDokumentasjonHistorikkinnslagTjeneste() {
+        //CDI
+    }
+
+    public void opprettHistorikkinnslag(VurderUttakDokumentasjonDto dto,
+                                        List<OppgittPeriodeEntitet> eksisterendePerioder) {
+        var builder = historikkTjenesteAdapter.tekstBuilder()
+            .medBegrunnelse(dto.getBegrunnelse())
+            .medSkjermlenke(SkjermlenkeType.VURDER_UTTAK_DOKUMENTASJON);
+        for (var periode : dto.getVurderingBehov()) {
+            var nyvurdering = VurderUttakDokumentasjonOppdaterer.mapVurdering(periode);
+            var eksisterendeVurdering = finnEksisterendePerioder(eksisterendePerioder, periode.fom(), periode.tom())
+                .map(OppgittPeriodeEntitet::getDokumentasjonVurdering).orElse(null);
+            if (nyvurdering != null && (eksisterendeVurdering == null || erEndringerIperiode(nyvurdering, eksisterendeVurdering))) {
+                opprettAvklaring(builder, nyvurdering, periode, eksisterendeVurdering);
+            }
+        }
+    }
+
+    private void opprettAvklaring(HistorikkInnslagTekstBuilder builder,
+                                  DokumentasjonVurdering nyVurdering,
+                                  DokumentasjonVurderingBehovDto periode,
+                                  DokumentasjonVurdering eksisterendeVurdering) {
+        var fraVerdi = Objects.equals(eksisterendeVurdering, nyVurdering) ? null : eksisterendeVurdering;
+        var tekstperiode = String.format("%s - %s", formatString(periode.fom()), formatString(periode.tom()));
+        builder.medEndretFelt(HistorikkEndretFeltType.UTTAKPERIODE_DOK_AVKLARING, tekstperiode,
+            Optional.ofNullable(fraVerdi).map(Kodeverdi::getNavn).orElse(null), nyVurdering.getNavn());
+    }
+
+    private boolean erEndringerIperiode(DokumentasjonVurdering nyVurdering,
+                                        DokumentasjonVurdering eksisterendeVurdering) {
+        return !Objects.equals(nyVurdering, eksisterendeVurdering);
+    }
+
+    private Optional<OppgittPeriodeEntitet> finnEksisterendePerioder(List<OppgittPeriodeEntitet> eksisterendePerioder,
+                                                                            LocalDate fom,
+                                                                            LocalDate tom) {
+        return eksisterendePerioder.stream()
+            .filter(ep -> !ep.getFom().isBefore(fom) && !ep.getTom().isAfter(tom))
+            .findFirst();
+    }
+}
