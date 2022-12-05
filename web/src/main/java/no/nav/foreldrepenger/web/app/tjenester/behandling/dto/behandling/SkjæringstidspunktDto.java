c5f1fe2170ca68877dea8f6706819bec09511fc2
anjaaalerud <anja.gotesen.aalerud@nav.no>
2022-12-05 16:01:26 +0100

    TFP-5330 Fjerner kreverSammenhengendeUttak fra SkjæringstidspunktDto

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/dto/behandling/SkjæringstidspunktDto.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/dto/behandling/SkjæringstidspunktDto.java
@@ -8,10 +8,10 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import no.nav.foreldrepenger.behandling.Skjæringstidspunkt;
 
 @JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE, fieldVisibility = JsonAutoDetect.Visibility.ANY)
-public record SkjæringstidspunktDto(LocalDate dato, boolean kreverSammenhengendeUttak, boolean utenMinsterett) {
+public record SkjæringstidspunktDto(LocalDate dato, boolean utenMinsterett) {
 
     public static Optional<SkjæringstidspunktDto> fraSkjæringstidspunkt(Skjæringstidspunkt skjæringstidspunkt) {
         var dato = Optional.ofNullable(skjæringstidspunkt).flatMap(Skjæringstidspunkt::getSkjæringstidspunktHvisUtledet);
-        return dato.map(d -> new SkjæringstidspunktDto(d, skjæringstidspunkt.kreverSammenhengendeUttak(), skjæringstidspunkt.utenMinsterett()));
+        return dato.map(d -> new SkjæringstidspunktDto(d, skjæringstidspunkt.utenMinsterett()));
     }
 }
