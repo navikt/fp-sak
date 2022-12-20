a879d0bb9b1f92b0db7e967b7f1b26d740f03d5b
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2022-12-20 09:48:53 +0100

    Felles 4216 (#5202)

--- web/src/main/java/no/nav/foreldrepenger/web/app/oppgave/RedirectFactory.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/oppgave/RedirectFactory.java
@@ -3,35 +3,32 @@ package no.nav.foreldrepenger.web.app.oppgave;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
-import no.nav.foreldrepenger.konfig.Environment;
 import no.nav.foreldrepenger.konfig.KonfigVerdi;
 
 @ApplicationScoped
 public class RedirectFactory {
 
-    private static final Environment ENV = Environment.current();
-
     private static final String DEFAULT_PART_URL = "?punkt=default&fakta=default";
 
-    private String loadBalancerUrl;
+    private String redirectBaseUrl;
 
     public String lagRedirect(OppgaveRedirectData data) {
         if (data.harFeilmelding()) {
-            return String.format("%s/#?errormessage=%s", getBaseUrl(), data.getFeilmelding());
+            return String.format("%s/#?errormessage=%s", getRedirectBaseUrl(), data.getFeilmelding());
         }
         if (data.harBehandlingId()) {
-            return String.format("%s/fagsak/%s/behandling/%s/%s", getBaseUrl(), data.getSaksnummer().getVerdi(),
+            return String.format("%s/fagsak/%s/behandling/%s/%s", getRedirectBaseUrl(), data.getSaksnummer().getVerdi(),
                 data.getBehandlingUuid().toString(), DEFAULT_PART_URL);
         }
-        return String.format("%s/fagsak/%s/", getBaseUrl(), data.getSaksnummer().getVerdi());
+        return String.format("%s/fagsak/%s/", getRedirectBaseUrl(), data.getSaksnummer().getVerdi());
     }
 
-    protected String getBaseUrl() {
-        return loadBalancerUrl + ENV.getProperty("context.path","/fpsak");
+    protected String getRedirectBaseUrl() {
+        return redirectBaseUrl;
     }
 
     @Inject
-    public void setLoadBalancerUrl(@KonfigVerdi("loadbalancer.url") String loadBalancerUrl) {
-        this.loadBalancerUrl = loadBalancerUrl;
+    public void setRedirectBaseUrl(@KonfigVerdi(value = "gosys.redirect.url", defaultVerdi = "https://fpsak.intern.nav.no/") String redirectBaseUrl) {
+        this.redirectBaseUrl = redirectBaseUrl;
     }
 }
