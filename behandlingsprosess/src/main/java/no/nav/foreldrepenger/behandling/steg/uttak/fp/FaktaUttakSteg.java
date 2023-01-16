a036fa150b1dcdf7051e20dcbb6df925d2e7ad8a
Anders Palfi <anderspalfi@gmail.com>
2023-01-16 10:43:22 +0100

    TFP-4873 Ikke utlede fakta uttak AP hvis det finnes åpen overstyring AP

--- behandlingsprosess/src/main/java/no/nav/foreldrepenger/behandling/steg/uttak/fp/FaktaUttakSteg.java
+++ behandlingsprosess/src/main/java/no/nav/foreldrepenger/behandling/steg/uttak/fp/FaktaUttakSteg.java
@@ -1,5 +1,7 @@
 package no.nav.foreldrepenger.behandling.steg.uttak.fp;
 
+import java.util.List;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
@@ -11,8 +13,11 @@ import no.nav.foreldrepenger.behandlingskontroll.BehandlingTypeRef;
 import no.nav.foreldrepenger.behandlingskontroll.BehandlingskontrollKontekst;
 import no.nav.foreldrepenger.behandlingskontroll.FagsakYtelseTypeRef;
 import no.nav.foreldrepenger.behandlingslager.behandling.BehandlingStegType;
+import no.nav.foreldrepenger.behandlingslager.behandling.aksjonspunkt.AksjonspunktDefinisjon;
+import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakYtelseType;
 import no.nav.foreldrepenger.domene.uttak.fakta.v2.FaktaUttakAksjonspunktUtleder;
+import no.nav.foreldrepenger.domene.uttak.input.UttakInput;
 
 @BehandlingStegRef(BehandlingStegType.FAKTA_UTTAK)
 @BehandlingTypeRef
@@ -22,12 +27,15 @@ public class FaktaUttakSteg implements UttakSteg {
 
     private FaktaUttakAksjonspunktUtleder faktaUttakAksjonspunktUtleder;
     private UttakInputTjeneste uttakInputTjeneste;
+    private BehandlingRepository behandlingRepository;
 
     @Inject
     public FaktaUttakSteg(FaktaUttakAksjonspunktUtleder faktaUttakAksjonspunktUtleder,
-                          UttakInputTjeneste uttakInputTjeneste) {
+                          UttakInputTjeneste uttakInputTjeneste,
+                          BehandlingRepository behandlingRepository) {
         this.faktaUttakAksjonspunktUtleder = faktaUttakAksjonspunktUtleder;
         this.uttakInputTjeneste = uttakInputTjeneste;
+        this.behandlingRepository = behandlingRepository;
     }
 
     FaktaUttakSteg() {
@@ -37,7 +45,16 @@ public class FaktaUttakSteg implements UttakSteg {
     @Override
     public BehandleStegResultat utførSteg(BehandlingskontrollKontekst kontekst) {
         var uttakInput = uttakInputTjeneste.lagInput(kontekst.getBehandlingId());
-        var faktaUttakAP = faktaUttakAksjonspunktUtleder.utledAksjonspunkterFor(uttakInput);
+        var faktaUttakAP = utledAp(uttakInput);
         return BehandleStegResultat.utførtMedAksjonspunkter(faktaUttakAP);
+    }
+
+    private List<AksjonspunktDefinisjon> utledAp(UttakInput uttakInput) {
+        return harÅpentOverstyringAp(uttakInput.getBehandlingReferanse().behandlingId()) ? List.of() : faktaUttakAksjonspunktUtleder.utledAksjonspunkterFor(uttakInput);
+    }
+
+    private boolean harÅpentOverstyringAp(Long behandlingId) {
+        return behandlingRepository.hentBehandling(behandlingId)
+            .harÅpentAksjonspunktMedType(AksjonspunktDefinisjon.OVERSTYRING_FAKTA_UTTAK);
     }
 }
