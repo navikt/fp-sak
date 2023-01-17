31ee5f1244c7bb02d6d10a92deb7212ff30fbc7c
Anders Palfi <anderspalfi@gmail.com>
2023-01-17 13:10:40 +0100

    TFP-4873 inputvalidering fakta uttak dto (#5303)

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/uttak/fakta/FaktaUttakPeriodeDto.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/uttak/fakta/FaktaUttakPeriodeDto.java
@@ -3,6 +3,12 @@ package no.nav.foreldrepenger.web.app.tjenester.behandling.uttak.fakta;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 import no.nav.foreldrepenger.behandlingslager.behandling.ytelsefordeling.MorsAktivitet;
 import no.nav.foreldrepenger.behandlingslager.behandling.ytelsefordeling.periode.FordelingPeriodeKilde;
 import no.nav.foreldrepenger.behandlingslager.behandling.ytelsefordeling.periode.UttakPeriodeType;
@@ -10,10 +16,19 @@ import no.nav.foreldrepenger.behandlingslager.behandling.ytelsefordeling.årsak.
 import no.nav.foreldrepenger.behandlingslager.behandling.ytelsefordeling.årsak.OverføringÅrsak;
 import no.nav.foreldrepenger.behandlingslager.behandling.ytelsefordeling.årsak.UtsettelseÅrsak;
 import no.nav.foreldrepenger.behandlingslager.uttak.fp.SamtidigUttaksprosent;
+import no.nav.foreldrepenger.validering.ValidKodeverk;
 import no.nav.foreldrepenger.web.app.tjenester.behandling.uttak.dto.ArbeidsforholdDto;
 
-public record FaktaUttakPeriodeDto(LocalDate fom, LocalDate tom, UttakPeriodeType uttakPeriodeType, UtsettelseÅrsak utsettelseÅrsak,
-                                   OverføringÅrsak overføringÅrsak, OppholdÅrsak oppholdÅrsak, BigDecimal arbeidstidsprosent,
-                                   ArbeidsforholdDto arbeidsforhold, SamtidigUttaksprosent samtidigUttaksprosent, boolean flerbarnsdager,
-                                   MorsAktivitet morsAktivitet, FordelingPeriodeKilde periodeKilde) {
+public record FaktaUttakPeriodeDto(@NotNull LocalDate fom,
+                                   @NotNull LocalDate tom,
+                                   @ValidKodeverk UttakPeriodeType uttakPeriodeType,
+                                   @ValidKodeverk UtsettelseÅrsak utsettelseÅrsak,
+                                   @ValidKodeverk OverføringÅrsak overføringÅrsak,
+                                   @ValidKodeverk OppholdÅrsak oppholdÅrsak,
+                                   @Min(0) @Max(100) @Digits(integer = 3, fraction = 2) BigDecimal arbeidstidsprosent,
+                                   @Valid ArbeidsforholdDto arbeidsforhold,
+                                   @Valid SamtidigUttaksprosent samtidigUttaksprosent,
+                                   boolean flerbarnsdager,
+                                   @ValidKodeverk MorsAktivitet morsAktivitet,
+                                   @ValidKodeverk FordelingPeriodeKilde periodeKilde) {
 }
