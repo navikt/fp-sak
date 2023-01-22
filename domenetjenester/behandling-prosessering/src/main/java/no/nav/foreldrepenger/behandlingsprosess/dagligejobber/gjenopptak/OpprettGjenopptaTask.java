85a6aaedef4f975b0c5735cd9ce03627aca9e1aa
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-01-22 17:52:56 +0100

    Smartere gjenopptak og oppdatering

---
+++ domenetjenester/behandling-prosessering/src/main/java/no/nav/foreldrepenger/behandlingsprosess/dagligejobber/gjenopptak/OpprettGjenopptaTask.java
@@ -0,0 +1,44 @@
+package no.nav.foreldrepenger.behandlingsprosess.dagligejobber.gjenopptak;
+
+import java.time.LocalDateTime;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+
+import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepository;
+import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakProsesstaskRekkefølge;
+import no.nav.foreldrepenger.behandlingslager.task.GenerellProsessTask;
+import no.nav.foreldrepenger.behandlingsprosess.prosessering.BehandlingProsesseringTjeneste;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTask;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
+
+/**
+ * Oppretter videre tasks for gjenoppta og oppdater registerdata
+ */
+@Dependent
+@ProsessTask("batch.opprett.gjenoppta")
+@FagsakProsesstaskRekkefølge(gruppeSekvens = false)
+public class OpprettGjenopptaTask extends GenerellProsessTask {
+
+    private BehandlingRepository behandlingRepository;
+    private BehandlingProsesseringTjeneste behandlingProsesseringTjeneste;
+
+    OpprettGjenopptaTask() {
+        // for CDI proxy
+    }
+
+    @Inject
+    public OpprettGjenopptaTask(BehandlingRepository behandlingRepository,
+                                BehandlingProsesseringTjeneste behandlingProsesseringTjeneste) {
+        super();
+        this.behandlingRepository = behandlingRepository;
+        this.behandlingProsesseringTjeneste = behandlingProsesseringTjeneste;
+    }
+
+    @Override
+    protected void prosesser(ProsessTaskData prosessTaskData, Long fagsakId, Long behandlingId) {
+        var behandling = behandlingRepository.hentBehandlingReadOnly(behandlingId);
+        behandlingProsesseringTjeneste.opprettTasksForGjenopptaOppdaterFortsettBatch(behandling, LocalDateTime.now());
+    }
+
+}
--- web/src/main/java/no/nav/foreldrepenger/web/server/jetty/TimingFilter.java
+++ web/src/main/java/no/nav/foreldrepenger/web/server/jetty/TimingFilter.java
@@ -1,8 +1,5 @@
 package no.nav.foreldrepenger.web.server.jetty;
 
-import static io.micrometer.core.instrument.Metrics.timer;
-import static no.nav.vedtak.log.metrics.MetricsUtil.utvidMedMedian;
-
 import java.time.Duration;
 
 import javax.annotation.Priority;
@@ -13,22 +10,28 @@ import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
 
+import io.micrometer.core.instrument.Metrics;
+import no.nav.vedtak.log.metrics.MetricsUtil;
+
 @Provider
 @Priority(Priorities.USER)
 public class TimingFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
     private static final String STATUS = "status";
+    private static final String METHOD = "method";
     private static final String PATH = "path";
     private static final String METRIC_NAME = "rest";
+    private static final String COUNTER_NAME = "restantall";
     private static final ThreadLocalTimer TIMER = new ThreadLocalTimer();
 
     public TimingFilter() {
-        utvidMedMedian(METRIC_NAME);
+        MetricsUtil.utvidMedPercentiler(METRIC_NAME, 0.5, 0.95, 0.99);
     }
 
     @Override
     public void filter(ContainerRequestContext req, ContainerResponseContext res) {
-        timer(METRIC_NAME, PATH, req.getUriInfo().getPath(), STATUS, String.valueOf(res.getStatus())).record(Duration.ofMillis(TIMER.stop()));
+        Metrics.timer(METRIC_NAME, PATH, req.getUriInfo().getPath(), METHOD, req.getMethod()).record(Duration.ofMillis(TIMER.stop()));
+        Metrics.counter(COUNTER_NAME, METHOD, req.getMethod(), STATUS, String.valueOf(res.getStatus())).increment();
     }
 
     @Override
