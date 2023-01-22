3de714cde6ca0d3f9c4ba00c67fe7ad705a1601b
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2023-01-22 18:20:42 +0100

    Smartere gjenopptak og oppdatering (#5349)
    * Smartere gjenopptak og oppdatering
    
    * Fikse test

--- domenetjenester/behandling-prosessering/src/main/java/no/nav/foreldrepenger/behandlingsprosess/dagligejobber/gjenopptak/AutomatiskGjenopptagelseTjeneste.java
+++ domenetjenester/behandling-prosessering/src/main/java/no/nav/foreldrepenger/behandlingsprosess/dagligejobber/gjenopptak/AutomatiskGjenopptagelseTjeneste.java
@@ -14,10 +14,11 @@ import org.slf4j.LoggerFactory;
 
 import no.nav.foreldrepenger.behandlingslager.behandling.Behandling;
 import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingKandidaterRepository;
-import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.foreldrepenger.behandlingsprosess.prosessering.BehandlingProsesseringTjeneste;
 import no.nav.foreldrepenger.historikk.OppgaveÅrsak;
 import no.nav.foreldrepenger.produksjonsstyring.oppgavebehandling.OppgaveBehandlingKoblingRepository;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTaskTjeneste;
 import no.nav.vedtak.log.mdc.MDCOperations;
 
 @ApplicationScoped
@@ -29,16 +30,16 @@ public class AutomatiskGjenopptagelseTjeneste {
     private BehandlingProsesseringTjeneste behandlingProsesseringTjeneste;
     private BehandlingKandidaterRepository behandlingKandidaterRepository;
     private OppgaveBehandlingKoblingRepository oppgaveBehandlingKoblingRepository;
-    private BehandlingRepository behandlingRepository;
+    private ProsessTaskTjeneste taskTjeneste;
 
     @Inject
     public AutomatiskGjenopptagelseTjeneste(BehandlingKandidaterRepository behandlingKandidaterRepository,
                                             OppgaveBehandlingKoblingRepository oppgaveBehandlingKoblingRepository,
-                                            BehandlingRepository behandlingRepository,
+                                            ProsessTaskTjeneste taskTjeneste,
                                             BehandlingProsesseringTjeneste behandlingProsesseringTjeneste) {
         this.behandlingKandidaterRepository = behandlingKandidaterRepository;
         this.oppgaveBehandlingKoblingRepository = oppgaveBehandlingKoblingRepository;
-        this.behandlingRepository = behandlingRepository;
+        this.taskTjeneste = taskTjeneste;
         this.behandlingProsesseringTjeneste = behandlingProsesseringTjeneste;
     }
 
@@ -47,59 +48,70 @@ public class AutomatiskGjenopptagelseTjeneste {
     }
 
     public String gjenopptaBehandlinger() {
+        var callId = ensureCallId();
         LOG.info("BATCH Gjenoppta inngang");
-        var baseline = LocalTime.now();
         var feileteBehandlinger = behandlingProsesseringTjeneste.behandlingerMedFeiletProsessTask();
         var behandlingListe = behandlingKandidaterRepository.finnBehandlingerForAutomatiskGjenopptagelse();
+        var dato = LocalDate.now();
+        var baseline = LocalTime.now();
         LOG.info("BATCH Gjenoppta fant {} behandlinger", behandlingListe.size());
         behandlingListe.stream()
             .filter(b -> !feileteBehandlinger.contains(b.getId()))
-            .forEach(behandling -> opprettProsessTasks(behandling, baseline, 1439));
+            .forEach(behandling -> opprettProsessTasks(behandling, callId, dato, baseline, 1439));
         LOG.info("BATCH Gjenoppta utgang");
         return  "-" + behandlingListe.size();
     }
 
-    private void opprettProsessTasks(Behandling behandling, LocalTime baseline, int spread) {
-        var callId = MDCOperations.getCallId();
-        if (callId == null) {
-            MDCOperations.putCallId();
-            callId = MDCOperations.getCallId();
-        }
-        MDCOperations.putCallId(callId + "_" + behandling.getId());
-        var nesteKjøring = LocalDateTime.of(LocalDate.now(), baseline.plusSeconds(LocalDateTime.now().getNano() % spread));
-        behandlingProsesseringTjeneste.opprettTasksForGjenopptaOppdaterFortsettBatch(behandling, nesteKjøring);
-        MDCOperations.putCallId(callId);
+    private void opprettProsessTasks(Behandling behandling, String callId, LocalDate dato, LocalTime baseline, int spread) {
+        var nesteKjøring = LocalDateTime.of(dato, baseline.plusSeconds(Math.abs(System.nanoTime()) % spread));
+        var taskdata = ProsessTaskData.forProsessTask(OpprettGjenopptaTask.class);
+        taskdata.setBehandling(behandling.getFagsakId(), behandling.getId());
+        taskdata.setCallId(callId + "_" + behandling.getId());
+        taskdata.setNesteKjøringEtter(nesteKjøring);
+        taskdata.setPrioritet(50);
+        taskTjeneste.lagre(taskdata);
     }
 
 
     public String oppdaterBehandlingerFraOppgaveFrist() {
+        var callId = ensureCallId();
         LOG.info("BATCH Oppdater inngang");
         var tom = LocalDate.now().minusDays(1);
         var fom = DayOfWeek.MONDAY.equals(tom.getDayOfWeek()) ? tom.minusDays(2) : tom;
         var feileteBehandlinger = behandlingProsesseringTjeneste.behandlingerMedFeiletProsessTask();
         var behandlingListe = oppgaveBehandlingKoblingRepository.hentBehandlingerMedUferdigeOppgaverOpprettetTidsrom(fom, tom, OPPGAVE_TYPER);
+        var dato = LocalDate.now();
         var baseline = LocalTime.now();
         LOG.info("BATCH Oppdater fant {} oppgaver", behandlingListe.size());
         behandlingListe.stream()
             .filter(b -> !feileteBehandlinger.contains(b.getId()))
             .filter(b -> !b.erSaksbehandlingAvsluttet() && !b.isBehandlingPåVent() && b.erYtelseBehandling())
-            .forEach(behandling -> opprettProsessTasks(behandling, baseline, 1439));
+            .forEach(behandling -> opprettProsessTasks(behandling, callId, dato, baseline, 1439));
         LOG.info("BATCH Oppdater utgang");
         return "-" + behandlingListe.size();
     }
 
     public String gjenopplivBehandlinger() {
+        var callId = ensureCallId();
         LOG.info("BATCH Gjenoppliv inngang");
         var feileteBehandlinger = behandlingProsesseringTjeneste.behandlingerMedFeiletProsessTask();
         var sovende = behandlingKandidaterRepository.finnÅpneBehandlingerUtenÅpneAksjonspunktEllerAutopunkt().stream()
             .filter(b -> !feileteBehandlinger.contains(b.getId()))
             .toList();
+        var dato = LocalDate.now();
         var baseline = LocalTime.now();
         LOG.info("BATCH Gjenoppliv fant {} behandlinger", sovende.size());
         for (var behandling : sovende) {
-            opprettProsessTasks(behandling, baseline, 101);
+            opprettProsessTasks(behandling, callId, dato, baseline, 101);
         }
         LOG.info("BATCH Gjenoppliv utgang");
         return "-" + sovende.size();
+    }
+
+    private String ensureCallId() {
+        if (MDCOperations.getCallId() == null) {
+            MDCOperations.putCallId();
+        }
+        return MDCOperations.getCallId();
     }
 }
