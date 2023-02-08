23fc0fb22695622a252f548ddd133c3db8182588
Michal J. Sladek <michal.sladek@nav.no>
2023-02-08 14:37:35 +0100

    Swagger viser brukt app versjon.

--- web/src/main/java/no/nav/foreldrepenger/web/app/konfig/ApiConfig.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/konfig/ApiConfig.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -35,7 +36,7 @@ public class ApiConfig extends Application {
         var oas = new OpenAPI();
         var info = new Info()
             .title("FPSAK - Foreldrepenger, engangsstønad og svangerskapspenger")
-            .version(ENV.imageName())
+            .version(Optional.ofNullable(ENV.imageName()).orElse("1.0"))
             .description("REST grensesnitt for FPSAK.");
 
         oas.info(info).addServersItem(new Server().url(ENV.getProperty("context.path", "/fpsak")));
