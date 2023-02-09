0cb88fba06348ffc9befaec1d2e4875b61340e0d
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2023-02-09 10:01:53 +0100

    TFP-5414 fritt uttak kan ha gradering tilbake i tid (#5432)
    * TFP-5414 fritt uttak kan ha gradering tilbake i tid
    
    * Fjerne løs bruk av uttaksregel-mapper

--- domenetjenester/beregning-ytelse/src/main/java/no/nav/foreldrepenger/ytelse/beregning/BeregnYtelseTjeneste.java
+++ domenetjenester/beregning-ytelse/src/main/java/no/nav/foreldrepenger/ytelse/beregning/BeregnYtelseTjeneste.java
@@ -9,22 +9,20 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
 import no.nav.foreldrepenger.behandling.BehandlingReferanse;
 import no.nav.foreldrepenger.behandlingskontroll.FagsakYtelseTypeRef;
 import no.nav.foreldrepenger.behandlingslager.behandling.beregning.BeregningsresultatEntitet;
 import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakYtelseType;
-import no.nav.foreldrepenger.regler.jackson.JacksonJsonConfig;
 import no.nav.foreldrepenger.ytelse.beregning.adapter.MapInputFraVLTilRegelGrunnlag;
 import no.nav.foreldrepenger.ytelse.beregning.regelmodell.BeregningsresultatRegelmodell;
+import no.nav.fpsak.nare.json.JsonOutput;
+import no.nav.fpsak.nare.json.NareJsonException;
 
 @ApplicationScoped
 public class BeregnYtelseTjeneste {
 
     private static final Logger LOG = LoggerFactory.getLogger(BeregnYtelseTjeneste.class);
 
-    private final JacksonJsonConfig jackson = new JacksonJsonConfig();
     private Instance<BeregnFeriepengerTjeneste> beregnFeriepengerTjeneste;
     private MapInputFraVLTilRegelGrunnlag mapBeregningsresultatFraVLTilRegel;
 
@@ -70,9 +68,9 @@ public class BeregnYtelseTjeneste {
 
     private void log(BeregningsresultatRegelmodell grunnlag) {
         try {
-            String maskertRegelinput = jackson.toJson(grunnlag).replaceAll("\\d{13}|\\d{11}|\\d{9}", "*");
+            String maskertRegelinput = JsonOutput.asJson(grunnlag).replaceAll("\\d{13}|\\d{11}|\\d{9}", "*");
             LOG.info("Regelinput til beregning av tilkjent ytelse {}", maskertRegelinput);
-        } catch (JsonProcessingException jsonProcessingException) {
+        } catch (NareJsonException jsonProcessingException) {
             LOG.warn("Feil ved logging av regelinput", jsonProcessingException);
         }
     }
--- domenetjenester/uttak/src/main/java/no/nav/foreldrepenger/domene/uttak/fastsetteperioder/FastsettePerioderRegelAdapter.java
+++ domenetjenester/uttak/src/main/java/no/nav/foreldrepenger/domene/uttak/fastsetteperioder/FastsettePerioderRegelAdapter.java
@@ -9,22 +9,19 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
 import no.nav.foreldrepenger.behandlingslager.uttak.fp.UttakResultatPerioderEntitet;
 import no.nav.foreldrepenger.domene.uttak.input.UttakInput;
-import no.nav.foreldrepenger.regler.jackson.JacksonJsonConfig;
 import no.nav.foreldrepenger.regler.uttak.fastsetteperiode.FastsettePeriodeResultat;
 import no.nav.foreldrepenger.regler.uttak.fastsetteperiode.FastsettePerioderRegelOrkestrering;
 import no.nav.foreldrepenger.regler.uttak.fastsetteperiode.grunnlag.RegelGrunnlag;
+import no.nav.fpsak.nare.json.JsonOutput;
+import no.nav.fpsak.nare.json.NareJsonException;
 
 @ApplicationScoped
 public class FastsettePerioderRegelAdapter {
 
     private static final Logger LOG = LoggerFactory.getLogger(FastsettePerioderRegelAdapter.class);
     private static final FastsettePerioderRegelOrkestrering REGEL = new FastsettePerioderRegelOrkestrering();
-
-    private final JacksonJsonConfig jackson = new JacksonJsonConfig();
 
     private FastsettePerioderRegelGrunnlagBygger regelGrunnlagBygger;
     private FastsettePerioderRegelResultatKonverterer regelResultatKonverterer;
@@ -54,9 +51,9 @@ public class FastsettePerioderRegelAdapter {
 
     private void log(RegelGrunnlag grunnlag) {
         try {
-            LOG.info("Fastsette uttaksperioder feilet med grunnlag {}", jackson.toJson(grunnlag)
+            LOG.info("Fastsette uttaksperioder feilet med grunnlag {}", JsonOutput.asJson(grunnlag)
                 .replaceAll("\\d{13}|\\d{11}|\\d{9}", "*"));
-        } catch (JsonProcessingException jsonProcessingException) {
+        } catch (NareJsonException jsonProcessingException) {
             LOG.warn("Feil ved logging av regelgrunnlaget", jsonProcessingException);
         }
     }
