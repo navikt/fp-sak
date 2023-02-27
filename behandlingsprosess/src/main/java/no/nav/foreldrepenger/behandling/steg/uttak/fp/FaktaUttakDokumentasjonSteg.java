b29887f63aad6f9ec89ab4f484b664786e53f69e
Anders Palfi <anders.palfi@nav.no>
2023-02-27 14:14:01 +0100

    TFP-4873 ikke utfør AP hvis det avbrutt og ikke utført

--- behandlingsprosess/src/main/java/no/nav/foreldrepenger/behandling/steg/uttak/fp/FaktaUttakDokumentasjonSteg.java
+++ behandlingsprosess/src/main/java/no/nav/foreldrepenger/behandling/steg/uttak/fp/FaktaUttakDokumentasjonSteg.java
@@ -1,5 +1,7 @@
 package no.nav.foreldrepenger.behandling.steg.uttak.fp;
 
+import static no.nav.foreldrepenger.behandlingslager.behandling.aksjonspunkt.AksjonspunktDefinisjon.VURDER_UTTAK_DOKUMENTASJON;
+
 import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -14,10 +16,10 @@ import no.nav.foreldrepenger.behandlingskontroll.BehandlingTypeRef;
 import no.nav.foreldrepenger.behandlingskontroll.BehandlingskontrollKontekst;
 import no.nav.foreldrepenger.behandlingskontroll.FagsakYtelseTypeRef;
 import no.nav.foreldrepenger.behandlingslager.behandling.BehandlingStegType;
-import no.nav.foreldrepenger.behandlingslager.behandling.aksjonspunkt.AksjonspunktDefinisjon;
 import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakYtelseType;
 import no.nav.foreldrepenger.domene.uttak.fakta.uttak.VurderUttakDokumentasjonAksjonspunktUtleder;
+import no.nav.foreldrepenger.domene.uttak.input.UttakInput;
 
 @BehandlingStegRef(BehandlingStegType.FAKTA_UTTAK_DOKUMENTASJON)
 @BehandlingTypeRef
@@ -47,11 +49,17 @@ public class FaktaUttakDokumentasjonSteg implements UttakSteg {
         var behandling = behandlingRepository.hentBehandling(kontekst.getBehandlingId());
         var uttakInput = uttakInputTjeneste.lagInput(behandling);
         var utledetAp = vurderUttakDokumentasjonAksjonspunktUtleder.utledAksjonspunktFor(uttakInput);
-        if (utledetAp || (behandling.harAvbruttAksjonspunktMedType(AksjonspunktDefinisjon.VURDER_UTTAK_DOKUMENTASJON)
-            && !vurderUttakDokumentasjonAksjonspunktUtleder.utledDokumentasjonVurderingBehov(uttakInput).isEmpty())) {
-            var aksjonspunktResultat = AksjonspunktResultat.opprettAvbruttTilUtførtForAksjonspunkt(AksjonspunktDefinisjon.VURDER_UTTAK_DOKUMENTASJON);
+        if (utledetAp) {
+            return BehandleStegResultat.utførtMedAksjonspunkter(List.of(VURDER_UTTAK_DOKUMENTASJON));
+        }
+        if (behandling.harAvbruttAksjonspunktMedType(VURDER_UTTAK_DOKUMENTASJON) && harDokVurderingBehov(uttakInput)) {
+            var aksjonspunktResultat = AksjonspunktResultat.opprettAvbruttTilUtførtForAksjonspunkt(VURDER_UTTAK_DOKUMENTASJON);
             return BehandleStegResultat.utførtMedAksjonspunktResultater(List.of(aksjonspunktResultat));
         }
         return BehandleStegResultat.utførtUtenAksjonspunkter();
+    }
+
+    private boolean harDokVurderingBehov(UttakInput uttakInput) {
+        return !vurderUttakDokumentasjonAksjonspunktUtleder.utledDokumentasjonVurderingBehov(uttakInput).isEmpty();
     }
 }
