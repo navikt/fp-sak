54ce81ac1257e29e58ec24eb86e48d8a4d46f66d
anjaaalerud <anja.gotesen.aalerud@nav.no>
2023-03-06 10:08:56 +0100

    TFP-4124 Bruker SkjæringstidspunktTjeneste for å finne første uttaksdato for alle ytelser

--- web/src/test/java/no/nav/foreldrepenger/web/app/tjenester/fordeling/SakInfoDtoTjenesteTest.java
+++ web/src/test/java/no/nav/foreldrepenger/web/app/tjenester/fordeling/SakInfoDtoTjenesteTest.java
@@ -1,20 +1,12 @@
 package no.nav.foreldrepenger.web.app.tjenester.fordeling;
 
-import no.nav.foreldrepenger.behandlingslager.aktør.NavBruker;
-import no.nav.foreldrepenger.behandlingslager.behandling.Behandling;
-import no.nav.foreldrepenger.behandlingslager.behandling.familiehendelse.FamilieHendelseEntitet;
-import no.nav.foreldrepenger.behandlingslager.behandling.familiehendelse.FamilieHendelseGrunnlagEntitet;
-import no.nav.foreldrepenger.behandlingslager.behandling.familiehendelse.FamilieHendelseRepository;
-import no.nav.foreldrepenger.behandlingslager.behandling.familiehendelse.FamilieHendelseType;
-import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepository;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-import no.nav.foreldrepenger.behandlingslager.fagsak.Fagsak;
-import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakStatus;
-import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakYtelseType;
-import no.nav.foreldrepenger.behandlingslager.geografisk.Språkkode;
-import no.nav.foreldrepenger.domene.typer.AktørId;
-import no.nav.foreldrepenger.domene.typer.Saksnummer;
-import no.nav.foreldrepenger.web.app.tjenester.behandling.ytelsefordeling.FørsteUttaksdatoTjeneste;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,13 +14,21 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import no.nav.foreldrepenger.behandling.Skjæringstidspunkt;
+import no.nav.foreldrepenger.behandlingslager.aktør.NavBruker;
+import no.nav.foreldrepenger.behandlingslager.behandling.Behandling;
+import no.nav.foreldrepenger.behandlingslager.behandling.familiehendelse.FamilieHendelseEntitet;
+import no.nav.foreldrepenger.behandlingslager.behandling.familiehendelse.FamilieHendelseGrunnlagEntitet;
+import no.nav.foreldrepenger.behandlingslager.behandling.familiehendelse.FamilieHendelseRepository;
+import no.nav.foreldrepenger.behandlingslager.behandling.familiehendelse.FamilieHendelseType;
+import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepository;
+import no.nav.foreldrepenger.behandlingslager.fagsak.Fagsak;
+import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakStatus;
+import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakYtelseType;
+import no.nav.foreldrepenger.behandlingslager.geografisk.Språkkode;
+import no.nav.foreldrepenger.domene.typer.AktørId;
+import no.nav.foreldrepenger.domene.typer.Saksnummer;
+import no.nav.foreldrepenger.skjæringstidspunkt.SkjæringstidspunktTjeneste;
 
 @ExtendWith(MockitoExtension.class)
 class SakInfoDtoTjenesteTest {
@@ -42,17 +42,18 @@ class SakInfoDtoTjenesteTest {
     @Mock
     private FamilieHendelseEntitet familieHendelseEntitetMock;
     @Mock
-    private FørsteUttaksdatoTjeneste førsteUttaksdatoTjenesteMock;
-
+    private SkjæringstidspunktTjeneste skjæringstidspunktTjenesteMock;
+    @Mock
+    private Skjæringstidspunkt skjæringstidspunktMock;
     private SakInfoDtoTjeneste sakInfoDtoTjeneste;
 
     @BeforeEach
     void setUp() {
-        sakInfoDtoTjeneste = new SakInfoDtoTjeneste(behandlingRepositoryMock, førsteUttaksdatoTjenesteMock, familieHendelseRepositoryMock );
+        sakInfoDtoTjeneste = new SakInfoDtoTjeneste(behandlingRepositoryMock, familieHendelseRepositoryMock, skjæringstidspunktTjenesteMock );
     }
 
     @Test
-    void skalMappeSakInfoDtoMedAlleVerdier() {
+    void skalMappeFPSakInfoDtoMedAlleVerdier() {
         var saknr = new Saksnummer("TEST3");
         var opprettetTidSak1 = LocalDateTime.now().minusMonths(16);
         var skjæringstidspunkt = LocalDate.now().minusMonths(15);
@@ -65,7 +66,8 @@ class SakInfoDtoTjenesteTest {
         var behandling = Behandling.forFørstegangssøknad(fagsak).build();
 
         when(behandlingRepositoryMock.finnSisteIkkeHenlagteYtelseBehandlingFor(any())).thenReturn(Optional.of(behandling));
-        when(førsteUttaksdatoTjenesteMock.finnFørsteUttaksdato(behandling)).thenReturn(Optional.of(førsteuttaksdato));
+        when(skjæringstidspunktTjenesteMock.getSkjæringstidspunkter(behandling.getId())).thenReturn(skjæringstidspunktMock);
+        when(skjæringstidspunktMock.getFørsteUttaksdato()).thenReturn(førsteuttaksdato);
         when(familieHendelseRepositoryMock.hentAggregatHvisEksisterer(any())).thenReturn(Optional.of(familieHendelseGrunnlagEntitetMock));
         when(familieHendelseGrunnlagEntitetMock.getGjeldendeVersjon()).thenReturn(familieHendelseEntitetMock);
         when(familieHendelseEntitetMock.getSkjæringstidspunkt()).thenReturn(skjæringstidspunkt);
@@ -83,7 +85,41 @@ class SakInfoDtoTjenesteTest {
     }
 
     @Test
-    void skalMappeSakInfoDtoMenNullVerdier() {
+    void skalMappeSVPSakInfoDtoMedAlleVerdier() {
+        var saknr = new Saksnummer("TEST3");
+        var opprettetTidSak1 = LocalDateTime.now().minusMonths(16);
+        var skjæringstidspunkt = LocalDate.now().minusMonths(15);
+        var førsteuttaksdato = LocalDate.now().minusMonths(6);
+
+
+        Fagsak fagsak= Fagsak.opprettNy(FagsakYtelseType.SVANGERSKAPSPENGER, NavBruker.opprettNy(AKTØR_ID_MOR, Språkkode.NB), saknr);
+        fagsak.setOpprettetTidspunkt(opprettetTidSak1);
+        fagsak.setId(125L);
+        var behandling = Behandling.forFørstegangssøknad(fagsak).build();
+
+        when(behandlingRepositoryMock.finnSisteIkkeHenlagteYtelseBehandlingFor(any())).thenReturn(Optional.of(behandling));
+        when(familieHendelseRepositoryMock.hentAggregatHvisEksisterer(any())).thenReturn(Optional.of(familieHendelseGrunnlagEntitetMock));
+        when(familieHendelseGrunnlagEntitetMock.getGjeldendeVersjon()).thenReturn(familieHendelseEntitetMock);
+        when(familieHendelseEntitetMock.getSkjæringstidspunkt()).thenReturn(skjæringstidspunkt);
+        when(familieHendelseEntitetMock.getType()).thenReturn(FamilieHendelseType.FØDSEL);
+
+        when(skjæringstidspunktTjenesteMock.getSkjæringstidspunkter(behandling.getId())).thenReturn(skjæringstidspunktMock);
+        when(skjæringstidspunktMock.getFørsteUttaksdato()).thenThrow(new NullPointerException());
+        when(skjæringstidspunktMock.getSkjæringstidspunktHvisUtledet()).thenReturn(Optional.of(førsteuttaksdato));
+
+        var sakInfoDto = sakInfoDtoTjeneste.mapSakInfoDto(fagsak);
+
+        assertThat(sakInfoDto.saksnummer().getSaksnummer()).isEqualTo(saknr.getVerdi());
+        assertThat(sakInfoDto.status()).isEqualTo(sakInfoDtoTjeneste.mapFagsakStatusDto(FagsakStatus.OPPRETTET));
+        assertThat(sakInfoDto.opprettetDato()).isEqualTo(opprettetTidSak1.toLocalDate());
+        assertThat(sakInfoDto.ytelseType()).isEqualTo(sakInfoDtoTjeneste.mapFagsakYtelseTypeDto(FagsakYtelseType.SVANGERSKAPSPENGER));
+        assertThat(sakInfoDto.familiehendelseInfoDto().familiehendelseDato()).isEqualTo(skjæringstidspunkt);
+        assertThat(sakInfoDto.familiehendelseInfoDto().familihendelseType()).isEqualTo(sakInfoDtoTjeneste.mapFamilieHendelseTypeDto(FamilieHendelseType.FØDSEL));
+        assertThat(sakInfoDto.førsteUttaksdato()).isEqualTo(førsteuttaksdato);
+    }
+
+    @Test
+    void skalMappeSakInfoDtoMedNullVerdier() {
         var saknr = new Saksnummer("TEST4");
         var ytelseType = FagsakYtelseType.FORELDREPENGER;
         var opprettetTid = LocalDateTime.now().minusMonths(16);
