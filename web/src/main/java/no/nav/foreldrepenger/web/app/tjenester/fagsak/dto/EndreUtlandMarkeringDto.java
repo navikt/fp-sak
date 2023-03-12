376bbe4d2926b7c8cf703fbe7940e4aebec4a6fa
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-03-12 23:44:40 +0100

    Manglende validering

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/fagsak/dto/EndreUtlandMarkeringDto.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/fagsak/dto/EndreUtlandMarkeringDto.java
@@ -1,5 +1,6 @@
 package no.nav.foreldrepenger.web.app.tjenester.fagsak.dto;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
 
@@ -7,5 +8,5 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import no.nav.foreldrepenger.behandlingslager.fagsak.egenskaper.UtlandMarkering;
 
-public record EndreUtlandMarkeringDto(@JsonProperty("saksnummer") @NotNull @Digits(integer = 18, fraction = 0) String saksnummer, UtlandMarkering utlandMarkering) {
+public record EndreUtlandMarkeringDto(@JsonProperty("saksnummer") @NotNull @Digits(integer = 18, fraction = 0) String saksnummer, @NotNull@Valid UtlandMarkering utlandMarkering) {
 }
