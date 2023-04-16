4c0adc987b6a9a56c00d014005042551cb173c52
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-04-16 19:04:15 +0200

    Brukt task pga batch-tilgang

---
+++ domenetjenester/produksjonsstyring/src/main/java/no/nav/foreldrepenger/produksjonsstyring/batch/FlyttEnhetTask.java
@@ -0,0 +1,46 @@
+package no.nav.foreldrepenger.produksjonsstyring.batch;
+
+import java.util.Optional;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+
+import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingKandidaterRepository;
+import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakProsesstaskRekkefølge;
+import no.nav.foreldrepenger.produksjonsstyring.behandlingenhet.task.OppdaterBehandlendeEnhetTask;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTask;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTaskHandler;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTaskTjeneste;
+
+@Dependent
+@ProsessTask("oppgavebehandling.flytt.enhet")
+@FagsakProsesstaskRekkefølge(gruppeSekvens = false)
+class FlyttEnhetTask implements ProsessTaskHandler {
+
+    public static final String ENHET_ID = "enhetId";
+
+    private BehandlingKandidaterRepository behandlingKandidaterRepository;
+    private ProsessTaskTjeneste taskTjeneste;
+
+    @Inject
+    public FlyttEnhetTask(BehandlingKandidaterRepository behandlingKandidaterRepository, ProsessTaskTjeneste taskTjeneste) {
+        this.taskTjeneste =taskTjeneste;
+        this.behandlingKandidaterRepository = behandlingKandidaterRepository;
+
+    }
+
+    @Override
+    public void doTask(ProsessTaskData prosessTaskData) {
+        var enhet = Optional.ofNullable(prosessTaskData.getPropertyValue(ENHET_ID)).orElseThrow();
+        var kandidater = behandlingKandidaterRepository.finnBehandlingerIkkeAvsluttetPåAngittEnhet(enhet);
+        kandidater.forEach(beh -> {
+            var taskData = ProsessTaskData.forProsessTask(OppdaterBehandlendeEnhetTask.class);
+            taskData.setBehandling(beh.getFagsakId(), beh.getId(), beh.getAktørId().getId());
+            taskData.setCallIdFraEksisterende();
+            taskTjeneste.lagre(taskData);
+        });
+    }
+
+
+}
