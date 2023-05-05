278f88a978bf0074b905bee19ddfbc74533b95a8
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-05-05 22:14:58 +0200

    Utilsiktet task

--- domenetjenester/behandling-revurdering/src/main/java/no/nav/foreldrepenger/behandling/revurdering/satsregulering/GrunnbelopUtvalgTask.java
+++ domenetjenester/behandling-revurdering/src/main/java/no/nav/foreldrepenger/behandling/revurdering/satsregulering/GrunnbelopUtvalgTask.java
@@ -1,26 +1,18 @@
 package no.nav.foreldrepenger.behandling.revurdering.satsregulering;
 
-import java.math.BigDecimal;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import no.nav.foreldrepenger.behandling.revurdering.RevurderingTjeneste;
 import no.nav.foreldrepenger.behandling.revurdering.flytkontroll.BehandlingFlytkontroll;
-import no.nav.foreldrepenger.behandlingskontroll.FagsakYtelseTypeRef;
-import no.nav.foreldrepenger.behandlingslager.behandling.BehandlingÅrsakType;
-import no.nav.foreldrepenger.behandlingslager.behandling.beregning.BeregningSatsType;
 import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepositoryProvider;
 import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakProsesstaskRekkefølge;
 import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakRepository;
 import no.nav.foreldrepenger.behandlingsprosess.prosessering.BehandlingProsesseringTjeneste;
-import no.nav.foreldrepenger.domene.entiteter.BeregningsgrunnlagEntitet;
 import no.nav.foreldrepenger.domene.entiteter.BeregningsgrunnlagRepository;
-import no.nav.foreldrepenger.domene.typer.Beløp;
 import no.nav.foreldrepenger.produksjonsstyring.behandlingenhet.BehandlendeEnhetTjeneste;
 import no.nav.foreldrepenger.skjæringstidspunkt.SkjæringstidspunktTjeneste;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTask;
@@ -67,40 +59,7 @@ public class GrunnbelopUtvalgTask implements ProsessTaskHandler {
     @Override
     public void doTask(ProsessTaskData prosessTaskData) {
         // For å sikre at fagsaken hentes opp i cache - ellers dukker den opp via readonly-query og det blir problem.
-        var fagsak = fagsakRepository.finnEksaktFagsak(fagsakId);
 
-        // Implisitt precondition fra utvalget i batches: Ingen ytelsesbehandlinger
-        // utenom evt berørt behandling.
-        var åpneYtelsesBehandlinger = behandlingRepository.harÅpenOrdinærYtelseBehandlingerForFagsakId(fagsakId);
-        if (åpneYtelsesBehandlinger) {
-            LOG.info("GrunnbeløpRegulering finnes allerede åpen revurdering på fagsakId = {}", fagsakId);
-            return;
-        }
-        if (prosessTaskData.getPropertyValue(MANUELL_KEY) == null) {
-            var sisteVedtatte = behandlingRepository.finnSisteAvsluttedeIkkeHenlagteBehandling(fagsakId).orElseThrow();
-            var skjæringstidspunkt = skjæringstidspunktTjeneste.getSkjæringstidspunkterForAvsluttetBehandling(sisteVedtatte.getId());
-            var grunnbeløpFraSisteVedtatt = beregningsgrunnlagRepository.hentBeregningsgrunnlagForBehandling(sisteVedtatte.getId())
-                .map(BeregningsgrunnlagEntitet::getGrunnbeløp)
-                .map(Beløp::getVerdi).map(BigDecimal::longValue).orElse(0L);
-            var skalBrukeGrunnbeløp = beregningsgrunnlagRepository.finnEksaktSats(BeregningSatsType.GRUNNBELØP, skjæringstidspunkt.getFørsteUttaksdatoGrunnbeløp()).getVerdi();
-            if (grunnbeløpFraSisteVedtatt == skalBrukeGrunnbeløp) {
-                LOG.info("GrunnbeløpRegulering har rett G for saksnummer = {} stp {}", fagsak.getSaksnummer().getVerdi(), skjæringstidspunkt.getFørsteUttaksdatoGrunnbeløp());
-                return;
-            }
-        }
-
-        var skalKøes = flytkontroll.nyRevurderingSkalVente(fagsak);
-        var enhet = enhetTjeneste.finnBehandlendeEnhetFor(fagsak);
-        var revurderingTjeneste = FagsakYtelseTypeRef.Lookup.find(RevurderingTjeneste.class, fagsak.getYtelseType()).orElseThrow();
-        var revurdering = revurderingTjeneste.opprettAutomatiskRevurdering(fagsak, BehandlingÅrsakType.RE_SATS_REGULERING, enhet);
-
-        LOG.info("GrunnbeløpRegulering har opprettet revurdering på saksnummer = {}", fagsak.getSaksnummer().getVerdi());
-
-        if (skalKøes) {
-            flytkontroll.settNyRevurderingPåVent(revurdering);
-        } else {
-            behandlingProsesseringTjeneste.opprettTasksForStartBehandling(revurdering);
-        }
 
 
     }
