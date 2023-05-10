5d6b57472051860c2175bcaa7482bcbb60fbe3a7
Anders Palfi <anderspalfi@gmail.com>
2023-05-10 14:12:00 +0200

    familiehendelse til fpoversikt (#5757)

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/fpoversikt/SvpSak.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/fpoversikt/SvpSak.java
@@ -1,9 +1,9 @@
 package no.nav.foreldrepenger.web.app.tjenester.fpoversikt;
 
-record SvpSak(String saksnummer, String aktørId) implements Sak {
+record SvpSak(String saksnummer, String aktørId, FamilieHendelse familieHendelse) implements Sak {
 
     @Override
     public String toString() {
-        return "SvpSak{" + "saksnummer='" + saksnummer + '\'' + '}';
+        return "SvpSak{" + "saksnummer='" + saksnummer + '\'' + ", familieHendelse=" + familieHendelse + '}';
     }
 }
