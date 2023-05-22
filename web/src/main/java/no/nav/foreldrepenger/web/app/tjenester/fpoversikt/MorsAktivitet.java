cf6b3fa65434016c72ddf0f9245f43e2e617bc0c
Anders Palfi <anderspalfi@gmail.com>
2023-05-22 14:47:14 +0200

    fpoversikt: søknadsperioder utvidelse (#5770)

---
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/fpoversikt/MorsAktivitet.java
@@ -0,0 +1,13 @@
+package no.nav.foreldrepenger.web.app.tjenester.fpoversikt;
+
+public enum MorsAktivitet {
+    ARBEID,
+    UTDANNING,
+    KVALPROG,
+    INTROPROG,
+    TRENGER_HJELP,
+    INNLAGT,
+    ARBEID_OG_UTDANNING,
+    UFØRE,
+    IKKE_OPPGITT
+}
