ea275face3120d7d42956491f5e73af870701a71
Anders Palfi <anders.palfi@nav.no>
2023-06-08 12:09:35 +0200

    FAGSYSTEM-279629 fikser bug rundt saldovisning ved gradering og minsteretter

--- domenetjenester/uttak/src/main/java/no/nav/foreldrepenger/domene/uttak/fastsetteperioder/validering/SaldoValidering.java
+++ domenetjenester/uttak/src/main/java/no/nav/foreldrepenger/domene/uttak/fastsetteperioder/validering/SaldoValidering.java
@@ -1,11 +1,13 @@
 package no.nav.foreldrepenger.domene.uttak.fastsetteperioder.validering;
 
+import java.math.RoundingMode;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import no.nav.foreldrepenger.domene.uttak.ForeldrepengerUttakPeriode;
+import no.nav.foreldrepenger.regler.uttak.fastsetteperiode.Trekkdager;
 import no.nav.foreldrepenger.regler.uttak.fastsetteperiode.grunnlag.Stønadskontotype;
 import no.nav.foreldrepenger.regler.uttak.fastsetteperiode.saldo.SaldoUtregning;
 
@@ -39,19 +41,19 @@ public class SaldoValidering implements OverstyrUttakPerioderValidering {
         }
         if (saldoUtregning.getMaxDagerFlerbarnsdager().merEnn0()) {
             var restSaldoDagerFlerbarnsdager = saldoUtregning.restSaldoFlerbarnsdager();
-            if (restSaldoDagerFlerbarnsdager.mindreEnn0()) {
+            if (round(restSaldoDagerFlerbarnsdager) < 0) {
                 throw OverstyrUttakValideringFeil.trekkdagerOverskriderKontoMaksDager();
             }
         }
         if (saldoUtregning.getMaxDagerUtenAktivitetskrav().merEnn0()) {
             var restSaldoDagerUtenAktivitetskrav = saldoUtregning.restSaldoDagerUtenAktivitetskrav();
-            if (restSaldoDagerUtenAktivitetskrav.mindreEnn0()) {
+            if (round(restSaldoDagerUtenAktivitetskrav) < 0) {
                 throw OverstyrUttakValideringFeil.trekkdagerOverskriderKontoMaksDager();
             }
         }
         if (saldoUtregning.getMaxDagerMinsterett().merEnn0()) {
             var restSaldoDagerMinsterett = saldoUtregning.restSaldoMinsterett();
-            if (restSaldoDagerMinsterett.mindreEnn0()) {
+            if (round(restSaldoDagerMinsterett) < 0) {
                 throw OverstyrUttakValideringFeil.trekkdagerOverskriderKontoMaksDager();
             }
         }
@@ -72,6 +74,13 @@ public class SaldoValidering implements OverstyrUttakPerioderValidering {
             return new SaldoValideringResultat(false, false);
         }
         return new SaldoValideringResultat(true, false);
+    }
+
+    public static int round(Trekkdager trekkdager) {
+        //Ved gradering kan saldo med desimal være feks -0.2. Dette skal tolkes som 0. Man skal ikke bikke over til -1 så lenge trekkdager er mellom -1 og 0
+        return trekkdager.mindreEnn0() ? trekkdager.decimalValue()
+            .setScale(0, RoundingMode.DOWN)
+            .intValue() : trekkdager.rundOpp();
     }
 
     public record SaldoValideringResultat(boolean isGyldig, boolean isNegativPgaSamtidigUttak) {
