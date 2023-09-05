93486c98051d913c61814d48e85985eee6232a28
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-09-05 13:17:11 +0200

    Foreslå Vedtak - flere aksjonspunkt krever eksplisitt historikk

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/vedtak/aksjonspunkt/VurderDokumentFørVedtakOppdaterer.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/vedtak/aksjonspunkt/VurderDokumentFørVedtakOppdaterer.java
@@ -7,7 +7,10 @@ import no.nav.foreldrepenger.behandling.aksjonspunkt.AksjonspunktOppdaterParamet
 import no.nav.foreldrepenger.behandling.aksjonspunkt.AksjonspunktOppdaterer;
 import no.nav.foreldrepenger.behandling.aksjonspunkt.DtoTilServiceAdapter;
 import no.nav.foreldrepenger.behandling.aksjonspunkt.OppdateringResultat;
+import no.nav.foreldrepenger.behandlingslager.behandling.historikk.HistorikkAktør;
+import no.nav.foreldrepenger.behandlingslager.behandling.historikk.Historikkinnslag;
 import no.nav.foreldrepenger.behandlingslager.behandling.historikk.HistorikkinnslagType;
+import no.nav.foreldrepenger.historikk.HistorikkInnslagTekstBuilder;
 import no.nav.foreldrepenger.historikk.HistorikkTjenesteAdapter;
 
 @ApplicationScoped
@@ -27,8 +30,16 @@ class VurderDokumentFørVedtakOppdaterer implements AksjonspunktOppdaterer<Vurde
 
     @Override
     public OppdateringResultat oppdater(VurdereDokumentFørVedtakDto dto, AksjonspunktOppdaterParameter param) {
-        historikkAdapter.tekstBuilder().medBegrunnelse("Vurder dokument", true);
-        historikkAdapter.opprettHistorikkInnslag(param.getBehandlingId(), HistorikkinnslagType.OPPGAVE_VEDTAK);
+        var tekstBuilder = new HistorikkInnslagTekstBuilder()
+            .medHendelse(HistorikkinnslagType.OPPGAVE_VEDTAK)
+            .medBegrunnelse("Vurder dokument", true);
+
+        var innslag = new Historikkinnslag();
+        innslag.setType(HistorikkinnslagType.OPPGAVE_VEDTAK);
+        innslag.setAktør(HistorikkAktør.SAKSBEHANDLER);
+        innslag.setBehandlingId(param.getBehandlingId());
+        tekstBuilder.build(innslag);
+        historikkAdapter.lagInnslag(innslag);
         return OppdateringResultat.utenOveropp();
     }
 }
