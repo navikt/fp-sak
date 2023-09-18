091df3b1876f7142a5104ac711aca95a8bfcaa64
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-09-18 22:10:17 +0200

    TFP-5566 fjerne mulighet til å opprette anke fra fpsak

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/dto/BehandlingOpprettingDto.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/dto/BehandlingOpprettingDto.java
@@ -2,21 +2,5 @@ package no.nav.foreldrepenger.web.app.tjenester.behandling.dto;
 
 import no.nav.foreldrepenger.behandlingslager.behandling.BehandlingType;
 
-public class BehandlingOpprettingDto {
-
-    private BehandlingType behandlingType;
-    private boolean kanOppretteBehandling;
-
-    public BehandlingOpprettingDto(BehandlingType behandlingType, boolean kanOppretteBehandling) {
-        this.behandlingType = behandlingType;
-        this.kanOppretteBehandling = kanOppretteBehandling;
-    }
-
-    public BehandlingType getBehandlingType() {
-        return behandlingType;
-    }
-
-    public boolean isKanOppretteBehandling() {
-        return kanOppretteBehandling;
-    }
+public record BehandlingOpprettingDto(BehandlingType behandlingType, boolean kanOppretteBehandling) {
 }
