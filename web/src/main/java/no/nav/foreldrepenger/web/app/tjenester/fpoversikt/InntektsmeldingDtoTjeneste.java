5393355003f8621ceec6e813de12cf593cba7b55
Anders Palfi <anderspalfi@gmail.com>
2023-09-18 15:37:28 +0200

    Utvider IM for fpoversikt med mottatt tidspunkt fra mottatt dok (#5980)

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/fpoversikt/InntektsmeldingDtoTjeneste.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/fpoversikt/InntektsmeldingDtoTjeneste.java
@@ -1,11 +1,14 @@
 package no.nav.foreldrepenger.web.app.tjenester.fpoversikt;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
+import no.nav.foreldrepenger.behandlingslager.behandling.MottattDokument;
+import no.nav.foreldrepenger.behandlingslager.behandling.repository.MottatteDokumentRepository;
 import no.nav.foreldrepenger.domene.arbeidsforhold.InntektsmeldingTjeneste;
 import no.nav.foreldrepenger.domene.iay.modell.Inntektsmelding;
 import no.nav.foreldrepenger.domene.typer.Saksnummer;
@@ -14,10 +17,12 @@ import no.nav.foreldrepenger.domene.typer.Saksnummer;
 class InntektsmeldingDtoTjeneste {
 
     private InntektsmeldingTjeneste inntektsmeldingTjeneste;
+    private MottatteDokumentRepository mottatteDokumentRepository;
 
     @Inject
-    InntektsmeldingDtoTjeneste(InntektsmeldingTjeneste inntektsmeldingTjeneste) {
+    InntektsmeldingDtoTjeneste(InntektsmeldingTjeneste inntektsmeldingTjeneste, MottatteDokumentRepository mottatteDokumentRepository) {
         this.inntektsmeldingTjeneste = inntektsmeldingTjeneste;
+        this.mottatteDokumentRepository = mottatteDokumentRepository;
     }
 
     InntektsmeldingDtoTjeneste() {
@@ -34,7 +39,12 @@ class InntektsmeldingDtoTjeneste {
 
     private InntektsmeldingDto map(Inntektsmelding inntektsmelding) {
         var arbeidsgiver = new Arbeidsgiver(inntektsmelding.getArbeidsgiver().getIdentifikator());
+        var mottattTidspunkt = mottatteDokumentRepository.hentMottattDokument(inntektsmelding.getJournalpostId())
+            .stream()
+            .map(MottattDokument::getMottattTidspunkt)
+            .min(LocalDateTime::compareTo)
+            .orElseThrow();
         return new InntektsmeldingDto(inntektsmelding.getJournalpostId(),
-            arbeidsgiver, inntektsmelding.getInnsendingstidspunkt(), inntektsmelding.getInntektBeløp().getVerdi());
+            arbeidsgiver, inntektsmelding.getInnsendingstidspunkt(), inntektsmelding.getInntektBeløp().getVerdi(), mottattTidspunkt);
     }
 }
--- web/src/test/java/no/nav/foreldrepenger/web/app/tjenester/fpoversikt/InntektsmeldingDtoTjenesteTest.java
+++ web/src/test/java/no/nav/foreldrepenger/web/app/tjenester/fpoversikt/InntektsmeldingDtoTjenesteTest.java
@@ -5,26 +5,36 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
+import jakarta.persistence.EntityManager;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import no.nav.foreldrepenger.behandlingslager.behandling.Behandling;
+import no.nav.foreldrepenger.behandlingslager.behandling.MottattDokument;
+import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepositoryProvider;
 import no.nav.foreldrepenger.behandlingslager.testutilities.behandling.ScenarioMorSøkerForeldrepenger;
 import no.nav.foreldrepenger.behandlingslager.virksomhet.Arbeidsgiver;
+import no.nav.foreldrepenger.dbstoette.JpaExtension;
 import no.nav.foreldrepenger.domene.abakus.AbakusInMemoryInntektArbeidYtelseTjeneste;
 import no.nav.foreldrepenger.domene.arbeidsforhold.InntektsmeldingTjeneste;
 import no.nav.foreldrepenger.domene.iay.modell.InntektsmeldingBuilder;
 import no.nav.foreldrepenger.domene.typer.Beløp;
 import no.nav.foreldrepenger.domene.typer.JournalpostId;
 
+@ExtendWith(JpaExtension.class)
 class InntektsmeldingDtoTjenesteTest {
 
     @Test
-    void henter_im() {
+    void henter_im(EntityManager entityManager) {
         var iayTjeneste = new AbakusInMemoryInntektArbeidYtelseTjeneste();
-        var tjeneste = new InntektsmeldingDtoTjeneste(new InntektsmeldingTjeneste(iayTjeneste));
+        var repositoryProvider = new BehandlingRepositoryProvider(entityManager);
+        var tjeneste = new InntektsmeldingDtoTjeneste(new InntektsmeldingTjeneste(iayTjeneste), repositoryProvider.getMottatteDokumentRepository());
 
         var scenario = ScenarioMorSøkerForeldrepenger.forFødsel();
-        var behandling = scenario.lagMocked();
+        var behandling = scenario.lagre(repositoryProvider);
         var innsendingstidspunkt = LocalDateTime.now();
+        var mottattTidspunkt = LocalDateTime.now().minusWeeks(1);
         var inntekt = new Beløp(400000);
         var arbeidsgiver = Arbeidsgiver.virksomhet("123");
         var journalpostId = new JournalpostId("456");
@@ -33,15 +43,28 @@ class InntektsmeldingDtoTjenesteTest {
             .medBeløp(inntekt.getVerdi())
             .medJournalpostId(journalpostId)
             .medInnsendingstidspunkt(innsendingstidspunkt);
-        iayTjeneste.lagreInntektsmeldinger(behandling.getFagsak().getSaksnummer(), behandling.getId(), List.of(imBuilder));
+        var fagsak = behandling.getFagsak();
+        iayTjeneste.lagreInntektsmeldinger(fagsak.getSaksnummer(), behandling.getId(), List.of(imBuilder));
 
-        var inntektsmeldingerForSak = tjeneste.hentInntektsmeldingerForSak(behandling.getFagsak().getSaksnummer());
+        repositoryProvider.getMottatteDokumentRepository().lagre(mottattDokument(behandling, journalpostId, mottattTidspunkt));
+
+        var inntektsmeldingerForSak = tjeneste.hentInntektsmeldingerForSak(fagsak.getSaksnummer());
 
         assertThat(inntektsmeldingerForSak).hasSize(1);
         var im = inntektsmeldingerForSak.stream().findFirst().get();
         assertThat(im.arbeidsgiver().identifikator()).isEqualTo(arbeidsgiver.getIdentifikator());
         assertThat(im.innsendingstidspunkt()).isEqualTo(innsendingstidspunkt);
+        assertThat(im.mottattTidspunkt()).isEqualTo(mottattTidspunkt);
         assertThat(im.inntekt()).isEqualTo(inntekt.getVerdi());
         assertThat(im.journalpostId()).isEqualTo(journalpostId);
+    }
+
+    private static MottattDokument mottattDokument(Behandling behandling, JournalpostId journalPostId, LocalDateTime mottattTidspunkt) {
+        return new MottattDokument.Builder()
+            .medJournalPostId(journalPostId)
+            .medMottattTidspunkt(mottattTidspunkt)
+            .medFagsakId(behandling.getFagsakId())
+            .medBehandlingId(behandling.getId())
+            .build();
     }
 }
