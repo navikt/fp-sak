599930c09554803eb1e84de8b267ab8b30dfe760
AnjaAalerud <52454508+AnjaAalerud@users.noreply.github.com>
2023-10-17 10:16:45 +0200

    TFP-3739 Lagt til mellomlagring av formkrav med fritekst (#6022)

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/klage/aksjonspunkt/KlageTilbakekrevingDto.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/klage/aksjonspunkt/KlageTilbakekrevingDto.java
@@ -1,7 +1,13 @@
 package no.nav.foreldrepenger.web.app.tjenester.behandling.klage.aksjonspunkt;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+import no.nav.vedtak.util.InputValideringRegex;
+
 import java.time.LocalDate;
 import java.util.UUID;
 
-public record KlageTilbakekrevingDto(UUID tilbakekrevingUuid, LocalDate tilbakekrevingVedtakDato, String tilbakekrevingBehandlingType) {
+public record KlageTilbakekrevingDto(@NotNull UUID tilbakekrevingUuid, LocalDate tilbakekrevingVedtakDato, @Size(min = 2, max = 2) @Pattern(regexp = InputValideringRegex.KODEVERK) String tilbakekrevingBehandlingType) {
 }
