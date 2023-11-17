b3b4509f5df050d80aa21a0f44143ec597c88191
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-11-17 11:14:27 +0100

    Logging for å fange et tilfelle i prod

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/uttak/aksjonspunkt/BekreftAleneomsorgOppdaterer.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/uttak/aksjonspunkt/BekreftAleneomsorgOppdaterer.java
@@ -2,6 +2,9 @@ package no.nav.foreldrepenger.web.app.tjenester.behandling.uttak.aksjonspunkt;
 
 import java.util.Objects;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
@@ -9,6 +12,7 @@ import no.nav.foreldrepenger.behandling.aksjonspunkt.AksjonspunktOppdaterParamet
 import no.nav.foreldrepenger.behandling.aksjonspunkt.AksjonspunktOppdaterer;
 import no.nav.foreldrepenger.behandling.aksjonspunkt.DtoTilServiceAdapter;
 import no.nav.foreldrepenger.behandling.aksjonspunkt.OppdateringResultat;
+import no.nav.foreldrepenger.behandling.steg.simulering.SimulerOppdragSteg;
 import no.nav.foreldrepenger.web.app.tjenester.behandling.uttak.app.FaktaOmsorgRettTjeneste;
 import no.nav.foreldrepenger.web.app.tjenester.behandling.uttak.dto.AvklarAleneomsorgVurderingDto;
 import no.nav.vedtak.exception.FunksjonellException;
@@ -17,6 +21,7 @@ import no.nav.vedtak.exception.FunksjonellException;
 @DtoTilServiceAdapter(dto = AvklarAleneomsorgVurderingDto.class, adapter = AksjonspunktOppdaterer.class)
 public class BekreftAleneomsorgOppdaterer implements AksjonspunktOppdaterer<AvklarAleneomsorgVurderingDto> {
 
+    private static final Logger LOG = LoggerFactory.getLogger(BekreftAleneomsorgOppdaterer.class);
 
     private FaktaOmsorgRettTjeneste faktaOmsorgRettTjeneste;
 
@@ -33,6 +38,7 @@ public class BekreftAleneomsorgOppdaterer implements AksjonspunktOppdaterer<Avkl
     public OppdateringResultat oppdater(AvklarAleneomsorgVurderingDto dto, AksjonspunktOppdaterParameter param) {
         // Må ha valgt aleneomsorg = true, annenForelderRett = true, eller annenForelderRettEØS = true, eller valgt uføretrygd true/false
         if (måVelgeUføre(dto) && dto.getAnnenforelderMottarUføretrygd() == null) {
+            LOG.warn("Avklar aleneomsorg: får inn denne Dto'en som gir feil {}", dto);
             throw new FunksjonellException("FP-093924", "Avkreftet aleneomsorg mangler verdi for annen forelder rett eller uføretrygd.",
                 "Angi om annen forelder har rett eller om annen forelder mottar uføretrygd.");
         }
