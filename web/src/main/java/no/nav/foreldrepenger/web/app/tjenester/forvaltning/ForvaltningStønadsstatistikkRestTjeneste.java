9d973bbabaf3652455be99b4dcccd95cda1c2eb0
Anders Palfi <anders.palfi@nav.no>
2023-11-23 12:32:47 +0100

    Init stønadsstatistikk v2

---
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/forvaltning/ForvaltningStønadsstatistikkRestTjeneste.java
@@ -0,0 +1,45 @@
+package no.nav.foreldrepenger.web.app.tjenester.forvaltning;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.BeanParam;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import io.swagger.v3.oas.annotations.Operation;
+import no.nav.foreldrepenger.datavarehus.v2.StønadsstatistikkTjeneste;
+import no.nav.foreldrepenger.datavarehus.v2.StønadsstatistikkVedtak;
+import no.nav.foreldrepenger.web.app.tjenester.forvaltning.dto.ForvaltningBehandlingIdDto;
+import no.nav.vedtak.sikkerhet.abac.BeskyttetRessurs;
+import no.nav.vedtak.sikkerhet.abac.beskyttet.ActionType;
+import no.nav.vedtak.sikkerhet.abac.beskyttet.ResourceType;
+
+@Path("/forvaltning-stonadsstatistikk")
+@ApplicationScoped
+@Transactional
+public class ForvaltningStønadsstatistikkRestTjeneste {
+
+    private StønadsstatistikkTjeneste stønadsstatistikkTjeneste;
+
+    @Inject
+    public ForvaltningStønadsstatistikkRestTjeneste(StønadsstatistikkTjeneste stønadsstatistikkTjeneste) {
+        this.stønadsstatistikkTjeneste = stønadsstatistikkTjeneste;
+    }
+
+    ForvaltningStønadsstatistikkRestTjeneste() {
+        // CDI
+    }
+
+    @POST
+    @Path("/for-behandling")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(description = "Genereres vedtak json for en behandling", tags = "FORVALTNING-stønadsstatistikk")
+    @BeskyttetRessurs(actionType = ActionType.READ, resourceType = ResourceType.DRIFT, sporingslogg = false)
+    public StønadsstatistikkVedtak beregnKontoer(@BeanParam @Valid ForvaltningBehandlingIdDto dto) {
+        return stønadsstatistikkTjeneste.genererVedtak(dto.getBehandlingUuid());
+    }
+}
