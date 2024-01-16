562d75b0bea3e1976ba40cee3607c1a292894931
Anders Palfi <anders.palfi@nav.no>
2024-01-16 13:41:57 +0100

    Stønadsstats ignore uttaksdok for ytelse

--- domenetjenester/datavarehus/src/main/java/no/nav/foreldrepenger/datavarehus/v2/StønadsstatistikkTjeneste.java
+++ domenetjenester/datavarehus/src/main/java/no/nav/foreldrepenger/datavarehus/v2/StønadsstatistikkTjeneste.java
@@ -237,7 +237,7 @@ public class StønadsstatistikkTjeneste {
 
     private List<StønadsstatistikkUttakPeriode> mapForeldrepengerUttaksperioder(Behandling behandling, RettighetType rettighetType) {
         var logContext = String.format("saksnummer %s behandling %s", behandling.getFagsak().getSaksnummer().getVerdi(), behandling.getUuid().toString());
-        return foreldrepengerUttakTjeneste.hentUttakHvisEksisterer(behandling.getId())
+        return foreldrepengerUttakTjeneste.hentUttakHvisEksisterer(behandling.getId(), true)
             .map(u -> StønadsstatistikkUttakPeriodeMapper.mapUttak(behandling.getRelasjonsRolleType(), rettighetType, u.getGjeldendePerioder(), logContext))
             .orElse(List.of());
     }
