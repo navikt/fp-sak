ac4973919dd3cca30cdde4c46f9b3b4fe3e5f14c
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2024-01-16 20:03:37 +0100

    Fikse test

--- web/src/test/java/no/nav/foreldrepenger/web/app/tjenester/registrering/fp/SøknadMapperTest.java
+++ web/src/test/java/no/nav/foreldrepenger/web/app/tjenester/registrering/fp/SøknadMapperTest.java
@@ -584,7 +584,7 @@ class SøknadMapperTest {
         oversetter.trekkUtDataOgPersister((SøknadWrapper) SøknadWrapper.tilXmlWrapper(soeknad), mottattDokument.build(), behandling,
             Optional.empty());
 
-        verify(iayTjeneste, times(1)).lagreOppgittOpptjening(anyLong(), any(OppgittOpptjeningBuilder.class));
+        verify(iayTjeneste, times(1)).lagreOverstyrtOppgittOpptjening(anyLong(), any(OppgittOpptjeningBuilder.class));
     }
 
     @Test
