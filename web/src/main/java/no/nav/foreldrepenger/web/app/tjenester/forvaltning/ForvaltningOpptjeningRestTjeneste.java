86f62afe544e71f1485966f51b06810bbbb41415
Petter Ekern <petter.ekern@nav.no>
2024-01-25 12:35:34 +0100

    TFP-5638: Endrer swagger til å flette inn oppgitt opptjening og lagre som overstyrt

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/forvaltning/ForvaltningOpptjeningRestTjeneste.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/forvaltning/ForvaltningOpptjeningRestTjeneste.java
@@ -74,17 +74,15 @@ public class ForvaltningOpptjeningRestTjeneste {
     @BeskyttetRessurs(actionType = ActionType.CREATE, resourceType = ResourceType.DRIFT, sporingslogg = false)
     public Response leggTilOppgittFrilans(@BeanParam @Valid LeggTilOppgittFrilansDto dto) {
         var behandling = behandlingsprosessTjeneste.hentBehandling(dto.getBehandlingUuid());
-        var iayGrunnlag = inntektArbeidYtelseTjeneste.hentGrunnlag(behandling.getId());
-        if (iayGrunnlag.getGjeldendeOppgittOpptjening().isPresent() || behandling.erSaksbehandlingAvsluttet()) {
-            return Response.status(Response.Status.BAD_REQUEST).build();
-        }
+        var oppgittOpptjening = inntektArbeidYtelseTjeneste.hentGrunnlag(behandling.getId()).getGjeldendeOppgittOpptjening();
         var nyoppstartet = dto.getStpOpptjening().minusMonths(3).isBefore(dto.getFrilansFom());
         var periode = dto.getFrilansTom() != null ? DatoIntervallEntitet.fraOgMedTilOgMed(dto.getFrilansFom(), dto.getFrilansTom())
                 : DatoIntervallEntitet.fraOgMed(dto.getFrilansFom());
-        var ooBuilder = OppgittOpptjeningBuilder.ny(iayGrunnlag.getEksternReferanse(), iayGrunnlag.getOpprettetTidspunkt())
-                .leggTilAnnenAktivitet(new OppgittAnnenAktivitet(periode, ArbeidType.FRILANSER))
-                .leggTilFrilansOpplysninger(new OppgittFrilans(false, nyoppstartet, false));
-        inntektArbeidYtelseTjeneste.lagreOppgittOpptjening(behandling.getId(), ooBuilder);
+        var ooBuilder = OppgittOpptjeningBuilder.oppdater(oppgittOpptjening)
+            .leggTilAnnenAktivitet(new OppgittAnnenAktivitet(periode, ArbeidType.FRILANSER))
+            .leggTilFrilansOpplysninger(new OppgittFrilans(false, nyoppstartet, false));
+
+        inntektArbeidYtelseTjeneste.lagreOverstyrtOppgittOpptjening(behandling.getId(), ooBuilder);
 
         return Response.noContent().build();
     }
@@ -96,10 +94,7 @@ public class ForvaltningOpptjeningRestTjeneste {
     @BeskyttetRessurs(actionType = ActionType.CREATE, resourceType = ResourceType.DRIFT, sporingslogg = false)
     public Response leggTilOppgittNæring(@BeanParam @Valid LeggTilOppgittNæringDto dto) {
         var behandling = behandlingsprosessTjeneste.hentBehandling(dto.getBehandlingUuid());
-        var iayGrunnlag = inntektArbeidYtelseTjeneste.hentGrunnlag(behandling.getId());
-        if (iayGrunnlag.getGjeldendeOppgittOpptjening().isPresent() || behandling.erSaksbehandlingAvsluttet()) {
-            return Response.status(Response.Status.BAD_REQUEST).build();
-        }
+        var oppgittOpptjening = inntektArbeidYtelseTjeneste.hentGrunnlag(behandling.getId()).getGjeldendeOppgittOpptjening();
         Optional<Virksomhet> virksomhet = dto.getOrgnummer() != null ? virksomhetTjeneste.finnOrganisasjon(dto.getOrgnummer()) : Optional.empty();
         var brutto = new BigDecimal(dto.getBruttoBeløp());
         var periode = dto.getTom() != null ? DatoIntervallEntitet.fraOgMedTilOgMed(dto.getFom(), dto.getTom())
@@ -121,9 +116,9 @@ public class ForvaltningOpptjeningRestTjeneste {
         if (JA.equals(dto.getNyoppstartet())) {
             enBuilder.medNyoppstartet(true);
         }
-        var ooBuilder = OppgittOpptjeningBuilder.ny(iayGrunnlag.getEksternReferanse(), iayGrunnlag.getOpprettetTidspunkt())
-                .leggTilEgenNæring(List.of(enBuilder));
-        inntektArbeidYtelseTjeneste.lagreOppgittOpptjening(behandling.getId(), ooBuilder);
+        var ooBuilder = OppgittOpptjeningBuilder.oppdater(oppgittOpptjening)
+            .leggTilEgenNæring(List.of(enBuilder));
+        inntektArbeidYtelseTjeneste.lagreOverstyrtOppgittOpptjening(behandling.getId(), ooBuilder);
 
         return Response.noContent().build();
     }
@@ -146,10 +141,6 @@ public class ForvaltningOpptjeningRestTjeneste {
         return Response.noContent().build();
     }
 
-    private Behandling getBehandling(ForvaltningBehandlingIdDto dto) {
-        return behandlingsprosessTjeneste.hentBehandling(dto.getBehandlingUuid());
-    }
-
     @GET
     @Path("/hentOppgittOpptjening")
     @Consumes(MediaType.APPLICATION_JSON)
@@ -160,5 +151,9 @@ public class ForvaltningOpptjeningRestTjeneste {
         var behandling = getBehandling(dto);
         var iayGrunnlag = inntektArbeidYtelseTjeneste.hentGrunnlagKontrakt(behandling.getId());
         return iayGrunnlag.getOppgittOpptjening();
+    }
+
+    private Behandling getBehandling(ForvaltningBehandlingIdDto dto) {
+        return behandlingsprosessTjeneste.hentBehandling(dto.getBehandlingUuid());
     }
 }
