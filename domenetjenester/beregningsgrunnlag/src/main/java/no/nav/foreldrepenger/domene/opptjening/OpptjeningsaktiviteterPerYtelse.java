78d4264841c8ba242c9ba29ef55e509e788b3a1d
Petter Ekern <petter.ekern@nav.no>
2024-01-29 15:29:06 +0100

    TFP-5700: Tar inn frilans når det er oppgitt i opptjening uavhengig av hva som ligger i søknad

--- domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/opptjening/OpptjeningsaktiviteterPerYtelse.java
+++ domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/opptjening/OpptjeningsaktiviteterPerYtelse.java
@@ -13,11 +13,9 @@ public class OpptjeningsaktiviteterPerYtelse {
 
     private static final Map<FagsakYtelseType, Set<OpptjeningAktivitetType>> EKSKLUDERTE_AKTIVITETER_PER_YTELSE = Map.of(
         FagsakYtelseType.FORELDREPENGER, Set.of(
-            OpptjeningAktivitetType.FRILOPP,
             OpptjeningAktivitetType.VIDERE_ETTERUTDANNING,
             OpptjeningAktivitetType.UTENLANDSK_ARBEIDSFORHOLD),
         FagsakYtelseType.SVANGERSKAPSPENGER, Set.of(
-            OpptjeningAktivitetType.FRILOPP,
             OpptjeningAktivitetType.VIDERE_ETTERUTDANNING,
             OpptjeningAktivitetType.UTENLANDSK_ARBEIDSFORHOLD,
             OpptjeningAktivitetType.DAGPENGER,
@@ -30,21 +28,7 @@ public class OpptjeningsaktiviteterPerYtelse {
     public OpptjeningsaktiviteterPerYtelse(FagsakYtelseType fagsakYtelseType) {
         ekskluderteAktiviteter = EKSKLUDERTE_AKTIVITETER_PER_YTELSE.get(fagsakYtelseType);
     }
-
-    public boolean erRelevantAktivitet(OpptjeningAktivitetType opptjeningAktivitetType, InntektArbeidYtelseGrunnlag iayGrunnlag) {
-        if (OpptjeningAktivitetType.FRILANS.equals(opptjeningAktivitetType)) {
-            return harOppgittFrilansISøknad(iayGrunnlag);
-        }
-        return erRelevantAktivitet(opptjeningAktivitetType);
-    }
-
     boolean erRelevantAktivitet(OpptjeningAktivitetType opptjeningAktivitetType) {
         return !ekskluderteAktiviteter.contains(opptjeningAktivitetType);
-    }
-
-    private boolean harOppgittFrilansISøknad(InntektArbeidYtelseGrunnlag grunnlag) {
-        return grunnlag.getGjeldendeOppgittOpptjening().stream()
-            .flatMap(oppgittOpptjening -> oppgittOpptjening.getAnnenAktivitet().stream())
-            .anyMatch(annenAktivitet -> annenAktivitet.getArbeidType().equals(ArbeidType.FRILANSER));
     }
 }
