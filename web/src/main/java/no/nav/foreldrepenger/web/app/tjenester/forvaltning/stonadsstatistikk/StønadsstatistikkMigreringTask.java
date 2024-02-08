8dad11543aebe758f73d0e15913af4362959e227
Anders Palfi <anders.palfi@nav.no>
2024-02-08 09:48:11 +0100

    Publiserer maks 100 vedtak per task til stønadsstatistikk

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/forvaltning/stonadsstatistikk/StønadsstatistikkMigreringTask.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/forvaltning/stonadsstatistikk/StønadsstatistikkMigreringTask.java
@@ -24,10 +24,10 @@ import no.nav.foreldrepenger.datavarehus.v2.StønadsstatistikkKafkaProducer;
 import no.nav.foreldrepenger.datavarehus.v2.StønadsstatistikkTjeneste;
 import no.nav.foreldrepenger.datavarehus.v2.StønadsstatistikkVedtak;
 import no.nav.foreldrepenger.skjæringstidspunkt.SkjæringstidspunktTjeneste;
-import no.nav.vedtak.exception.TekniskException;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTask;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskHandler;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTaskTjeneste;
 import no.nav.vedtak.mapper.json.DefaultJsonMapper;
 
 @Dependent
@@ -37,6 +37,7 @@ class StønadsstatistikkMigreringTask implements ProsessTaskHandler {
 
     private static final Logger LOG = LoggerFactory.getLogger(StønadsstatistikkMigreringTask.class);
     static final String DATO_KEY = "dato";
+    private static final String FRA_VEDTAK_ID = "fraVedtakId";
 
     private final BehandlingRepository behandlingRepository;
     private final StønadsstatistikkTjeneste stønadsstatistikkTjeneste;
@@ -44,18 +45,21 @@ class StønadsstatistikkMigreringTask implements ProsessTaskHandler {
     private final Validator validator;
     private final StønadsstatistikkKafkaProducer kafkaProducer;
     private final EntityManager entityManager;
+    private final ProsessTaskTjeneste prosessTaskTjeneste;
 
     @Inject
     public StønadsstatistikkMigreringTask(BehandlingRepository behandlingRepository,
                                           StønadsstatistikkTjeneste stønadsstatistikkTjeneste,
                                           SkjæringstidspunktTjeneste skjæringstidspunktTjeneste,
                                           StønadsstatistikkKafkaProducer kafkaProducer,
-                                          EntityManager entityManager) {
+                                          EntityManager entityManager,
+                                          ProsessTaskTjeneste prosessTaskTjeneste) {
         this.behandlingRepository = behandlingRepository;
         this.stønadsstatistikkTjeneste = stønadsstatistikkTjeneste;
         this.skjæringstidspunktTjeneste = skjæringstidspunktTjeneste;
         this.kafkaProducer = kafkaProducer;
         this.entityManager = entityManager;
+        this.prosessTaskTjeneste = prosessTaskTjeneste;
 
         @SuppressWarnings("resource") var factory = Validation.buildDefaultValidatorFactory();
         // hibernate validator implementations er thread-safe, trenger ikke close
@@ -64,30 +68,39 @@ class StønadsstatistikkMigreringTask implements ProsessTaskHandler {
 
     @Override
     public void doTask(ProsessTaskData prosessTaskData) {
-        try {
-            var vedtaksdato = LocalDate.parse(prosessTaskData.getPropertyValue(DATO_KEY), DateTimeFormatter.ISO_LOCAL_DATE);
+        var vedtaksdato = LocalDate.parse(prosessTaskData.getPropertyValue(DATO_KEY), DateTimeFormatter.ISO_LOCAL_DATE);
+        var vedtakIdProperty = prosessTaskData.getPropertyValue(FRA_VEDTAK_ID);
+        var fraVedtakId = vedtakIdProperty == null ? null : Long.valueOf(vedtakIdProperty);
 
-            var dtos = finnAlleVedtakForDato(vedtaksdato).stream()
-                .map(this::lagDto)
-                .collect(Collectors.toSet());
-            LOG.info("Publiser migreringshendelse for {} vedtak opprettet {}", dtos.size(), vedtaksdato);
+        var vedtakPåDato = finnAlleVedtakForDato(vedtaksdato, fraVedtakId);
+        var dtos = vedtakPåDato.stream()
+            .map(this::lagDto)
+            .collect(Collectors.toSet());
+        LOG.info("Publiserer migreringshendelse for {} vedtak opprettet {}", dtos.size(), vedtaksdato);
 
-            dtos.forEach(v -> kafkaProducer.sendJson(v.getSaksnummer().id(), DefaultJsonMapper.toJson(v)));
-        } catch (Exception e) {
-            throw new TekniskException("MIGRERING1","Migreringstask feilet", e);
-        }
+        dtos.forEach(v -> kafkaProducer.sendJson(v.getSaksnummer().id(), DefaultJsonMapper.toJson(v)));
+
+        vedtakPåDato.stream()
+            .map(BehandlingVedtak::getId)
+            .max(Long::compareTo)
+            .ifPresent(nesteId -> prosessTaskTjeneste.lagre(opprettTaskForDato(vedtaksdato, nesteId)));
+
     }
 
-    private List<BehandlingVedtak> finnAlleVedtakForDato(LocalDate vedtaksdato) {
+    private List<BehandlingVedtak> finnAlleVedtakForDato(LocalDate vedtaksdato, Long fraVedtakId) {
         var sql ="""
-            select bv.* From BEHANDLING_VEDTAK bv
+            select * from (select bv.* from BEHANDLING_VEDTAK bv
             join BEHANDLING_RESULTAT br on br.id = bv.BEHANDLING_RESULTAT_ID
             join BEHANDLING b on b.id = br.BEHANDLING_ID
             where b.BEHANDLING_TYPE in ('BT-002','BT-004') and trunc(bv.OPPRETTET_TID) =:vedtaksdato
+            and bv.id >:fraVedtakId
+            order by bv.id)
+            fetch first 100 rows only
             """;
 
         var query = entityManager.createNativeQuery(sql, BehandlingVedtak.class)
             .setParameter("vedtaksdato", vedtaksdato)
+            .setParameter("fraVedtakId", fraVedtakId == null ? 0 : fraVedtakId)
             .setHint(HibernateHints.HINT_READ_ONLY, "true");
         return query.getResultList();
     }
@@ -111,5 +124,15 @@ class StønadsstatistikkMigreringTask implements ProsessTaskHandler {
             throw new IllegalArgumentException("stønadsstatistikk valideringsfeil \n " + allErrors);
         }
         return generertVedtak;
+    }
+
+    private static ProsessTaskData opprettTaskForDato(LocalDate dato, Long fraVedtakId) {
+        var prosessTaskData = ProsessTaskData.forProsessTask(StønadsstatistikkMigreringTask.class);
+
+        prosessTaskData.setProperty(StønadsstatistikkMigreringTask.DATO_KEY, dato.toString());
+        prosessTaskData.setProperty(StønadsstatistikkMigreringTask.FRA_VEDTAK_ID, String.valueOf(fraVedtakId));
+        prosessTaskData.setCallIdFraEksisterende();
+        prosessTaskData.setPrioritet(150);
+        return prosessTaskData;
     }
 }
