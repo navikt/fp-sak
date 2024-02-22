db8efa5d8ccaee3766f54b64ab853d5e79af46b5
anjaaalerud <anja.gotesen.aalerud@nav.no>
2024-02-22 14:48:25 +0100

    TFP-5698 fikset tekst

--- behandlingslager/domene/src/main/java/no/nav/foreldrepenger/behandlingslager/behandling/BehandlingÅrsakType.java
+++ behandlingslager/domene/src/main/java/no/nav/foreldrepenger/behandlingslager/behandling/BehandlingÅrsakType.java
@@ -71,7 +71,7 @@ public enum BehandlingÅrsakType implements Kodeverdi {
     RE_VEDTAK_PLEIEPENGER("RE-VEDTAK-PSB", "Pleiepenger"),
 
     // Håndtering av diverse feilsituaqsjoner
-    FEIL_PRAKSIS_UTSETTELSE("FEIL_PRAKSIS_UTSETTELSE", "Feilpraksis utsettelse"),
+    FEIL_PRAKSIS_UTSETTELSE("FEIL_PRAKSIS_UTSETTELSE", "Feil praksis utsettelse"),
 
     // Skille klageområder
     KLAGE_TILBAKEBETALING("KLAGE_TILBAKE", "Tilbakebetaling"),
