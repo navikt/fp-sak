9a957ec70eec1df8f764e9e3418689c7e2cd0792
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2024-02-23 11:25:56 +0100

    Opprett revurdering, ikke førstegang

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/forvaltning/praksisutsettelse/FeilPraksisOpprettBehandlingTjeneste.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/forvaltning/praksisutsettelse/FeilPraksisOpprettBehandlingTjeneste.java
@@ -12,17 +12,13 @@ import no.nav.foreldrepenger.behandling.revurdering.RevurderingTjeneste;
 import no.nav.foreldrepenger.behandlingskontroll.FagsakYtelseTypeRef;
 import no.nav.foreldrepenger.behandlingslager.behandling.Behandling;
 import no.nav.foreldrepenger.behandlingslager.behandling.BehandlingStatus;
-import no.nav.foreldrepenger.behandlingslager.behandling.BehandlingType;
 import no.nav.foreldrepenger.behandlingslager.behandling.BehandlingÅrsakType;
 import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepositoryProvider;
-import no.nav.foreldrepenger.behandlingslager.behandling.vedtak.BehandlingVedtak;
-import no.nav.foreldrepenger.behandlingslager.behandling.vedtak.BehandlingVedtakRepository;
-import no.nav.foreldrepenger.behandlingslager.behandling.vedtak.VedtakResultatType;
 import no.nav.foreldrepenger.behandlingslager.fagsak.Fagsak;
 import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakLåsRepository;
 import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakYtelseType;
-import no.nav.foreldrepenger.behandlingsprosess.prosessering.BehandlingOpprettingTjeneste;
+import no.nav.foreldrepenger.behandlingsprosess.prosessering.BehandlingProsesseringTjeneste;
 import no.nav.foreldrepenger.produksjonsstyring.behandlingenhet.BehandlendeEnhetTjeneste;
 
 /**
@@ -36,21 +32,19 @@ public class FeilPraksisOpprettBehandlingTjeneste {
 
     private BehandlingRepository behandlingRepository;
     private FagsakLåsRepository fagsakLåsRepository;
-    private BehandlingVedtakRepository behandlingVedtakRepository;
-    private BehandlingOpprettingTjeneste behandlingOpprettingTjeneste;
     private RevurderingTjeneste revurderingTjeneste;
     private BehandlendeEnhetTjeneste behandlendeEnhetTjeneste;
+    private BehandlingProsesseringTjeneste behandlingProsesseringTjeneste;
 
     @Inject
     public FeilPraksisOpprettBehandlingTjeneste(BehandlingRepositoryProvider behandlingRepositoryProvider,
-                                                BehandlingOpprettingTjeneste behandlingOpprettingTjeneste,
                                                 @FagsakYtelseTypeRef(FagsakYtelseType.FORELDREPENGER) RevurderingTjeneste revurderingTjeneste,
-                                                BehandlendeEnhetTjeneste behandlendeEnhetTjeneste) {
+                                                BehandlendeEnhetTjeneste behandlendeEnhetTjeneste,
+                                                BehandlingProsesseringTjeneste behandlingProsesseringTjeneste) {
         this.fagsakLåsRepository = behandlingRepositoryProvider.getFagsakLåsRepository();
         this.behandlingRepository = behandlingRepositoryProvider.getBehandlingRepository();
-        this.behandlingVedtakRepository = behandlingRepositoryProvider.getBehandlingVedtakRepository();
-        this.behandlingOpprettingTjeneste = behandlingOpprettingTjeneste;
         this.behandlendeEnhetTjeneste = behandlendeEnhetTjeneste;
+        this.behandlingProsesseringTjeneste = behandlingProsesseringTjeneste;
         this.revurderingTjeneste = revurderingTjeneste;
     }
 
@@ -69,23 +63,12 @@ public class FeilPraksisOpprettBehandlingTjeneste {
             LOG.info("FeilPraksisUtsettelse: Fant ingen vedtatte behandlinger saksnummer {}", fagsak.getSaksnummer());
             return;
         }
-        var sisteVedtak = behandlingVedtakRepository.hentForBehandlingHvisEksisterer(sisteVedtatte.getId())
-            .map(BehandlingVedtak::getVedtakResultatType).orElse(VedtakResultatType.UDEFINERT);
-        if (VedtakResultatType.UDEFINERT.equals(sisteVedtak)) {
-            LOG.info("FeilPraksisUtsettelse: Fant ikke siste vedtak saksnummer {}", fagsak.getSaksnummer());
-            return;
-        }
+
         var enhet = behandlendeEnhetTjeneste.finnBehandlendeEnhetFra(sisteVedtatte);
-        if (VedtakResultatType.AVSLAG.equals(sisteVedtak)) {
-            var behandling = behandlingOpprettingTjeneste.opprettBehandling(fagsak, BehandlingType.FØRSTEGANGSSØKNAD, enhet, BehandlingÅrsakType.FEIL_PRAKSIS_UTSETTELSE);
-            behandlingOpprettingTjeneste.asynkStartBehandlingsprosess(behandling);
-            LOG.info("FeilPraksisUtsettelse: Opprettet førstegangsbehandling med behandlingId {} saksnummer {}", behandling.getId(), fagsak.getSaksnummer());
-        } else {
-            fagsakLåsRepository.taLås(fagsak.getId());
-            var revurdering = revurderingTjeneste.opprettAutomatiskRevurdering(fagsak, BehandlingÅrsakType.FEIL_PRAKSIS_UTSETTELSE, enhet);
-            behandlingOpprettingTjeneste.asynkStartBehandlingsprosess(revurdering);
-            LOG.info("FeilPraksisUtsettelse: Opprettet revurdering med behandlingId {} saksnummer {}", revurdering.getId(), fagsak.getSaksnummer());
-        }
+        fagsakLåsRepository.taLås(fagsak.getId());
+        var revurdering = revurderingTjeneste.opprettAutomatiskRevurdering(fagsak, BehandlingÅrsakType.FEIL_PRAKSIS_UTSETTELSE, enhet);
+        behandlingProsesseringTjeneste.opprettTasksForStartBehandling(revurdering);
+        LOG.info("FeilPraksisUtsettelse: Opprettet revurdering med behandlingId {} saksnummer {}", revurdering.getId(), fagsak.getSaksnummer());
 
     }
 
