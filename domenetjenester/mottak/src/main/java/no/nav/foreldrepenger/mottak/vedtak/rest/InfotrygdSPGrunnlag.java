11321041b04b0e85d093e6b810ccb27d09abde3a
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2024-02-29 22:06:44 +0100

    Legge om til egen tjeneste mot SP subbase

--- domenetjenester/mottak/src/main/java/no/nav/foreldrepenger/mottak/vedtak/rest/InfotrygdSPGrunnlag.java
+++ domenetjenester/mottak/src/main/java/no/nav/foreldrepenger/mottak/vedtak/rest/InfotrygdSPGrunnlag.java
@@ -8,8 +8,8 @@ import no.nav.vedtak.felles.integrasjon.rest.TokenFlow;
 
 @ApplicationScoped
 @RestClientConfig(tokenConfig = TokenFlow.AZUREAD_CC, endpointProperty = "fpsak.it.sp.grunnlag.url",
-    endpointDefault = "http://infotrygd-sykepenger-fp.infotrygd/grunnlag",
-    scopesProperty = "fpsak.it.sp.scopes", scopesDefault = "api://prod-fss.infotrygd.infotrygd-sykepenger-fp/.default")
+    endpointDefault = "http://fp-infotrygd-sykepenger/grunnlag",
+    scopesProperty = "fpsak.it.sp.scopes", scopesDefault = "api://prod-fss.teamforeldrepenger.fp-infotrygd-sykepenger/.default")
 public class InfotrygdSPGrunnlag extends AbstractInfotrygdGrunnlag {
 
     public InfotrygdSPGrunnlag() {
