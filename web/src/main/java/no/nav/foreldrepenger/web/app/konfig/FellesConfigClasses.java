e2f2bd390ef364f60c786496786fea93e6bc0861
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2024-03-26 16:00:18 +0100

    Eksempel på omlegging fra JA-spi til JaxRs (#6159)
    * Eksempel på omlegging fra JA-spi til JaxRs
    
    * Skip ekstra contextpath
    
    * Sett riktig prioritet på AuthFilter
    
    * Rename authfilter

---
+++ web/src/main/java/no/nav/foreldrepenger/web/app/konfig/FellesConfigClasses.java
@@ -0,0 +1,39 @@
+package no.nav.foreldrepenger.web.app.konfig;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
+import no.nav.foreldrepenger.web.app.exceptions.ConstraintViolationMapper;
+import no.nav.foreldrepenger.web.app.exceptions.GeneralRestExceptionMapper;
+import no.nav.foreldrepenger.web.app.exceptions.JsonMappingExceptionMapper;
+import no.nav.foreldrepenger.web.app.exceptions.JsonParseExceptionMapper;
+import no.nav.foreldrepenger.web.app.jackson.JacksonJsonConfig;
+
+public class FellesConfigClasses {
+
+    private FellesConfigClasses() {
+    }
+    public static Set<Class<?>> getFellesConfigClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+        // Autentisering
+        classes.add(AuthenticationFilter.class);
+        // Monitorering
+        classes.add(TimingFilter.class);
+        // swagger
+        classes.add(OpenApiResource.class);
+
+        // (De)Serialisering
+        classes.add(JacksonJsonConfig.class);
+
+        // ExceptionMappers pga de som finnes i Jackson+Jersey-media
+        classes.add(ConstraintViolationMapper.class);
+        classes.add(JsonMappingExceptionMapper.class);
+        classes.add(JsonParseExceptionMapper.class);
+
+        // Generell exceptionmapper m/logging for øvrige tilfelle
+        classes.add(GeneralRestExceptionMapper.class);
+        return Collections.unmodifiableSet(classes);
+    }
+}
