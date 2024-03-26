2ef3a92bf87a785578a154fc2026732a94b53b9d
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2024-03-26 11:18:50 +0100

    Slå på personoversikt i prod m/etterpopulering

--- domenetjenester/produksjonsstyring/src/main/java/no/nav/foreldrepenger/produksjonsstyring/sakogbehandling/PersonoversiktHendelseProducer.java
+++ domenetjenester/produksjonsstyring/src/main/java/no/nav/foreldrepenger/produksjonsstyring/sakogbehandling/PersonoversiktHendelseProducer.java
@@ -6,7 +6,6 @@ import jakarta.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import no.nav.foreldrepenger.konfig.Environment;
 import no.nav.foreldrepenger.konfig.KonfigVerdi;
 import no.nav.vedtak.felles.integrasjon.kafka.KafkaSender;
 
@@ -15,8 +14,6 @@ import no.nav.vedtak.felles.integrasjon.kafka.KafkaSender;
 public class PersonoversiktHendelseProducer {
 
     private static final Logger LOG = LoggerFactory.getLogger(PersonoversiktHendelseProducer.class);
-
-    private static final boolean IS_DEV = Environment.current().isDev();
 
     private KafkaSender producer;
     private String topicName;
@@ -31,9 +28,6 @@ public class PersonoversiktHendelseProducer {
     }
 
     public void sendJsonMedNøkkel(String nøkkel, String json) {
-        if (!IS_DEV) {
-            return;
-        }
         LOG.info("Sender melding om personoversikt-hendelse med nøkkel {} på topic='{}'", nøkkel, topicName);
         var recordMetadata = producer.send(nøkkel, json);
         LOG.info("Sendte melding om personoversikt-hendelse til {}, partition {}, offset {}", recordMetadata.topic(), recordMetadata.partition(), recordMetadata.offset());
