70e642ffaa12855b1382566d8e116ccb80dfdfb1
Anders Palfi <anderspalfi@gmail.com>
2024-04-03 14:41:04 +0200

    Fjerner env sjekk på å produsere stønadsstatistikk (#6383)

--- domenetjenester/datavarehus/src/main/java/no/nav/foreldrepenger/datavarehus/v2/StønadsstatistikkKafkaProducer.java
+++ domenetjenester/datavarehus/src/main/java/no/nav/foreldrepenger/datavarehus/v2/StønadsstatistikkKafkaProducer.java
@@ -7,7 +7,6 @@ import jakarta.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import no.nav.foreldrepenger.konfig.Environment;
 import no.nav.foreldrepenger.konfig.KonfigVerdi;
 import no.nav.vedtak.felles.integrasjon.kafka.KafkaSender;
 
@@ -15,7 +14,6 @@ import no.nav.vedtak.felles.integrasjon.kafka.KafkaSender;
 public class StønadsstatistikkKafkaProducer {
 
     private static final Logger LOG = LoggerFactory.getLogger(StønadsstatistikkKafkaProducer.class);
-    private static final Environment ENV = Environment.current();
 
     private KafkaSender producer;
 
@@ -28,9 +26,6 @@ public class StønadsstatistikkKafkaProducer {
     }
 
     public void sendJson(String nøkkel, String json) {
-        if (ENV.isLocal() || ENV.isDev()) { // TODO fjerne denne. Topic skal finnes i autotest
-            return;
-        }
         LOG.info("Sender melding om stønadsstatistikkvedtak med nøkkel {} på topic='{}'", nøkkel, producer.getTopicName());
         var recordMetadata = producer.send(nøkkel, json);
         LOG.info("Sendte melding om stønadsstatistikkvedtak til {}, partition {}, offset {}", recordMetadata.topic(), recordMetadata.partition(), recordMetadata.offset());
