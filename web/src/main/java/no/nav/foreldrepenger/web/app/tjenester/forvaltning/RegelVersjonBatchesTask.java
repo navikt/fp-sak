9eaa63cf024c24e89e751425e0af894c8d8b312a
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2024-06-22 18:13:31 +0200

    Regelversjon restmigrering

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/forvaltning/RegelVersjonBatchesTask.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/forvaltning/RegelVersjonBatchesTask.java
@@ -35,17 +35,15 @@ class RegelVersjonBatchesTask implements ProsessTaskHandler {
     @Override
     public void doTask(ProsessTaskData prosessTaskData) {
         oppdaterInngangsvilkår();
-        oppdaterTilkjent();
-        oppdaterFeriepenger();
-        oppdaterSvangerskapspenger();
-        oppdaterStønadskontoUttak();
+        oppdaterInngangsvilkårBeregning();
+        slettProsesstaskUtenGruppe();
     }
 
     private void oppdaterInngangsvilkår() {
         var sql ="""
             update vilkar
             set regel_versjon = 'fp-inngangsvilkar:1.0.0'
-            where opprettet_tid > :tidspunkt and regel_versjon is null and regel_evaluering like '%"inngangsvilkår"%'
+            where coalesce(endret_tid,opprettet_tid) > :tidspunkt and regel_versjon is null and regel_evaluering like '%"inngangsvilkår"%'
             """;
 
         var antall = entityManager.createNativeQuery(sql)
@@ -54,57 +52,23 @@ class RegelVersjonBatchesTask implements ProsessTaskHandler {
         LOG.info("FPSAK REGEL IV {}", antall);
     }
 
-    private void oppdaterTilkjent() {
+    private void oppdaterInngangsvilkårBeregning() {
         var sql ="""
-            update BR_BEREGNINGSRESULTAT
-            set regel_versjon = 'fp-ytelse-beregn:1.0.2'
-            where opprettet_tid > :tidspunkt and regel_versjon is null and regel_sporing like '%"beregningsresultat" : "UNKNOWN"%'
+            update vilkar
+            set regel_versjon = 'ft-beregning:'||cast(substr(substr(regel_evaluering, instr(regel_evaluering, '"beregningsgrunnlag"') + 24), 0, instr(substr(regel_evaluering, instr(regel_evaluering, '"beregningsgrunnlag"') + 24), '"') - 1) as varchar2(50))
+            where vilkar_type = 'FP_VK_41' and coalesce(endret_tid, opprettet_tid) > :tidspunkt and regel_versjon is null and regel_evaluering like '%"beregningsgrunnlag"%'
             """;
 
         var antall = entityManager.createNativeQuery(sql)
-            .setParameter("tidspunkt", LocalDate.of(2024, Month.JUNE, 16).atStartOfDay())
+            .setParameter("tidspunkt", LocalDate.of(2023, Month.FEBRUARY, 10).atStartOfDay())
             .executeUpdate();
-        LOG.info("FPSAK REGEL Tilkjent {}", antall);
+        LOG.info("FPSAK REGEL IV {}", antall);
     }
 
-    private void oppdaterFeriepenger() {
-        var sql ="""
-            update BR_FERIEPENGER
-            set regel_versjon = 'fp-ytelse-beregn:1.0.2'
-            where opprettet_tid > :tidspunkt and regel_versjon is null and feriepenger_regel_sporing like '%"beregningsresultat" : "UNKNOWN"%'
-            """;
-
-        var antall = entityManager.createNativeQuery(sql)
-            .setParameter("tidspunkt", LocalDate.of(2024, Month.JUNE, 16).atStartOfDay())
+    private void slettProsesstaskUtenGruppe() {
+        entityManager.createNativeQuery("DELETE from prosess_task where task_gruppe is null and status = 'FERDIG''")
             .executeUpdate();
-        LOG.info("FPSAK REGEL Ferie {}", antall);
     }
 
-    private void oppdaterSvangerskapspenger() {
-        var sql ="""
-            update SVP_UTTAK_PERIODE
-            set regel_versjon = 'svp-uttak:'||cast(substr(substr(regel_evaluering, instr(regel_evaluering, '"svp-uttak"') + 15), 0, instr(substr(regel_evaluering, instr(regel_evaluering, '"svp-uttak"') + 15), '"') - 1) as varchar2(50))
-            where opprettet_tid > :tidspunkt and regel_versjon is null and regel_evaluering like '%"svp-uttak"%'
-            """;
-
-        var antall = entityManager.createNativeQuery(sql)
-            .setParameter("tidspunkt", LocalDate.of(2023, Month.FEBRUARY, 1).atStartOfDay())
-            .executeUpdate();
-        LOG.info("FPSAK REGEL SVP {}", antall);
-    }
-
-    private void oppdaterStønadskontoUttak() {
-        var sql ="""
-            update STOENADSKONTOBEREGNING
-            set regel_versjon = 'fp-uttak:'||cast(substr(regel_evaluering, instr(regel_evaluering, '"fp-uttak"') + 14, 6) as varchar2(50))
-            where opprettet_tid > :tidspunktFra and opprettet_tid < :tidspunktTil and regel_versjon is null and regel_evaluering like '%"fp-uttak"%'
-            """;
-
-        var antall = entityManager.createNativeQuery(sql)
-            .setParameter("tidspunktFra", LocalDate.of(2023, Month.FEBRUARY, 1).atStartOfDay())
-            .setParameter("tidspunktTil", LocalDate.of(2023, Month.MARCH, 2).atStartOfDay())
-            .executeUpdate();
-        LOG.info("FPSAK REGEL KontoUttak {}", antall);
-    }
 
 }
