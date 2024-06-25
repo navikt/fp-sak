745a55eb2b153f67934ffd154b827b3a61683155
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2024-06-25 22:35:37 +0200

    Endepunkt som fptilbake kan bruke for å sjekke om oppdrag sendt

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/oppdrag/OppdragDto.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/oppdrag/OppdragDto.java
@@ -1,58 +1,11 @@
 package no.nav.foreldrepenger.web.app.tjenester.behandling.oppdrag;
 
-import java.util.List;
-
 import no.nav.foreldrepenger.behandlingslager.økonomioppdrag.Oppdragskontroll;
 
-public class OppdragDto extends SporingDto {
-
-    private String saksnummer;
-
-    private Long behandlingId;
-
-    private boolean venterKvittering;
-
-    private List<Oppdrag110Dto> oppdrag110;
+public record OppdragDto(String saksnummer, Long behandlingId)  {
 
     public OppdragDto(Oppdragskontroll entitet) {
-        super(entitet, entitet.getVersjon(), entitet.getId());
+        this(entitet.getSaksnummer().getVerdi(), entitet.getBehandlingId());
     }
 
-    public Long getBehandlingId() {
-        return behandlingId;
-    }
-
-    public void setBehandlingId(Long behandlingId) {
-        this.behandlingId = behandlingId;
-    }
-
-    public List<Oppdrag110Dto> getOppdrag110() {
-        return oppdrag110;
-    }
-
-    public void setOppdrag110(List<Oppdrag110Dto> oppdrag110) {
-        this.oppdrag110 = oppdrag110;
-    }
-
-    public String getSaksnummer() {
-        return saksnummer;
-    }
-
-    public OppdragDto setSaksnummer(String saksnummer) {
-        this.saksnummer = saksnummer;
-        return this;
-    }
-
-    public boolean getVenterKvittering() {
-        return venterKvittering;
-    }
-
-    public static OppdragDto fraDomene(Oppdragskontroll oppdragskontroll) {
-        var oppdragDto = new OppdragDto(oppdragskontroll);
-        oppdragDto.saksnummer = oppdragskontroll.getSaksnummer().getVerdi();
-        oppdragDto.behandlingId = oppdragskontroll.getBehandlingId();
-        oppdragDto.oppdrag110 = oppdragskontroll.getOppdrag110Liste().stream().map(Oppdrag110Dto::fraDomene).toList();
-        oppdragDto.venterKvittering = oppdragskontroll.getVenterKvittering();
-        return oppdragDto;
-    }
 }
