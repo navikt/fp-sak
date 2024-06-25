7fa6ac0f24e77d7d761df6961df304509290bfec
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2024-06-25 13:27:42 +0200

    Multiple fagsakmarkeringer, expand fase 1 (#6527)
    * Multiple fagsakmarkeringer, expand fase 1
    
    * Rette feil

---
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/fagsak/dto/FagsakMarkeringDto.java
@@ -0,0 +1,5 @@
+package no.nav.foreldrepenger.web.app.tjenester.fagsak.dto;
+
+import no.nav.foreldrepenger.behandlingslager.fagsak.egenskaper.FagsakMarkering;
+
+public record FagsakMarkeringDto(FagsakMarkering fagsakMarkering, String kortNavn) { }
