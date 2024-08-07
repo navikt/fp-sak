bf13dfc63c2bc956b60055c8d0edd6664a1bca92
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2024-08-07 10:30:45 +0200

    Mindre delays ved oppslag

--- web/src/main/java/no/nav/foreldrepenger/tilganger/BrukerProfilTjeneste.java
+++ web/src/main/java/no/nav/foreldrepenger/tilganger/BrukerProfilTjeneste.java
@@ -1,6 +1,7 @@
 package no.nav.foreldrepenger.tilganger;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -11,12 +12,16 @@ import org.slf4j.LoggerFactory;
 import no.nav.foreldrepenger.konfig.KonfigVerdi;
 import no.nav.foreldrepenger.web.app.util.LdapUtil;
 import no.nav.vedtak.sikkerhet.kontekst.KontekstHolder;
+import no.nav.vedtak.util.LRUCache;
 
 @ApplicationScoped
 public class BrukerProfilTjeneste {
     private static final Logger LOG = LoggerFactory.getLogger(BrukerProfilTjeneste.class);
 
     private static final EntraBrukerOppslag ENTRA_BRUKER_OPPSLAG = new EntraBrukerOppslag();
+    private static final long CACHE_ELEMENT_LIVE_TIME_MS = TimeUnit.MILLISECONDS.convert(24, TimeUnit.HOURS);
+
+    private static final LRUCache <String, InnloggetNavAnsattDto> AZURE_CACHE = new LRUCache<>(600, CACHE_ELEMENT_LIVE_TIME_MS);
 
     private String gruppenavnSaksbehandler;
     private String gruppenavnVeileder;
@@ -54,16 +59,20 @@ public class BrukerProfilTjeneste {
     }
 
     private void sammenlignMedAzureGraphFailSoft(InnloggetNavAnsattDto ldapBrukerInfo) {
-        LOG.info("PROFIL Azure. Henter fra azure.");
+        // Less intrusive - kall til fptilgang krever OBO-veksling på 200-400ms.
         try {
-            var før = System.nanoTime();
-            var azureBrukerInfo = mapTilDomene(ENTRA_BRUKER_OPPSLAG.brukerInfo());
-            if (!ldapBrukerInfo.equals(azureBrukerInfo)) {
-                LOG.info("PROFIL Azure. tilganger fra ldap og azure er ikke like. Azure: {} != LDAP: {}", azureBrukerInfo, ldapBrukerInfo);
-            } else {
-                LOG.info("PROFIL Azure. Azure == LDAP :)");
+            if (AZURE_CACHE.get(KontekstHolder.getKontekst().getUid()) == null) {
+                LOG.info("PROFIL Azure. Henter fra azure.");
+                var før = System.nanoTime();
+                var azureBrukerInfo = mapTilDomene(ENTRA_BRUKER_OPPSLAG.brukerInfo());
+                LOG.info("Azure bruker profil oppslag: {}ms. ", Duration.ofNanos(System.nanoTime() - før).toMillis());
+                AZURE_CACHE.put(KontekstHolder.getKontekst().getUid(), azureBrukerInfo);
+                if (!ldapBrukerInfo.equals(azureBrukerInfo)) {
+                    LOG.info("PROFIL Azure. tilganger fra ldap og azure er ikke like. Azure: {} != LDAP: {}", azureBrukerInfo, ldapBrukerInfo);
+                } else {
+                    LOG.info("PROFIL Azure. Azure == LDAP :)");
+                }
             }
-            LOG.info("Azure bruker profil oppslag: {}ms. ", Duration.ofNanos(System.nanoTime() - før).toMillis());
         } catch (Exception ex) {
             LOG.info("PROFIL Azure. Klienten feilet med exception: {}", ex.getMessage());
         }
