312b3aba2a232cb277bbf4a0fb1a8dcdc4af28e4
Petter Ekern <petter.ekern@nav.no>
2024-08-12 15:02:01 +0200

    Benytter beregningsgrunnlag modell istedenfor entitet under utregning av tilkjent ytelse

--- domenetjenester/beregning-ytelse/src/main/java/no/nav/foreldrepenger/ytelse/beregning/adapter/ArbeidsforholdMapper.java
+++ domenetjenester/beregning-ytelse/src/main/java/no/nav/foreldrepenger/ytelse/beregning/adapter/ArbeidsforholdMapper.java
@@ -5,8 +5,8 @@ import java.util.Optional;
 import no.nav.foreldrepenger.behandlingslager.behandling.beregning.AktivitetStatus;
 import no.nav.foreldrepenger.behandlingslager.uttak.UttakArbeidType;
 import no.nav.foreldrepenger.behandlingslager.virksomhet.Arbeidsgiver;
-import no.nav.foreldrepenger.domene.entiteter.BGAndelArbeidsforhold;
-import no.nav.foreldrepenger.domene.entiteter.BeregningsgrunnlagPrStatusOgAndel;
+import no.nav.foreldrepenger.domene.modell.BGAndelArbeidsforhold;
+import no.nav.foreldrepenger.domene.modell.BeregningsgrunnlagPrStatusOgAndel;
 import no.nav.foreldrepenger.domene.typer.InternArbeidsforholdRef;
 import no.nav.foreldrepenger.ytelse.beregning.regelmodell.beregningsgrunnlag.Arbeidsforhold;
 
