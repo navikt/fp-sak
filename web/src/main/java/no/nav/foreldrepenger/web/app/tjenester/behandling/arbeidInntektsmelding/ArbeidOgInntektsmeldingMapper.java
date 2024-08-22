a2420c06734f52865ab29cba80ec8eb69455f758
Johannes Moskvil <johannbm@users.noreply.github.com>
2024-08-22 08:19:37 +0200

    Fiks mer korrekt logikk for IM sine tilknyttede behandlinger (#6634)

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/arbeidInntektsmelding/ArbeidOgInntektsmeldingMapper.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/arbeidInntektsmelding/ArbeidOgInntektsmeldingMapper.java
@@ -59,8 +59,7 @@ public class ArbeidOgInntektsmeldingMapper {
                                                         Optional<String> dokumentId,
                                                         List<ArbeidsforholdMangel> mangler,
                                                         List<ArbeidsforholdValg> saksbehandlersVurderinger,
-                                                        List<UUID> behandlingsIder,
-                                                        List<UUID> aktiveBehandlingsIder) {
+                                                        List<UUID> tilknyttedeBehandlingIder) {
         var inntekstmeldingMangel = finnIdentifisertInntektsmeldingMangel(im.getArbeidsgiver(), im.getArbeidsforholdRef(), mangler);
         var vurderingPåInntektsmelding = finnSaksbehandlersVurderingPåInntektsmelding(im.getArbeidsgiver(), im.getArbeidsforholdRef(), saksbehandlersVurderinger);
         return new InntektsmeldingDto(
@@ -83,8 +82,7 @@ public class ArbeidOgInntektsmeldingMapper {
                 im.getNaturalYtelser(),
                 im.getEndringerRefusjon(),
                 im.getInntektsmeldingInnsendingsårsak(),
-            behandlingsIder,
-            aktiveBehandlingsIder
+            tilknyttedeBehandlingIder != null ? tilknyttedeBehandlingIder : List.of()
             );
     }
 
