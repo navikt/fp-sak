44c61d44dfa6d0dced1282ddc8f90706d9b5a51e
Petter Ekern <43809623+pekern@users.noreply.github.com>
2024-08-23 10:19:42 +0200

    FIX: Oppdaterer kobling med data fra grunnlag underveis i beregningen (#6636)
    * FIX: Oppdaterer kobling med data fra grunnlag underveis i beregningen

---
+++ domenetjenester/beregningsgrunnlag/src/test/java/no/nav/foreldrepenger/domene/prosess/GrunnbeløpReguleringsutlederTest.java
@@ -0,0 +1,139 @@
+package no.nav.foreldrepenger.domene.prosess;
+
+import no.nav.foreldrepenger.behandlingslager.virksomhet.Arbeidsgiver;
+import no.nav.foreldrepenger.domene.modell.BGAndelArbeidsforhold;
+import no.nav.foreldrepenger.domene.modell.Beregningsgrunnlag;
+import no.nav.foreldrepenger.domene.modell.BeregningsgrunnlagAktivitetStatus;
+import no.nav.foreldrepenger.domene.modell.BeregningsgrunnlagGrunnlagBuilder;
+import no.nav.foreldrepenger.domene.modell.BeregningsgrunnlagPeriode;
+import no.nav.foreldrepenger.domene.modell.BeregningsgrunnlagPrStatusOgAndel;
+import no.nav.foreldrepenger.domene.modell.kodeverk.AktivitetStatus;
+import no.nav.foreldrepenger.domene.modell.kodeverk.BeregningsgrunnlagTilstand;
+import no.nav.vedtak.konfig.Tid;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GrunnbeløpReguleringsutlederTest {
+
+    @Test
+    void skal_ikke_regulere_når_ikke_avkortet() {
+        // Arrange
+        var arbfor = BGAndelArbeidsforhold.builder().medArbeidsgiver(Arbeidsgiver.virksomhet("999999999")).build();
+        var andel = BeregningsgrunnlagPrStatusOgAndel.builder()
+            .medAktivitetStatus(AktivitetStatus.ARBEIDSTAKER)
+            .medBruttoPrÅr(BigDecimal.valueOf(500000))
+            .medAndelsnr(1L)
+            .medBGAndelArbeidsforhold(arbfor)
+            .build();
+        var periode = BeregningsgrunnlagPeriode.builder()
+            .medBeregningsgrunnlagPeriode(LocalDate.now(), Tid.TIDENES_ENDE)
+            .leggTilBeregningsgrunnlagPrStatusOgAndel(andel)
+            .medBruttoPrÅr(BigDecimal.valueOf(500000))
+            .build();
+        var bg = Beregningsgrunnlag.builder()
+            .medSkjæringstidspunkt(LocalDate.now())
+            .leggTilAktivitetStatus(BeregningsgrunnlagAktivitetStatus.builder().medAktivitetStatus(AktivitetStatus.ARBEIDSTAKER).build())
+            .medGrunnbeløp(BigDecimal.valueOf(100000))
+            .leggTilBeregningsgrunnlagPeriode(periode)
+            .build();
+        var gr = BeregningsgrunnlagGrunnlagBuilder.nytt().medBeregningsgrunnlag(bg).build(BeregningsgrunnlagTilstand.FASTSATT);
+
+        // Act
+        var kanTrengeGregulering = GrunnbeløpReguleringsutleder.kanPåvirkesAvGrunnbeløpRegulering(gr);
+
+        // Assert
+        assertThat(kanTrengeGregulering).isFalse();
+    }
+
+    @Test
+    void skal_regulere_når_avkortet() {
+        // Arrange
+        var arbfor = BGAndelArbeidsforhold.builder().medArbeidsgiver(Arbeidsgiver.virksomhet("999999999")).build();
+        var andel = BeregningsgrunnlagPrStatusOgAndel.builder()
+            .medAktivitetStatus(AktivitetStatus.ARBEIDSTAKER)
+            .medBruttoPrÅr(BigDecimal.valueOf(700000))
+            .medAndelsnr(1L)
+            .medBGAndelArbeidsforhold(arbfor)
+            .build();
+        var periode = BeregningsgrunnlagPeriode.builder()
+            .medBeregningsgrunnlagPeriode(LocalDate.now(), Tid.TIDENES_ENDE)
+            .leggTilBeregningsgrunnlagPrStatusOgAndel(andel)
+            .medBruttoPrÅr(BigDecimal.valueOf(700000))
+            .build();
+        var bg = Beregningsgrunnlag.builder()
+            .medSkjæringstidspunkt(LocalDate.now())
+            .leggTilAktivitetStatus(BeregningsgrunnlagAktivitetStatus.builder().medAktivitetStatus(AktivitetStatus.ARBEIDSTAKER).build())
+            .medGrunnbeløp(BigDecimal.valueOf(100000))
+            .leggTilBeregningsgrunnlagPeriode(periode)
+            .build();
+        var gr = BeregningsgrunnlagGrunnlagBuilder.nytt().medBeregningsgrunnlag(bg).build(BeregningsgrunnlagTilstand.FASTSATT);
+
+        // Act
+        var kanTrengeGregulering = GrunnbeløpReguleringsutleder.kanPåvirkesAvGrunnbeløpRegulering(gr);
+
+        // Assert
+        assertThat(kanTrengeGregulering).isTrue();
+    }
+
+    @Test
+    void skal_regulere_når_næringsdrivendet() {
+        // Arrange
+        var andel = BeregningsgrunnlagPrStatusOgAndel.builder()
+            .medAktivitetStatus(AktivitetStatus.SELVSTENDIG_NÆRINGSDRIVENDE)
+            .medBruttoPrÅr(BigDecimal.valueOf(200000))
+            .medAndelsnr(1L)
+            .build();
+        var periode = BeregningsgrunnlagPeriode.builder()
+            .medBeregningsgrunnlagPeriode(LocalDate.now(), Tid.TIDENES_ENDE)
+            .leggTilBeregningsgrunnlagPrStatusOgAndel(andel)
+            .medBruttoPrÅr(BigDecimal.valueOf(200000))
+            .build();
+        var bg = Beregningsgrunnlag.builder()
+            .medSkjæringstidspunkt(LocalDate.now())
+            .leggTilAktivitetStatus(BeregningsgrunnlagAktivitetStatus.builder().medAktivitetStatus(AktivitetStatus.SELVSTENDIG_NÆRINGSDRIVENDE).build())
+            .medGrunnbeløp(BigDecimal.valueOf(100000))
+            .leggTilBeregningsgrunnlagPeriode(periode)
+            .build();
+        var gr = BeregningsgrunnlagGrunnlagBuilder.nytt().medBeregningsgrunnlag(bg).build(BeregningsgrunnlagTilstand.FASTSATT);
+
+        // Act
+        var kanTrengeGregulering = GrunnbeløpReguleringsutleder.kanPåvirkesAvGrunnbeløpRegulering(gr);
+
+        // Assert
+        assertThat(kanTrengeGregulering).isTrue();
+    }
+
+    @Test
+    void skal_regulere_når_militær_med_lavt_grunnlag() {
+        // Arrange
+        var andel = BeregningsgrunnlagPrStatusOgAndel.builder()
+            .medAktivitetStatus(AktivitetStatus.MILITÆR_ELLER_SIVIL)
+            .medBruttoPrÅr(BigDecimal.valueOf(300000))
+            .medAndelsnr(1L)
+            .build();
+        var periode = BeregningsgrunnlagPeriode.builder()
+            .medBeregningsgrunnlagPeriode(LocalDate.now(), Tid.TIDENES_ENDE)
+            .leggTilBeregningsgrunnlagPrStatusOgAndel(andel)
+            .medBruttoPrÅr(BigDecimal.valueOf(300000))
+            .build();
+        var bg = Beregningsgrunnlag.builder()
+            .medSkjæringstidspunkt(LocalDate.now())
+            .leggTilAktivitetStatus(BeregningsgrunnlagAktivitetStatus.builder().medAktivitetStatus(AktivitetStatus.MILITÆR_ELLER_SIVIL).build())
+            .medGrunnbeløp(BigDecimal.valueOf(100000))
+            .leggTilBeregningsgrunnlagPeriode(periode)
+            .build();
+        var gr = BeregningsgrunnlagGrunnlagBuilder.nytt().medBeregningsgrunnlag(bg).build(BeregningsgrunnlagTilstand.FASTSATT);
+
+        // Act
+        var kanTrengeGregulering = GrunnbeløpReguleringsutleder.kanPåvirkesAvGrunnbeløpRegulering(gr);
+
+        // Assert
+        assertThat(kanTrengeGregulering).isTrue();
+    }
+
+}
