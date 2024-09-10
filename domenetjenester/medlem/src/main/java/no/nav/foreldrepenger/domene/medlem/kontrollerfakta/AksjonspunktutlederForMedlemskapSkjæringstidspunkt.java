633760b9a06e8ca18b4a9d0bbcd14097623d1495
Anders Palfi <anders.palfi@nav.no>
2024-09-10 09:45:50 +0200

    TFP-5732 utleder ikke gamle medlemskaps aksjonspunkt i dev/lokal

--- domenetjenester/medlem/src/main/java/no/nav/foreldrepenger/domene/medlem/kontrollerfakta/AksjonspunktutlederForMedlemskapSkjæringstidspunkt.java
+++ domenetjenester/medlem/src/main/java/no/nav/foreldrepenger/domene/medlem/kontrollerfakta/AksjonspunktutlederForMedlemskapSkjæringstidspunkt.java
@@ -9,10 +9,12 @@ import no.nav.foreldrepenger.behandling.aksjonspunkt.AksjonspunktUtleder;
 import no.nav.foreldrepenger.behandling.aksjonspunkt.AksjonspunktUtlederInput;
 import no.nav.foreldrepenger.behandlingskontroll.AksjonspunktResultat;
 import no.nav.foreldrepenger.domene.medlem.VurderMedlemskapTjeneste;
+import no.nav.foreldrepenger.konfig.Environment;
 
 @ApplicationScoped
 public class AksjonspunktutlederForMedlemskapSkjæringstidspunkt implements AksjonspunktUtleder {
 
+    private static final Environment ENV = Environment.current();
     private VurderMedlemskapTjeneste tjeneste;
 
     AksjonspunktutlederForMedlemskapSkjæringstidspunkt() {
@@ -26,9 +28,13 @@ public class AksjonspunktutlederForMedlemskapSkjæringstidspunkt implements Aksj
 
     @Override
     public List<AksjonspunktResultat> utledAksjonspunkterFor(AksjonspunktUtlederInput param) {
-        var skjæringstidspunkt = param.getSkjæringstidspunkt().getUtledetSkjæringstidspunkt();
-        var resultat = tjeneste.vurderMedlemskap(param.getRef(), param.getSkjæringstidspunkt(), skjæringstidspunkt);
-        return resultat.stream().map(mr -> AksjonspunktResultat.opprettForAksjonspunkt(mr.getAksjonspunktDefinisjon())).toList();
+        if (ENV.isProd()) {
+            var skjæringstidspunkt = param.getSkjæringstidspunkt().getUtledetSkjæringstidspunkt();
+            var resultat = tjeneste.vurderMedlemskap(param.getRef(), param.getSkjæringstidspunkt(), skjæringstidspunkt);
+            return resultat.stream().map(mr -> AksjonspunktResultat.opprettForAksjonspunkt(mr.getAksjonspunktDefinisjon())).toList();
+        } else {
+            return List.of();
+        }
     }
 
 }
