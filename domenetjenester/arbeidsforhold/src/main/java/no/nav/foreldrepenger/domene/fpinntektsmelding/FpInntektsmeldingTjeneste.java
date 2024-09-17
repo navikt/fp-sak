11124abca307caf45a701f0fbdb730843c4d256d
Petter Ekern <43809623+pekern@users.noreply.github.com>
2024-09-17 08:20:28 +0200

    POFIM-43: Swaggertjeneste for å opphøre refusjon på gitt inntektsmelding (#6696)
    * POFIM-43: Swaggertjeneste for å opphøre refusjon på gitt inntektsmelding

--- domenetjenester/arbeidsforhold/src/main/java/no/nav/foreldrepenger/domene/fpinntektsmelding/FpInntektsmeldingTjeneste.java
+++ domenetjenester/arbeidsforhold/src/main/java/no/nav/foreldrepenger/domene/fpinntektsmelding/FpInntektsmeldingTjeneste.java
@@ -7,24 +7,39 @@ import no.nav.foreldrepenger.behandling.BehandlingReferanse;
 import no.nav.foreldrepenger.behandling.Skjæringstidspunkt;
 import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakYtelseType;
 import no.nav.foreldrepenger.behandlingslager.virksomhet.OrganisasjonsNummerValidator;
+import no.nav.foreldrepenger.domene.iay.modell.Inntektsmelding;
+import no.nav.foreldrepenger.domene.iay.modell.NaturalYtelse;
+import no.nav.foreldrepenger.domene.iay.modell.Refusjon;
+import no.nav.foreldrepenger.domene.typer.Beløp;
 import no.nav.foreldrepenger.konfig.Environment;
+import no.nav.foreldrepenger.skjæringstidspunkt.SkjæringstidspunktTjeneste;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskTjeneste;
 import no.nav.vedtak.felles.prosesstask.api.TaskType;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class FpInntektsmeldingTjeneste {
     private FpinntektsmeldingKlient klient;
     private ProsessTaskTjeneste prosessTaskTjeneste;
+    private SkjæringstidspunktTjeneste skjæringstidspunktTjeneste;
 
     FpInntektsmeldingTjeneste() {
         // CDI
     }
 
     @Inject
-    public FpInntektsmeldingTjeneste(FpinntektsmeldingKlient klient, ProsessTaskTjeneste prosessTaskTjeneste) {
+    public FpInntektsmeldingTjeneste(FpinntektsmeldingKlient klient,
+                                     ProsessTaskTjeneste prosessTaskTjeneste,
+                                     SkjæringstidspunktTjeneste skjæringstidspunktTjeneste) {
         this.klient = klient;
         this.prosessTaskTjeneste = prosessTaskTjeneste;
+        this.skjæringstidspunktTjeneste = skjæringstidspunktTjeneste;
     }
 
     public void lagForespørselTask(String ag, BehandlingReferanse ref) {
@@ -37,6 +52,41 @@ public class FpInntektsmeldingTjeneste {
         taskdata.setCallIdFraEksisterende();
         taskdata.setProperty(FpinntektsmeldingTask.ARBEIDSGIVER_KEY, ag);
         prosessTaskTjeneste.lagre(taskdata);
+    }
+
+    public void overstyrInntektsmelding(Inntektsmelding inntektsmeldingSomSkalOverstyres,
+                                        LocalDate opphørFom,
+                                        String saksbehandlerIdent,
+                                        BehandlingReferanse ref) {
+        var ytelse = ref.fagsakYtelseType().equals(FagsakYtelseType.FORELDREPENGER) ? OverstyrInntektsmeldingRequest.YtelseType.FORELDREPENGER : OverstyrInntektsmeldingRequest.YtelseType.SVANGERSKAPSPENGER;
+        var startdato = inntektsmeldingSomSkalOverstyres.getStartDatoPermisjon()
+            .orElseGet(() -> skjæringstidspunktTjeneste.getSkjæringstidspunkter(ref.behandlingId()).getUtledetSkjæringstidspunkt());
+        var refusjon = Optional.ofNullable(inntektsmeldingSomSkalOverstyres.getRefusjonBeløpPerMnd()).map(Beløp::getVerdi).orElse(null);
+        var arbeidsgiver = new OverstyrInntektsmeldingRequest.ArbeidsgiverDto(
+            inntektsmeldingSomSkalOverstyres.getArbeidsgiver().getIdentifikator());
+        var aktørId = new OverstyrInntektsmeldingRequest.AktørIdDto(ref.aktørId().getId());
+        var endringerIRefusjon = mapRefusjonsendringer(inntektsmeldingSomSkalOverstyres.getEndringerRefusjon(), opphørFom);
+        var naturalytelser = mapNaturalytelser(inntektsmeldingSomSkalOverstyres.getNaturalYtelser());
+        var request = new OverstyrInntektsmeldingRequest(aktørId, arbeidsgiver, startdato, ytelse,
+            inntektsmeldingSomSkalOverstyres.getInntektBeløp().getVerdi(), refusjon, endringerIRefusjon, naturalytelser, saksbehandlerIdent);
+        klient.overstyrInntektsmelding(request);
+    }
+
+    private List<OverstyrInntektsmeldingRequest.BortfaltNaturalytelseRequestDto> mapNaturalytelser(List<NaturalYtelse> naturalYtelser) {
+        return naturalYtelser.stream()
+            .map(n -> new OverstyrInntektsmeldingRequest.BortfaltNaturalytelseRequestDto(n.getPeriode().getFomDato(), n.getPeriode().getTomDato(), n.getType(), n.getBeloepPerMnd().getVerdi()))
+            .toList();
+    }
+
+    private List<OverstyrInntektsmeldingRequest.RefusjonendringRequestDto> mapRefusjonsendringer(List<Refusjon> endringerRefusjon, LocalDate opphørFom) {
+        // Endringer etter opphørsdato er ikke relevant
+        var endringer = endringerRefusjon.stream()
+            .filter(e -> e.getFom().isBefore(opphørFom))
+            .map(e -> new OverstyrInntektsmeldingRequest.RefusjonendringRequestDto(e.getFom(), e.getRefusjonsbeløp().getVerdi()))
+            .collect(Collectors.toList());
+        // Setter opphør
+        endringer.add(new OverstyrInntektsmeldingRequest.RefusjonendringRequestDto(opphørFom, BigDecimal.ZERO));
+        return endringer;
     }
 
     void lagForespørsel(String ag, BehandlingReferanse ref, Skjæringstidspunkt stp) {
@@ -60,4 +110,5 @@ public class FpInntektsmeldingTjeneste {
             case UDEFINERT,ENGANGSTØNAD -> throw new IllegalArgumentException("Kan ikke opprette forespørsel for ytelsetype " + fagsakYtelseType);
         };
     }
+
 }
