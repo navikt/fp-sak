219894f95349b800b54009b3d993558c13735aee
Steffen Pøhner <steffen.h.pohner@nav.no>
2024-09-26 23:39:32 +0200

    TFP-5853 legger til feriekilde for å støtte innsending fra søknad (skille mellom saksbehandler og søknad). Første skudd.

--- behandlingslager/domene/src/main/java/no/nav/foreldrepenger/behandlingslager/behandling/tilrettelegging/SvpGrunnlagEntitet.java
+++ behandlingslager/domene/src/main/java/no/nav/foreldrepenger/behandlingslager/behandling/tilrettelegging/SvpGrunnlagEntitet.java
@@ -13,6 +13,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 import no.nav.foreldrepenger.behandlingslager.BaseEntitet;
