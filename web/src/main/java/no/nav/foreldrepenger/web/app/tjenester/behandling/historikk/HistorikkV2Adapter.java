e960a1ecd873fa75897e0d81b2ae9cac7657f9da
espenwaaga <36693523+espenwaaga@users.noreply.github.com>
2024-10-15 15:16:19 +0200

    EndretFelter mapping for mal 5

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/historikk/HistorikkV2Adapter.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/historikk/HistorikkV2Adapter.java
@@ -114,7 +114,19 @@ public class HistorikkV2Adapter {
     private static String fraEndretFelt(HistorikkinnslagFelt felt) {
         var endretFeltNavn = HistorikkEndretFeltType.fraKode(felt.getNavn());
 
-        var tekstNavn = switch (endretFeltNavn) {
+        var feltNavn = kodeverdiTilStreng(endretFeltNavn, felt.getNavnVerdi());
+        var fraVerdi = kodeverdiTilStreng(endretFeltNavn, felt.getFraVerdi());
+        var tilVerdi = kodeverdiTilStreng(endretFeltNavn, felt.getTilVerdi());
+
+        if (felt.getFraVerdi() == null || endretFeltNavn.equals(HistorikkEndretFeltType.FORDELING_FOR_NY_ANDEL)) {
+            return String.format("<b>%s</b> er satt til <b>%s</b>.", feltNavn, tilVerdi);
+        } else {
+            return String.format("<b>%s</b> endret fra %s til <b>%s</b>", feltNavn, fraVerdi, tilVerdi);
+        }
+    }
+
+    private static String kodeverdiTilStreng(HistorikkEndretFeltType endretFeltNavn, String verdi) {
+        var tekstFrontend = switch (endretFeltNavn) {
             case ADOPSJONSVILKARET -> "Adopsjonsvilkåret";
             case OPPTJENINGSVILKARET -> "Opptjeningsvilkåret";
             case MEDLEMSKAPSVILKÅRET -> "Medlemskap";
@@ -134,129 +146,126 @@ public class HistorikkV2Adapter {
             case BRUTTO_NAERINGSINNTEKT -> "Brutto næringsinntekt";
             case DODSDATO -> "Dødsdato";
             case DOKUMENTASJON_FORELIGGER -> "Dokumentasjon foreligger";
-            case EKTEFELLES_BARN -> "";
-            case ENDRING_NAERING -> "";
-            case ENDRING_TIDSBEGRENSET_ARBEIDSFORHOLD -> "";
-            case ER_SOKER_BOSATT_I_NORGE -> "";
-            case FODSELSVILKARET -> "";
-            case FODSELSDATO -> "";
-            case FORDELING_FOR_ANDEL -> "";
-            case FORDELING_FOR_NY_ANDEL -> "";
-            case FORDELING_ETTER_BESTEBEREGNING -> "";
-            case FORELDREANSVARSVILKARET -> "";
-            case FRILANS_INNTEKT -> "";
-            case FRILANSVIRKSOMHET -> "";
-            case GYLDIG_MEDLEM_FOLKETRYGDEN -> "";
-            case INNTEKT_FRA_ARBEIDSFORHOLD -> "";
-            case LØNNSENDRING_I_PERIODEN -> "";
-            case MANN_ADOPTERER -> "";
-            case MOTTAR_YTELSE_ARBEID -> "";
-            case MOTTAR_YTELSE_FRILANS -> "";
-            case MOTTATT_DATO -> "";
-            case OMSORG -> "";
-            case OMSORGSOVERTAKELSESDATO -> "";
-            case OMSORGSVILKAR -> "";
-            case IKKE_OMSORG_PERIODEN -> "";
-            case INNTEKTSKATEGORI_FOR_ANDEL -> "";
-            case OPPHOLDSRETT_EOS -> "";
-            case OPPHOLDSRETT_IKKE_EOS -> "";
-            case OVERSTYRT_BEREGNING -> "";
-            case OVERSTYRT_VURDERING -> "";
-            case SELVSTENDIG_NÆRINGSDRIVENDE -> "";
-            case SOKERSOPPLYSNINGSPLIKT -> "";
-            case SVANGERSKAPSPENGERVILKÅRET -> "";
-            case SOKNADSFRIST -> "";
-            case SOKNADSFRISTVILKARET -> "";
-            case STARTDATO_FRA_SOKNAD -> "";
-            case TERMINBEKREFTELSE -> "";
-            case TERMINDATO -> "";
-            case UTSTEDTDATO -> "";
-            case VILKAR_SOM_ANVENDES -> "";
-            case FASTSETT_RESULTAT_PERIODEN -> "";
-            case AVKLART_PERIODE -> "";
-            case ANDEL_ARBEID -> "";
-            case UTTAK_TREKKDAGER -> "";
-            case UTTAK_STØNADSKONTOTYPE -> "";
-            case UTTAK_PERIODE_RESULTAT_TYPE -> "";
-            case UTTAK_PROSENT_UTBETALING -> "";
-            case UTTAK_SAMTIDIG_UTTAK -> "";
-            case UTTAK_TREKKDAGER_FLERBARN_KVOTE -> "";
-            case UTTAK_PERIODE_RESULTAT_ÅRSAK -> "";
-            case UTTAK_GRADERING_ARBEIDSFORHOLD -> "";
-            case UTTAK_GRADERING_AVSLAG_ÅRSAK -> "";
-            case UTTAK_SPLITT_TIDSPERIODE -> "";
-            case SYKDOM -> "";
-            case ARBEIDSFORHOLD -> "";
-            case NY_FORDELING -> "";
-            case NY_AKTIVITET -> "";
-            case NYTT_REFUSJONSKRAV -> "";
-            case INNTEKT -> "";
-            case INNTEKTSKATEGORI -> "";
-            case NAVN -> "";
-            case FNR -> "";
-            case PERIODE_FOM -> "";
-            case PERIODE_TOM -> "";
-            case MANDAT -> "";
-            case KONTAKTPERSON -> "";
-            case BRUKER_TVUNGEN -> "";
-            case TYPE_VERGE -> "";
-            case DAGPENGER_INNTEKT -> "";
-            case KLAGE_RESULTAT_NFP -> "";
-            case KLAGE_RESULTAT_KA -> "";
-            case KLAGE_OMGJØR_ÅRSAK -> "";
-            case ER_KLAGER_PART -> "";
-            case ER_KLAGE_KONKRET -> "";
-            case ER_KLAGEFRIST_OVERHOLDT -> "";
-            case ER_KLAGEN_SIGNERT -> "";
-            case PA_KLAGD_BEHANDLINGID -> "";
-            case ANKE_RESULTAT -> "";
-            case KONTROLL_AV_BESTEBEREGNING -> "";
-            case ANKE_OMGJØR_ÅRSAK -> "";
-            case ER_ANKER_IKKE_PART -> "";
-            case ER_ANKE_IKKE_KONKRET -> "";
-            case ER_ANKEFRIST_IKKE_OVERHOLDT -> "";
-            case ER_ANKEN_IKKE_SIGNERT -> "";
-            case PA_ANKET_BEHANDLINGID -> "";
-            case VURDER_ETTERLØNN_SLUTTPAKKE -> "";
-            case FASTSETT_ETTERLØNN_SLUTTPAKKE -> "";
-            case ER_VILKÅRENE_TILBAKEKREVING_OPPFYLT -> "";
-            case ER_SÆRLIGE_GRUNNER_TIL_REDUKSJON -> "";
-            case FASTSETT_VIDERE_BEHANDLING -> "";
-            case RETT_TIL_FORELDREPENGER -> "";
-            case MOR_MOTTAR_UFØRETRYGD -> "";
-            case MOR_MOTTAR_STØNAD_EØS -> "";
-            case ANNEN_FORELDER_RETT_EØS -> "";
-            case VURDER_GRADERING_PÅ_ANDEL_UTEN_BG -> "";
-            case DEKNINGSGRAD -> "";
-            case TILBAKETREKK -> "";
-            case SAKSMARKERING -> "";
-            case INNHENT_SED -> "";
-            case HEL_TILRETTELEGGING_FOM -> "";
-            case DELVIS_TILRETTELEGGING_FOM -> "";
-            case STILLINGSPROSENT -> "";
-            case SLUTTE_ARBEID_FOM -> "";
-            case TILRETTELEGGING_BEHOV_FOM -> "";
-            case TILRETTELEGGING_SKAL_BRUKES -> "";
-            case FARESIGNALER -> "";
-            case MILITÆR_ELLER_SIVIL -> "";
-            case NY_REFUSJONSFRIST -> "";
-            case NY_STARTDATO_REFUSJON -> "";
-            case DELVIS_REFUSJON_FØR_STARTDATO -> "";
-            case ORGANISASJONSNUMMER -> "";
-            case ARBEIDSFORHOLD_BEKREFTET_TOM_DATO -> "";
-            case ANKE_AVVIST_ÅRSAK -> "";
-            case AKTIVITETSKRAV_AVKLARING -> "";
-            case UTTAKPERIODE_DOK_AVKLARING -> "";
-            case FAKTA_UTTAK_PERIODE -> "";
-            case SVP_OPPHOLD_PERIODE -> "";
-            case VURDERT_ETTERBETALING_TIL_SØKER -> "";
+            case EKTEFELLES_BARN -> "Ektefelles/samboers barn";
+            case ENDRING_NAERING -> "Endring i næringsvirksomhet";
+            case ENDRING_TIDSBEGRENSET_ARBEIDSFORHOLD -> "Arbeidsforhold hos {value}";
+            case ER_SOKER_BOSATT_I_NORGE -> "Bosted";
+            case FODSELSVILKARET -> "Fødselsvilkåret";
+            case FODSELSDATO -> "Fødselsdato";
+            case FORDELING_FOR_ANDEL -> "{value}";
+            case FORDELING_FOR_NY_ANDEL -> "{value}";
+            case FORDELING_ETTER_BESTEBEREGNING -> "Fordeling etter besteberegning";
+            case FORELDREANSVARSVILKARET -> "Foreldreansvarsvilkåret";
+            case FRILANS_INNTEKT -> "Frilansinntekt";
+            case FRILANSVIRKSOMHET -> "Frilansvirksomhet";
+            case GYLDIG_MEDLEM_FOLKETRYGDEN -> "Vurder om søker har gyldig medlemskap i perioden";
+            case INNTEKT_FRA_ARBEIDSFORHOLD -> "Inntekt fra {value}";
+            case LØNNSENDRING_I_PERIODEN -> "Lønnsendring siste tre måneder";
+            case MANN_ADOPTERER -> "Mann adopterer";
+            case MOTTAR_YTELSE_ARBEID -> "Mottar søker ytelse for arbeid i {value}";
+            case MOTTAR_YTELSE_FRILANS -> "Mottar søker ytelse for frilansaktiviteten";
+            case MOTTATT_DATO -> "Dato for når søknaden kan anses som mottatt";
+            case OMSORG -> "Omsorg";
+            case OMSORGSOVERTAKELSESDATO -> "Omsorgsovertakelsesdato";
+            case OMSORGSVILKAR -> "Omsorgsvilkåret";
+            case IKKE_OMSORG_PERIODEN -> "Søker har ikke omsorg for barnet i perioden";
+            case INNTEKTSKATEGORI_FOR_ANDEL -> "Inntektskategori for {value}";
+            case OPPHOLDSRETT_EOS -> "Oppholdsrett";
+            case OPPHOLDSRETT_IKKE_EOS -> "Lovlig opphold";
+            case OVERSTYRT_BEREGNING -> "Overstyrt beregning:; Beløpet er endret fra";
+            case OVERSTYRT_VURDERING -> "Overstyrt vurdering:; Utfallet er endret fra";
+            case SELVSTENDIG_NÆRINGSDRIVENDE -> "Selvstendig næringsdrivende";
+            case SOKERSOPPLYSNINGSPLIKT -> "Søkers opplysningsplikt";
+            case SVANGERSKAPSPENGERVILKÅRET -> "Svangerskapsvilkåret";
+            case SOKNADSFRIST -> "Søknadsfrist";
+            case SOKNADSFRISTVILKARET -> "Søknadsfristvilkåret";
+            case STARTDATO_FRA_SOKNAD -> "Startdato fra søknad";
+            case TERMINBEKREFTELSE -> "Terminbekreftelse";
+            case TERMINDATO -> "Termindato";
+            case UTSTEDTDATO -> "Utstedt dato";
+            case VILKAR_SOM_ANVENDES -> "Vilkår som anvendes";
+            case FASTSETT_RESULTAT_PERIODEN -> "Resultat for perioden";
+            case AVKLART_PERIODE -> "Avklart periode";
+            case ANDEL_ARBEID -> "Andel i arbeid";
+            case UTTAK_TREKKDAGER -> "Trekk";
+            case UTTAK_STØNADSKONTOTYPE -> "Stønadskonto";
+            case UTTAK_PERIODE_RESULTAT_TYPE -> "Resultatet";
+            case UTTAK_PROSENT_UTBETALING -> "Utbetalingsgrad";
+            case UTTAK_SAMTIDIG_UTTAK -> "Samtidig uttak";
+            case UTTAK_TREKKDAGER_FLERBARN_KVOTE -> "Flerbarnsdager";
+            case UTTAK_PERIODE_RESULTAT_ÅRSAK -> "Årsak resultat";
+            case UTTAK_GRADERING_ARBEIDSFORHOLD -> "Gradering av arbeidsforhold";
+            case UTTAK_GRADERING_AVSLAG_ÅRSAK -> "Årsak avslag gradering";
+            case UTTAK_SPLITT_TIDSPERIODE -> endretFeltNavn.getNavn(); // Ikke i frontend
+            case SYKDOM -> "Sykdom";
+            case ARBEIDSFORHOLD -> "Arbeidsforhold hos {value}";
+            case NY_FORDELING -> "Ny fordeling <b>{value}</b>";
+            case NY_AKTIVITET -> "Det er lagt til ny aktivitet for <b>{value}</b>";
+            case NYTT_REFUSJONSKRAV -> "Nytt refusjonskrav";
+            case INNTEKT -> "Inntekt fra {value}";
+            case INNTEKTSKATEGORI -> "Inntektskategori";
+            case NAVN -> "Navn";
+            case FNR -> "Fødselsnummer";
+            case PERIODE_FOM -> "Periode f.o.m.";
+            case PERIODE_TOM -> "Periode t.o.m.";
+            case MANDAT -> "Mandat";
+            case KONTAKTPERSON -> "Kontaktperson";
+            case BRUKER_TVUNGEN -> "Søker er under tvungen forvaltning";
+            case TYPE_VERGE -> "Type verge";
+            case DAGPENGER_INNTEKT -> "Dagpenger";
+            case KLAGE_RESULTAT_NFP -> "Resultat";
+            case KLAGE_RESULTAT_KA -> "Ytelsesvedtak";
+            case KLAGE_OMGJØR_ÅRSAK -> "Årsak til omgjøring";
+            case ER_KLAGER_PART -> "Er klager part i saken";
+            case ER_KLAGE_KONKRET -> "Klages det på konkrete elementer i vedtaket";
+            case ER_KLAGEFRIST_OVERHOLDT -> "Er klagefristen overholdt";
+            case ER_KLAGEN_SIGNERT -> "Er klagen signert";
+            case PA_KLAGD_BEHANDLINGID -> "Vedtaket som er påklagd";
+            case ANKE_RESULTAT -> "Vedtaket som er anket";
+            case KONTROLL_AV_BESTEBEREGNING -> "Godkjenning av automatisk besteberegning";
+            case ANKE_OMGJØR_ÅRSAK -> "Omgjøringsårsak";
+            case ER_ANKER_IKKE_PART -> "Er anker ikke part";
+            case ER_ANKE_IKKE_KONKRET -> "Er anke ikke konkret";
+            case ER_ANKEFRIST_IKKE_OVERHOLDT -> "Er ankefrist ikke overholdt";
+            case ER_ANKEN_IKKE_SIGNERT -> "Er anken ikke signer";
+            case PA_ANKET_BEHANDLINGID -> "Vedtaket som er påklagd";
+            case VURDER_ETTERLØNN_SLUTTPAKKE -> "Inntekt fra etterlønn eller sluttpakke";
+            case FASTSETT_ETTERLØNN_SLUTTPAKKE -> "Inntekten";
+            case ER_VILKÅRENE_TILBAKEKREVING_OPPFYLT -> endretFeltNavn.getNavn(); // Ikke i frontend
+            case ER_SÆRLIGE_GRUNNER_TIL_REDUKSJON -> endretFeltNavn.getNavn(); // Ikke i frontend
+            case FASTSETT_VIDERE_BEHANDLING -> endretFeltNavn.getNavn(); // Ikke i frontend
+            case RETT_TIL_FORELDREPENGER -> "Rett til foreldrepenger";
+            case MOR_MOTTAR_UFØRETRYGD -> "Mor mottar uføretrygd";
+            case MOR_MOTTAR_STØNAD_EØS -> endretFeltNavn.getNavn(); // Ikke i frontend
+            case ANNEN_FORELDER_RETT_EØS -> "Annen forelder har tilstrekkelig opptjening fra land i EØS";
+            case VURDER_GRADERING_PÅ_ANDEL_UTEN_BG -> "Inntektsgrunnlag ved gradering";
+            case DEKNINGSGRAD -> "Dekningsgrad";
+            case TILBAKETREKK -> "Tilbaketrekk";
+            case SAKSMARKERING -> "Saksmarkering";
+            case INNHENT_SED -> "Innhent dokumentasjon";
+            case HEL_TILRETTELEGGING_FOM -> "Hel tilrettelegging fra og med";
+            case DELVIS_TILRETTELEGGING_FOM -> "Delvis tilrettelegging fra og med";
+            case STILLINGSPROSENT -> "stillingsprosent";
+            case SLUTTE_ARBEID_FOM -> "Slutte arbeid fra og med";
+            case TILRETTELEGGING_BEHOV_FOM -> "Tilrettelegging er nødvendig fra og med";
+            case TILRETTELEGGING_SKAL_BRUKES -> "Tilrettelegging skal brukes";
+            case FARESIGNALER -> "Resultat";
+            case MILITÆR_ELLER_SIVIL -> "Har søker militær- eller siviltjeneste i opptjeningsperioden";
+            case NY_REFUSJONSFRIST -> "Utvidelse av frist for fremsatt refusjonskrav for {value}";
+            case NY_STARTDATO_REFUSJON -> "Startdato for refusjon til {value}";
+            case DELVIS_REFUSJON_FØR_STARTDATO -> "Delvis refusjon før ";
+            case ORGANISASJONSNUMMER -> "Organisasjonsnummer";
+            case ARBEIDSFORHOLD_BEKREFTET_TOM_DATO -> endretFeltNavn.getNavn(); // Ikke i frontend
+            case ANKE_AVVIST_ÅRSAK -> "Avvisningsårsak";
+            case AKTIVITETSKRAV_AVKLARING -> endretFeltNavn.getNavn(); // Ikke i frontend
+            case UTTAKPERIODE_DOK_AVKLARING -> "Perioden {value}";
+            case FAKTA_UTTAK_PERIODE -> "Perioden {value}";
+            case SVP_OPPHOLD_PERIODE -> "Periode med opphold";
+            case VURDERT_ETTERBETALING_TIL_SØKER -> "Vurdering av etterbetaling til søker";
             case UDEFINIERT -> throw new IllegalStateException("UDEFINTER ");
         };
 
-
-        var endretFeltVerdiFra= null;
-        var endretFeltVerdiTil= null;
-
+        return tekstFrontend.replace("{value}", verdi);
     }
 
     private static String findEndretFeltVerdi(HistorikkinnslagDel del, String verdi) {
