ef36a1d39d794f6d5cd4da7cc5b6b59a75573c57
espenwaaga <36693523+espenwaaga@users.noreply.github.com>
2024-10-16 12:07:59 +0200

    Maltype5 utkast 1 ferdig

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/historikk/HistorikkDtoTjeneste.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/historikk/HistorikkDtoTjeneste.java
@@ -6,16 +6,19 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
 import no.nav.foreldrepenger.behandlingslager.behandling.historikk.HistorikkRepository;
+import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.foreldrepenger.domene.typer.Saksnummer;
 
 @ApplicationScoped
 public class HistorikkDtoTjeneste {
 
     private HistorikkRepository historikkRepository;
+    private BehandlingRepository behandlingRepository;
 
     @Inject
-    public HistorikkDtoTjeneste(HistorikkRepository historikkRepository) {
+    public HistorikkDtoTjeneste(HistorikkRepository historikkRepository, BehandlingRepository behandlingRepository) {
         this.historikkRepository = historikkRepository;
+        this.behandlingRepository = behandlingRepository;
     }
 
     HistorikkDtoTjeneste() {
@@ -24,7 +27,7 @@ public class HistorikkDtoTjeneste {
 
     public List<HistorikkinnslagDtoV2> hentForSak(Saksnummer saksnummer) {
         return historikkRepository.hentHistorikkForSaksnummer(saksnummer)
-            .stream().map(HistorikkV2Adapter::map)
+            .stream().map(historikkinnslag -> HistorikkV2Adapter.map(historikkinnslag, behandlingRepository.hentBehandling(historikkinnslag.getBehandlingId()).getUuid()))
             .toList();
     }
 
