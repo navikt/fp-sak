139fbec533f22e1da5889888cd2c51e51e839d31
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2024-10-18 20:16:29 +0200

    Felles 7.3.0

--- domenetjenester/produksjonsstyring/src/main/java/no/nav/foreldrepenger/produksjonsstyring/behandlingenhet/nom/SkjermetPersonKlient.java
+++ domenetjenester/produksjonsstyring/src/main/java/no/nav/foreldrepenger/produksjonsstyring/behandlingenhet/nom/SkjermetPersonKlient.java
@@ -12,8 +12,6 @@ import no.nav.vedtak.felles.integrasjon.skjerming.AbstractSkjermetPersonGCPKlien
  * OBS: Bruker OnPrem pga ustabil forbindelse tiltjenesten i prod-gcp - mye timouts
  */
 @Dependent
-//@RestClientConfig(tokenConfig = TokenFlow.NO_AUTH_NEEDED, endpointProperty = "skjermet.person.onprem.rs.url", endpointDefault = "http://skjermede-personer-pip.nom")
-//public class SkjermetPersonKlient extends AbstractSkjermetPersonOnPremKlient {
 @RestClientConfig(tokenConfig = TokenFlow.AZUREAD_CC, endpointProperty = "skjermet.person.rs.url", endpointDefault = "https://skjermede-personer-pip.intern.nav.no",
     scopesProperty = "skjermet.person.scopes", scopesDefault = "api://prod-gcp.nom.skjermede-personer-pip/.default")
 public class SkjermetPersonKlient extends AbstractSkjermetPersonGCPKlient {
