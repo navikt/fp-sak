8088ea84c74d2008332a2b0a833a4646b7164960
johannbm <johannes.moskvil@gmail.com>
2024-10-22 08:46:16 +0200

    fjer ubrukt import

--- domenetjenester/arbeidsforhold/src/main/java/no/nav/foreldrepenger/domene/iay/modell/InntektsmeldingBuilder.java
+++ domenetjenester/arbeidsforhold/src/main/java/no/nav/foreldrepenger/domene/iay/modell/InntektsmeldingBuilder.java
@@ -3,7 +3,6 @@ package no.nav.foreldrepenger.domene.iay.modell;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
