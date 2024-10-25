86a9ff3984f9a47ad00a625c802cc68585985aba
Michal J. Sladek <michal.sladek@nav.no>
2024-10-25 11:31:34 +0200

    Fikser aktørId og OrgNr toString maskering.

--- domenetjenester/arbeidsforhold/src/main/java/no/nav/foreldrepenger/domene/fpinntektsmelding/OpprettForespørselRequest.java
+++ domenetjenester/arbeidsforhold/src/main/java/no/nav/foreldrepenger/domene/fpinntektsmelding/OpprettForespørselRequest.java
@@ -7,14 +7,48 @@ import com.fasterxml.jackson.annotation.JsonValue;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
+import no.nav.foreldrepenger.domene.typer.AktørId;
+
 public record OpprettForespørselRequest(@NotNull @Valid AktørIdDto aktørId,
                                         @NotNull @Valid OrganisasjonsnummerDto orgnummer,
                                         @NotNull LocalDate skjæringstidspunkt,
                                         @NotNull YtelseType ytelsetype,
                                         @NotNull @Valid SaksnummerDto fagsakSaksnummer) {
-    protected record AktørIdDto(@NotNull @JsonValue String id){}
+    protected record AktørIdDto(@NotNull @JsonValue String id){
+        @Override
+        public String toString() {
+            return getClass().getSimpleName() + "<" + maskerAktørId() + ">";
+        }
+
+        private String maskerAktørId() {
+            if (id == null) {
+                return "";
+            }
+            var length = id.length();
+            if (length <= 4) {
+                return "*".repeat(length);
+            }
+            return "*".repeat(length - 4) + id.substring(length - 4);
+        }
+    }
     protected record SaksnummerDto(@NotNull @JsonValue String saksnr){}
-    protected record OrganisasjonsnummerDto(@NotNull @JsonValue String orgnr){}
+    protected record OrganisasjonsnummerDto(@NotNull @JsonValue String orgnr){
+        @Override
+        public String toString() {
+            return getClass().getSimpleName() + "<" + tilMaskertNummer(orgnr) + ">";
+        }
+
+        public static String tilMaskertNummer(String orgNummer) {
+            if (orgNummer == null) {
+                return null;
+            }
+            var length = orgNummer.length();
+            if (length <= 4) {
+                return "*".repeat(length);
+            }
+            return "*".repeat(length - 4) + orgNummer.substring(length - 4);
+        }
+    }
     protected enum YtelseType {
         FORELDREPENGER,
         SVANGERSKAPSPENGER
