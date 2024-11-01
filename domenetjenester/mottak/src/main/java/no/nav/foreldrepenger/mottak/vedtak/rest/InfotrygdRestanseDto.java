32d7bba5dbba9aa203d627f5231c002b4b7d4fe9
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2024-11-01 10:53:00 +0100

    Forvaltningsmetoder for å hente Infotrygd-restanser (#6834)

---
+++ domenetjenester/mottak/src/main/java/no/nav/foreldrepenger/mottak/vedtak/rest/InfotrygdRestanseDto.java
@@ -0,0 +1,6 @@
+package no.nav.foreldrepenger.mottak.vedtak.rest;
+
+import java.time.LocalDate;
+
+public record InfotrygdRestanseDto(String fnr, String valg, String type, LocalDate registrert, LocalDate mottatt, String reellEnhet) {
+}
