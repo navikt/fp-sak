fe135be3e5ff4d77ba713b52c11156f262409243
Anders Palfi <anderspalfi@gmail.com>
2024-11-05 12:26:07 +0100

    TFP-5858 rydder bort opphørsdato fra beregningsresultat dto (#6846)

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/beregningsresultat/dto/BeregningsresultatMedUttaksplanDto.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/beregningsresultat/dto/BeregningsresultatMedUttaksplanDto.java
@@ -1,6 +1,5 @@
 package no.nav.foreldrepenger.web.app.tjenester.behandling.beregningsresultat.dto;
 
-import java.time.LocalDate;
 import java.util.List;
 
-public record BeregningsresultatMedUttaksplanDto(LocalDate opphoersdato, List<BeregningsresultatPeriodeDto> perioder) {}
+public record BeregningsresultatMedUttaksplanDto(List<BeregningsresultatPeriodeDto> perioder) {}
