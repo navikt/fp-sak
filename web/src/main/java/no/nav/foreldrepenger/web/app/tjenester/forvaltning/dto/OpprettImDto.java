56e9fa4a1b0e5cb0c6fa90c21c66016c1ab32603
AnjaAalerud <52454508+AnjaAalerud@users.noreply.github.com>
2024-11-05 14:23:26 +0100

    Legger til resssurstype fpsak for forvaltningsendepunktet opprettIMFo… (#6853)
    * Legger til resssurstype fpsak for forvaltningsendepunktet opprettIMForesporselForBehandling for å kunne hente informasjon om manglende inntektsmeldinger fra abakus
    
    * Oppretter en midlertidig OpprettImDto for bruk av ForvaltningUttrekkRestTjeneste endepunktet /opprettIMForesporselForBehandling som ikke legger til abac attributter.

---
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/forvaltning/dto/OpprettImDto.java
@@ -0,0 +1,44 @@
+package no.nav.foreldrepenger.web.app.tjenester.forvaltning.dto;
+
+import java.util.UUID;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+import jakarta.ws.rs.QueryParam;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import no.nav.foreldrepenger.behandlingslager.behandling.aksjonspunkt.AksjonspunktDefinisjon;
+import no.nav.vedtak.sikkerhet.abac.AbacDataAttributter;
+import no.nav.vedtak.sikkerhet.abac.AbacDto;
+//Midlertidig dto for forvaltningsendepunktet /opprettIMForesporselForBehandling i klassen ForvaltningsUttrekkRestTjeneste
+public class OpprettImDto implements AbacDto {
+    @Valid
+    @QueryParam("behandlingUuid")
+    private UUID behandlingUuid;
+
+    @Valid
+    @NotNull
+    @Digits(integer = 4, fraction = 0)
+    @QueryParam("aksjonspunktKode")
+    private String aksjonspunktKode;
+
+    @Override
+    public AbacDataAttributter abacAttributter() {
+        return AbacDataAttributter.opprett();
+    }
+
+    public UUID getBehandlingUuid() {
+        return behandlingUuid;
+    }
+
+    public String getAksjonspunktKode() {
+        return aksjonspunktKode;
+    }
+
+    @JsonIgnore
+    public AksjonspunktDefinisjon getAksjonspunktDefinisjon() {
+        return AksjonspunktDefinisjon.fraKode(aksjonspunktKode);
+    }
+}
