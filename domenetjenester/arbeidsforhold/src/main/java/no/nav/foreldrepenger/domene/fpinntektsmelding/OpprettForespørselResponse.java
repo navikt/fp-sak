8afafd5e6c5a793473a182a77b9c77a11eb94851
anjaaalerud <anja.gotesen.aalerud@nav.no>
2024-11-18 15:37:17 +0100

    POFIM-131 Legger til ny OpprettForespørselResponse IKKE_OPPRETTET_FINNES_ALLEREDE for å erstatte  IKKE_OPPRETTET_FINNES_ALLEREDE_ÅPEN. Forbedrer også logging dersom ikke opprettet slår til

--- domenetjenester/arbeidsforhold/src/main/java/no/nav/foreldrepenger/domene/fpinntektsmelding/OpprettForespørselResponse.java
+++ domenetjenester/arbeidsforhold/src/main/java/no/nav/foreldrepenger/domene/fpinntektsmelding/OpprettForespørselResponse.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotNull;
 public record OpprettForespørselResponse(@NotNull @Valid ForespørselResultat forespørselResultat) {
     protected enum ForespørselResultat {
         FORESPØRSEL_OPPRETTET,
+        IKKE_OPPRETTET_FINNES_ALLEREDE,
         IKKE_OPPRETTET_FINNES_ALLEREDE_ÅPEN
     }
 }
