65400e83bbdecc3245099fdf919c8c2a33db1787
espenwaaga <36693523+espenwaaga@users.noreply.github.com>
2024-11-22 12:03:30 +0100

    Legger til genereing av historikkinnslag for FaktaBeregningHistorikkKalkulusTjeneste som brukes i BeregningsgrunnlagOverstyringshåndterer og VurderFaktaOmBeregningOppdaterer

--- domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/rest/historikk/kalkulus/RefusjonHistorikkKalkulusTjeneste.java
+++ domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/rest/historikk/kalkulus/RefusjonHistorikkKalkulusTjeneste.java
@@ -1,19 +1,20 @@
 package no.nav.foreldrepenger.domene.rest.historikk.kalkulus;
 
+import static no.nav.foreldrepenger.behandlingslager.behandling.historikk.HistorikkinnslagTekstlinjeBuilder.fraTilEquals;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
 import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
 
-import no.nav.foreldrepenger.behandlingslager.behandling.historikk.HistorikkEndretFeltType;
+import no.nav.foreldrepenger.behandlingslager.behandling.historikk.HistorikkinnslagTekstlinjeBuilder;
 import no.nav.foreldrepenger.behandlingslager.behandling.opptjening.OpptjeningAktivitetType;
 import no.nav.foreldrepenger.domene.aksjonspunkt.BeregningsgrunnlagPrStatusOgAndelEndring;
 import no.nav.foreldrepenger.domene.aksjonspunkt.RefusjonEndring;
 import no.nav.foreldrepenger.domene.iay.modell.ArbeidsforholdOverstyring;
 import no.nav.foreldrepenger.domene.rest.historikk.ArbeidsgiverHistorikkinnslag;
-import no.nav.foreldrepenger.historikk.HistorikkInnslagTekstBuilder;
-
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Optional;
 
 /**
  * Historikktjeneste for endring av inntekt
@@ -32,41 +33,24 @@ public class RefusjonHistorikkKalkulusTjeneste {
         this.arbeidsgiverHistorikkinnslag = arbeidsgiverHistorikkinnslag;
     }
 
-    public void lagHistorikkOmEndret(HistorikkInnslagTekstBuilder tekstBuilder, List<ArbeidsforholdOverstyring> arbeidsforholdOverstyringer, BeregningsgrunnlagPrStatusOgAndelEndring andelEndring) {
-        Optional<RefusjonEndring> refusjonEndring = andelEndring.getRefusjonEndring();
-        refusjonEndring.ifPresent(endring -> opprettRefusjonHistorikk(tekstBuilder, arbeidsforholdOverstyringer, andelEndring, endring));
-    }
-
-    private void opprettRefusjonHistorikk(HistorikkInnslagTekstBuilder tekstBuilder, List<ArbeidsforholdOverstyring> arbeidsforholdOverstyringer, BeregningsgrunnlagPrStatusOgAndelEndring andelEndring, RefusjonEndring refusjonEndring) {
-        if (andelEndring.getAktivitetStatus().erArbeidstaker()) {
-            opprettHistorikkArbeidstakerInntekt(tekstBuilder, arbeidsforholdOverstyringer, andelEndring, refusjonEndring);
+    public Optional<HistorikkinnslagTekstlinjeBuilder> lagHistorikkOmEndret(List<ArbeidsforholdOverstyring> arbeidsforholdOverstyringer, BeregningsgrunnlagPrStatusOgAndelEndring andelEndring) {
+        if (!andelEndring.getAktivitetStatus().erArbeidstaker()) {
+            return Optional.empty();
         }
+        return andelEndring.getRefusjonEndring()
+            .map(refusjonEndring -> opprettRefusjonHistorikkForArbeidstakerInntekt(arbeidsforholdOverstyringer, andelEndring, refusjonEndring));
     }
 
-    private void opprettHistorikkArbeidstakerInntekt(HistorikkInnslagTekstBuilder tekstBuilder, List<ArbeidsforholdOverstyring> arbeidsforholdOverstyringer, BeregningsgrunnlagPrStatusOgAndelEndring andelEndring, RefusjonEndring refusjonEndring) {
+    private HistorikkinnslagTekstlinjeBuilder opprettRefusjonHistorikkForArbeidstakerInntekt(List<ArbeidsforholdOverstyring> arbeidsforholdOverstyringer, BeregningsgrunnlagPrStatusOgAndelEndring andelEndring, RefusjonEndring refusjonEndring) {
         if (OpptjeningAktivitetType.ETTERLØNN_SLUTTPAKKE.equals(andelEndring.getArbeidsforholdType())) {
-            opprettHistorikkEtterlønnSluttpakke(tekstBuilder, refusjonEndring);
-        } else if (andelEndring.getArbeidsgiver().isPresent()) {
-            opprettHistorikkArbeidsinntekt(
-                tekstBuilder,
-                arbeidsforholdOverstyringer,
-                andelEndring,
-                refusjonEndring);
+            return fraTilEquals("Fastsett søkers månedsinntekt fra etterlønn eller sluttpakke", refusjonEndring.getFraRefusjon(), refusjonEndring.getTilRefusjon());
         }
-    }
 
-    private void opprettHistorikkEtterlønnSluttpakke(HistorikkInnslagTekstBuilder tekstBuilder, RefusjonEndring inntektEndring) {
-        tekstBuilder.medEndretFelt(HistorikkEndretFeltType.FASTSETT_ETTERLØNN_SLUTTPAKKE,
-            inntektEndring.getFraRefusjon(),
-            inntektEndring.getTilRefusjon());
-    }
-
-    private void opprettHistorikkArbeidsinntekt(HistorikkInnslagTekstBuilder tekstBuilder, List<ArbeidsforholdOverstyring> arbeidsforholdOverstyringer, BeregningsgrunnlagPrStatusOgAndelEndring andelEndring, RefusjonEndring refusjonEndring) {
-        String arbeidsforholdInfo = arbeidsgiverHistorikkinnslag.lagTekstForArbeidsgiver(
-            andelEndring.getArbeidsgiver().get(),
-            arbeidsforholdOverstyringer);
-        tekstBuilder.medEndretFelt(HistorikkEndretFeltType.INNTEKT_FRA_ARBEIDSFORHOLD,
-            arbeidsforholdInfo,
+        var arbeidsforholdInfo = andelEndring.getArbeidsgiver().isPresent()
+            ? arbeidsgiverHistorikkinnslag.lagTekstForArbeidsgiver(andelEndring.getArbeidsgiver().get(), arbeidsforholdOverstyringer)
+            : andelEndring.getAktivitetStatus().getNavn();
+        return fraTilEquals(
+            String.format("Inntekt fra arbeidsforhold %s", arbeidsforholdInfo),
             tilInt(refusjonEndring.getFraRefusjon()),
             tilInt(refusjonEndring.getTilRefusjon()));
     }
