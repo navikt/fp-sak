6d4cf001165af44e18503b4ce6786d0da54d3f37
espenwaaga <36693523+espenwaaga@users.noreply.github.com>
2024-11-22 13:58:04 +0100

    Generer historikkinnslag for KontrollerStorEtterbetalingOppdaterer

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/tilbakekreving/aksjonspunkt/KontrollerStorEtterbetalingOppdaterer.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/tilbakekreving/aksjonspunkt/KontrollerStorEtterbetalingOppdaterer.java
@@ -1,5 +1,7 @@
 package no.nav.foreldrepenger.web.app.tjenester.behandling.tilbakekreving.aksjonspunkt;
 
+import static no.nav.foreldrepenger.behandlingslager.behandling.historikk.HistorikkinnslagTekstlinjeBuilder.fraTilEquals;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
@@ -7,34 +9,39 @@ import no.nav.foreldrepenger.behandling.aksjonspunkt.AksjonspunktOppdaterParamet
 import no.nav.foreldrepenger.behandling.aksjonspunkt.AksjonspunktOppdaterer;
 import no.nav.foreldrepenger.behandling.aksjonspunkt.DtoTilServiceAdapter;
 import no.nav.foreldrepenger.behandling.aksjonspunkt.OppdateringResultat;
-import no.nav.foreldrepenger.behandlingslager.behandling.historikk.HistorikkEndretFeltType;
-import no.nav.foreldrepenger.behandlingslager.behandling.historikk.HistorikkinnslagType;
+import no.nav.foreldrepenger.behandlingslager.behandling.historikk.HistorikkAktør;
+import no.nav.foreldrepenger.behandlingslager.behandling.historikk.Historikkinnslag2;
+import no.nav.foreldrepenger.behandlingslager.behandling.historikk.Historikkinnslag2Repository;
 import no.nav.foreldrepenger.behandlingslager.behandling.skjermlenke.SkjermlenkeType;
-import no.nav.foreldrepenger.historikk.HistorikkTjenesteAdapter;
 
 
 @ApplicationScoped
 @DtoTilServiceAdapter(dto = KontrollerStorEtterbetalingSøkerDto.class, adapter = AksjonspunktOppdaterer.class)
 class KontrollerStorEtterbetalingOppdaterer implements AksjonspunktOppdaterer<KontrollerStorEtterbetalingSøkerDto> {
 
-    private HistorikkTjenesteAdapter historikkTjenesteAdapter;
+    private Historikkinnslag2Repository historikkinnslagRepository;
 
     public KontrollerStorEtterbetalingOppdaterer() {
         //CDI
     }
 
     @Inject
-    public KontrollerStorEtterbetalingOppdaterer(HistorikkTjenesteAdapter historikkTjenesteAdapter) {
-        this.historikkTjenesteAdapter = historikkTjenesteAdapter;
+    public KontrollerStorEtterbetalingOppdaterer(Historikkinnslag2Repository historikkinnslagRepository) {
+        this.historikkinnslagRepository = historikkinnslagRepository;
     }
 
     @Override
     public OppdateringResultat oppdater(KontrollerStorEtterbetalingSøkerDto dto, AksjonspunktOppdaterParameter param) {
-        historikkTjenesteAdapter.tekstBuilder()
-            .medEndretFelt(HistorikkEndretFeltType.VURDERT_ETTERBETALING_TIL_SØKER, null, "Godkjent")
-            .medSkjermlenke(SkjermlenkeType.FAKTA_OM_SIMULERING)
-            .medBegrunnelse(dto.getBegrunnelse());
-        historikkTjenesteAdapter.opprettHistorikkInnslag(param.getBehandlingId(), HistorikkinnslagType.FAKTA_ENDRET);
+        var ref = param.getRef();
+        var historikkinnslag = new Historikkinnslag2.Builder()
+            .medAktør(HistorikkAktør.SAKSBEHANDLER)
+            .medFagsakId(ref.fagsakId())
+            .medBehandlingId(ref.behandlingId())
+            .medTittel(SkjermlenkeType.FAKTA_OM_SIMULERING)
+            .addTekstlinje(fraTilEquals("Vurdert etterbetaling til søker", null, "Godkjent"))
+            .addTekstlinje(dto.getBegrunnelse())
+            .build();
+        historikkinnslagRepository.lagre(historikkinnslag);
         return OppdateringResultat.utenOverhopp();
     }
 
