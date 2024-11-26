c00441551bf6187b7fdbf6abeab4f736abfee902
Anders Palfi <anders.palfi@nav.no>
2024-11-26 09:33:04 +0100

    testfix

--- domenetjenester/arbeidsforhold/src/test/java/no/nav/foreldrepenger/domene/arbeidInntektsmelding/historikk/ArbeidPermHistorikkInnslagTjenesteTest.java
+++ domenetjenester/arbeidsforhold/src/test/java/no/nav/foreldrepenger/domene/arbeidInntektsmelding/historikk/ArbeidPermHistorikkInnslagTjenesteTest.java
@@ -8,47 +8,45 @@ import static org.mockito.Mockito.when;
 
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
+import jakarta.inject.Inject;
+
 import org.junit.jupiter.api.Test;
 
 import no.nav.foreldrepenger.behandling.BehandlingReferanse;
-import no.nav.foreldrepenger.behandlingslager.behandling.BehandlingStatus;
-import no.nav.foreldrepenger.behandlingslager.behandling.historikk.Historikkinnslag2Repository;
 import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepositoryProvider;
 import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakYtelseType;
 import no.nav.foreldrepenger.behandlingslager.virksomhet.OrgNummer;
-import no.nav.foreldrepenger.dbstoette.EntityManagerAwareTest;
+import no.nav.foreldrepenger.dbstoette.CdiDbAwareTest;
 import no.nav.foreldrepenger.domene.arbeidInntektsmelding.AvklarPermisjonUtenSluttdatoDto;
+import no.nav.foreldrepenger.domene.arbeidsforhold.testutilities.behandling.IAYRepositoryProvider;
+import no.nav.foreldrepenger.domene.arbeidsforhold.testutilities.behandling.IAYScenarioBuilder;
 import no.nav.foreldrepenger.domene.arbeidsgiver.ArbeidsgiverOpplysninger;
 import no.nav.foreldrepenger.domene.arbeidsgiver.ArbeidsgiverTjeneste;
 import no.nav.foreldrepenger.domene.iay.modell.kodeverk.BekreftetPermisjonStatus;
-import no.nav.foreldrepenger.domene.typer.AktørId;
-import no.nav.foreldrepenger.domene.typer.Saksnummer;
 import no.nav.foreldrepenger.historikk.VurderArbeidsforholdHistorikkinnslag;
 
 
-class ArbeidPermHistorikkInnslagTjenesteTest extends EntityManagerAwareTest {
+@CdiDbAwareTest
+class ArbeidPermHistorikkInnslagTjenesteTest {
     private static final String KUNSTIG_ORG = OrgNummer.KUNSTIG_ORG;
     private static final String NAV_ORGNR = "889640782";
     private static final String INTERN_ARBEIDSFORHOLD_ID = "a6ea6724-868f-11e9-bc42-526af7764f64";
     private static final String INTERN_ARBEIDSFORHOLD_ID_2 = "a6ea6724-868f-11e9-bc42-526af7764f65";
     private static final String ARB_NAVN = "Arbeidsgivernavn";
 
-    private ArbeidPermHistorikkInnslagTjeneste arbeidPermHistorikkInnslagTjenesteTest;
-    private Historikkinnslag2Repository historikkRepository;
+    @Inject
+    private BehandlingRepositoryProvider repositoryProvider;
 
-    @BeforeEach
-    void setUp() {
-        var repositoryProvider = new BehandlingRepositoryProvider(getEntityManager());
-        var arbeidsgiverTjeneste = mock(ArbeidsgiverTjeneste.class);
-        when(arbeidsgiverTjeneste.hent(any())).thenReturn(new ArbeidsgiverOpplysninger(KUNSTIG_ORG, ARB_NAVN ));
-
-        historikkRepository = repositoryProvider.getHistorikkinnslag2Repository();
-        arbeidPermHistorikkInnslagTjenesteTest = new ArbeidPermHistorikkInnslagTjeneste(historikkRepository, arbeidsgiverTjeneste);
-    }
 
     @Test
     void lagTekstMedArbeidsgiver() {
+
+        var arbeidsgiverTjeneste = mock(ArbeidsgiverTjeneste.class);
+        when(arbeidsgiverTjeneste.hent(any())).thenReturn(new ArbeidsgiverOpplysninger(KUNSTIG_ORG, ARB_NAVN ));
+
+        var historikkRepository = repositoryProvider.getHistorikkinnslag2Repository();
+        var arbeidPermHistorikkInnslagTjenesteTest = new ArbeidPermHistorikkInnslagTjeneste(historikkRepository, arbeidsgiverTjeneste);
+
         var avklarteArbForhold = List.of(
             new AvklarPermisjonUtenSluttdatoDto(KUNSTIG_ORG, INTERN_ARBEIDSFORHOLD_ID, BekreftetPermisjonStatus.BRUK_PERMISJON),
             new AvklarPermisjonUtenSluttdatoDto(NAV_ORGNR, INTERN_ARBEIDSFORHOLD_ID_2, BekreftetPermisjonStatus.IKKE_BRUK_PERMISJON)
@@ -65,21 +63,12 @@ class ArbeidPermHistorikkInnslagTjenesteTest extends EntityManagerAwareTest {
         assertThat(historikkinnslag.getFirst().getTekstlinjer()).hasSize(3);
         assertThat(historikkinnslag.getFirst().getTekstlinjer().get(0).getTekst()).contains(ARB_NAVN, VurderArbeidsforholdHistorikkinnslag.SØKER_ER_I_PERMISJON.getNavn());
         assertThat(historikkinnslag.getFirst().getTekstlinjer().get(1).getTekst()).contains(ARB_NAVN, VurderArbeidsforholdHistorikkinnslag.SØKER_ER_IKKE_I_PERMISJON.getNavn());
-        assertThat(historikkinnslag.getFirst().getTekstlinjer().get(1).getTekst()).contains("begrunnelse");
+        assertThat(historikkinnslag.getFirst().getTekstlinjer().get(2).getTekst()).contains("begrunnelse");
     }
 
-    private static BehandlingReferanse getBehandlingReferanse() {
-        return new BehandlingReferanse(
-            new Saksnummer("1234456"),
-            1000001L,
-            FagsakYtelseType.FORELDREPENGER,
-            1000001L,
-            null,
-            BehandlingStatus.OPPRETTET,
-            null,
-            null,
-            AktørId.dummy(),
-            null
-        );
+    private BehandlingReferanse getBehandlingReferanse() {
+        var behandling = IAYScenarioBuilder.morSøker(FagsakYtelseType.FORELDREPENGER)
+            .lagre(new IAYRepositoryProvider(repositoryProvider.getEntityManager()));
+        return BehandlingReferanse.fra(behandling);
     }
 }
