18a6a150a127e25c6e6f0319dd2424035a7013f2
anjaaalerud <anja.gotesen.aalerud@nav.no>
2024-11-29 10:35:59 +0100

    TFP-5921 La til test

--- domenetjenester/mottak/src/test/java/no/nav/foreldrepenger/mottak/kompletthettjeneste/impl/fp/KompletthetsjekkerImplTest.java
+++ domenetjenester/mottak/src/test/java/no/nav/foreldrepenger/mottak/kompletthettjeneste/impl/fp/KompletthetsjekkerImplTest.java
@@ -47,6 +47,7 @@ import no.nav.foreldrepenger.domene.arbeidsforhold.InntektsmeldingTjeneste;
 import no.nav.foreldrepenger.domene.arbeidsforhold.impl.InntektsmeldingRegisterTjeneste;
 import no.nav.foreldrepenger.domene.fpinntektsmelding.FpInntektsmeldingTjeneste;
 import no.nav.foreldrepenger.domene.iay.modell.InntektsmeldingBuilder;
+import no.nav.foreldrepenger.domene.typer.AktørId;
 import no.nav.foreldrepenger.domene.typer.EksternArbeidsforholdRef;
 import no.nav.foreldrepenger.domene.typer.InternArbeidsforholdRef;
 import no.nav.foreldrepenger.kompletthet.ManglendeVedlegg;
@@ -246,6 +247,26 @@ class KompletthetsjekkerImplTest extends EntityManagerAwareTest {
         assertThat(kompletthetResultat.erOppfylt()).isFalse();
         assertThat(kompletthetResultat.ventefrist().toLocalDate()).isEqualTo(LocalDate.now().plusWeeks(3));
         verify(dokumentBestillerTjenesteMock, times(1)).bestillDokument(any(DokumentBestilling.class), any());
+    }
+
+    @Test
+    void skal_ikke_sende_brev_når_inntektsmelding_mangler_for_privat_arbeidsgivere() {
+        // Arrange
+        var behandling = ScenarioMorSøkerForeldrepenger.forFødsel().lagre(repositoryProvider);
+        HashMap<Arbeidsgiver, Set<InternArbeidsforholdRef>>  manglendeImPrivatArbGiver = new HashMap<>();
+        manglendeImPrivatArbGiver.put(Arbeidsgiver.fra(AktørId.dummy()), new HashSet<>());
+        mockManglendeInntektsmeldingKompletthet(manglendeImPrivatArbGiver);
+        testUtil.byggOgLagreFørstegangsSøknadMedMottattdato(behandling, LocalDate.now().minusWeeks(1),
+            STARTDATO_PERMISJON);
+
+        // Act
+        var kompletthetResultat = kompletthetsjekkerImpl.vurderEtterlysningInntektsmelding(
+            lagRef(behandling), lagStp(STARTDATO_PERMISJON));
+
+        // Assert
+        assertThat(kompletthetResultat.erOppfylt()).isTrue();
+        assertThat(kompletthetResultat.ventefrist()).isNull();
+        verify(dokumentBestillerTjenesteMock, times(0)).bestillDokument(any(DokumentBestilling.class), any());
     }
 
     @Test
