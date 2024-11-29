fbdcfae2830964f948d10770a20ee96f1552972b
Siri Mykland <siri.mykland@nav.no>
2024-11-29 15:04:12 +0100

    linting på BekreftSøkersOpplysningspliktManuellOppdaterer

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/søknad/aksjonspunkt/BekreftSøkersOpplysningspliktManuellOppdaterer.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/søknad/aksjonspunkt/BekreftSøkersOpplysningspliktManuellOppdaterer.java
@@ -10,7 +10,6 @@ import no.nav.foreldrepenger.behandling.aksjonspunkt.OppdateringResultat;
 import no.nav.foreldrepenger.behandlingskontroll.transisjoner.FellesTransisjoner;
 import no.nav.foreldrepenger.behandlingslager.behandling.aksjonspunkt.AksjonspunktStatus;
 import no.nav.foreldrepenger.behandlingslager.behandling.historikk.HistorikkAktør;
-import no.nav.foreldrepenger.behandlingslager.behandling.historikk.HistorikkEndretFeltVerdiType;
 import no.nav.foreldrepenger.behandlingslager.behandling.historikk.Historikkinnslag2;
 import no.nav.foreldrepenger.behandlingslager.behandling.historikk.Historikkinnslag2Repository;
 import no.nav.foreldrepenger.behandlingslager.behandling.historikk.HistorikkinnslagTekstlinjeBuilder;
@@ -19,10 +18,9 @@ import no.nav.foreldrepenger.behandlingslager.behandling.skjermlenke.Skjermlenke
 import no.nav.foreldrepenger.behandlingslager.behandling.vilkår.Avslagsårsak;
 import no.nav.foreldrepenger.behandlingslager.behandling.vilkår.VilkårType;
 
-import static no.nav.foreldrepenger.behandlingslager.behandling.historikk.HistorikkinnslagTekstlinjeBuilder.fraTilEquals;
 
 @ApplicationScoped
-@DtoTilServiceAdapter(dto = BekreftSokersOpplysningspliktManuDto.class, adapter=AksjonspunktOppdaterer.class)
+@DtoTilServiceAdapter(dto = BekreftSokersOpplysningspliktManuDto.class, adapter = AksjonspunktOppdaterer.class)
 public class BekreftSøkersOpplysningspliktManuellOppdaterer implements AksjonspunktOppdaterer<BekreftSokersOpplysningspliktManuDto> {
 
     private Historikkinnslag2Repository historikkinnslag2Repository;
@@ -57,12 +55,10 @@ public class BekreftSøkersOpplysningspliktManuellOppdaterer implements Aksjonsp
             return resultatBuilder.build();
         } else {
             // Hoppe rett til foreslå vedtak uten totrinnskontroll
-            åpneAksjonspunkter.stream()
-                .filter(a -> !a.getAksjonspunktDefinisjon().equals(dto.getAksjonspunktDefinisjon())) // Ikke seg selv
+            åpneAksjonspunkter.stream().filter(a -> !a.getAksjonspunktDefinisjon().equals(dto.getAksjonspunktDefinisjon())) // Ikke seg selv
                 .forEach(a -> resultatBuilder.medEkstraAksjonspunktResultat(a.getAksjonspunktDefinisjon(), AksjonspunktStatus.AVBRUTT));
 
-            return resultatBuilder
-                .medFremoverHopp(FellesTransisjoner.FREMHOPP_VED_AVSLAG_VILKÅR)
+            return resultatBuilder.medFremoverHopp(FellesTransisjoner.FREMHOPP_VED_AVSLAG_VILKÅR)
                 .leggTilManueltAvslåttVilkår(VilkårType.SØKERSOPPLYSNINGSPLIKT, Avslagsårsak.MANGLENDE_DOKUMENTASJON)
                 .build();
         }
@@ -74,11 +70,9 @@ public class BekreftSøkersOpplysningspliktManuellOppdaterer implements Aksjonsp
         var historikkinnslag = new Historikkinnslag2.Builder().medAktør(HistorikkAktør.SAKSBEHANDLER)
             .medBehandlingId(param.getBehandlingId())
             .medFagsakId(param.getFagsakId())
-            .medTittel(SkjermlenkeType.OPPLYSNINGSPLIKT);
-
-
-        historikkinnslag.addTekstlinje(new HistorikkinnslagTekstlinjeBuilder().til("Søkers opplysningsplikt", tilVerdi));
-        historikkinnslag.addTekstlinje(begrunnelse);
+            .medTittel(SkjermlenkeType.OPPLYSNINGSPLIKT)
+            .addTekstlinje(new HistorikkinnslagTekstlinjeBuilder().til("Søkers opplysningsplikt", tilVerdi))
+            .addTekstlinje(begrunnelse);
 
         historikkinnslag2Repository.lagre(historikkinnslag.build());
     }
