1465f144d78fbe911f860db5a62425d53d7639fd
Anders Palfi <anders.palfi@nav.no>
2024-11-29 12:11:10 +0100

    BekreftOmsorgOppdaterer 2

--- web/src/test/java/no/nav/foreldrepenger/web/app/tjenester/behandling/ytelsefordeling/BekreftOmsorgOppdatererTest.java
+++ web/src/test/java/no/nav/foreldrepenger/web/app/tjenester/behandling/ytelsefordeling/BekreftOmsorgOppdatererTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import no.nav.foreldrepenger.behandling.BehandlingReferanse;
 import no.nav.foreldrepenger.behandling.aksjonspunkt.AksjonspunktOppdaterParameter;
@@ -17,14 +16,10 @@ import no.nav.foreldrepenger.behandlingslager.testutilities.behandling.ScenarioM
 import no.nav.foreldrepenger.dbstoette.EntityManagerAwareTest;
 import no.nav.foreldrepenger.domene.ytelsefordeling.YtelseFordelingTjeneste;
 import no.nav.foreldrepenger.familiehendelse.rest.BekreftFaktaForOmsorgVurderingDto;
-import no.nav.foreldrepenger.historikk.HistorikkInnslagTekstBuilder;
-import no.nav.foreldrepenger.historikk.HistorikkTjenesteAdapter;
 
 class BekreftOmsorgOppdatererTest extends EntityManagerAwareTest {
 
     private static final AksjonspunktDefinisjon AKSJONSPUNKT_DEF = AksjonspunktDefinisjon.AVKLAR_LØPENDE_OMSORG;
-
-    private final HistorikkInnslagTekstBuilder tekstBuilder = new HistorikkInnslagTekstBuilder();
 
     private BehandlingRepositoryProvider behandlingRepositoryProvider;
     private YtelseFordelingTjeneste ytelseFordelingTjeneste;
@@ -56,18 +51,12 @@ class BekreftOmsorgOppdatererTest extends EntityManagerAwareTest {
         dto.setOmsorg(oppdatertOmsorg);
         var aksjonspunkt = behandling.getAksjonspunktFor(dto.getAksjonspunktDefinisjon());
         // Act
-        new BekreftOmsorgOppdaterer(lagMockHistory(), ytelseFordelingTjeneste, behandlingRepositoryProvider.getHistorikkinnslag2Repository())
+        new BekreftOmsorgOppdaterer(ytelseFordelingTjeneste, behandlingRepositoryProvider.getHistorikkinnslag2Repository())
             .oppdater(dto, new AksjonspunktOppdaterParameter(BehandlingReferanse.fra(behandling), dto, aksjonspunkt));
 
         var historikkinnslag = behandlingRepositoryProvider.getHistorikkinnslag2Repository().hent(behandling.getId()).getFirst();
         assertThat(historikkinnslag.getTekstlinjer()).hasSize(2);
         assertThat(historikkinnslag.getTekstlinjer().getFirst().getTekst()).contains("Omsorg", "Søker har omsorg for barnet");
         assertThat(historikkinnslag.getTekstlinjer().get(1).getTekst()).contains(dto.getBegrunnelse());
-    }
-
-    private HistorikkTjenesteAdapter lagMockHistory() {
-        var mockHistory = Mockito.mock(HistorikkTjenesteAdapter.class);
-        Mockito.when(mockHistory.tekstBuilder()).thenReturn(tekstBuilder);
-        return mockHistory;
     }
 }
