387bcf6ce19e36dc9e4e2103f0ee189e02dce5da
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2024-12-03 19:53:52 +0100

    Fjerne utgått dokumentbestiller

--- domenetjenester/dokumentbestiller/src/main/java/no/nav/foreldrepenger/dokumentbestiller/formidling/Dokument.java
+++ domenetjenester/dokumentbestiller/src/main/java/no/nav/foreldrepenger/dokumentbestiller/formidling/Dokument.java
@@ -1,33 +1,15 @@
 package no.nav.foreldrepenger.dokumentbestiller.formidling;
 
-import no.nav.foreldrepenger.kontrakter.formidling.v1.DokumentbestillingDto;
-import no.nav.foreldrepenger.kontrakter.formidling.v1.DokumentbestillingV2Dto;
 import no.nav.foreldrepenger.kontrakter.formidling.v3.DokumentBestillingDto;
 import no.nav.foreldrepenger.kontrakter.formidling.v3.DokumentForhåndsvisDto;
 
 public interface Dokument {
 
     /**
-     * @deprecated Erstattes av bestill med V3 kontrakt
-     * Produserer og journalfører et dokument definert i dtoen.
-     * @param dokumentbestillingV2Dto
-     */
-    @Deprecated(forRemoval = true)
-    void bestill(DokumentbestillingV2Dto dokumentbestillingV2Dto);
-
-    /**
      * Produserer og journalfører et dokument definert i dtoen.
      * @param dokumentBestillingDto
      */
     void bestill(DokumentBestillingDto dokumentBestillingDto);
-
-    /**
-     * @deprecated Erstattes av forhåndsvis med V3 kontrakt
-     * Forhåndsviser et dokument definert i dtoen.
-     * @param dokumentbestillingDto
-     */
-    @Deprecated(forRemoval = true)
-    byte[] forhåndsvis(DokumentbestillingDto dokumentbestillingDto);
 
     /**
      * Forhåndsviser et dokument definert i dtoen.
