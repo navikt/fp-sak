d49a7310b5db671aab9e7a3fed0ca0d2d91010be
Anders Palfi <anders.palfi@nav.no>
2024-12-03 10:56:13 +0100

    DokumentBestilt + testfix

--- domenetjenester/dokumentbestiller/src/test/java/no/nav/foreldrepenger/dokumentbestiller/formidling/DokumentBestiltTest.java
+++ domenetjenester/dokumentbestiller/src/test/java/no/nav/foreldrepenger/dokumentbestiller/formidling/DokumentBestiltTest.java
@@ -1,42 +1,19 @@
 package no.nav.foreldrepenger.dokumentbestiller.formidling;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
 
 import java.util.UUID;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-import no.nav.foreldrepenger.behandlingslager.aktør.NavBruker;
-import no.nav.foreldrepenger.behandlingslager.behandling.Behandling;
 import no.nav.foreldrepenger.behandlingslager.behandling.historikk.HistorikkAktør;
-import no.nav.foreldrepenger.behandlingslager.behandling.historikk.HistorikkRepository;
-import no.nav.foreldrepenger.behandlingslager.behandling.historikk.Historikkinnslag;
-import no.nav.foreldrepenger.behandlingslager.fagsak.Fagsak;
-import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakYtelseType;
-import no.nav.foreldrepenger.behandlingslager.geografisk.Språkkode;
+import no.nav.foreldrepenger.behandlingslager.testutilities.behandling.ScenarioMorSøkerEngangsstønad;
+import no.nav.foreldrepenger.behandlingslager.testutilities.behandling.ScenarioMorSøkerForeldrepenger;
 import no.nav.foreldrepenger.dokumentbestiller.DokumentBestilling;
 import no.nav.foreldrepenger.dokumentbestiller.DokumentMalType;
-import no.nav.foreldrepenger.domene.typer.AktørId;
 import no.nav.foreldrepenger.domene.typer.Saksnummer;
 
-@ExtendWith(MockitoExtension.class)
 class DokumentBestiltTest {
-
-    @Mock
-    private HistorikkRepository historikkRepository;
-
-    private DokumentBestilt dokumentBestilt;
-
-    @BeforeEach
-    void setUp() {
-        dokumentBestilt = new DokumentBestilt(historikkRepository);
-    }
 
     @Test
     void historikk_innslag_med_fritekst_mal() {
@@ -47,16 +24,18 @@ class DokumentBestiltTest {
 
         var bestilling = lagBestilling(malBrukt, journalførSom);
 
-        dokumentBestilt.opprettHistorikkinnslag(saksbehandler, dummyBehandling(), bestilling);
+        var scenario = ScenarioMorSøkerForeldrepenger.forFødsel();
+        var behandling = scenario.lagMocked();
+        var behandlingRepositoryProvider = scenario.mockBehandlingRepositoryProvider();
+        var dokumentBestilt = new DokumentBestilt(behandlingRepositoryProvider.getHistorikkinnslag2Repository());
+        dokumentBestilt.opprettHistorikkinnslag(saksbehandler, behandling, bestilling);
 
-        var captor = ArgumentCaptor.forClass(Historikkinnslag.class);
-        verify(historikkRepository).lagre(captor.capture());
-        var historikkinnslag = captor.getValue();
+        var historikkinnslag = behandlingRepositoryProvider.getHistorikkinnslag2Repository().hent(behandling.getId()).getFirst();
 
         assertThat(historikkinnslag.getAktør()).isEqualTo(saksbehandler);
-        assertThat(historikkinnslag.getHistorikkinnslagDeler()).hasSize(1);
-        assertThat(historikkinnslag.getHistorikkinnslagDeler().getFirst().getBegrunnelse()).isPresent();
-        assertThat(historikkinnslag.getHistorikkinnslagDeler().getFirst().getBegrunnelse().get()).containsSubsequence(journalførSom.getNavn(), malBrukt.getNavn());
+        assertThat(historikkinnslag.getTittel()).isEqualTo("Brev bestilt");
+        assertThat(historikkinnslag.getTekstlinjer()).hasSize(1);
+        assertThat(historikkinnslag.getTekstlinjer().getFirst().getTekst()).containsSubsequence(journalførSom.getNavn(), malBrukt.getNavn());
     }
 
     @Test
@@ -66,21 +45,18 @@ class DokumentBestiltTest {
 
         var bestilling = lagBestilling(malBrukt, null);
 
-        dokumentBestilt.opprettHistorikkinnslag(saksbehandler, dummyBehandling(), bestilling);
+        var scenario = ScenarioMorSøkerEngangsstønad.forFødsel();
+        var behandling = scenario.lagMocked();
+        var behandlingRepositoryProvider = scenario.mockBehandlingRepositoryProvider();
+        var dokumentBestilt = new DokumentBestilt(behandlingRepositoryProvider.getHistorikkinnslag2Repository());
+        dokumentBestilt.opprettHistorikkinnslag(saksbehandler, behandling, bestilling);
 
-        var captor = ArgumentCaptor.forClass(Historikkinnslag.class);
-        verify(historikkRepository).lagre(captor.capture());
-        var historikkinnslag = captor.getValue();
+        var historikkinnslag = behandlingRepositoryProvider.getHistorikkinnslag2Repository().hent(behandling.getId()).getFirst();
 
         assertThat(historikkinnslag.getAktør()).isEqualTo(saksbehandler);
-        assertThat(historikkinnslag.getHistorikkinnslagDeler()).hasSize(1);
-        assertThat(historikkinnslag.getHistorikkinnslagDeler().getFirst().getBegrunnelse()).isPresent();
-        assertThat(historikkinnslag.getHistorikkinnslagDeler().getFirst().getBegrunnelse().get()).contains(malBrukt.getNavn());
-    }
-
-    private static Behandling dummyBehandling() {
-        return Behandling.forFørstegangssøknad(Fagsak.opprettNy(FagsakYtelseType.FORELDREPENGER, NavBruker.opprettNy(AktørId.dummy(), Språkkode.NB)))
-            .build();
+        assertThat(historikkinnslag.getTittel()).isEqualTo("Brev bestilt");
+        assertThat(historikkinnslag.getTekstlinjer()).hasSize(1);
+        assertThat(historikkinnslag.getTekstlinjer().getFirst().getTekst()).containsSubsequence(malBrukt.getNavn());
     }
 
     private DokumentBestilling lagBestilling(DokumentMalType dokumentMal, DokumentMalType journalførSomMal) {
