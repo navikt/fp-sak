53d336fd9b49bacc93b345a804ac653b04fbc9c1
Anders Palfi <anders.palfi@nav.no>
2024-12-04 11:04:41 +0100

    oppdater sql og todos

--- migreringer/src/main/resources/db/migration/defaultDS/4.1/V4.1_34__TFP-5554-historikkinnslag.sql
+++ migreringer/src/main/resources/db/migration/defaultDS/4.1/V4.1_34__TFP-5554-historikkinnslag.sql
@@ -1,19 +1,21 @@
 create table HISTORIKKINNSLAG2
 (
-    ID            NUMBER(19)                             not null
+    ID             NUMBER(19)                             not null
         constraint PK_HISTORIKKINNSLAG2
             primary key,
-    FAGSAK_ID     NUMBER(19)                             not null
+    FAGSAK_ID      NUMBER(19)                             not null
         constraint FK_HISTORIKKINNSLAG2_01 references FAGSAK,
-    BEHANDLING_ID NUMBER(19)
+    BEHANDLING_ID  NUMBER(19)
         constraint FK_HISTORIKKINNSLAG2_02 references BEHANDLING,
-    AKTOER        VARCHAR2(100 char)                     not null,
-    SKJERMLENKE   VARCHAR2(100 char),
-    TITTEL        VARCHAR2(1000 char),
-    OPPRETTET_AV  VARCHAR2(20 char) default 'VL'         not null,
-    OPPRETTET_TID TIMESTAMP(3)      default systimestamp not null,
-    ENDRET_AV     VARCHAR2(20 char),
-    ENDRET_TID    TIMESTAMP(3)
+    AKTOER         VARCHAR2(100 char)                     not null,
+    SKJERMLENKE    VARCHAR2(100 char),
+    TITTEL         VARCHAR2(1000 char),
+    OPPRETTET_AV   VARCHAR2(20 char) default 'VL'         not null,
+    OPPRETTET_TID  TIMESTAMP(3)      default systimestamp not null,
+    ENDRET_AV      VARCHAR2(20 char),
+    ENDRET_TID     TIMESTAMP(3),
+    MIGRERT_FRA_ID NUMBER(19)
+        constraint FK_HISTORIKKINNSLAG2_03 references FPSAK.HISTORIKKINNSLAG
 );
 
 comment on table HISTORIKKINNSLAG2 is 'Historikk over hendelser i saken';
@@ -23,12 +25,16 @@ comment on column HISTORIKKINNSLAG2.BEHANDLING_ID is 'FK behandling';
 comment on column HISTORIKKINNSLAG2.AKTOER is 'Hvilken aktoer';
 comment on column HISTORIKKINNSLAG2.SKJERMLENKE is 'Skjermlenke til endring i saken';
 comment on column HISTORIKKINNSLAG2.TITTEL is 'Tittel';
+comment on column HISTORIKKINNSLAG2.MIGRERT_FRA_ID is 'Temp tabell til bruk i migrering. Kan fjernes etter migrering fra tidligere tabell er utfort';
 
 create index IDX_HISTORIKKINNSLAG2_01
     on HISTORIKKINNSLAG2 (BEHANDLING_ID);
 
 create index IDX_HISTORIKKINNSLAG2_02
     on HISTORIKKINNSLAG2 (FAGSAK_ID);
+
+create index IDX_HISTORIKKINNSLAG2_03
+    on HISTORIKKINNSLAG2 (MIGRERT_FRA_ID);
 
 create sequence SEQ_HISTORIKKINNSLAG2
     minvalue 1000000
@@ -43,7 +49,7 @@ create table HISTORIKKINNSLAG2_TEKSTLINJE
     HISTORIKKINNSLAG_ID NUMBER(19)                             not null
         constraint FK_HISTORIKKINNSLAG2_TEKSTLINJE_1 references HISTORIKKINNSLAG2,
     TEKST               VARCHAR2(4000 char),
-    REKKEFOELGE_INDEKS  VARCHAR2(100 char)                     not null,
+    SEKVENS_NR  VARCHAR2(100 char)                     not null,
     OPPRETTET_AV        VARCHAR2(20 char) default 'VL'         not null,
     OPPRETTET_TID       TIMESTAMP(3)      default systimestamp not null,
     ENDRET_AV           VARCHAR2(20 char),
@@ -53,7 +59,7 @@ create table HISTORIKKINNSLAG2_TEKSTLINJE
 comment on table HISTORIKKINNSLAG2_TEKSTLINJE is 'Tekstlinjer i historikkinnslag';
 comment on column HISTORIKKINNSLAG2_TEKSTLINJE.ID is 'PK';
 comment on column HISTORIKKINNSLAG2_TEKSTLINJE.TEKST is 'Innholdet. Forklarer hva som har skjedd i saken';
-comment on column HISTORIKKINNSLAG2_TEKSTLINJE.REKKEFOELGE_INDEKS is 'Rekkefølger på tekstlinje i historikkinnslaget';
+comment on column HISTORIKKINNSLAG2_TEKSTLINJE.SEKVENS_NR is 'Rekkefølge på tekstlinjer innad historikkinnslaget';
 
 create index IDX_HISTORIKKINNSLAG2_TEKSTLINJE_01
     on HISTORIKKINNSLAG2_TEKSTLINJE (HISTORIKKINNSLAG_ID);
@@ -65,19 +71,19 @@ create sequence SEQ_HISTORIKKINNSLAG2_TEKSTLINJE
 
 create table HISTORIKKINNSLAG2_DOK_LINK
 (
-    ID NUMBER(19) not null
+    ID                  NUMBER(19)                             not null
         constraint PK_HISTORIKKINNSLAG2_DOK_LINK
             primary key,
-    LINK_TEKST VARCHAR2(100 char) not null,
-    HISTORIKKINNSLAG_ID NUMBER(19) not null
+    LINK_TEKST          VARCHAR2(100 char)                     not null,
+    HISTORIKKINNSLAG_ID NUMBER(19)                             not null
         constraint FK_HISTORIKKINNSLAG2_DOK_LINK_01
             references HISTORIKKINNSLAG2,
-    JOURNALPOST_ID VARCHAR2(100 char),
-    DOKUMENT_ID VARCHAR2(100 char),
-    OPPRETTET_AV VARCHAR2(20 char) default 'VL' not null,
-    OPPRETTET_TID TIMESTAMP(3) default systimestamp not null,
-    ENDRET_AV VARCHAR2(20 char),
-    ENDRET_TID TIMESTAMP(3)
+    JOURNALPOST_ID      VARCHAR2(100 char),
+    DOKUMENT_ID         VARCHAR2(100 char),
+    OPPRETTET_AV        VARCHAR2(20 char) default 'VL'         not null,
+    OPPRETTET_TID       TIMESTAMP(3)      default systimestamp not null,
+    ENDRET_AV           VARCHAR2(20 char),
+    ENDRET_TID          TIMESTAMP(3)
 );
 
 create sequence SEQ_HISTORIKKINNSLAG2_DOK_LINK
@@ -91,11 +97,11 @@ comment on column HISTORIKKINNSLAG2_DOK_LINK.ID is 'Primary Key';
 
 comment on column HISTORIKKINNSLAG2_DOK_LINK.LINK_TEKST is 'Tekst som vises for link til dokumentet';
 
-comment on column HISTORIKKINNSLAG2_DOK_LINK.HISTORIKKINNSLAG_ID is 'FK:HISTORIKKINNSLAG Fremmednøkkel til riktig innslag i historikktabellen';
+comment on column HISTORIKKINNSLAG2_DOK_LINK.HISTORIKKINNSLAG_ID is 'FK:HISTORIKKINNSLAG2 Fremmednøkkel til riktig innslag i historikktabellen';
 
-comment on column HISTORIKKINNSLAG2_DOK_LINK.JOURNALPOST_ID is 'FK';
+comment on column HISTORIKKINNSLAG2_DOK_LINK.JOURNALPOST_ID is 'Journalpost id';
 
-comment on column HISTORIKKINNSLAG2_DOK_LINK.DOKUMENT_ID is 'FK:';
+comment on column HISTORIKKINNSLAG2_DOK_LINK.DOKUMENT_ID is 'Dokument id';
 
 create index IDX_HISTINNSLAG2_DOK_LINK_01
     on HISTORIKKINNSLAG2_DOK_LINK (HISTORIKKINNSLAG_ID)
