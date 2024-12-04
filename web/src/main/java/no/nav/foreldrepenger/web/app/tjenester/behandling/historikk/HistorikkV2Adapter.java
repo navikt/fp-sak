525517da45baaafe3fad8d655c3da9b6de34292b
espenwaaga <36693523+espenwaaga@users.noreply.github.com>
2024-12-04 09:41:00 +0100

    Fikser TODO i HistorikkV2Adapter.java

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/historikk/HistorikkV2Adapter.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/historikk/HistorikkV2Adapter.java
@@ -91,7 +91,6 @@ public class HistorikkV2Adapter {
         };
     }
 
-    // TODO: Fiks begrunnelse... .her er det duplisering. Får både tittel og begrunnelse som er lik der hvor historikkinnslag ble laget av BehandlingOpprettingTjeneste.
     private static HistorikkinnslagDtoV2 fraMaltype1(Historikkinnslag innslag, UUID behandlingUUID, List<JournalpostId> journalPosterForSak, URI dokumentPath) {
         var del = innslag.getHistorikkinnslagDeler().getFirst();
         var begrunnelse = begrunnelseFraDel(del);
