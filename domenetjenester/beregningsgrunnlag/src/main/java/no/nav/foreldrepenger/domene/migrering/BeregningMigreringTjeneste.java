828369f84e115080be43151c83a2b276f057fa54
Petter Ekern <43809623+pekern@users.noreply.github.com>
2025-01-08 14:40:23 +0100

    TFP-5908: Sammenligner regelsporing i tillegg til beregningsgrunnlag (#6981)

--- domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/migrering/BeregningMigreringTjeneste.java
+++ domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/migrering/BeregningMigreringTjeneste.java
@@ -1,10 +1,17 @@
 package no.nav.foreldrepenger.domene.migrering;
 
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.Objects;
 import java.util.Optional;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+
+import no.nav.foreldrepenger.domene.entiteter.BeregningsgrunnlagEntitet;
+
+import no.nav.foreldrepenger.domene.mappers.fra_kalkulator_til_entitet.KodeverkFraKalkulusMapper;
+import no.nav.foreldrepenger.domene.modell.kodeverk.BeregningsgrunnlagPeriodeRegelType;
 
 import org.jboss.weld.exceptions.IllegalStateException;
 import org.slf4j.Logger;
@@ -79,7 +86,8 @@ public class BeregningMigreringTjeneste {
             var request = lagMigreringRequest(referanse, kobling, originalKobling, migreringsDto);
             var response = klient.migrerGrunnlag(request);
             sammenlignGrunnlag(response, referanse);
-
+            LOG.info(String.format("Vellykket migrering og verifisering av beregningsgrunnlag på sak %s, behandlingId %s og grunnlag %s.", referanse.saksnummer(),
+                referanse.behandlingId(), grunnlag.map(BeregningsgrunnlagGrunnlagEntitet::getId)));
         } catch (Exception e) {
             var msg = String.format("Feil ved mapping av grunnlag for sak %s, behandlingId %s og grunnlag %s. Fikk feil %s", referanse.saksnummer(),
                 referanse.behandlingId(), grunnlag.map(BeregningsgrunnlagGrunnlagEntitet::getId), e);
@@ -89,14 +97,47 @@ public class BeregningMigreringTjeneste {
 
     private void sammenlignGrunnlag(MigrerBeregningsgrunnlagResponse response, BehandlingReferanse referanse) {
         var entitet = beregningsgrunnlagRepository.hentBeregningsgrunnlagGrunnlagEntitet(referanse.behandlingId()).orElseThrow();
+        verifiserRegelsporinger(response, entitet);
         var fpsakGrunnlag = FraEntitetTilBehandlingsmodellMapper.mapBeregningsgrunnlagGrunnlag(entitet);
         var kalkulusGrunnlag = KalkulusTilFpsakMapper.map(response.grunnlag(), Optional.ofNullable(response.besteberegningGrunnlag()));
         var fpJson = StandardJsonConfig.toJson(fpsakGrunnlag);
         var kalkJson = StandardJsonConfig.toJson(kalkulusGrunnlag);
-        if (fpJson.equals(kalkJson)) {
-            LOG.info("Det er likt!");
-        } else {
-            LOG.info("Det er ulikt!");
+        if (!fpJson.equals(kalkJson)) {
+            throw new IllegalStateException("Missmatch mellom kalkulus json og fpsak json av samme beregninsgrunnlag");
+        }
+    }
+
+    private void verifiserRegelsporinger(MigrerBeregningsgrunnlagResponse response, BeregningsgrunnlagGrunnlagEntitet entitet) {
+        // Verifiser grunnlagsporinger
+        var grunnlagSporinger = response.sporingerGrunnlag();
+        var fpsakGrunnlagSporinger = entitet.getBeregningsgrunnlag()
+            .map(BeregningsgrunnlagEntitet::getRegelSporinger)
+            .orElse(Collections.emptyMap());
+        var alleGrunnlagSporingerMatcher = grunnlagSporinger.size() == fpsakGrunnlagSporinger.size() && grunnlagSporinger.stream().allMatch(kalkulusRegelGrunnlag -> {
+            var type = KodeverkFraKalkulusMapper.mapRegelGrunnlagType(kalkulusRegelGrunnlag.type());
+            var fpsakSporing = fpsakGrunnlagSporinger.get(type);
+            return fpsakSporing != null && fpsakSporing.getRegelEvaluering().equals(kalkulusRegelGrunnlag.regelevaluering()) && fpsakSporing.getRegelInput()
+                .equals(kalkulusRegelGrunnlag.regelinput()) && Objects.equals(fpsakSporing.getRegelVersjon(), kalkulusRegelGrunnlag.regelversjon());
+        });
+        if (!alleGrunnlagSporingerMatcher) {
+            throw new IllegalStateException("Feil med matching av regelsporing på grunnlagsnivå");
+        }
+
+        // Verifiser periodesporinger
+        var bgPerioder = entitet.getBeregningsgrunnlag().map(BeregningsgrunnlagEntitet::getBeregningsgrunnlagPerioder).orElse(Collections.emptyList());
+        var allePeriodeSporingerMatcher = response.sporingerPeriode().stream().allMatch(kalkulusRegelPeriode -> {
+            var matchetBgPeriode = bgPerioder.stream()
+                .filter(b -> b.getPeriode().getFomDato().equals(kalkulusRegelPeriode.periode().getFom()))
+                .findFirst()
+                .orElseThrow();
+            BeregningsgrunnlagPeriodeRegelType fpsakType = KodeverkFraKalkulusMapper.mapRegelPeriodeType(kalkulusRegelPeriode.type());
+            var fpsakSporing = matchetBgPeriode.getRegelSporinger().get(fpsakType);
+            return fpsakSporing != null && fpsakSporing.getRegelEvaluering().equals(kalkulusRegelPeriode.regelevaluering())
+                && fpsakSporing.getRegelInput().equals(kalkulusRegelPeriode.regelinput()) && Objects.equals(fpsakSporing.getRegelVersjon(),
+                kalkulusRegelPeriode.regelversjon());
+        });
+        if (!allePeriodeSporingerMatcher) {
+            throw new IllegalStateException("Feil med matching av regelsporing på periodenivå");
         }
     }
 
