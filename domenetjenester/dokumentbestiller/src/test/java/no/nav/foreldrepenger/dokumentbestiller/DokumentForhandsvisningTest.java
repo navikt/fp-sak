c1adce970341a2f6c5d9d326faa64c5e9aa1a778
Michal J. Sladek <mrsladek@users.noreply.github.com>
2025-02-04 16:03:26 +0100

    test: legger inn en test som tester oppførsel til DokumentForhåndsvisning (#7028)
    test: legger inn en test som tester oppførsel til DokumentForhåndsvisning, chore: forenkler Dockerfile oppsett

---
+++ domenetjenester/dokumentbestiller/src/test/java/no/nav/foreldrepenger/dokumentbestiller/DokumentForhandsvisningTest.java
@@ -0,0 +1,67 @@
+package no.nav.foreldrepenger.dokumentbestiller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.UUID;
+
+import no.nav.foreldrepenger.behandlingslager.behandling.RevurderingVarslingÅrsak;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Test;
+
+import no.nav.foreldrepenger.domene.typer.Saksnummer;
+
+class DokumentForhandsvisningTest {
+
+    @Test
+    void builder_test() {
+        var expectedFritekst = "fritekst";
+        var expectedDokumentMal = DokumentMalType.VARSEL_OM_REVURDERING;
+        var expectedBehandlignUuid = UUID.randomUUID();
+        var expectedÅrsak = RevurderingVarslingÅrsak.BRUKER_REGISTRERT_UTVANDRET;
+        var expectedSaksnummer = "123";
+        var expectedTittel = "tittel";
+        var expectedDokumentType = DokumentForhandsvisning.DokumentType.AUTOMATISK;
+
+        var dokumentForhandsvisning = DokumentForhandsvisning.builder()
+                .medBehandlingUuid(expectedBehandlignUuid)
+                .medDokumentMal(expectedDokumentMal)
+                .medFritekst(expectedFritekst)
+                .medRevurderingÅrsak(expectedÅrsak)
+                .medSaksnummer(new Saksnummer(expectedSaksnummer))
+                .medTittel(expectedTittel)
+                .medDokumentType(expectedDokumentType)
+                .build();
+
+        assertThat(dokumentForhandsvisning).isNotNull();
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(dokumentForhandsvisning.behandlingUuid()).isEqualTo(expectedBehandlignUuid);
+            softly.assertThat(dokumentForhandsvisning.dokumentMal()).isEqualTo(expectedDokumentMal);
+            softly.assertThat(dokumentForhandsvisning.fritekst()).isEqualTo(expectedFritekst);
+            softly.assertThat(dokumentForhandsvisning.revurderingÅrsak()).isEqualTo(expectedÅrsak);
+            softly.assertThat(dokumentForhandsvisning.saksnummer().getVerdi()).isEqualTo(expectedSaksnummer);
+            softly.assertThat(dokumentForhandsvisning.tittel()).isEqualTo(expectedTittel);
+            softly.assertThat(dokumentForhandsvisning.dokumentType()).isEqualTo(expectedDokumentType);
+        });
+    }
+
+    @Test
+    void should_fail_when_revurderingAarsak_is_not_set_and_dokumentMal_is_VARSEL_OM_REVURDERING() {
+        var expectedFritekst = "fritekst";
+        var expectedDokumentMal = DokumentMalType.VARSEL_OM_REVURDERING;
+        var expectedBehandlignUuid = UUID.randomUUID();
+        var expectedSaksnummer = "123";
+        var expectedTittel = "tittel";
+        var expectedDokumentType = DokumentForhandsvisning.DokumentType.AUTOMATISK;
+
+        assertThatThrownBy(() -> DokumentForhandsvisning.builder()
+            .medBehandlingUuid(expectedBehandlignUuid)
+            .medDokumentMal(expectedDokumentMal)
+            .medFritekst(expectedFritekst)
+            .medSaksnummer(new Saksnummer(expectedSaksnummer))
+            .medTittel(expectedTittel)
+            .medDokumentType(expectedDokumentType)
+            .build()).isInstanceOf(NullPointerException.class).hasMessageContaining("Revurdering årsak må være satt.");
+    }
+}
