c20872afabef82c762b5ff323094043a393b1e9d
Petter Ekern <43809623+pekern@users.noreply.github.com>
2025-03-17 14:27:51 +0100

    Feature/tfp 6047 v4 (#7175)
    * TFP-6047: Sorterer på arbeidsgiverIdent og arbeidsforholdRef hvis fom, tom og opptjeningstype er lik

--- domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/mappers/fra_entitet_til_domene/FraEntitetTilBehandlingsmodellMapper.java
+++ domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/mappers/fra_entitet_til_domene/FraEntitetTilBehandlingsmodellMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+import no.nav.foreldrepenger.behandlingslager.virksomhet.Arbeidsgiver;
 import no.nav.foreldrepenger.domene.entiteter.BGAndelArbeidsforhold;
 import no.nav.foreldrepenger.domene.entiteter.BeregningAktivitetAggregatEntitet;
 import no.nav.foreldrepenger.domene.entiteter.BeregningAktivitetEntitet;
@@ -89,7 +90,10 @@ public class FraEntitetTilBehandlingsmodellMapper {
             .stream()
             .map(FraEntitetTilBehandlingsmodellMapper::mapAktivitetOverstyring)
             .sorted(Comparator.comparing(BeregningAktivitetOverstyring::getOpptjeningAktivitetType)
-                .thenComparing(a -> a.getPeriode().getFomDato()).thenComparing(a -> a.getPeriode().getTomDato()))
+                .thenComparing(a -> a.getPeriode().getFomDato())
+                .thenComparing(a -> a.getPeriode().getTomDato())
+                .thenComparing(a -> a.getArbeidsgiver().map(Arbeidsgiver::getIdentifikator).orElse(null))
+                .thenComparing(a -> a.getArbeidsforholdRef() == null ? null : a.getArbeidsforholdRef().getReferanse()))
             .forEach(builder::leggTilOverstyring);
         return builder.build();
     }
@@ -112,7 +116,10 @@ public class FraEntitetTilBehandlingsmodellMapper {
             .stream()
             .map(FraEntitetTilBehandlingsmodellMapper::mapBeregningAktivitet)
             .sorted(Comparator.comparing(BeregningAktivitet::getOpptjeningAktivitetType)
-                .thenComparing(a -> a.getPeriode().getFomDato()).thenComparing(a -> a.getPeriode().getTomDato()))
+                .thenComparing(a -> a.getPeriode().getFomDato())
+                .thenComparing(a -> a.getPeriode().getTomDato())
+                .thenComparing(a -> a.getArbeidsgiver() == null ? null : a.getArbeidsgiver().getIdentifikator())
+                .thenComparing(a -> a.getArbeidsforholdRef() == null ? null : a.getArbeidsforholdRef().getReferanse()))
             .forEach(builder::leggTilAktivitet);
         return builder.build();
     }
--- domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/mappers/fra_kalkulus_til_domene/KalkulusTilFpsakMapper.java
+++ domenetjenester/beregningsgrunnlag/src/main/java/no/nav/foreldrepenger/domene/mappers/fra_kalkulus_til_domene/KalkulusTilFpsakMapper.java
@@ -158,7 +158,9 @@ public final class KalkulusTilFpsakMapper {
         overstyringer.getOverstyringer().stream().map(KalkulusTilFpsakMapper::mapAktivitetOverstyring)
             .sorted(Comparator.comparing(BeregningAktivitetOverstyring::getOpptjeningAktivitetType)
             .thenComparing(a -> a.getPeriode().getFomDato())
-            .thenComparing(a -> a.getPeriode().getTomDato()))
+            .thenComparing(a -> a.getPeriode().getTomDato())
+            .thenComparing(a -> a.getArbeidsgiver().map(Arbeidsgiver::getIdentifikator).orElse(null))
+            .thenComparing(a -> a.getArbeidsforholdRef() == null ? null : a.getArbeidsforholdRef().getReferanse()))
             .forEach(builder::leggTilOverstyring);
         return builder.build();
     }
@@ -177,7 +179,10 @@ public final class KalkulusTilFpsakMapper {
         var builder = BeregningAktivitetAggregat.builder().medSkjæringstidspunktOpptjening(registerAktiviteter.getSkjæringstidspunktOpptjening());
         registerAktiviteter.getAktiviteter().stream().map(KalkulusTilFpsakMapper::mapAktivitet)
             .sorted(Comparator.comparing(BeregningAktivitet::getOpptjeningAktivitetType)
-                .thenComparing(a -> a.getPeriode().getFomDato()).thenComparing(a -> a.getPeriode().getTomDato()))
+                .thenComparing(a -> a.getPeriode().getFomDato())
+                .thenComparing(a -> a.getPeriode().getTomDato())
+                .thenComparing(a -> a.getArbeidsgiver() == null ? null : a.getArbeidsgiver().getIdentifikator())
+                .thenComparing(a -> a.getArbeidsforholdRef() == null ? null : a.getArbeidsforholdRef().getReferanse()))
             .forEach(builder::leggTilAktivitet);
         return builder.build();
     }
