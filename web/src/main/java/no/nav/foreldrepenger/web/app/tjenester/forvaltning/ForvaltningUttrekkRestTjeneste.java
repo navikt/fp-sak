9d7981eb7db0d9d8197c8d89db27142e66bb6425
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2025-03-19 22:25:40 +0100

    query-fiks

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/forvaltning/ForvaltningUttrekkRestTjeneste.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/forvaltning/ForvaltningUttrekkRestTjeneste.java
@@ -165,14 +165,14 @@ public class ForvaltningUttrekkRestTjeneste {
             from aksjonspunkt
             where aksjonspunkt_def = '5074' and aksjonspunkt_status = 'OPPR'
             and behandling_id not in (select behandling_id from aksjonspunkt where aksjonspunkt_def > '7000' and aksjonspunkt_status = 'OPPR')
-            and behandling_id in (select behandling_id from fpsak.gr_ytelses_fordeling gy join fpsak.YF_FORDELING_PERIODE op on gy.so_fordeling_id = op.fordeling_id
+            and behandling_id in (select behandling_id from gr_ytelses_fordeling gy join YF_FORDELING_PERIODE op on gy.so_fordeling_id = op.fordeling_id
                         where mors_aktivitet = 'ARBEID')
-            and exists (select * from fpsak.behandling b join fpsak.fagsak f on b.fagsak_id = f.id join fpsak.fagsak_relasjon fr on f.id in (fagsak_en_id, fagsak_to_id)
+            and exists (select * from behandling b join fagsak f on b.fagsak_id = f.id join fagsak_relasjon fr on f.id in (fagsak_en_id, fagsak_to_id)
                         where b.id = behandling_id and bruker_rolle <> 'MORA' and fagsak_to_id is not null)
             and behandling_id in (
-              select behandling_id from fpsak.gr_soeknad gs join fpsak.soeknad_vedlegg sv on gs.soeknad_id = sv.soeknad_id where skjemanummer = 'I000132' and innsendingsvalg = 'LASTET_OPP'
+              select behandling_id from gr_soeknad gs join soeknad_vedlegg sv on gs.soeknad_id = sv.soeknad_id where skjemanummer = 'I000132' and innsendingsvalg = 'LASTET_OPP'
               union all
-              select behandling_id from fpsak.mottatt_dokument where type = 'I000132'
+              select behandling_id from mottatt_dokument where type = 'I000132'
               )
              """);
         @SuppressWarnings("unchecked")
