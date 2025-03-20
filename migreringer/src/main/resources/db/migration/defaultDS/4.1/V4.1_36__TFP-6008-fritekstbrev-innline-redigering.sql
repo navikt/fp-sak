ccfe5e469295275e142f15b4bffca74d6bb3b326
espenwaaga <36693523+espenwaaga@users.noreply.github.com>
2025-03-20 13:52:25 +0100

    Fiks tekst

--- migreringer/src/main/resources/db/migration/defaultDS/4.1/V4.1_36__TFP-6008-fritekstbrev-innline-redigering.sql
+++ migreringer/src/main/resources/db/migration/defaultDS/4.1/V4.1_36__TFP-6008-fritekstbrev-innline-redigering.sql
@@ -1,2 +1,2 @@
 ALTER TABLE BEHANDLING_DOKUMENT ADD overstyrt_brev_fritekst_html CLOB;
-COMMENT ON COLUMN BEHANDLING_DOKUMENT.overstyrt_brev_fritekst_html is 'Fritekstbrev på html format';
+COMMENT ON COLUMN BEHANDLING_DOKUMENT.overstyrt_brev_fritekst_html is 'Innhold av overstyrt brev html';
