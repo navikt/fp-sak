77d48696c695f6fc7284e0e3d222f3161d45cff8
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2025-03-30 15:04:59 +0200

    TFP-5900 oppgavefilter ikraft fri utsettelse

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/forvaltning/praksisutsettelse/FeilPraksisUtsettelseIkrafttredelseSingleTask.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/forvaltning/praksisutsettelse/FeilPraksisUtsettelseIkrafttredelseSingleTask.java
@@ -1,7 +1,16 @@
 package no.nav.foreldrepenger.web.app.tjenester.forvaltning.praksisutsettelse;
 
+import static no.nav.foreldrepenger.produksjonsstyring.behandlingenhet.EnhetsTjeneste.MIDLERTIDIG_ENHET;
+
+import java.util.List;
+import java.util.Optional;
+
 import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import no.nav.foreldrepenger.behandlingslager.behandling.historikk.HistorikkAktør;
 import no.nav.foreldrepenger.behandlingslager.behandling.historikk.Historikkinnslag;
 import no.nav.foreldrepenger.behandlingslager.behandling.historikk.HistorikkinnslagRepository;
@@ -10,13 +19,11 @@ import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakEgenskapRepository;
 import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakProsesstaskRekkefølge;
 import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakRepository;
 import no.nav.foreldrepenger.behandlingslager.fagsak.egenskaper.FagsakMarkering;
+import no.nav.foreldrepenger.produksjonsstyring.oppgavebehandling.OppgaveTjeneste;
+import no.nav.vedtak.felles.integrasjon.oppgave.v1.Oppgavetype;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTask;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.Optional;
 
 @Dependent
 @ProsessTask(value = "behandling.friutsettelse.ikrafttredelse.single", prioritet = 4, maxFailedRuns = 1)
@@ -26,21 +33,26 @@ class FeilPraksisUtsettelseIkrafttredelseSingleTask implements ProsessTaskHandle
     private static final Logger LOG = LoggerFactory.getLogger(FeilPraksisUtsettelseIkrafttredelseSingleTask.class);
     static final String FAGSAK_ID = "fagsakId";
     static final String DRY_RUN = "dryRun";
+    private static final List<String> OPPGAVE_TYPER = List.of(Oppgavetype.VURDER_DOKUMENT.getKode(), Oppgavetype.VURDER_KONSEKVENS_YTELSE.getKode(),
+        "VURD_HENV", "VUR_SVAR", "KONT_BRUK");
 
     private final FeilPraksisOpprettBehandlingTjeneste feilPraksisOpprettBehandlingTjeneste;
     private final HistorikkinnslagRepository historikkinnslagRepository;
     private final FagsakRepository fagsakRepository;
     private final FagsakEgenskapRepository fagsakEgenskapRepository;
+    private final OppgaveTjeneste oppgaveTjeneste;
 
     @Inject
     public FeilPraksisUtsettelseIkrafttredelseSingleTask(FeilPraksisOpprettBehandlingTjeneste feilPraksisOpprettBehandlingTjeneste,
                                                          HistorikkinnslagRepository historikkinnslagRepository,
                                                          FagsakRepository fagsakRepository,
-                                                         FagsakEgenskapRepository fagsakEgenskapRepository) {
+                                                         FagsakEgenskapRepository fagsakEgenskapRepository,
+                                                         OppgaveTjeneste oppgaveTjeneste) {
         this.feilPraksisOpprettBehandlingTjeneste = feilPraksisOpprettBehandlingTjeneste;
         this.historikkinnslagRepository = historikkinnslagRepository;
         this.fagsakRepository = fagsakRepository;
         this.fagsakEgenskapRepository = fagsakEgenskapRepository;
+        this.oppgaveTjeneste = oppgaveTjeneste;
     }
 
     @Override
@@ -50,7 +62,11 @@ class FeilPraksisUtsettelseIkrafttredelseSingleTask implements ProsessTaskHandle
             .map(fid -> fagsakRepository.finnEksaktFagsak(Long.parseLong(fid)))
             .orElseThrow();
         if (fagsakEgenskapRepository.finnFagsakMarkeringer(fagsak.getId()).contains(FagsakMarkering.PRAKSIS_UTSETTELSE)) {
-            LOG.info("Fagsak {} har allerede saksmerking {}", fagsak.getSaksnummer(), FagsakMarkering.PRAKSIS_UTSETTELSE);
+            LOG.info("FeilPraksisUtsettelse Fagsak {} har allerede saksmerking {}", fagsak.getSaksnummer(), FagsakMarkering.PRAKSIS_UTSETTELSE);
+            return;
+        }
+        if (harOppgaverIGosysMidlertidigEnhet(fagsak)) {
+            LOG.info("FeilPraksisUtsettelse Fagsak {} har oppgaver i gosys - oppretter ikke revurdering med brev", fagsak.getSaksnummer());
             return;
         }
 
@@ -69,6 +85,10 @@ class FeilPraksisUtsettelseIkrafttredelseSingleTask implements ProsessTaskHandle
             .addLinje(String.format("Saksmerkering %s er lagt til", ny.getNavn()))
             .build();
         historikkinnslagRepository.lagre(historikkinnslag);
+    }
+
+    private boolean harOppgaverIGosysMidlertidigEnhet(Fagsak fagsak) {
+        return !oppgaveTjeneste.hentOppgaver(OPPGAVE_TYPER, fagsak.getAktørId().getId(), MIDLERTIDIG_ENHET.enhetId(), "50").isEmpty();
     }
 
 }
