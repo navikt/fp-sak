5bf647f86bf00cd66f63262c42b10302c82c0b93
Michal J. Sladek <michal.sladek@nav.no>
2025-04-08 06:53:02 +0200

    Review

---
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/formidling/rest/kodeverk/KonsekvensForYtelsen.java
@@ -0,0 +1,9 @@
+package no.nav.foreldrepenger.web.app.tjenester.formidling.rest.kodeverk;
+
+public enum KonsekvensForYtelsen {
+    FORELDREPENGER_OPPHØRER,
+    ENDRING_I_BEREGNING,
+    ENDRING_I_UTTAK,
+    ENDRING_I_FORDELING_AV_YTELSEN,
+    INGEN_ENDRING;
+}
