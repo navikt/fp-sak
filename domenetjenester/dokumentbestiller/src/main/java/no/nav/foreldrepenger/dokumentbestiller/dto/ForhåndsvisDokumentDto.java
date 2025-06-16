10becaa54b5f59d0eeac15768ef6ec33f746a973
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2025-06-16 22:14:09 +0200

    chore: bump felles. bruk Fritekst

--- domenetjenester/dokumentbestiller/src/main/java/no/nav/foreldrepenger/dokumentbestiller/dto/ForhåndsvisDokumentDto.java
+++ domenetjenester/dokumentbestiller/src/main/java/no/nav/foreldrepenger/dokumentbestiller/dto/ForhåndsvisDokumentDto.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
+import no.nav.folketrygdloven.kalkulus.opptjening.v1.Fritekst;
 import no.nav.foreldrepenger.behandlingslager.behandling.RevurderingVarslingÅrsak;
 import no.nav.foreldrepenger.dokumentbestiller.DokumentMalType;
 import no.nav.foreldrepenger.validering.ValidKodeverk;
@@ -17,6 +18,6 @@ public record ForhåndsvisDokumentDto(@Valid @NotNull UUID behandlingUuid,
                                      @ValidKodeverk RevurderingVarslingÅrsak arsakskode,
                                      boolean automatiskVedtaksbrev,
                                      @Size(max = 200) @Pattern(regexp = InputValideringRegex.FRITEKST) String tittel,
-                                     @Valid FritekstDto fritekst) { // HTML eller rå tekst avhengig av maltype FRITEKSTBREV/FRITEKSTBREV_HTML
+                                     @Valid @Fritekst @Size(max = 20_000) String fritekst) { // HTML eller rå tekst avhengig av maltype FRITEKSTBREV/FRITEKSTBREV_HTML
 }
 
