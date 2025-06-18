c71e15b0bcdd94a28bb045c1871d2ccc960e8d38
espenwaaga <36693523+espenwaaga@users.noreply.github.com>
2025-06-18 14:33:17 +0200

    TFP-6302: Legger til enitet for å lagre annenparts uttak i EØS.

---
+++ behandlingslager/domene/src/main/java/no/nav/foreldrepenger/behandlingslager/behandling/eøs/EøsUttaksperiodeEntitet.java
@@ -0,0 +1,105 @@
+package no.nav.foreldrepenger.behandlingslager.behandling.eøs;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import no.nav.foreldrepenger.behandlingslager.BaseCreateableEntitet;
+import no.nav.foreldrepenger.behandlingslager.behandling.ytelsefordeling.periode.UttakPeriodeType;
+import no.nav.foreldrepenger.behandlingslager.uttak.fp.Trekkdager;
+import no.nav.foreldrepenger.domene.tid.DatoIntervallEntitet;
+
+@Entity(name = "EøsUttaksperiode")
+@Table(name = "EOS_UTTAKSPERIODE") // TODO: Inkludere annenpart i navn? GR_ANNNEPART_EOS_UTTAKSPERIODE?
+public class EøsUttaksperiodeEntitet extends BaseCreateableEntitet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_EOS_UTTAKSPERIODE")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "eos_uttaksperioder_id", updatable = false, nullable = false)
+    private EøsUttaksperioderEntitet eosUttaksperioder;
+
+    @Embedded
+    private DatoIntervallEntitet periode;
+
+    @Convert(converter = UttakPeriodeType.KodeverdiConverter.class)
+    @Column(name="trekkonto", nullable = false)
+    private UttakPeriodeType trekkonto = UttakPeriodeType.UDEFINERT;
+
+    @Embedded
+    private Trekkdager trekkdager;
+
+    EøsUttaksperiodeEntitet() {
+        // For Hibernate
+    }
+
+    public DatoIntervallEntitet getPeriode() {
+        return periode;
+    }
+
+    public UttakPeriodeType getTrekkonto() {
+        return trekkonto;
+    }
+
+    public Trekkdager getTrekkdager() {
+        return trekkdager;
+    }
+
+    public void setEosUttaksperioder(EøsUttaksperioderEntitet eosUttaksperioder) {
+        this.eosUttaksperioder = eosUttaksperioder;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass())
+            return false;
+        EøsUttaksperiodeEntitet that = (EøsUttaksperiodeEntitet) o;
+        return Objects.equals(periode, that.periode) && trekkonto == that.trekkonto && Objects.equals(trekkdager, that.trekkdager);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(periode, trekkonto, trekkdager);
+    }
+
+    public static class Builder {
+        private final EøsUttaksperiodeEntitet kladd;
+
+        public Builder() {
+            kladd = new EøsUttaksperiodeEntitet();
+        }
+
+        public Builder medPeriode(DatoIntervallEntitet periode) {
+            kladd.periode = periode;
+            return this;
+        }
+
+        public Builder medTrekkonto(UttakPeriodeType trekkonto) {
+            kladd.trekkonto = trekkonto;
+            return this;
+        }
+
+        public Builder medTrekkdager(Trekkdager trekkdager) {
+            kladd.trekkdager = trekkdager;
+            return this;
+        }
+
+        public EøsUttaksperiodeEntitet build() {
+            Objects.requireNonNull(kladd.periode, "periode");
+            Objects.requireNonNull(kladd.trekkonto, "trekkonto");
+            Objects.requireNonNull(kladd.trekkdager, "trekkdager");
+            return kladd;
+        }
+    }
+}
