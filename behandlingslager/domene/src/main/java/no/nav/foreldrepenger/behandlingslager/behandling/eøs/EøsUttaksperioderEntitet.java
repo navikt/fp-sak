0df09061e6086ea4e42113e695401f212a29fabe
espenwaaga <36693523+espenwaaga@users.noreply.github.com>
2025-06-18 14:33:17 +0200

    TFP-6302: Legger til enitet for å lagre annenparts uttak i EØS.

---
+++ behandlingslager/domene/src/main/java/no/nav/foreldrepenger/behandlingslager/behandling/eøs/EøsUttaksperioderEntitet.java
@@ -0,0 +1,69 @@
+package no.nav.foreldrepenger.behandlingslager.behandling.eøs;
+
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import no.nav.foreldrepenger.behandlingslager.BaseCreateableEntitet;
+
+@Entity(name = "EøsUttaksperioder")
+@Table(name = "EOS_UTTAKSPERIODER")
+public class EøsUttaksperioderEntitet extends BaseCreateableEntitet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_EOS_UTTAKSPERIODER")
+    private Long id;
+
+    @OneToMany(mappedBy = "eosUttaksperioder")
+    private List<EøsUttaksperiodeEntitet> perioder;
+
+    EøsUttaksperioderEntitet() {
+        // For Hibernate
+    }
+
+    public List<EøsUttaksperiodeEntitet> getPerioder() {
+        return perioder;
+    }
+
+    void leggTilEøsUttaksperiode(EøsUttaksperiodeEntitet eøsUttaksperiode) {
+        eøsUttaksperiode.setEosUttaksperioder(this); // TODO: ??
+        perioder.add(eøsUttaksperiode);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass())
+            return false;
+        EøsUttaksperioderEntitet that = (EøsUttaksperioderEntitet) o;
+        return Objects.equals(perioder, that.perioder);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(perioder);
+    }
+
+    public static class Builder {
+        private final EøsUttaksperioderEntitet kladd;
+
+        public Builder() {
+            this.kladd = new EøsUttaksperioderEntitet();
+        }
+
+        public Builder leggTil(EøsUttaksperiodeEntitet.Builder builder) {
+            kladd.leggTilEøsUttaksperiode(builder.build());
+            return this;
+        }
+
+        public EøsUttaksperioderEntitet build() {
+            return this.kladd;
+        }
+
+    }
+}
