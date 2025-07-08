3681f430eba08e504eb693810abc6308392f6449
Anders Palfi <anders.palfi@nav.no>
2025-07-08 11:22:02 +0200

    Forenkler navn

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/uttak/eøs/EøsUttakTjeneste.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/uttak/eøs/EøsUttakTjeneste.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 
 import no.nav.foreldrepenger.behandlingslager.behandling.eøs.EøsUttakRepository;
 import no.nav.foreldrepenger.behandlingslager.behandling.eøs.EøsUttaksperiodeEntitet;
@@ -13,22 +11,22 @@ import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRe
 import no.nav.foreldrepenger.web.app.tjenester.behandling.dto.UuidDto;
 
 @ApplicationScoped
-public class AvklarUttakEøsForAnnenforelderTjeneste {
+public class EøsUttakTjeneste {
 
     private BehandlingRepository behandlingRepository;
     private EøsUttakRepository eøsUttakRepository;
 
-    public AvklarUttakEøsForAnnenforelderTjeneste() {
+    public EøsUttakTjeneste() {
         // CDI
     }
 
     @Inject
-    public AvklarUttakEøsForAnnenforelderTjeneste(BehandlingRepository behandlingRepository, EøsUttakRepository eøsUttakRepository) {
+    public EøsUttakTjeneste(BehandlingRepository behandlingRepository, EøsUttakRepository eøsUttakRepository) {
         this.behandlingRepository = behandlingRepository;
         this.eøsUttakRepository = eøsUttakRepository;
     }
 
-    public List<AvklarUttakEøsForAnnenforelderDto.EøsUttakPeriodeDto> annenpartsPerioder(@NotNull @Valid UuidDto uuidDto) {
+    public List<EøsUttakDto.EøsUttakPeriodeDto> annenpartsPerioder(UuidDto uuidDto) {
         var behandlingId = behandlingRepository.hentBehandling(uuidDto.getBehandlingUuid()).getId();
         var grunnlagOpt = eøsUttakRepository.hentGrunnlag(behandlingId);
         return grunnlagOpt.stream()
@@ -37,8 +35,8 @@ public class AvklarUttakEøsForAnnenforelderTjeneste {
             .toList();
     }
 
-    private AvklarUttakEøsForAnnenforelderDto.EøsUttakPeriodeDto tilDto(EøsUttaksperiodeEntitet periode) {
-        return new AvklarUttakEøsForAnnenforelderDto.EøsUttakPeriodeDto(
+    private EøsUttakDto.EøsUttakPeriodeDto tilDto(EøsUttaksperiodeEntitet periode) {
+        return new EøsUttakDto.EøsUttakPeriodeDto(
             periode.getPeriode().getFomDato(),
             periode.getPeriode().getTomDato(),
             periode.getTrekkdager().decimalValue(),
