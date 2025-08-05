344de0571aee3c812c05b9bc51f4091c028e6d20
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2025-08-05 13:01:04 +0200

    bugfix: fjern alle space fra søkestrenger (#7465)
    * bugfix: fjern alle space fra søkestrenger
    
    * Fikse getter-navn
    
    * bruk record

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/fagsak/dto/SokefeltDto.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/fagsak/dto/SokefeltDto.java
@@ -3,29 +3,13 @@ package no.nav.foreldrepenger.web.app.tjenester.fagsak.dto;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotNull;
 
-import no.nav.foreldrepenger.domene.typer.Saksnummer;
-
-public class SokefeltDto {
-
-    @NotNull
-    @Digits(integer = 18, fraction = 0)
-    private String searchString;
-
-    @SuppressWarnings("unused")
-    private SokefeltDto() {
-    }
-
-    public SokefeltDto(String searchString) {
-        this.searchString = searchString;
-    }
-
-    public SokefeltDto(Saksnummer saksnummer) {
-        this.searchString = saksnummer.getVerdi();
-    }
+public record SokefeltDto(@NotNull
+                          @Digits(integer = 18, fraction = 0)
+                          String searchString) {
 
     public String getSearchString() {
-        return searchString;
+        // fjerner alle space-tegn fra søkestrengen - også unicode-spaces
+        return searchString != null ? searchString.replaceAll("\\p{IsWhite_Space}", "") : "";
     }
-
 
 }
