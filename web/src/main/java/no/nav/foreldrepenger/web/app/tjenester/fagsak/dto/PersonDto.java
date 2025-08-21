068ed726bca57dcb4f87c4ec7d82105ad6766d17
johannbm <johannes.moskvil@gmail.com>
2025-08-21 09:54:22 +0200

    oppdater PersonDto

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/fagsak/dto/PersonDto.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/fagsak/dto/PersonDto.java
@@ -3,11 +3,24 @@ package no.nav.foreldrepenger.web.app.tjenester.fagsak.dto;
 import java.time.LocalDate;
 import java.util.Objects;
 
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import jakarta.validation.constraints.NotNull;
 import no.nav.foreldrepenger.behandlingslager.aktør.NavBrukerKjønn;
 import no.nav.foreldrepenger.behandlingslager.geografisk.Språkkode;
 
-public record PersonDto(String aktørId, String navn, String fødselsnummer, NavBrukerKjønn kjønn, String diskresjonskode,
-                        LocalDate fødselsdato, LocalDate dødsdato, @Deprecated(forRemoval = true) LocalDate dodsdato, Språkkode språkkode) {
+public record PersonDto(
+    @Schema(nullable = true, requiredMode = Schema.RequiredMode.REQUIRED) String aktørId,
+    @NotNull String navn,
+    @NotNull String fødselsnummer,
+    @NotNull NavBrukerKjønn kjønn,
+    @Schema(nullable = true, requiredMode = Schema.RequiredMode.REQUIRED) String diskresjonskode,
+    @NotNull LocalDate fødselsdato,
+    @Schema(nullable = true, requiredMode = Schema.RequiredMode.REQUIRED) LocalDate dødsdato,
+    @Deprecated(forRemoval = true) LocalDate dodsdato,
+    @NotNull Språkkode språkkode
+) {
 
 
     @Override
