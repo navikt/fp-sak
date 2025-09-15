e02bb1f7abd9c44fa709c3e7c5a25ca0ef3ebe81
Anders Palfi <anders.palfi@nav.no>
2025-09-15 09:23:04 +0200

    TFP-6071 fjerner ubrukt resttjeneste for familiehendelse

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/familiehendelse/FamiliehendelseRestTjeneste.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/familiehendelse/FamiliehendelseRestTjeneste.java
@@ -14,9 +14,6 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -28,10 +25,6 @@ import no.nav.foreldrepenger.behandlingslager.behandling.familiehendelse.Omsorgs
 import no.nav.foreldrepenger.behandlingslager.behandling.familiehendelse.TerminbekreftelseEntitet;
 import no.nav.foreldrepenger.behandlingslager.behandling.familiehendelse.UidentifisertBarn;
 import no.nav.foreldrepenger.behandlingslager.behandling.repository.BehandlingRepository;
-import no.nav.foreldrepenger.behandlingslager.behandling.vedtak.BehandlingVedtak;
-import no.nav.foreldrepenger.behandlingslager.behandling.vedtak.BehandlingVedtakRepository;
-import no.nav.foreldrepenger.familiehendelse.rest.FamilieHendelseGrunnlagDto;
-import no.nav.foreldrepenger.familiehendelse.rest.FamiliehendelseDataDtoTjeneste;
 import no.nav.foreldrepenger.web.app.tjenester.behandling.dto.BehandlingAbacSuppliers;
 import no.nav.foreldrepenger.web.app.tjenester.behandling.dto.UuidDto;
 import no.nav.vedtak.sikkerhet.abac.BeskyttetRessurs;
@@ -45,41 +38,22 @@ import no.nav.vedtak.sikkerhet.abac.beskyttet.ResourceType;
 @Transactional
 public class FamiliehendelseRestTjeneste {
 
-
-    private static final Logger LOG = LoggerFactory.getLogger(FamiliehendelseRestTjeneste.class);
     private BehandlingRepository behandlingRepository;
-    private BehandlingVedtakRepository behandlingVedtakRepository;
     private FamilieHendelseRepository familieHendelseRepository;
 
     static final String BASE_PATH = "/behandling";
-    private static final String FAMILIEHENDELSE_V2_PART_PATH = "/familiehendelse/v2";
     private static final String FAMILIEHENDELSE_V3_PART_PATH = "/familiehendelse/v3";
-    public static final String FAMILIEHENDELSE_V2_PATH = BASE_PATH + FAMILIEHENDELSE_V2_PART_PATH;
     public static final String FAMILIEHENDELSE_V3_PATH = BASE_PATH + FAMILIEHENDELSE_V3_PART_PATH;
 
     @Inject
     public FamiliehendelseRestTjeneste(BehandlingRepository behandlingRepository,
-                                       BehandlingVedtakRepository behandlingVedtakRepository,
                                        FamilieHendelseRepository familieHendelseRepository) {
         this.behandlingRepository = behandlingRepository;
-        this.behandlingVedtakRepository = behandlingVedtakRepository;
         this.familieHendelseRepository = familieHendelseRepository;
     }
 
     FamiliehendelseRestTjeneste() {
         // for CDI proxy
-    }
-
-    @GET
-    @Path(FAMILIEHENDELSE_V2_PART_PATH)
-    @Operation(description = "Hent informasjon om familiehendelse til grunn for ytelse", tags = "familiehendelse", responses = {@ApiResponse(responseCode = "200", description = "Returnerer hele FamilieHendelse grunnlaget", content = @Content(mediaType = "application/json", schema = @Schema(implementation = FamilieHendelseGrunnlagDto.class)))})
-    @BeskyttetRessurs(actionType = ActionType.READ, resourceType = ResourceType.FAGSAK, sporingslogg = false)
-    public FamilieHendelseGrunnlagDto getFamiliehendelseGrunnlagDto(@TilpassetAbacAttributt(supplierClass = BehandlingAbacSuppliers.UuidAbacDataSupplier.class) @NotNull @QueryParam(UuidDto.NAME) @Parameter(description = UuidDto.DESC) @Valid UuidDto uuidDto) {
-        var behandling = behandlingRepository.hentBehandling(uuidDto.getBehandlingUuid());
-        var grunnlag = familieHendelseRepository.hentAggregatHvisEksisterer(behandling.getId());
-        var vedtaksdato = behandlingVedtakRepository.hentForBehandlingHvisEksisterer(behandling.getId()).map(BehandlingVedtak::getVedtaksdato);
-        LOG.info("Familiehendelse v2 i bruk");
-        return FamiliehendelseDataDtoTjeneste.mapGrunnlagFra(grunnlag, vedtaksdato, behandling);
     }
 
     @GET
