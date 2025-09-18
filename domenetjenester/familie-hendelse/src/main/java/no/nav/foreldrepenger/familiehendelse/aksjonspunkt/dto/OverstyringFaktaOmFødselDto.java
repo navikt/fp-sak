ab819891f4977ff0c8ae3014bf70fa8da6f89ec5
Siri Mykland <siri.mykland@nav.no>
2025-09-18 14:59:29 +0200

    fjern ubrukt import

--- domenetjenester/familie-hendelse/src/main/java/no/nav/foreldrepenger/familiehendelse/aksjonspunkt/dto/OverstyringFaktaOmFødselDto.java
+++ domenetjenester/familie-hendelse/src/main/java/no/nav/foreldrepenger/familiehendelse/aksjonspunkt/dto/OverstyringFaktaOmFødselDto.java
@@ -8,8 +8,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
