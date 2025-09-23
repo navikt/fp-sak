e3979d9d4b0796b2bbd97f9e514d4f33c0100a18
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2025-09-23 11:43:24 +0200

    TFP-6395 nye endepunkt for fptilbake (#7567)
    * TFP-6395 nye endepunkt for fptilbake
    
    * Navngivning og tester
    
    * Revert utilsiktet refactor
    
    * fjerne imports

---
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/tilbake/TilbakeDto.java
@@ -0,0 +1,42 @@
+package no.nav.foreldrepenger.web.app.tjenester.tilbake;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public record TilbakeDto(@NotNull BehandlingDto behandling,
+                         @NotNull FagsakDto fagsak,
+                         @NotNull FamilieHendelseDto familieHendelse,
+                         FeilutbetalingDto feilutbetaling,
+                         boolean sendtoppdrag,
+                         VergeDto verge) {
+
+    public enum YtelseType { FORELDREPENGER, SVANGERSKAPSPENGER, ENGANGSSTØNAD }
+
+    public enum FamilieHendelseType { FØDSEL, ADOPSJON }
+
+    public enum VergeType { BARN, FORELDRELØS, VOKSEN, ADVOKAT, FULLMEKTIG }
+
+    public enum FeilutbetalingValg { OPPRETT, OPPDATER, IGNORER, INNTREKK }
+
+    public enum Språkkode { NB, NN, EN }
+
+    public record BehandlingDto(@NotNull UUID uuid, @NotNull HenvisningDto henvisning,
+                                String behandlendeEnhetId, String behandlendeEnhetNavn,
+                                Språkkode språkkode, LocalDate vedtaksdato) {}
+
+    public record FamilieHendelseDto(@NotNull FamilieHendelseType familieHendelseType, @NotNull Integer antallBarn) {}
+
+    public record FagsakDto(@NotNull String aktørId, @NotNull String saksnummer, @NotNull YtelseType fagsakYtelseType) { }
+
+    public record FeilutbetalingDto(@NotNull FeilutbetalingValg feilutbetalingValg, String varseltekst) { }
+
+    public record HenvisningDto(@NotNull @Min(0) @Max(Long.MAX_VALUE) Long henvisning) { }
+
+    public record VergeDto(@NotNull VergeType vergeType, String aktørId, String navn, String organisasjonsnummer,
+                           @NotNull LocalDate gyldigFom, @NotNull LocalDate gyldigTom) { }
+
+}
