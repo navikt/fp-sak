b0f3499017326a7c129d3fbc72e8195a9ea5d75c
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2025-10-14 10:10:16 +0200

    TFP-6401 adopsjon - nytt vilkår og aksjonspunkt (#7594)
    * TFP-6401 adopsjon - nytt vilkår og aksjonspunkt
    
    * lineær liste avslagsårsaker
    
    * Ta med avtuelle avslagsårsaker i VilkarDto

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/vilkår/VilkårDto.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/vilkår/VilkårDto.java
@@ -1,96 +1,13 @@
 package no.nav.foreldrepenger.web.app.tjenester.behandling.vilkår;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.annotation.JsonRawValue;
+import java.util.List;
 
 import jakarta.validation.constraints.NotNull;
 
+import no.nav.foreldrepenger.behandlingslager.behandling.vilkår.Avslagsårsak;
 import no.nav.foreldrepenger.behandlingslager.behandling.vilkår.VilkårType;
 import no.nav.foreldrepenger.behandlingslager.behandling.vilkår.VilkårUtfallType;
 
-public class VilkårDto {
-
-    @NotNull private VilkårType vilkarType;
-    @NotNull private VilkårUtfallType vilkarStatus;
-    private String avslagKode;
-    private String lovReferanse;
-    @NotNull private Boolean overstyrbar;
-
-    @JsonRawValue
-    @JsonInclude(Include.NON_NULL)
-    private String evaluering;
-
-    @JsonRawValue
-    @JsonInclude(Include.NON_NULL)
-    private String input;
-
-    public VilkårDto(VilkårType vilkårType,
-                     VilkårUtfallType vilkårUtfallType,
-                     String avslagKode,
-                     String lovReferanse) {
-        this.vilkarType = vilkårType;
-        this.vilkarStatus = vilkårUtfallType;
-        this.avslagKode = avslagKode;
-        this.lovReferanse = lovReferanse;
-    }
-
-    public VilkårDto() {
-    }
-
-    public VilkårType getVilkarType() {
-        return vilkarType;
-    }
-
-    public VilkårUtfallType getVilkarStatus() {
-        return vilkarStatus;
-    }
-
-    public String getAvslagKode() {
-        return avslagKode;
-    }
-
-    public String getLovReferanse() {
-        return lovReferanse;
-    }
-
-    public String getEvaluering() {
-        return evaluering;
-    }
-
-    public String getInput() {
-        return input;
-    }
-
-    public void setVilkarType(VilkårType vilkarType) {
-        this.vilkarType = vilkarType;
-    }
-
-    public void setVilkarStatus(VilkårUtfallType vilkarStatus) {
-        this.vilkarStatus = vilkarStatus;
-    }
-
-    public void setAvslagKode(String avslagKode) {
-        this.avslagKode = avslagKode;
-    }
-
-    public void setEvaluering(String evaluering) {
-        this.evaluering = evaluering;
-    }
-
-    public void setInput(String input) {
-        this.input = input;
-    }
-
-    public void setLovReferanse(String lovReferanse) {
-        this.lovReferanse = lovReferanse;
-    }
-
-    public void setOverstyrbar(boolean overstyrbar) {
-        this.overstyrbar = overstyrbar;
-    }
-
-    public boolean isOverstyrbar() {
-        return overstyrbar;
-    }
+public record VilkårDto(@NotNull VilkårType vilkarType, String lovReferanse, @NotNull List<Avslagsårsak> aktuelleAvslagsårsaker,
+                        @NotNull VilkårUtfallType vilkarStatus, String avslagKode, @NotNull Boolean overstyrbar) {
 }
--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/vilkår/VilkårDtoMapper.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/vilkår/VilkårDtoMapper.java
@@ -1,13 +1,16 @@
 package no.nav.foreldrepenger.web.app.tjenester.behandling.vilkår;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import no.nav.foreldrepenger.behandlingslager.behandling.Behandling;
 import no.nav.foreldrepenger.behandlingslager.behandling.BehandlingType;
 import no.nav.foreldrepenger.behandlingslager.behandling.Behandlingsresultat;
+import no.nav.foreldrepenger.behandlingslager.behandling.vilkår.Avslagsårsak;
 import no.nav.foreldrepenger.behandlingslager.behandling.vilkår.Vilkår;
 import no.nav.foreldrepenger.behandlingslager.behandling.vilkår.VilkårResultat;
+import no.nav.foreldrepenger.behandlingslager.behandling.vilkår.VilkårType;
 import no.nav.foreldrepenger.behandlingslager.hendelser.StartpunktType;
 
 
@@ -26,18 +29,17 @@ public final class VilkårDtoMapper {
     }
 
     private static VilkårDto lagVilkårDto(Behandling behandling, Vilkår vilkår) {
-        var dto = new VilkårDto();
-        dto.setAvslagKode(vilkår.getAvslagsårsak() != null ? vilkår.getAvslagsårsak().getKode() : null);
-        dto.setVilkarType(vilkår.getVilkårType());
-        dto.setLovReferanse(vilkår.getVilkårType().getLovReferanse(behandling.getFagsakYtelseType()));
-        dto.setVilkarStatus(vilkår.getGjeldendeVilkårUtfall());
-        dto.setOverstyrbar(erOverstyrbar(vilkår, behandling));
-
-        return dto;
+        return new VilkårDto(vilkår.getVilkårType(), vilkår.getVilkårType().getLovReferanse(behandling.getFagsakYtelseType()),
+            new ArrayList<>(vilkår.getVilkårType().getAvslagsårsaker()), vilkår.getGjeldendeVilkårUtfall(),
+            Optional.ofNullable(vilkår.getAvslagsårsak()).map(Avslagsårsak::getKode).orElse(null), erOverstyrbar(vilkår, behandling));
     }
 
     // Angir om vilkåret kan overstyres (forutsetter at bruker har tilgang til å overstyre)
     private static boolean erOverstyrbar(Vilkår vilkår, Behandling behandling) {
+        if (VilkårType.OMSORGSOVERTAKELSEVILKÅR.equals(vilkår.getVilkårType())) {
+            // Tilbyr ikke overstyring
+            return false;
+        }
         if (behandling.erÅpnetForEndring()) {
             // Manuelt åpnet for endring, må dermed også tillate overstyring
             return true;
