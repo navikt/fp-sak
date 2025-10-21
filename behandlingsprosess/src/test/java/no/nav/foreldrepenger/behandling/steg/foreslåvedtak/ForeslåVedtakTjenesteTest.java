60982af479cc88259dd9f5e053fb9bda366a7d94
Michal J. Sladek <mrsladek@users.noreply.github.com>
2025-10-21 11:08:33 +0200

    chore: fp-bom 4.1.0 - fixed breaking changes (#7609)

--- behandlingsprosess/src/test/java/no/nav/foreldrepenger/behandling/steg/foreslåvedtak/ForeslåVedtakTjenesteTest.java
+++ behandlingsprosess/src/test/java/no/nav/foreldrepenger/behandling/steg/foreslåvedtak/ForeslåVedtakTjenesteTest.java
@@ -1,7 +1,7 @@
 package no.nav.foreldrepenger.behandling.steg.foreslåvedtak;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.lenient;
