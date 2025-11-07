a5e170b7b41871eba91c83dfbe4489cdc6b00d37
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2025-11-07 13:05:32 +0100

    TFP-6401 fjern ubrukt lineæravslagsårsak. Innfør JsonSubTypes for Svp

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/registrering/svp/SvpTilretteleggingFrilanserDto.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/registrering/svp/SvpTilretteleggingFrilanserDto.java
@@ -1,8 +1,5 @@
 package no.nav.foreldrepenger.web.app.tjenester.registrering.svp;
 
-import com.fasterxml.jackson.annotation.JsonTypeName;
-
-@JsonTypeName("FR")
 public class SvpTilretteleggingFrilanserDto extends SvpTilretteleggingArbeidsforholdDto {
 
 }
