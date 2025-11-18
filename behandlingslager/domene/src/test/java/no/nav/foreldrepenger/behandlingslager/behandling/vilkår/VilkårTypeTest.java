ab4278343851e74e138b2d02b4ebb65a71781995
Siri Mykland <35257015+sirimykland@users.noreply.github.com>
2025-11-18 14:15:06 +0100

    standariser annotering av lovreferanser (#7669)
    * standariser anotering av lovreferanser
    
    * fiks regex for paragraf

--- behandlingslager/domene/src/test/java/no/nav/foreldrepenger/behandlingslager/behandling/vilkår/VilkårTypeTest.java
+++ behandlingslager/domene/src/test/java/no/nav/foreldrepenger/behandlingslager/behandling/vilkår/VilkårTypeTest.java
@@ -10,7 +10,7 @@ class VilkårTypeTest {
 
     @Test
     void skal_hente_ut_riktig_lovreferanse_basert_på_fagsakYtelseType_engangsstønad() {
-        assertThat(VilkårType.SØKERSOPPLYSNINGSPLIKT.getLovReferanse(FagsakYtelseType.ENGANGSTØNAD)).isEqualTo("§§ 21-3");
+        assertThat(VilkårType.SØKERSOPPLYSNINGSPLIKT.getLovReferanse(FagsakYtelseType.ENGANGSTØNAD)).isEqualTo("§ 21-3");
     }
 
     @Test
