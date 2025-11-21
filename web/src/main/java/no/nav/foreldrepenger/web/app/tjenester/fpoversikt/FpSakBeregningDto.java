9ebd727cac04ef0ada9add419b7d6cf15669bf08
johannbm <johannes.moskvil@gmail.com>
2025-11-21 10:12:56 +0100

    Lag DTO

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/fpoversikt/FpSakBeregningDto.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/fpoversikt/FpSakBeregningDto.java
@@ -1,6 +1,25 @@
 package no.nav.foreldrepenger.web.app.tjenester.fpoversikt;
 
-import java.math.BigDecimal;
+import no.nav.foreldrepenger.behandlingslager.behandling.beregning.AktivitetStatus;
+import no.nav.foreldrepenger.domene.modell.kodeverk.Hjemmel;
 
-record FpSakBeregningDto(BigDecimal dagsats) {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+record FpSakBeregningDto(LocalDate skjæringsTidspunkt, List<BeregningsAndel> beregningsAndeler, List<BeregningAktivitetStatus> beregningAktivitetStatuser) {
+
+    // TODO: bruke denne AktivtetStatus?
+    record BeregningsAndel(AktivitetStatus aktivitetStatus, BigDecimal fastsattPrMnd, InntektsKilde inntektsKilde, Arbeidsforhold arbeidsforhold, BigDecimal dagsats) {}
+
+    record Arbeidsforhold(String arbeidsgiverIdent, BigDecimal refusjonPrMnd) {}
+
+    record BeregningAktivitetStatus(AktivitetStatus aktivitetStatus, Hjemmel hjemmel) {}
+
+    enum InntektsKilde {
+        INNTEKTSMELDING,
+        A_INNTEKT,
+        SKJØNNSFASTSATT,
+        PGI // Pensjonsgivendeinntekt
+    }
 }
