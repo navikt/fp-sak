f01b29b61f3db41b7bc99e9854a75551b64525b5
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2025-11-24 13:08:17 +0100

    bugfix: Ikke opprett berørt ved overlappende utsettelse (#7686)
    * bugfix: Ikke opprett berørt ved overlappende utsettelse
    
    * fokuser kun på utsettelser

--- domenetjenester/uttak/src/main/java/no/nav/foreldrepenger/domene/uttak/uttaksgrunnlag/fp/EndringsdatoBerørtUtleder.java
+++ domenetjenester/uttak/src/main/java/no/nav/foreldrepenger/domene/uttak/uttaksgrunnlag/fp/EndringsdatoBerørtUtleder.java
@@ -46,6 +46,7 @@ public final class EndringsdatoBerørtUtleder {
         ForeldrepengerGrunnlag fpGrunnlag = uttakInput.getYtelsespesifiktGrunnlag();
         var familieHendelser = fpGrunnlag.getFamilieHendelser();
         var utenMinsterett = uttakInput.getSkjæringstidspunkt().filter(Skjæringstidspunkt::utenMinsterett).isPresent();
+        var sammenhengendeUttak = uttakInput.getSkjæringstidspunkt().filter(Skjæringstidspunkt::kreverSammenhengendeUttak).isPresent();
         if (berørtUttakOpt.isEmpty() || finnMinAktivDato(berørtUttakOpt.get()).isEmpty() || finnMinAktivDato(utløsendeUttak, berørtUttakOpt.get()).isEmpty()) {
             return Optional.empty();
         }
@@ -70,7 +71,7 @@ public final class EndringsdatoBerørtUtleder {
         var periodeTom = finnMaxAktivDato(utløsendeUttak, berørtUttak).filter(endringsdato::isBefore).orElse(endringsdato);
         var periodeFomEndringsdato = new LocalDateInterval(endringsdato, periodeTom);
 
-        var overlapp = overlappSomIkkeErFulltSamtidigUttak(familieHendelser, utenMinsterett, periodeFomEndringsdato, utløsendeUttak,
+        var overlapp = overlappSomIkkeErFulltSamtidigUttak(familieHendelser, utenMinsterett, periodeFomEndringsdato, sammenhengendeUttak, utløsendeUttak,
             berørtUttak);
         if (overlapp.isPresent()) {
             LOG.info("{}: OverlappUtenSamtidig fom {} endringsdato {}", loggPrefix, overlapp.get(), endringsdato);
@@ -114,10 +115,11 @@ public final class EndringsdatoBerørtUtleder {
     private static Optional<LocalDate> overlappSomIkkeErFulltSamtidigUttak(FamilieHendelser familieHendelser,
                                                                            boolean utenMinsterett,
                                                                            LocalDateInterval periodeFomEndringsdato,
+                                                                           boolean sammenhengendeUttak,
                                                                            ForeldrepengerUttak brukersUttak,
                                                                            ForeldrepengerUttak annenpartsUttak) {
-        var tidslinjeBruker = lagTidslinje(brukersUttak, p -> !p.isOpphold(), EndringsdatoBerørtUtleder::helgFomMandagSegment);
-        var tidslinjeAnnenpart = lagTidslinje(annenpartsUttak, p -> !p.isOpphold() && p.erFraSøknad(), EndringsdatoBerørtUtleder::helgFomMandagSegment);
+        var tidslinjeBruker = lagTidslinje(brukersUttak, p -> inkluderPeriode(p, sammenhengendeUttak), EndringsdatoBerørtUtleder::helgFomMandagSegment);
+        var tidslinjeAnnenpart = lagTidslinje(annenpartsUttak, p -> inkluderPeriode(p, sammenhengendeUttak) && p.erFraSøknad(), EndringsdatoBerørtUtleder::helgFomMandagSegment);
         // Tidslinje der begge har uttak - fom endringsdato.
         var tidslinjeOverlappendeUttakFomEndringsdato = tidslinjeAnnenpart.intersection(tidslinjeBruker).intersection(periodeFomEndringsdato);
         if (tidslinjeOverlappendeUttakFomEndringsdato.isEmpty()) {
@@ -131,6 +133,11 @@ public final class EndringsdatoBerørtUtleder {
 
         var overlappUtenomAkseptertFulltSamtidigUttak = tidslinjeOverlappendeUttakFomEndringsdato.disjoint(tidslinjeSamtidigUttak);
         return overlappUtenomAkseptertFulltSamtidigUttak.isEmpty() ? Optional.empty() : Optional.of(overlappUtenomAkseptertFulltSamtidigUttak.getMinLocalDate());
+    }
+
+    private static boolean inkluderPeriode(ForeldrepengerUttakPeriode periode, boolean sammenhengendeUttak) {
+        // Filtrer vekk opphold og utsettelser for fritt uttak
+        return !periode.isOpphold() && (sammenhengendeUttak || !periode.isInnvilgetUtsettelse());
     }
 
     private static boolean akseptertFulltSamtidigUttak(ForeldrepengerUttakPeriode periode, LocalDateInterval farRundtFødsel) {
