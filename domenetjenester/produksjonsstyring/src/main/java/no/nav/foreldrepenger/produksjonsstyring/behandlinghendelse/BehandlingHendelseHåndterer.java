781ea2d8414c1722920eab5948b2af02291cb15f
anjaaalerud <anja.gotesen.aalerud@nav.no>
2025-12-12 13:35:23 +0100

    TFP-6517 - Sanerer topic personoversikt.modia-soknadstatus-hendelse, og all kode rundt det å publisere meldinger der

--- domenetjenester/produksjonsstyring/src/main/java/no/nav/foreldrepenger/produksjonsstyring/behandlinghendelse/BehandlingHendelseHåndterer.java
+++ domenetjenester/produksjonsstyring/src/main/java/no/nav/foreldrepenger/produksjonsstyring/behandlinghendelse/BehandlingHendelseHåndterer.java
@@ -10,19 +10,13 @@ import org.slf4j.LoggerFactory;
 
 import no.nav.foreldrepenger.behandling.FagsakTjeneste;
 import no.nav.foreldrepenger.behandlingslager.behandling.BehandlingStatus;
-import no.nav.foreldrepenger.behandlingslager.behandling.BehandlingType;
-import no.nav.foreldrepenger.behandlingslager.fagsak.Fagsak;
 import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakStatus;
-import no.nav.foreldrepenger.behandlingslager.kodeverk.Fagsystem;
 import no.nav.foreldrepenger.domene.json.StandardJsonConfig;
 import no.nav.foreldrepenger.domene.typer.Saksnummer;
 import no.nav.foreldrepenger.konfig.KonfigVerdi;
 import no.nav.foreldrepenger.produksjonsstyring.fagsakstatus.OppdaterFagsakStatusTjeneste;
-import no.nav.foreldrepenger.produksjonsstyring.sakogbehandling.OppdaterPersonoversiktTask;
 import no.nav.vedtak.exception.VLException;
 import no.nav.vedtak.felles.integrasjon.kafka.KafkaMessageHandler;
-import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
-import no.nav.vedtak.felles.prosesstask.api.ProsessTaskTjeneste;
 import no.nav.vedtak.hendelser.behandling.BehandlingHendelse;
 import no.nav.vedtak.hendelser.behandling.Hendelse;
 import no.nav.vedtak.hendelser.behandling.Kildesystem;
@@ -40,7 +34,6 @@ public class BehandlingHendelseHåndterer implements KafkaMessageHandler.KafkaSt
 
     private String topicName;
     private FagsakTjeneste fagsakTjeneste;
-    private ProsessTaskTjeneste taskTjeneste;
     private OppdaterFagsakStatusTjeneste fagsakStatusTjeneste;
 
     public BehandlingHendelseHåndterer() {
@@ -49,11 +42,9 @@ public class BehandlingHendelseHåndterer implements KafkaMessageHandler.KafkaSt
     @Inject
     public BehandlingHendelseHåndterer(@KonfigVerdi(value = "kafka.behandlinghendelse.topic") String topicName,
                                        FagsakTjeneste fagsakTjeneste,
-                                       ProsessTaskTjeneste taskTjeneste,
                                        OppdaterFagsakStatusTjeneste fagsakStatusTjeneste) {
         this.topicName = topicName;
         this.fagsakTjeneste = fagsakTjeneste;
-        this.taskTjeneste = taskTjeneste;
         this.fagsakStatusTjeneste = fagsakStatusTjeneste;
     }
 
@@ -81,41 +72,14 @@ public class BehandlingHendelseHåndterer implements KafkaMessageHandler.KafkaSt
                 if (!FagsakStatus.AVSLUTTET.equals(fagsak.getStatus())) {
                     fagsakStatusTjeneste.lagBehandlingAvsluttetTask(fagsak, null);
                 }
-                lagPersonoversiktTask(fagsak, mottattHendelse);
             } else if (Hendelse.OPPRETTET.equals(mottattHendelse.getHendelse())) {
                 if (!FagsakStatus.UNDER_BEHANDLING.equals(fagsak.getStatus())) {
                     fagsakStatusTjeneste.oppdaterFagsakNårBehandlingOpprettet(fagsak, null, BehandlingStatus.UTREDES);
                 }
-                lagPersonoversiktTask(fagsak, mottattHendelse);
             } else if (!FagsakStatus.UNDER_BEHANDLING.equals(fagsak.getStatus())){
                 fagsakStatusTjeneste.oppdaterFagsakNårBehandlingOpprettet(fagsak, null, BehandlingStatus.UTREDES);
             }
         }
-    }
-
-    private void lagPersonoversiktTask(Fagsak fagsak, BehandlingHendelseV1 hendelse) {
-        var behandlingType = switch (hendelse.getBehandlingstype()) {
-            case TILBAKEBETALING -> BehandlingType.TILBAKEKREVING_ORDINÆR;
-            case TILBAKEBETALING_REVURDERING -> BehandlingType.TILBAKEKREVING_REVURDERING;
-            default -> null;
-        };
-        var behandlingStatus = switch (hendelse.getHendelse()) {
-            case OPPRETTET -> BehandlingStatus.OPPRETTET;
-            case AVSLUTTET -> BehandlingStatus.AVSLUTTET;
-            default -> null;
-        };
-        if (behandlingType == null || behandlingStatus == null) {
-            return;
-        }
-        var behandlingRef = String.format("%s_T%s", Fagsystem.FPSAK.getOffisiellKode(), hendelse.getBehandlingUuid().toString());
-
-        var prosessTaskData = ProsessTaskData.forProsessTask(OppdaterPersonoversiktTask.class);
-        prosessTaskData.setFagsak(fagsak.getSaksnummer().getVerdi(), fagsak.getId());
-        prosessTaskData.setProperty(OppdaterPersonoversiktTask.PH_REF_KEY, behandlingRef);
-        prosessTaskData.setProperty(OppdaterPersonoversiktTask.PH_STATUS_KEY, behandlingStatus.getKode());
-        prosessTaskData.setProperty(OppdaterPersonoversiktTask.PH_TID_KEY, hendelse.getTidspunkt().toString());
-        prosessTaskData.setProperty(OppdaterPersonoversiktTask.PH_TYPE_KEY, behandlingType.getKode());
-        taskTjeneste.lagre(prosessTaskData);
     }
 
     private static void setCallIdForHendelse(BehandlingHendelseV1 hendelse) {
