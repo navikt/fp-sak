702a07f87e34738e9bf26b3706428400301dbc0b
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2026-01-12 17:32:49 +0100

    TFP-6525 fjern los-spørringer - de gjøres nå i LOS. fjern secureLogs (#7741)

--- behandlingslager/domene/src/main/java/no/nav/foreldrepenger/behandlingslager/behandling/nøkkeltallbehandling/NøkkeltallBehandlingVentefristUtløper.java
+++
@@ -1,9 +0,0 @@
-package no.nav.foreldrepenger.behandlingslager.behandling.nøkkeltallbehandling;
-
-import no.nav.foreldrepenger.behandlingslager.fagsak.FagsakYtelseType;
-
-
-public record NøkkeltallBehandlingVentefristUtløper(String behandlendeEnhet, FagsakYtelseType fagsakYtelseType,
-                                                    String fristUke, Long antall) {
-
-}
