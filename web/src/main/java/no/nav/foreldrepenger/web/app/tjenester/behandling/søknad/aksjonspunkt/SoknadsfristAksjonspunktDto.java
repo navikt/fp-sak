d6a2231d79e1cfa982aeb09bd067e997b5e2c789
Anders Palfi <anders.palfi@nav.no>
2026-02-20 08:51:38 +0100

    Chore: fjerner unødvendig aksjonspunkt interface

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/søknad/aksjonspunkt/SoknadsfristAksjonspunktDto.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/søknad/aksjonspunkt/SoknadsfristAksjonspunktDto.java
@@ -8,7 +8,6 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
-import no.nav.foreldrepenger.behandling.aksjonspunkt.AvslagbartAksjonspunktDto;
 import no.nav.foreldrepenger.behandling.aksjonspunkt.BekreftetAksjonspunktDto;
 import no.nav.foreldrepenger.behandlingslager.behandling.aksjonspunkt.AksjonspunktKodeDefinisjon;
 import no.nav.foreldrepenger.behandlingslager.behandling.vilkår.Avslagsårsak;
@@ -16,7 +15,7 @@ import no.nav.vedtak.util.InputValideringRegex;
 
 @JsonAutoDetect(getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE, fieldVisibility = Visibility.ANY)
 @JsonTypeName(AksjonspunktKodeDefinisjon.MANUELL_VURDERING_AV_SØKNADSFRISTVILKÅRET_KODE)
-public class SoknadsfristAksjonspunktDto extends BekreftetAksjonspunktDto implements AvslagbartAksjonspunktDto {
+public class SoknadsfristAksjonspunktDto extends BekreftetAksjonspunktDto {
 
     @NotNull
     private boolean erVilkårOk;
@@ -35,13 +34,11 @@ public class SoknadsfristAksjonspunktDto extends BekreftetAksjonspunktDto implem
         this.avslagskode = avslagskode;
     }
 
-    @Override
     public boolean getErVilkårOk() {
         return erVilkårOk;
     }
 
     // TODO: fjerne denne håndteringen når frontend er oppdatert til å sende med avslagskode
-    @Override
     public String getAvslagskode() {
         return avslagskode == null && !erVilkårOk ? Avslagsårsak.SØKT_FOR_SENT.getKode() : avslagskode;
     }
