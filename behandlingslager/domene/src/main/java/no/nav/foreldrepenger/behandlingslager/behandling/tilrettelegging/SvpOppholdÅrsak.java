a2d4780a31ea8655e7d83c889634764e3c4ed029
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2026-03-10 09:28:12 +0100

    forklaring

--- behandlingslager/domene/src/main/java/no/nav/foreldrepenger/behandlingslager/behandling/tilrettelegging/SvpOppholdÅrsak.java
+++ behandlingslager/domene/src/main/java/no/nav/foreldrepenger/behandlingslager/behandling/tilrettelegging/SvpOppholdÅrsak.java
@@ -3,6 +3,7 @@ package no.nav.foreldrepenger.behandlingslager.behandling.tilrettelegging;
 import no.nav.foreldrepenger.behandlingslager.kodeverk.DatabaseKode;
 
 public enum SvpOppholdÅrsak implements DatabaseKode {
+    // Ikke endre rekkefølgen på disse - de er lagret i databasen som 0,1,2 osv.
     SYKEPENGER,
     FERIE
 }
