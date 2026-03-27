4872cadfa8afda5663014050dd70e00914df070c
johannbm <johannes.moskvil@gmail.com>
2026-03-27 13:16:04 +0100

    Revert "TFP-6912-Toggle om fpsak går mot fpformidling i dev eller prod (#7827)"
    This reverts commit e402238162002624ae3140e3ddaa1d85533e1b96.

--- domenetjenester/dokumentbestiller/src/main/java/no/nav/foreldrepenger/dokumentbestiller/formidling/klient/FormidlingRestKlient.java
+++ domenetjenester/dokumentbestiller/src/main/java/no/nav/foreldrepenger/dokumentbestiller/formidling/klient/FormidlingRestKlient.java
@@ -2,44 +2,54 @@ package no.nav.foreldrepenger.dokumentbestiller.formidling.klient;
 
 import java.net.URI;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.UriBuilder;
 
+import no.nav.foreldrepenger.dokumentbestiller.formidling.Dokument;
 import no.nav.foreldrepenger.kontrakter.formidling.v3.DokumentBestillingDto;
 import no.nav.foreldrepenger.kontrakter.formidling.v3.DokumentBestillingHtmlDto;
 import no.nav.foreldrepenger.kontrakter.formidling.v3.DokumentForhåndsvisDto;
+import no.nav.vedtak.felles.integrasjon.rest.FpApplication;
 import no.nav.vedtak.felles.integrasjon.rest.RestClient;
+import no.nav.vedtak.felles.integrasjon.rest.RestClientConfig;
 import no.nav.vedtak.felles.integrasjon.rest.RestConfig;
 import no.nav.vedtak.felles.integrasjon.rest.RestRequest;
+import no.nav.vedtak.felles.integrasjon.rest.TokenFlow;
 
-public abstract class FormidlingRestKlient {
+@ApplicationScoped
+@RestClientConfig(tokenConfig = TokenFlow.ADAPTIVE, application = FpApplication.FPFORMIDLING)
+public class FormidlingRestKlient implements Dokument {
+
+    private final RestClient restClient;
+    private final RestConfig restConfig;
 
     private static final String BESTILL_URL = "/bestill";
     private static final String FORHÅNDSVIS_URL = "/forhaandsvis";
     private static final String GENERER_HTML_URL = "/generer/html";
     private static final String V3 = "/v3";
-
-    private final RestClient restClient;
-    private final RestConfig restConfig;
     private final URI uri;
 
-    protected FormidlingRestKlient(RestClient restClient, RestConfig restConfig) {
-        this.restClient = restClient;
-        this.restConfig = restConfig;
+    public FormidlingRestKlient() {
+        this.restClient = RestClient.client();
+        this.restConfig = RestConfig.forClient(this.getClass());
         this.uri = UriBuilder.fromUri(restConfig.fpContextPath()).path("/api/brev").build();
     }
 
+    @Override
     public void bestill(DokumentBestillingDto dokumentBestillingDto) {
         var request = RestRequest.newPOSTJson(dokumentBestillingDto, UriBuilder.fromUri(uri).path(BESTILL_URL).path(V3).build(), restConfig);
         restClient.sendReturnOptional(request, String.class);
     }
 
+    @Override
     public byte[] forhåndsvis(DokumentForhåndsvisDto dokumentForhåndsvisDto) {
         var request = RestRequest.newPOSTJson(dokumentForhåndsvisDto, UriBuilder.fromUri(uri).path(FORHÅNDSVIS_URL).path(V3).build(), restConfig);
         return restClient.sendReturnByteArray(request);
     }
 
+    @Override
     public String genererHtml(DokumentBestillingHtmlDto dokumentForhåndsvisDto) {
         var request = RestRequest.newPOSTJson(dokumentForhåndsvisDto, UriBuilder.fromUri(uri).path(GENERER_HTML_URL).path(V3).build(), restConfig);
         request.setAndReplaceHeader(HttpHeaders.ACCEPT, MediaType.TEXT_HTML);
