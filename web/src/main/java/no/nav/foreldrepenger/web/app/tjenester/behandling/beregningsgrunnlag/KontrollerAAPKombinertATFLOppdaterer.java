81481f8085339ac28b379855ca3d30c2bf9b5376
remhol <193506135+remhol@users.noreply.github.com>
2026-05-26 13:26:56 +0200

    TFP-6944: Renamer panelet til Arbeidsavklaringspenger

--- web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/beregningsgrunnlag/KontrollerAAPKombinertATFLOppdaterer.java
+++ web/src/main/java/no/nav/foreldrepenger/web/app/tjenester/behandling/beregningsgrunnlag/KontrollerAAPKombinertATFLOppdaterer.java
@@ -39,7 +39,7 @@ public class KontrollerAAPKombinertATFLOppdaterer implements AksjonspunktOppdate
         var historikkinnslag = new Historikkinnslag.Builder().medAktør(HistorikkAktør.SAKSBEHANDLER)
             .medFagsakId(ref.fagsakId())
             .medBehandlingId(ref.behandlingId())
-            .medTittel(SkjermlenkeType.AAP_KOMBINERT_ATFL)
+            .medTittel(SkjermlenkeType.ARBEIDSAVKLARINGSPENGER)
             .addLinje("Beregning for AAP kombinert med arbeidstaker og/eller frilans er godkjent")
             .addLinje(dto.getBegrunnelse())
             .build();
